41e9104a849c8f2217d4b455b916804bc035fcb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:38 +0000

    Add package math-base/0.1.0/math_base-0.1.0.tar.gz

---
+++ packages/math-base/math_base-0.1.0.tar.gz/math_base-0.1.0/math_base/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Math Base."""
+
+__author__ = """Oh June"""
+__email__ = 'tic.for.tac@gmail.com'
+__version__ = '0.1.0'
---
+++ packages/math-base/math_base-0.1.0.tar.gz/math_base-0.1.0/math_base/math_base.py
@@ -0,0 +1,13 @@
+"""Main module."""
+
+
+def add(x, y):
+    # type: (int, int) -> int
+    """
+
+    @return:
+    @param x:
+    @param y:
+    @return:
+    """
+    return x + y
