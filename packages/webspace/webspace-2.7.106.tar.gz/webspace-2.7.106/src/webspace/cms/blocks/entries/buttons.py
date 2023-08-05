622f96841d878d61efbcdea71c88645b514da16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:44 +0000

    Add package webspace/2.7.106/webspace-2.7.106.tar.gz

---
+++ packages/webspace/webspace-2.7.106.tar.gz/webspace-2.7.106/src/webspace/cms/blocks/entries/buttons.py
@@ -0,0 +1,39 @@
+from wagtail.core import blocks
+
+from webspace.cms import constants
+from webspace.cms.blocks.choice import AlignTextChoiceBlock
+from webspace.cms.blocks.common import ButtonBlock, EntryBlock
+
+
+class ButtonsEntry(EntryBlock):
+    align = AlignTextChoiceBlock(required=False)
+    buttons = blocks.StreamBlock(
+        [
+            ('button', ButtonBlock()),
+        ],
+        max_num=3
+    )
+
+    def mock(self, btn_one=constants.BUTTON_SECONDARY_FULL, btn_two=constants.BUTTON_SECONDARY, align='left',
+             *args, **kwargs):
+        self.mock_data.update({
+            'type': 'buttons',
+            'value': {
+                'buttons': [
+                    {
+                        'type': 'button',
+                        'value': self.button(btn_one)
+                    },
+                    {
+                        'type': 'button',
+                        'value': self.button(btn_two)
+                    },
+                ],
+                'align': align
+            }
+        })
+        return super().mock(*args, **kwargs)
+
+    class Meta:
+        template = '%s/entries/buttons.html' % constants.BLOCK_TEMPLATES_PATH
+        label = "Buttons"
