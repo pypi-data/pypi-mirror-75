da5f844122e7a75d33fe06e7ff43516127e0c017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:38 +0000

    Add package pydist-prob/0.1/pydist_prob-0.1.tar.gz

---
+++ packages/pydist-prob/pydist_prob-0.1.tar.gz/pydist_prob-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pydist_prob',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions'],
+      author='Elahi Concha',
+      author_email='elijahshellsanchez@outlook.com',
+      zip_safe=False)
