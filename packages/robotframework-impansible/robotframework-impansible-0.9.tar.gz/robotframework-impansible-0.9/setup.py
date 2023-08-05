631bac5193aa899101fb26a953338f1f75c80533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:57 +0000

    Add package robotframework-impansible/0.9/robotframework-impansible-0.9.tar.gz

---
+++ packages/robotframework-impansible/robotframework-impansible-0.9.tar.gz/robotframework-impansible-0.9/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from os.path import abspath, dirname, join
+CURDIR = dirname(abspath(__file__))
+with open(join(CURDIR, 'README.rst')) as f:
+    DESCRIPTION = f.read()
+setup(
+  name = 'robotframework-impansible',
+  packages = ['Impansible'],
+  version = '0.9',
+  license='MIT',
+  description = 'Robotframework library to access all ansible internal modules.',
+  long_description=DESCRIPTION,
+  author = 'Adam Przybyla',
+  author_email = 'adam.przybyla@gmail.com',
+  url = 'https://github.com/AdamPrzybyla/impansible',
+  download_url = 'https://github.com/AdamPrzybyla/Impansible/archive/v_09.tar.gz',
+  keywords = ['robotframework', 'ansible', 'automatisation','nsm'],
+  install_requires=[
+          'ansible',
+          'robotframework',
+          'robotframework-nsm',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 2',
+  ],
+)
