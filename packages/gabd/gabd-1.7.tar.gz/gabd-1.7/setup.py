1179bd98b1a65f446a6a9a2f07e19e3ed38affa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:28 +0000

    Add package gabd/1.7/gabd-1.7.tar.gz

---
+++ packages/gabd/gabd-1.7.tar.gz/gabd-1.7/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gabd',
+      version='1.7',
+      description='Gaussian and Binomial distributions',
+      packages=['gabd'],
+	  author = 'Harshit Sachdeva',
+	  author_email = 'harshitsachdeva98@gmail.com',
+    zip_safe=False)
