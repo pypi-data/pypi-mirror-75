4880a60c8757a5de28b8c5ca293e718287c45caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package spell/0.38.15/spell-0.38.15-py2.py3-none-any.whl

---
+++ packages/spell/spell-0.38.15-py2.py3-none-any.whl/spell/serving/background.py
@@ -0,0 +1,13 @@
+from typing import AnyStr, Optional, Union
+
+from numpy import bool as np_bool, integer as np_int, floating as np_float
+from starlette.background import BackgroundTasks as StarletteBackgroundTasks
+
+from spell.metrics import send_metric
+
+MetricsValue = Union[bool, np_bool, int, np_int, float, np_float, AnyStr]
+
+
+class BackgroundTasks(StarletteBackgroundTasks):
+    def send_metric(self, name: str, value: MetricsValue, index: Optional[int] = None) -> None:
+        super().add_task(send_metric, name, value, index=index)
