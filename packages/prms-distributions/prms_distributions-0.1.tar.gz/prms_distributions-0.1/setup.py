bf4955bb88607dfb7a516b1c9b1ecb3fa8cb4f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:50 +0000

    Add package prms-distributions/0.1/prms_distributions-0.1.tar.gz

---
+++ packages/prms-distributions/prms_distributions-0.1.tar.gz/prms_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prms_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['prms_distributions'],
+      zip_safe=False)
