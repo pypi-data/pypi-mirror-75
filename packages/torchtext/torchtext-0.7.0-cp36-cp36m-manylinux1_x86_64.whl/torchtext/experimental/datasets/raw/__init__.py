53240727f68656151ed3963091cb9e90c3c9c7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:19 +0000

    Add package torchtext/0.7.0/torchtext-0.7.0-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/torchtext/torchtext-0.7.0-cp36-cp36m-manylinux1_x86_64.whl/torchtext/experimental/datasets/raw/__init__.py
@@ -0,0 +1,27 @@
+from .text_classification import AG_NEWS, SogouNews, DBpedia, YelpReviewPolarity, \
+    YelpReviewFull, YahooAnswers, \
+    AmazonReviewPolarity, AmazonReviewFull, IMDB
+from .sequence_tagging import UDPOS, CoNLL2000Chunking
+from .translation import Multi30k, IWSLT, WMT14
+from .language_modeling import WikiText2, WikiText103, PennTreebank, WMTNewsCrawl
+from .question_answer import SQuAD1, SQuAD2
+
+__all__ = ['IMDB',
+           'AG_NEWS',
+           'SogouNews',
+           'DBpedia',
+           'YelpReviewPolarity',
+           'YelpReviewFull',
+           'YahooAnswers',
+           'AmazonReviewPolarity',
+           'AmazonReviewFull',
+           'UDPOS',
+           'CoNLL2000Chunking',
+           'Multi30k',
+           'IWSLT',
+           'WMT14',
+           'WikiText2',
+           'WikiText103',
+           'PennTreebank',
+           'WMTNewsCrawl',
+           'SQuAD1', 'SQuAD2']
