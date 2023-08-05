40677439c04e0ebe9d6ba3e73f7af75205bc98d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package distributionsabcd/0.1/distributionsabcd-0.1.tar.gz

---
+++ packages/distributionsabcd/distributionsabcd-0.1.tar.gz/distributionsabcd-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsabcd',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsabcd'],
+      zip_safe=False)
