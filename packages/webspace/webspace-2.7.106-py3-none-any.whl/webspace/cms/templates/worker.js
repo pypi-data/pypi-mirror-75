ba1074014110ba52fea2506e30127d29ba87f733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:44 +0000

    Add package webspace/2.7.106/webspace-2.7.106-py3-none-any.whl

---
+++ packages/webspace/webspace-2.7.106-py3-none-any.whl/webspace/cms/templates/worker.js
@@ -0,0 +1,23 @@
+const cacheName = 'cache-v{{ version }}';
+const precacheResources = [
+    {% for page in pages.items %}
+    '{{ page.get_url }}',
+    {% endfor %}
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(precacheResources)),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request)
+    .then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }));
+});
