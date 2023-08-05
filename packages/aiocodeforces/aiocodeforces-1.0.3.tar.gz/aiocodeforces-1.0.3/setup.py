825f64958cc0793b330697db3a7257ac15f5d1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package aiocodeforces/1.0.3/aiocodeforces-1.0.3.tar.gz

---
+++ packages/aiocodeforces/aiocodeforces-1.0.3.tar.gz/aiocodeforces-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+]
+
+setup(
+    name="aiocodeforces",
+    version="1.0.3",
+    package=find_packages(),
+    author="Julian Park",
+    author_email="jpark9013@gmail.com",
+    description="An asyncio wrapper for the CodeForces API",
+    keywords="asyncio wrapper codeforces api",
+    url="https://github.com/jpark9013/aiocodeforces",
+    download_url="https://github.com/jpark9013/aiocodeforces/releases/tag/1.0.3",
+    license="MIT",
+    install_requires=["aiohttp"]
+)
