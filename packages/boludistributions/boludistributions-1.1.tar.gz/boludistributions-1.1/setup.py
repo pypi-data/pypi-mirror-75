1d0a2dffe790d9bb7e7a119e3862cb4d45ff1a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:19 +0000

    Add package boludistributions/1.1/boludistributions-1.1.tar.gz

---
+++ packages/boludistributions/boludistributions-1.1.tar.gz/boludistributions-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='boludistributions',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['boludistributions'],
+      zip_safe=False)
