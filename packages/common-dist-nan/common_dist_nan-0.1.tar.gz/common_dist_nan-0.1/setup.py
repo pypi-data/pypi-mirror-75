cb1172e787775f7a080f177e7d3c40e7a95536e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package common-dist-nan/0.1/common_dist_nan-0.1.tar.gz

---
+++ packages/common-dist-nan/common_dist_nan-0.1.tar.gz/common_dist_nan-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='common_dist_nan',
+      version='0.1',
+      description='Common distributions',
+      packages=['common_dist_nan'],
+      author = 'Nguyet Anh Nguyen',
+      github = 'nna0702',
+      zip_safe=False)
