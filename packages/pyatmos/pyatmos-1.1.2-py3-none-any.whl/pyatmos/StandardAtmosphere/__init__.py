d80d6d56c18ec9d195db1e1cbc05bf2a0b79faa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:12 +0000

    Add package pyatmos/1.1.2/pyatmos-1.1.2-py3-none-any.whl

---
+++ packages/pyatmos/pyatmos-1.1.2-py3-none-any.whl/pyatmos/StandardAtmosphere/__init__.py
@@ -0,0 +1,11 @@
+'''
+pyatmos StandardAtmosphere subpackage
+
+This subpackage defines the following functions:
+
+# StandardAtmosphere.py
+
+isa - Implements the International Standard Atmosphere(ISA) Model up to 86km. 
+
+lapse_tp - Calculate the temperature and pressure at a given geopotential altitude above base of a specific layer.
+'''              
