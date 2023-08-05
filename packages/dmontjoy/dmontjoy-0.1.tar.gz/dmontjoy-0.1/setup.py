436b74c6de99725f27c7ee746e549c1b262b4686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package dmontjoy/0.1/dmontjoy-0.1.tar.gz

---
+++ packages/dmontjoy/dmontjoy-0.1.tar.gz/dmontjoy-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dmontjoy',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dmontjoy'],
+      author = 'daniel montjoy',
+      author_email = 'dmontjoy@gmail.com',
+      zip_safe=False)
