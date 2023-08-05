78411d5f00121307b699531c0d4e68afa480670a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:17 +0000

    Add package currencycloud-python-client/4.5.0.post1/currencycloud-python-client-4.5.0.post1.tar.gz

---
+++ packages/currencycloud-python-client/currencycloud-python-client-4.5.0.post1.tar.gz/currencycloud-python-client-4.5.0.post1/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='currencycloud-python-client',
+    version='4.5.0.post1',
+    license='MIT',
+    description="Currencycloud API v2 Python client (FORK)",
+    long_description='Fork of the official Python SDK for v2 of Currencycloud\'s API',
+
+    author='Francesco Boffa, Ed Addario',
+    author_email='francesco.boffa@currencycloud.com, eaddario@hotmail.com',
+    url='https://github.com/addario-org/currencycloud-python-client',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    keywords=[],
+    install_requires=['requests', 'PyYAML', 'deprecation'],
+    tests_require=['pytest', 'mock', 'requests-mock', 'betamax', 'betamax-serializers'],
+    test_suite='tests'
+)
