1a8cd0b7f4c592415e3f359a24abe820ea6e662c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:48 +0000

    Add package Gausian-package/1.0/Gausian_package-1.0.tar.gz

---
+++ packages/Gausian-package/Gausian_package-1.0.tar.gz/Gausian_package-1.0/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+
+setup(name='Gausian_package',version='1.0',description='gaussian distribution',package=['Gaussian_package'],author="Rahul Karanam",author_email="rahulkarnam777@gmail.com",zip_safe=False)
