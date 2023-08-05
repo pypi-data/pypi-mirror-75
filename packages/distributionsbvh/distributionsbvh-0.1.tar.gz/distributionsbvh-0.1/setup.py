20de9b904166659f4b3fc6401d50f5125b9eced6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:03 +0000

    Add package distributionsbvh/0.1/distributionsbvh-0.1.tar.gz

---
+++ packages/distributionsbvh/distributionsbvh-0.1.tar.gz/distributionsbvh-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsbvh',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsbvh'],
+      zip_safe=False)
