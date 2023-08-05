d4dfe85bdf8bde09207e48e60c0b6c0ba1e8b6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:18 +0000

    Add package dvc/1.2.1/dvc-1.2.1.tar.gz

---
+++ packages/dvc/dvc-1.2.1.tar.gz/dvc-1.2.1/dvc/scheme.py
@@ -0,0 +1,11 @@
+class Schemes:
+    SSH = "ssh"
+    HDFS = "hdfs"
+    S3 = "s3"
+    AZURE = "azure"
+    HTTP = "http"
+    HTTPS = "https"
+    GS = "gs"
+    GDRIVE = "gdrive"
+    LOCAL = "local"
+    OSS = "oss"
