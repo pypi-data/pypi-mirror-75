6cdf9892cdbc9309957d23d561d4d375da1aa548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package TensorPlot/0.0.2/TensorPlot-0.0.2.tar.gz

---
+++ packages/TensorPlot/TensorPlot-0.0.2.tar.gz/TensorPlot-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TensorPlot',
+    version='0.0.2',
+    packages=['tensorplot'],
+    url='http://github.com/manuelblancovalentin/tensorplot',
+    license='GPL',
+    author='Manu Blanco Valentin',
+    author_email='manuel.blanco.valentin@gmail.com',
+    description=''
+)
