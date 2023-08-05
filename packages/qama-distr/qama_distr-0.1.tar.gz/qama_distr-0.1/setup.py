610fb0fe1f181024731510aa2980ec0c6dfdf0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:17 +0000

    Add package qama-distr/0.1/qama_distr-0.1.tar.gz

---
+++ packages/qama-distr/qama_distr-0.1.tar.gz/qama_distr-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='qama_distr',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['qama_distr'],
+      author = 'Qamarudeen Muhammad',
+      email = 'crescentgroup2000@gmail.com',
+      zip_safe=False)
