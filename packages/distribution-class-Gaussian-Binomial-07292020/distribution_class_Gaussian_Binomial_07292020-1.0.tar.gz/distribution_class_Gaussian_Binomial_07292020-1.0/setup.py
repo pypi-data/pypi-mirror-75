c078b44455c9c9403556cd762c810e0958d9330b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:42 +0000

    Add package distribution-class-Gaussian-Binomial-07292020/1.0/distribution_class_Gaussian_Binomial_07292020-1.0.tar.gz

---
+++ packages/distribution-class-Gaussian-Binomial-07292020/distribution_class_Gaussian_Binomial_07292020-1.0.tar.gz/distribution_class_Gaussian_Binomial_07292020-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_class_Gaussian_Binomial_07292020',
+      version='1.0',
+      description='Gaussian and Binomial Distributions',
+      packages=['distribution_class_Gaussian_Binomial_07292020'],
+      zip_safe=False)
