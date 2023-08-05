34573d676117e35f94d9b524b25cfa33384cd1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:22 +0000

    Add package freeproxy-cn/1.6/freeproxy_cn-1.6.tar.gz

---
+++ packages/freeproxy-cn/freeproxy_cn-1.6.tar.gz/freeproxy_cn-1.6/freeproxy_cn/site/__init__.py
@@ -0,0 +1,18 @@
+from .crossin import Crossin
+from .eight9 import Eight9
+from .ip3366 import Ip3366
+from .iphai import IPHai
+from .ipjiang import IPJiang
+from .kuai import Kuai
+from .seofang import SeoFang
+from .xiaosu import XiaoSu
+from .xici import XiCi
+from .proxydocker import ProxyDocker
+from .xroxy import Xroxy
+from .super import Super
+from .I337 import I337
+from .freeip import FreeIP
+SITES = [
+    Crossin, Eight9, Ip3366, IPJiang, Kuai,
+    XiaoSu, XiCi, Super,FreeIP
+]
