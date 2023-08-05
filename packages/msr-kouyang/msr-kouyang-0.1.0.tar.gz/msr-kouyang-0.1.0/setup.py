6a50d6b916b0f536e26f832e7942dded50c4c650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package msr-kouyang/0.1.0/msr-kouyang-0.1.0.tar.gz

---
+++ packages/msr-kouyang/msr-kouyang-0.1.0.tar.gz/msr-kouyang-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="msr-kouyang",
+    version="0.1.0", # Semantic version
+    author="Kevin Ouyang",
+    author_email="kevinhouyang@gmail.com",
+    description="Symops coding challenge",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/symopsio/backend-challenge",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
