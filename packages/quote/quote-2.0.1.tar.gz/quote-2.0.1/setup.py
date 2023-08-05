0bfafe9894f41c0060a4d7ef99cb75bd6ab58d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:40 +0000

    Add package quote/2.0.1/quote-2.0.1.tar.gz

---
+++ packages/quote/quote-2.0.1.tar.gz/quote-2.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="quote",
+    version="2.0.1",
+    author="Max Humber",
+    author_email="max.humber@gmail.com",
+    description="quote is a wrapper for the Goodreads Quote API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxhumber/quote",
+    packages=["quote"],
+    install_requires=["gazpacho>=0.9.3"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords=["quote", "quotes", "goodreads", "wrapper", "api"],
+    entry_points={"console_scripts": ["quote=quote.cli:cli"]},
+    python_requires=">=3.7",
+    setup_requires=["setuptools>=38.6.0"],
+)
