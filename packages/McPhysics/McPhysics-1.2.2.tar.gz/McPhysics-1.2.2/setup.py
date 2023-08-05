e933c1bcef31a5a4d2d0beaf3ca1e9c99db33e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:54 +0000

    Add package McPhysics/1.2.2/McPhysics-1.2.2.tar.gz

---
+++ packages/McPhysics/McPhysics-1.2.2.tar.gz/McPhysics-1.2.2/setup.py
@@ -0,0 +1,16 @@
+
+
+version = '1.2.2'
+
+
+
+from distutils.core import setup
+setup(name           = 'McPhysics',
+      version        = version,
+      description    = 'Useful tools for the McGill Undergraduate Physics Labs',
+      author         = 'Jack Sankey',
+      author_email   = 'jack.sankey@gmail.com',
+      url            = 'https://github.com/sankeylab/mcphysics',
+      packages       = ['mcphysics'],
+      package_dir    = {'mcphysics' : '.'}
+     )
