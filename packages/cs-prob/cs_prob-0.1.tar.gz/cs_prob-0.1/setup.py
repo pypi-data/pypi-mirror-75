bfc881743c6eb6bfe51fa0fd2db465bc5044890d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package cs-prob/0.1/cs_prob-0.1.tar.gz

---
+++ packages/cs-prob/cs_prob-0.1.tar.gz/cs_prob-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='cs_prob',
+      version='0.1',
+      description='Probability distributions',
+      packages=['cs_prob'],
+      author='Carlos Sell',
+      zip_safe=False)
