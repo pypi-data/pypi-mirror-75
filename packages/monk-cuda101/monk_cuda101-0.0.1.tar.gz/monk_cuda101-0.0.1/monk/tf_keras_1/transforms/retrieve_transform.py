9eadd810f0aea71fa6951220ffec771ce8930be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:11 +0000

    Add package monk-cuda101/0.0.1/monk_cuda101-0.0.1.tar.gz

---
+++ packages/monk-cuda101/monk_cuda101-0.0.1.tar.gz/monk_cuda101-0.0.1/monk/tf_keras_1/transforms/retrieve_transform.py
@@ -0,0 +1,37 @@
+from monk.tf_keras_1.transforms.imports import *
+from monk.tf_keras_1.transforms.common import set_transforms
+from monk.system.imports import *
+
+
+@accepts(dict, post_trace=False)
+#@TraceFunction(trace_args=False, trace_rv=False)
+def retrieve_trainval_transforms(system_dict):
+    '''
+    Retrieve training and validdation transforms in copy-from, and resume states
+
+    Args:
+        system_dict (dict): System dictionary storing experiment state and set variables
+
+    Returns:
+        dict: updated system dict
+    '''
+    set_phases = ["train", "val"];
+    system_dict = set_transforms(system_dict, set_phases);
+    return system_dict;
+
+
+@accepts(dict, post_trace=False)
+#@TraceFunction(trace_args=False, trace_rv=False)
+def retrieve_test_transforms(system_dict):
+    '''
+    Retrieve testing transforms in copy-from, and resume states
+
+    Args:
+        system_dict (dict): System dictionary storing experiment state and set variables
+
+    Returns:
+        dict: updated system dict
+    '''
+    set_phases = ["test"];
+    system_dict= set_transforms(system_dict, set_phases);
+    return system_dict;
