c5d33e98e37dd9c82368c404dd7a6b5c936f1662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:51 +0000

    Add package supbot/0.1.8/supbot-0.1.8.tar.gz

---
+++ packages/supbot/supbot-0.1.8.tar.gz/supbot-0.1.8/supbot/service_manager.py
@@ -0,0 +1,63 @@
+"""
+service_manager.py
+
+contains functions which performs operations (actions/checkers) on whatsapp app
+they use `AppDriver` to perform it.
+
+Checkers will get a rework to support other than `check_new_chat` eventually
+"""
+from enum import Enum
+
+from supbot import g
+from supbot.action import actions
+from supbot.results import ActionStatus, GotoStateResult
+from supbot.statemanager.state import main_state, GUIState, ChatState
+from supbot.statemanager.transition import goto_state
+
+
+class Event(Enum):
+    MESSAGE_RECEIVED = 0
+
+
+# checker
+def check_for_new_chat(current: GUIState) -> GUIState:
+    """
+    Checks for new chat on the main screen, first changes screen to main then uses driver to check it,
+    if new chat is found, go into that chat and get the messages and call event for it
+
+    :param current: data of current state of the app
+    :return: resultant state of gui after operation
+    """
+    _, current = goto_state(current, main_state)
+    chat = g.driver.get_new_chat()
+    if chat is not None:
+        result, current = goto_state(current, ChatState(chat))
+
+        if result == GotoStateResult.SUCCESS:
+            messages = g.driver.get_new_messages()
+            for m in messages:
+                g.system.call_event(Event.MESSAGE_RECEIVED, (chat, m))
+    return current
+
+
+# action helper
+def execute_action(current: GUIState) -> GUIState:
+    """
+    Pop action from the buffer, and execute it, update the gui state
+    :param current: current gui state
+    :return: resultant state of gui after action is executed
+    """
+    try:
+        action_id, action = g.system.action_buffer.popitem()
+    except IndexError:
+        return current
+
+    success, current = actions[action.action_name](current, action.data)
+
+    action.status = ActionStatus.SUCCESS if success else ActionStatus.UNSUCCESS
+
+    if action.callback:
+        action.callback(action_id)
+
+    return current
+
