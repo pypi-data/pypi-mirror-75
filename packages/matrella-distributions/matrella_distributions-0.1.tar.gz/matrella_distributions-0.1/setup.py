08a0c74d034c174b3c5072c91096ce7bbe7a29c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package matrella-distributions/0.1/matrella_distributions-0.1.tar.gz

---
+++ packages/matrella-distributions/matrella_distributions-0.1.tar.gz/matrella_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='matrella_distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['matrella_distributions'],
+      author="Mathieu Rella",
+      zip_safe=False)
