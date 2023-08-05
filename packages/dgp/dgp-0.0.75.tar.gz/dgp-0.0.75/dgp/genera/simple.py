97589eb40171cd326eef268f74f1bdc1ceb89e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:48 +0000

    Add package dgp/0.0.75/dgp-0.0.75.tar.gz

---
+++ packages/dgp/dgp-0.0.75.tar.gz/dgp-0.0.75/dgp/genera/simple.py
@@ -0,0 +1,32 @@
+from dataflows import Flow
+
+from ..core import BaseDataGenusProcessor
+from .load import LoaderDGP, PostLoaderDGP
+from .transform import TransformDGP
+from .enrich import EnricherDGP
+
+
+class SimpleDGP(BaseDataGenusProcessor):
+
+    def init(self,
+             steps=[
+                 LoaderDGP,
+                 PostLoaderDGP,
+                 TransformDGP,
+                 EnricherDGP,
+             ]):
+
+        self.steps = self.init_classes(steps)
+
+    def flow(self):
+        flows = []
+        for i, step in enumerate(self.steps):
+            flow = step.flow()
+            if flow:
+                flows.append(flow)
+            else:
+                break
+            flow = step.preflow()
+            if flow:
+                flows.insert(0, flow)
+        return Flow(*flows)
