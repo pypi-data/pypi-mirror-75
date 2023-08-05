30a0f1b46b884c586151dd46ca249683c8254b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:01 +0000

    Add package gsbin-probability/0.1/gsbin_probability-0.1.tar.gz

---
+++ packages/gsbin-probability/gsbin_probability-0.1.tar.gz/gsbin_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gsbin_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gsbin_probability'],
+      zip_safe=False)
