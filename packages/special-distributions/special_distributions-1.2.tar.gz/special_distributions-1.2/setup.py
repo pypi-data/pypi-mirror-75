c8e0b6ed84fdf965ac3b31665abfd6a74ba1c2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:38 +0000

    Add package special-distributions/1.2/special_distributions-1.2.tar.gz

---
+++ packages/special-distributions/special_distributions-1.2.tar.gz/special_distributions-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='special_distributions',
+      version='1.2',
+      description='special_distributions',
+      author = 'Mohamed Osama',
+      packages=['special_distributions'],
+      zip_safe=False)
