9cd94468f2943d08254ee41d030a284fb52c88c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:48 +0000

    Add package spotiwise/0.10.2/spotiwise-0.10.2-py3-none-any.whl

---
+++ packages/spotiwise/spotiwise-0.10.2-py3-none-any.whl/spotiwise/constants.py
@@ -0,0 +1,6 @@
+CLIENT_CREDS_ENV_VARS = {
+    "client_id": "SPOTIPY_CLIENT_ID",
+    "client_secret": "SPOTIPY_CLIENT_SECRET",
+    "client_username": "SPOTIPY_CLIENT_USERNAME",
+    "redirect_uri": "SPOTIPY_REDIRECT_URI",
+}
