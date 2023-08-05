0d3091fc2fd85ee5b80afae194ef3da157820b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:34 +0000

    Add package pyit2fls/0.5.1/pyit2fls-0.5.1.tar.gz

---
+++ packages/pyit2fls/pyit2fls-0.5.1.tar.gz/pyit2fls-0.5.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pyit2fls',
+      version='0.5.1',
+      description='Interval Type 2 Fuzzy Logic Systems in Python',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/Haghrah/PyIT2FLS',
+      author='Amir Arslan Haghrah',
+      author_email='arslan.haghrah@gmail.com',
+      license='MIT',
+      packages=['pyit2fls'],
+      install_requires=['numpy', 'matplotlib', ],
+      python_requires='>=3.6',
+      zip_safe=False)
