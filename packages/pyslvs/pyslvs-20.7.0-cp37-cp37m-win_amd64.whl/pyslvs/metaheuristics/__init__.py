5456e9d8541899830649c78d7f017ecf9ea460e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:05 +0000

    Add package pyslvs/20.7.0/pyslvs-20.7.0-cp37-cp37m-win_amd64.whl

---
+++ packages/pyslvs/pyslvs-20.7.0-cp37-cp37m-win_amd64.whl/pyslvs/metaheuristics/__init__.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+"""Kernel of Metaheuristic Algorithm."""
+
+__all__ = [
+    'ObjFunc',
+    'AlgorithmBase',
+    'AlgorithmType',
+    'ALGORITHM',
+    'PARAMS',
+    'DEFAULT_PARAMS',
+]
+__author__ = "Yuan Chang"
+__copyright__ = "Copyright (C) 2016-2020"
+__license__ = "AGPL"
+__email__ = "pyslvs@gmail.com"
+
+from typing import Dict, Union, Type
+from enum import unique, Enum
+from .utility import ObjFunc, AlgorithmBase
+from .rga import Genetic
+from .firefly import Firefly
+from .de import Differential
+from .tlbo import TeachingLearning
+
+
+@unique
+class AlgorithmType(str, Enum):
+    """Enum type of algorithms."""
+    RGA = "Real-coded Genetic Algorithm"
+    Firefly = "Firefly Algorithm"
+    DE = "Differential Evolution"
+    TLBO = "Teaching Learning Based Optimization"
+
+
+ALGORITHM: Dict[AlgorithmType, Type[AlgorithmBase]] = {
+    AlgorithmType.RGA: Genetic,
+    AlgorithmType.Firefly: Firefly,
+    AlgorithmType.DE: Differential,
+    AlgorithmType.TLBO: TeachingLearning,
+}
+PARAMS: Dict[AlgorithmType, Dict[str, Union[int, float]]] = {
+    AlgorithmType.RGA: {
+        'nPop': 500,
+        'pCross': 0.95,
+        'pMute': 0.05,
+        'pWin': 0.95,
+        'bDelta': 5.,
+    },
+    AlgorithmType.Firefly: {
+        'n': 80,
+        'alpha': 0.01,
+        'beta_min': 0.2,
+        'gamma': 1.,
+        'beta0': 1.,
+    },
+    AlgorithmType.DE: {
+        'strategy': 1,
+        'NP': 400,
+        'F': 0.6,
+        'CR': 0.9,
+    },
+    AlgorithmType.TLBO: {
+        'class_size': 50,
+    }
+}
+DEFAULT_PARAMS = {'max_gen': 1000, 'report': 50}
---
+++ packages/pyslvs/pyslvs-20.7.0-cp37-cp37m-win_amd64.whl/pyslvs/metaheuristics/utility.pyi
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from abc import abstractmethod
+from typing import TypeVar, Tuple, List, Dict, Callable, Optional, Generic, Any
+from numpy import ndarray, double
+
+FVal = TypeVar('FVal')
+
+
+class ObjFunc(Generic[FVal]):
+
+    @abstractmethod
+    def fitness(self, v: ndarray) -> double:
+        """(`cdef` function) Return the fitness from the variable list `v`.
+        This function will be directly called in the algorithms.
+        """
+        ...
+
+    @abstractmethod
+    def get_upper(self) -> ndarray:
+        ...
+
+    @abstractmethod
+    def get_lower(self) -> ndarray:
+        ...
+
+    @abstractmethod
+    def result(self, v: ndarray) -> FVal:
+        ...
+
+
+class AlgorithmBase(Generic[FVal]):
+
+    func: ObjFunc[FVal]
+
+    def __class_getitem__(cls, item):
+        # PEP 560
+        raise NotImplemented
+
+    @abstractmethod
+    def __init__(
+        self,
+        func: ObjFunc[FVal],
+        settings: Dict[str, Any],
+        progress_fun: Optional[Callable[[int, str], None]] = None,
+        interrupt_fun: Optional[Callable[[], bool]] = None
+    ):
+        """The argument `func` is a object inherit from [ObjFunc],
+        and all abstract methods should be implemented.
+
+        The format of argument `settings` can be customized.
+
+        The argument `progress_fun` will be called when update progress,
+        and the argument `interrupt_fun` will check the interrupt status from GUI or subprocess.
+        """
+        ...
+
+    def history(self) -> List[Tuple[int, float, float]]:
+        ...
+
+    def run(self) -> FVal:
+        ...
