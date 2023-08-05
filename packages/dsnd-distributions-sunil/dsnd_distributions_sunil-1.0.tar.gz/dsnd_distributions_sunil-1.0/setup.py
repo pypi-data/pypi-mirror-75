f48615b544fb5bdbea4d140e494f7d144b1b4603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:06 +0000

    Add package dsnd-distributions-sunil/1.0/dsnd_distributions_sunil-1.0.tar.gz

---
+++ packages/dsnd-distributions-sunil/dsnd_distributions_sunil-1.0.tar.gz/dsnd_distributions_sunil-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_sunil',
+      version='1.0',
+      description='Gaussian and Binomial distributions by Sunil',
+      packages=['dsnd_distributions_sunil'],
+      author='Sunil Thakur',
+      author_email='sunilforu@gmail.com',
+      zip_safe=False)
