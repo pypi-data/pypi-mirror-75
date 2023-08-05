20551842a54e33b7322d8da38d7f7924a2284d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:44 +0000

    Add package distributions-testuno/0.1/distributions_testuno-0.1.tar.gz

---
+++ packages/distributions-testuno/distributions_testuno-0.1.tar.gz/distributions_testuno-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_testuno',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_testuno'],
+      zip_safe=False)
