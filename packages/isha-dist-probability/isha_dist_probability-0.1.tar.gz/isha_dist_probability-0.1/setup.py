fa84b2c6d9b6285c35b1258d8f9371fb2086383c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:12 +0000

    Add package isha-dist-probability/0.1/isha_dist_probability-0.1.tar.gz

---
+++ packages/isha-dist-probability/isha_dist_probability-0.1.tar.gz/isha_dist_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='isha_dist_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['isha_dist_probability'],
+      author ='isha anand',
+      zip_safe=False)
