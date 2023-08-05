57af0d66002c03c75cb52900cc1ceb336a522613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package py42/1.7.1/py42-1.7.1.tar.gz

---
+++ packages/py42/py42-1.7.1.tar.gz/py42-1.7.1/src/py42/clients/detectionlists/departing_employee.py
@@ -0,0 +1,157 @@
+import json
+
+from py42.clients import BaseClient
+from py42.clients.util import get_all_pages
+
+
+class DepartingEmployeeClient(BaseClient):
+    """A client for interacting with Code42 Departing Employee APIs."""
+
+    _uri_prefix = u"/svc/api/v2/departingemployee/{0}"
+
+    def __init__(self, session, user_context, detection_list_user_client):
+        super(DepartingEmployeeClient, self).__init__(session)
+        self._user_context = user_context
+        self._detection_list_user_client = detection_list_user_client
+
+    def add(self, user_id, departure_date=None):
+        """Adds a user to the Departing Employees list. Creates a detection list user profile if one \
+            didn't already exist.
+        `REST Documentation <https://ecm-east.us.code42.com/svc/swagger/index.html?urls.primaryName=v2#/>`__
+
+        Raises a :class:`Py42BadRequestError` when a user already exists in the Departing Employee \
+            detection list.
+
+        Args:
+            user_id (str or int): The Code42 userUid of the user you want to add to the departing \
+                employees list.
+            departure_date (str, optional): Date in yyyy-MM-dd format. Date is treated as UTC. Defaults to None.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        if self._detection_list_user_client.create_if_not_exists(user_id):
+            tenant_id = self._user_context.get_current_tenant_id()
+
+            data = {
+                u"tenantId": tenant_id,
+                u"userId": user_id,
+                u"departureDate": departure_date,
+            }
+            uri = self._uri_prefix.format(u"add")
+            return self._session.post(uri, data=json.dumps(data))
+
+    def get(self, user_id):
+        """Gets departing employee data of a user.
+        `REST Documentation <https://ecm-east.us.code42.com/svc/swagger/index.html?urls.primaryName=v2#/>`__
+
+        Args:
+            user_id (str or int): The Code42 userUid of the user.
+
+        Returns:
+            :class:`py42.sdk.response.Py42Response`
+        """
+        tenant_id = self._user_context.get_current_tenant_id()
+        uri = self._uri_prefix.format(u"get")
+        data = {u"userId": user_id, u"tenantId": tenant_id}
+        return self._session.post(uri, data=json.dumps(data))
+
+    def remove(self, user_id):
+        """Removes a user from the Departing Employees list.
+        `REST Documentation <https://ecm-east.us.code42.com/svc/swagger/index.html?urls.primaryName=v2#/>`__
+
+        Args:
+            user_id (str or int): The Code42 userUid of the user.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+
+        tenant_id = self._user_context.get_current_tenant_id()
+        uri = self._uri_prefix.format(u"remove")
+        data = {u"userId": user_id, u"tenantId": tenant_id}
+        return self._session.post(uri, data=json.dumps(data))
+
+    def _get_departing_employees_page(
+        self,
+        tenant_id=None,
+        filter_type=None,
+        sort_key=u"CREATED_AT",
+        sort_direction=u"DESC",
+        page_num=None,
+        page_size=None,
+    ):
+
+        uri = self._uri_prefix.format(u"search")
+        data = {
+            u"tenantId": tenant_id,
+            u"pgSize": 100,
+            u"pgNum": page_num,
+            u"filterType": filter_type,
+            u"srtKey": sort_key,
+            u"srtDirection": sort_direction,
+        }
+        return self._session.post(uri, data=json.dumps(data))
+
+    def get_all(
+        self, filter_type=u"OPEN", sort_key=u"CREATED_AT", sort_direction=u"DESC"
+    ):
+        """Gets all Departing Employees.
+
+        Args:
+            filter_type (str, optional): Filter results by status. Defaults to "OPEN".
+            sort_key (str, optional): Key to sort results on. Options: (``CREATED_AT``,
+                ``DEPARTURE_DATE``, ``DISPLAY_NAME``, ``NUM_EVENTS``, ``TOTAL_BYTES``). Defaults to
+                ``CREATED_AT``.
+            sort_direction (str, optional): Sort direction. Options: (``ASC``, ``DESC``). Defaults
+                to ``DESC``.
+
+        Returns:
+            generator: An object that iterates over :class:`py42.response.Py42Response` objects
+            that each contain a page of departing employees.
+        """
+        return get_all_pages(
+            self._get_departing_employees_page,
+            u"items",
+            tenant_id=self._user_context.get_current_tenant_id(),
+            filter_type=filter_type,
+            sort_key=sort_key,
+            sort_direction=sort_direction,
+        )
+
+    def set_alerts_enabled(self, alerts_enabled=True):
+        """Enable or disable email alerting on Departing Employee exposure events.
+        `REST Documentation <https://ecm-east.us.code42.com/svc/swagger/index.html?urls.primaryName=v2#/>`__
+
+        Args:
+            alerts_enabled (bool): Set alerting to on (True) or off (False). Defaults to True.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        tenant_id = self._user_context.get_current_tenant_id()
+        uri = self._uri_prefix.format(u"setalertstate")
+        data = {u"tenantId": tenant_id, u"alertsEnabled": alerts_enabled}
+        return self._session.post(uri, data=json.dumps(data))
+
+    def update_departure_date(self, user_id, departure_date):
+        """Add or modify details of an existing Departing Employee case.
+        `REST Documentation <https://ecm-east.us.code42.com/svc/swagger/index.html?urls.primaryName=v2#/>`__
+
+        Args:
+            user_id (str): The Code42 userUid of the user.
+            departure_date (date): Date in yyyy-MM-dd format. Date is treated as UTC.
+
+        Returns:
+            :class:`py42.sdk.response.Py42Response`
+        """
+
+        tenant_id = self._user_context.get_current_tenant_id()
+
+        uri = self._uri_prefix.format(u"update")
+        data = {
+            u"tenantId": tenant_id,
+            u"userId": user_id,
+            u"departureDate": departure_date,
+        }
+        return self._session.post(uri, data=json.dumps(data))
---
+++ packages/py42/py42-1.7.1.tar.gz/py42-1.7.1/src/py42/modules/detectionlists.py
@@ -0,0 +1,139 @@
+class DetectionListsModule(object):
+    def __init__(self, microservice_client_factory):
+        self._microservice_client_factory = microservice_client_factory
+        self._detection_list_user_client = None
+
+    @property
+    def departing_employee(self):
+        return self._microservice_client_factory.get_departing_employee_client()
+
+    @property
+    def high_risk_employee(self):
+        return self._microservice_client_factory.get_high_risk_employee_client()
+
+    def create_user(self, username):
+        """Create a detection list profile for a user.
+
+        Args:
+            username (str): The Code42 username of the user.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        factory = self._microservice_client_factory
+        self._detection_list_user_client = factory.get_detection_list_user_client()
+        return self._detection_list_user_client.create(username)
+
+    def get_user(self, username):
+        """Get user details by username.
+
+        Args:
+            username (str): The Code42 username of the user.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        factory = self._microservice_client_factory
+        self._detection_list_user_client = factory.get_detection_list_user_client()
+        return self._detection_list_user_client.get(username)
+
+    def get_user_by_id(self, user_id):
+        """Get user details by user_id.
+
+        Args:
+            user_id (str or int): The Code42 userId of the user.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        factory = self._microservice_client_factory
+        self._detection_list_user_client = factory.get_detection_list_user_client()
+        return self._detection_list_user_client.get_by_id(user_id)
+
+    def update_user_notes(self, user_id, notes):
+        """Add or update notes related to the user.
+
+        Args:
+            user_id (str or int): The userUid of the user whose notes you want to update.
+            notes (str): User profile notes.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        factory = self._microservice_client_factory
+        self._detection_list_user_client = factory.get_detection_list_user_client()
+        return self._detection_list_user_client.update_notes(user_id, notes)
+
+    def add_user_risk_tags(self, user_id, tags):
+        """Add one or more risk factor tags.
+
+        Args:
+            user_id (str or int): The userUid of the user whose risk factor tag(s) you want to update.
+            tags (str or list of str ): A single tag or multiple tags in a list to be added. For
+                example: ``"tag1"`` or ``["tag1", "tag2"]``. For python version 2.X, pass ``u"str"``
+                instead of ``"str"``.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        factory = self._microservice_client_factory
+        self._detection_list_user_client = factory.get_detection_list_user_client()
+        return self._detection_list_user_client.add_risk_tags(user_id, tags)
+
+    def remove_user_risk_tags(self, user_id, tags):
+        """Remove one or more risk factor tags.
+
+        Args:
+            user_id (str or int): The userUid of the user whose risk factor tag(s) needs you want to remove.
+            tags (str or list of str ): A single tag or multiple tags in a list to be removed. For
+                example: ``"tag1"`` or ``["tag1", "tag2"]``. For python version 2.X, pass ``u"str"``
+                instead of ``"str"``.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        factory = self._microservice_client_factory
+        self._detection_list_user_client = factory.get_detection_list_user_client()
+        return self._detection_list_user_client.remove_risk_tags(user_id, tags)
+
+    def add_user_cloud_alias(self, user_id, alias):
+        """Add a cloud alias to a user.
+
+        Args:
+            user_id (str or int): The userUid of the user whose alias you want to update.
+            alias (str): The alias to be added.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        factory = self._microservice_client_factory
+        self._detection_list_user_client = factory.get_detection_list_user_client()
+        return self._detection_list_user_client.add_cloud_alias(user_id, alias)
+
+    def remove_user_cloud_alias(self, user_id, alias):
+        """Remove a cloud alias from a user.
+
+        Args:
+            user_id (str or int): The userUid of the user whose alias needs to be removed.
+            alias (str): The alias to be removed.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        factory = self._microservice_client_factory
+        self._detection_list_user_client = factory.get_detection_list_user_client()
+        return self._detection_list_user_client.remove_cloud_alias(user_id, alias)
+
+    def refresh_user_scim_attributes(self, user_id):
+        """Refresh SCIM attributes of a user.
+        `REST documentation <https://ecm-default.prod.ffs.us2.code42.com/svc/swagger/index.html?urls.primaryName=v2#/User/UserControllerV2_RefreshUser>`__
+
+        Args:
+            user_id (str or int): The userUid of the user whose attributes you wish to refresh.
+
+        Returns:
+            :class:`py42.response.Py42Response`
+        """
+        factory = self._microservice_client_factory
+        self._detection_list_user_client = factory.get_detection_list_user_client()
+        return self._detection_list_user_client.refresh(user_id)
