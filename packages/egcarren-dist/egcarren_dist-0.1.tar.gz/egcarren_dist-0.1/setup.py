645f246780224d3892808643b3e14ff4599255fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package egcarren-dist/0.1/egcarren_dist-0.1.tar.gz

---
+++ packages/egcarren-dist/egcarren_dist-0.1.tar.gz/egcarren_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='egcarren_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['egcarren_dist'],
+      zip_safe=False)
