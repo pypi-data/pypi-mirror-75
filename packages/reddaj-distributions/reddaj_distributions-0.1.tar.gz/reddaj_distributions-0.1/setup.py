322b32c674c7a084ef569092a40c88c14cc08f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:37 +0000

    Add package reddaj-distributions/0.1/reddaj_distributions-0.1.tar.gz

---
+++ packages/reddaj-distributions/reddaj_distributions-0.1.tar.gz/reddaj_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='reddaj_distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['reddaj_distributions'],
+      author = 'Jadder Moya',
+      author_email = 'jadder.moya@gmail.com',
+      zip_safe=False)
