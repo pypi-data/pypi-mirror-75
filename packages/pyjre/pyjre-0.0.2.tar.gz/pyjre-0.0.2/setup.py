8c8a033dd5dbb0b4b2d21aed356fc6bec37013b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:38 +0000

    Add package pyjre/0.0.2/pyjre-0.0.2.tar.gz

---
+++ packages/pyjre/pyjre-0.0.2.tar.gz/pyjre-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import setuptools
+setup(
+	name="pyjre",
+	packages=["pyjre"],
+	version="0.0.2",
+	description="Wrapper for the Joe Rogan Experience podcase website data.",
+	url="https://github.com/arnavkohli/pyjre"
+)
+
+
