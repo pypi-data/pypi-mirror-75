674c61924d7afd1913b03d53b26b6213de6ba4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:29 +0000

    Add package dsdn-distribution-sc/0.1/dsdn_distribution_sc-0.1.tar.gz

---
+++ packages/dsdn-distribution-sc/dsdn_distribution_sc-0.1.tar.gz/dsdn_distribution_sc-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsdn_distribution_sc',
+      version='0.1',
+      description='Distributions',
+      packages=['dsdn_distribution_sc'],
+      author='Sonali Chawla',
+      author_email='sc@gmail.com',
+      zip_safe=False)
