7ce4b6531c3609a39313e03f415ded7256179232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:28 +0000

    Add package calc-dist-ml/0.1/calc_dist_ml-0.1.tar.gz

---
+++ packages/calc-dist-ml/calc_dist_ml-0.1.tar.gz/calc_dist_ml-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='calc_dist_ml',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['calc_dist_ml'],
+      zip_safe=False)
