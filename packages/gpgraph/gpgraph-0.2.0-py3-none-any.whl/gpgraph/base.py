d78c3b59119e4371f3239fc4e291a8d75302911f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:22 +0000

    Add package gpgraph/0.2.0/gpgraph-0.2.0-py3-none-any.whl

---
+++ packages/gpgraph/gpgraph-0.2.0-py3-none-any.whl/gpgraph/base.py
@@ -0,0 +1,105 @@
+import numpy as np
+import networkx as nx
+from .pyplot import draw_gpgraph
+from .models import strong_selection_weak_mutation
+from gpmap import GenotypePhenotypeMap
+
+def get_neighbors(genotype, mutations):
+    """Return all genotypes
+
+    Parameters
+    ----------
+    genotype : str
+        reference genotype.
+    mutations : dict
+        mutations dictionary from a genotype-phenotype map.
+
+    Returns
+    -------
+    neighbors : tuple
+        tuple of neighbors.
+    """
+    neighbors = tuple()
+    for i, char in enumerate(genotype):
+        # Copy reference genotype
+        genotype2 = list(genotype)[:]
+
+        # Find possible mutations at site i.
+        if mutations[i] is not None:
+            options = mutations[i][:]
+            options.remove(char)
+
+            # Construct neighbor genotypes.
+            for j in options:
+                genotype2[i] = j
+                genotype2_ = "".join(genotype2)
+                neighbors += (genotype2_,)
+    return neighbors
+
+
+class GenotypePhenotypeGraph(nx.DiGraph):
+    """Construct a NetworkX DiGraph object from a GenotypePhenotypeMap."""
+    def __init__(self, gpm, *args, **kwargs):
+        super(GenotypePhenotypeGraph, self).__init__(*args, **kwargs)
+        self.add_gpm(gpm)
+
+    def __repr__(self):
+        draw_gpgraph(self)
+        return super(GenotypePhenotypeGraph, self).__repr__()
+
+    def add_gpm(self, gpm):
+        """Attach a Network DiGraph to GenotypePhenotypeMap object."""
+        # Add gpm
+        self.gpm = gpm
+        data = self.gpm.data
+
+        # genotypes to index.
+        m = dict(zip(data.genotypes, data.index))
+
+        # Iterate through nodes.
+        edges = []
+        for i in data.index:
+            # Add node to graph
+            row = data.loc[i]
+            self.add_node(i, **row)
+
+            # Get edges from node.
+            neighbors = get_neighbors(row['genotypes'], self.gpm.mutations)
+            for neighbor in neighbors:
+                # Check if node is in data.
+                if neighbor in m:
+                    j = m[neighbor]
+                    edges.append((i, j))
+
+        # Add edges to network
+        self.add_edges_from(edges)
+
+    def add_model(self, model=strong_selection_weak_mutation, **params):
+        """Add a transition model to the edges."""
+        # Add model to class.
+        self.model = staticmethod(model)
+
+        for edge in self.edges():
+            node1 = edge[0]
+            node2 = edge[1]
+
+            phenotype1 = self.gpm.phenotypes[node1]
+            phenotype2 = self.gpm.phenotypes[node2]
+
+            self.edges[edge]['prob'] = model(phenotype1, phenotype2, **params)
+
+    @classmethod
+    def read_json(cls, fname):
+        """Read graph from json file."""
+        gpm = GenotypePhenotypeMap.read_json(fname)
+        return cls(gpm)
+
+    @classmethod
+    def read_csv(cls, fname, wildtype, mutations=None):
+        """Read graph from csv file."""
+        gpm = GenotypePhenotypeMap.read_json(
+            fname,
+            wildtype=wildtype,
+            mutations=mutations
+        )
+        return cls(gpm)
---
+++ packages/gpgraph/gpgraph-0.2.0-py3-none-any.whl/gpgraph/pyplot/paths.py
@@ -0,0 +1,195 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from gpgraph.paths import paths_prob_to_edges_flux, forward_paths_prob
+from gpgraph.pyplot.pos import flattened
+
+from .utils import despine
+
+
+def draw_paths(
+        G,
+        paths=None,
+        pos=None,
+        edge_list=None,
+        source = None,
+        target = None,
+        edge_equal=False,
+        edge_scalar=1.0,
+        edge_color='k',
+        width = 1.0,
+        style='solid',
+        edge_alpha=1.0,
+        arrows=False,
+        arrowstyle='-|>',
+        arrowsize=10,
+        nodelist=None,
+        node_size=300,
+        node_color='r',
+        node_shape='o',
+        alpha=1.0,
+        cmap='plasma',
+        cmap_truncate=False,
+        cmap_max=0.95,
+        cmap_min=0.05,
+        vmin=None,
+        vmax=None,
+        ax=None,
+        linewidths=0,
+        edgecolors="black",
+        label=None,
+        colorbar=False,
+):
+    """Draw paths in GenotypePhenotypeGraph
+
+    Parameters
+    ----------
+    G : graph
+       A networkx graph
+
+    paths : All forward paths from source genotype to
+        target genotype.
+
+    source: source genotype. Default value is the first value
+        in the list of genotypes.
+
+    target: target genotype. Default value is the last value
+        in the list of genotypes.
+
+    pos : dictionary
+       A dictionary with nodes as keys and positions as values.
+       Positions should be sequences of length 2.
+       Default value uses flattened function included in gpgraph.utils.
+
+    edge_list : collection of edge tuples
+       Draw only specified edges(default=G.edges())
+
+    width : float, or array of floats
+       Line width of edges (default=1.0)
+
+    edge_color : color string, or array of floats
+       Edge color. Can be a single color format string (default='r'),
+       or a sequence of colors with the same length as edgelist.
+       If numeric values are specified they will be mapped to
+       colors using the edge_cmap and edge_vmin,edge_vmax parameters.
+
+    style : string
+       Edge line style (default='solid') (solid|dashed|dotted,dashdot)
+
+    alpha : float
+       The edge transparency (default=1.0)
+
+    cmap_truncate : bool
+        Use only a subspace of the color map spectrum. If False whole color spectrum (0 to 1) is used.
+
+    cmap_min : float (default=0.05)
+        Lower bound of the color spectrum.
+
+    cmap_max : float (default=0.95)
+        Upper bound of the color spectrum.
+
+    edge_cmap : Matplotlib colormap
+       Colormap for mapping intensities of edges (default=None)
+
+    edge_vmin,edge_vmax : floats
+       Minimum and maximum for edge colormap scaling (default=None)
+
+    ax : Matplotlib Axes object, optional
+       Draw the graph in the specified Matplotlib axes.
+
+    arrows : bool, optional (default=True)
+       For directed graphs, if True draw arrowheads.
+       Note: Arrows will be the same color as edges.
+
+    arrowstyle : str, optional (default='-|>')
+       For directed graphs, choose the style of the arrow heads.
+       See :py:class: `matplotlib.patches.ArrowStyle` for more
+       options.
+
+    arrowsize : int, optional (default=10)
+       For directed graphs, choose the size of the arrow head head's length and
+       width. See :py:class: `matplotlib.patches.FancyArrowPatch` for attribute
+       `mutation_scale` for more info.
+
+    label : [None| string]
+       Label for legend
+
+    """
+    # Check what values were passed to function, or assign default values
+    if not pos:
+        pos = flattened(G)
+
+    if not edge_list:
+        edge_list = G.edges()
+
+    if not nodelist:
+        nodelist = G.nodes()
+
+    if not source:
+        source = G.gpm.genotypes[0]
+
+    if not target:
+        target = G.gpm.genotypes[-1]
+
+    if not paths:
+        paths = forward_paths_prob(G, source, target)
+
+    # Get Figure.
+    if ax is None:
+        fig, ax = plt.subplots()
+        despine(ax)
+    else:
+        fig = ax.get_figure()
+
+    # Get flux through edges
+    edges = paths_prob_to_edges_flux(paths)
+    edge_list = list(edges.keys())
+
+    # Default options
+    node_options = dict(
+        nodelist=G.nodes(),
+        vmin=vmin,
+        vmax=vmax,
+        node_shape=node_shape,
+        node_size=node_size,
+        node_color=[G.nodes[n]['phenotypes'] for n in nodelist],
+        linewidths=linewidths,
+        edgecolors=edgecolors,
+        cmap=cmap,
+        cmap_truncate=False,
+        labels={n: G.nodes[n]['genotypes'] for n in nodelist}
+    )
+
+    # Draw edges
+    nx.draw_networkx_edges(
+        G=G,
+        pos=pos,
+        edgelist=edge_list,
+        width=width,
+        edge_color=edge_color,
+        ax=ax,
+        style=style,
+        alpha=edge_alpha,
+        arrows=arrows,
+        arrowstyle=arrowstyle,
+        arrowsize=arrowsize,
+    )
+
+    # Draw nodes.
+    nx.draw_networkx_nodes(
+        G=G,
+        pos=pos,
+        ax=ax,
+        **node_options
+    )
+
+    # Add a color bar
+    if colorbar:
+        norm = mpl.colors.Normalize(
+            vmin=vmin,
+            vmax=vmax)
+
+        # create a ScalarMappable and initialize a data structure
+        cm = mpl.cm.ScalarMappable(cmap=cmap, norm=norm)
+        cm.set_array([])
+        fig.colorbar(cm)
