b9a5c90c35b6c81293fd92e50865ec1e7afea264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package pyproba/1.0.1/pyproba-1.0.1.tar.gz

---
+++ packages/pyproba/pyproba-1.0.1.tar.gz/pyproba-1.0.1/README.md
@@ -0,0 +1,5 @@
+## PYPROBA
+
+Open source library which will help programmers to easily understand the different 
+probability distributions like Binomial distribution and Gaussian distribution.
+
---
+++ packages/pyproba/pyproba-1.0.1.tar.gz/pyproba-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'pyproba',
+      version = '1.0.1',
+      description = 'Exploring Probability distributions',
+      packages = ['pyproba'],
+      author = 'Aditya Kalra',
+      author_email = 'adityakalra581@gmail.com',
+      url = 'https://github.com/adityakalra581/pyproba',   
+      keywords = ['probability-distribution'],
+      zip_safe = False)
