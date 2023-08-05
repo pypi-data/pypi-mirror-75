663618a24fa016f1e1813eb16c0957351058c101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:03 +0000

    Add package pyproba/1.0/pyproba-1.0.tar.gz

---
+++ packages/pyproba/pyproba-1.0.tar.gz/pyproba-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'pyproba',
+      version = '1.0',
+      description = 'Exploring Probability distributions',
+      packages = ['pyproba'],
+      author = 'Aditya Kalra',
+      author_email = 'adityakalra581@gmail.com',
+      zip_safe = False)
