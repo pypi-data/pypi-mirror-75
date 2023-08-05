a98ac879ce59ba2859d049763592cc04768afb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:13 +0000

    Add package rosmobilelib/0.1.2.2/rosmobilelib-0.1.2.2-py3-none-any.whl

---
+++ packages/rosmobilelib/rosmobilelib-0.1.2.2-py3-none-any.whl/rosmobilelib/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.1.0'
+
+from .rosclients import MobileClient, CameraListener
+
+__all__ = ['MobileClient', 'CameraListener']
+
+## EX: 
+# import rosmobilelib
+# import rosmobilelib.rostools 
