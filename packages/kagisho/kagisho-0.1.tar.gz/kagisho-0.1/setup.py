a792a787616af1ebf29b2d3656971b3f80147fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:52 +0000

    Add package kagisho/0.1/kagisho-0.1.tar.gz

---
+++ packages/kagisho/kagisho-0.1.tar.gz/kagisho-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='kagisho',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['kagisho'],
+      author= 'nkadimeng',
+      zip_safe=False)
