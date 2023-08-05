a2b29bbb13f4923b229961f697e720339e2737d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:04 +0000

    Add package maddyscreen-distributions/0.1/maddyscreen_distributions-0.1.tar.gz

---
+++ packages/maddyscreen-distributions/maddyscreen_distributions-0.1.tar.gz/maddyscreen_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='maddyscreen_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['maddyscreen_distributions'],
+      zip_safe=False)
