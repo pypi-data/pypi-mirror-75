684f7098ad6685007042ccb3a504a01aca39e37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:56 +0000

    Add package mendelai-brat-parser/0.0.4/mendelai_brat_parser-0.0.4.tar.gz

---
+++ packages/mendelai-brat-parser/mendelai_brat_parser-0.0.4.tar.gz/mendelai_brat_parser-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mendelai_brat_parser",
+    version="0.0.4",
+    author="Khaled Essam",
+    author_email="khaled.essam@mendel.ai",
+    description="BRAT Parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mendelhealth/brat-parser",
+    package_dir={"": "brat_parser"},
+    packages=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
