7b04ea6042eb15417f36ae5caf4455ca077c338e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:43 +0000

    Add package lm.ext/0.0.1/lm.ext-0.0.1.tar.gz

---
+++ packages/lm.ext/lm.ext-0.0.1.tar.gz/lm.ext-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="lm.ext",
+    version="0.0.1",
+    author="雷默工作室",
+    author_email="3089124325@qq.com",
+    description="雷默工作室的强大功能库",
+    long_description_contect_type="text/markdown",
+    url="https://github.com/",
+    packpages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
