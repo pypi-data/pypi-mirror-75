fa1e4a3b17b60f4c1560c7873d40e21833e33dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:43 +0000

    Add package kevy-stats-pac/0.0/kevy_stats_pac-0.0.tar.gz

---
+++ packages/kevy-stats-pac/kevy_stats_pac-0.0.tar.gz/kevy_stats_pac-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='kevy_stats_pac',
+      version='0.0',
+      description='Calculate and visuallize, Gaussian, Binomial and Normal distributions',
+      packages=['kevy_stats_pac'],
+      author_email='mpofukelvintafadzwa@gmail.com',
+      zip_safe=False)
