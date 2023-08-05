14c0d01a642df1cebd002f9229dc21719ff1f176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:36 +0000

    Add package dolores/1.0.1/dolores-1.0.1.tar.gz

---
+++ packages/dolores/dolores-1.0.1.tar.gz/dolores-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dolores",
+    version="1.0.1",
+    author="Phil Mohun, Malcolm Navarro, DNE LLC",
+    author_email="dolores@philmohun.com",
+    description="Dolores is a Python library for developers using GPT-3.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dne-digital/calvin",
+    download_url="https://pypi.org/project/dolores/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    	"License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
