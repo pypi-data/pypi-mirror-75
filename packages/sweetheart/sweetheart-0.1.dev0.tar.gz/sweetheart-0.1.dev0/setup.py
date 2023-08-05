4c6b416c7694760eeb20dac50e2d7d1b97e2cdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:43 +0000

    Add package sweetheart/0.1.dev0/sweetheart-0.1.dev0.tar.gz

---
+++ packages/sweetheart/sweetheart-0.1.dev0.tar.gz/sweetheart-0.1.dev0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name="sweetheart",
+    version= "0.1-dev",
+    license= "CeCILL-C",
+    packages=[""],
+
+    # metadata to display on PyPI
+    author="Nicolas Champion",
+    author_email="champion.nicolas@gmail.com",
+    description="a supercharged heart for the non-expert hands",
+    #long_description = "",
+    keywords="ubuntu mongodb starlette webapp industry4.0",
+    plateformes = "Ubuntu 20.04",
+
+    classifiers = [
+        "Environment :: Web Environment",
+        "Framework :: CherryPy",
+        "Intended Audience :: Manufacturing",
+        "License :: CeCILL-C Free Software License Agreement (CECILL-C)",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Database :: Database Engines/Servers",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Office/Business",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: User Interfaces",
+        "Topic :: System :: Installation/Setup" ]
+)
