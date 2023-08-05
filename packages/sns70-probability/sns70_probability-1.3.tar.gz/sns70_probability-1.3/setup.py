0539c988d95643334efe92a1dd570d5681741669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:51 +0000

    Add package sns70-probability/1.3/sns70_probability-1.3.tar.gz

---
+++ packages/sns70-probability/sns70_probability-1.3.tar.gz/sns70_probability-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sns70_probability',
+      version='1.3',
+      description='sns70: Gaussian and Binomial distributions',
+      packages=['sns70_probability'],
+      author="Somesh Saini1970",
+      author_email="somesh.saini1970@gmail.com",
+      zip_safe=False)
