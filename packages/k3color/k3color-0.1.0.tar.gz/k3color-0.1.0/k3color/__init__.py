5e0292db298b041df3cddd82e01213970c264102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:02 +0000

    Add package k3color/0.1.0/k3color-0.1.0.tar.gz

---
+++ packages/k3color/k3color-0.1.0.tar.gz/k3color-0.1.0/k3color/__init__.py
@@ -0,0 +1,29 @@
+"""
+k3color creates colored text on terminal.
+"""
+
+from .color import (
+    percentage,
+    Str,
+    fading_color,
+
+    blue,
+    cyan,
+    green,
+    purple,
+    red,
+    white,
+    yellow,
+
+    dark,
+
+    optimal,
+    normal,
+    loaded,
+    warn,
+    danger,
+
+)
+
+__version__ = "0.1.0"
+__name__ = "k3color"
