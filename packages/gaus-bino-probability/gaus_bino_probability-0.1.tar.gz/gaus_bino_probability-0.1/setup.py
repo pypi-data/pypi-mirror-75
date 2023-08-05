4887c77267563ba089eb0ca61ffd9460406b64f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package gaus-bino-probability/0.1/gaus_bino_probability-0.1.tar.gz

---
+++ packages/gaus-bino-probability/gaus_bino_probability-0.1.tar.gz/gaus_bino_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaus_bino_probability',
+      version='0.1',
+      description='Gaussian And Binomial distributions',
+      packages=['gaus_bino_probability'],
+      author='Nishant Gupta',
+      zip_safe=False)
