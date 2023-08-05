4e4ee2d2b75791d4ef551ad5d87dcdc676ca18ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:53 +0000

    Add package distributions-EH/0.2/distributions_EH-0.2.tar.gz

---
+++ packages/distributions-EH/distributions_EH-0.2.tar.gz/distributions_EH-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_EH',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_EH'],
+      author='Erkan Hatipoglu',
+      author_email='e109261@gmail.com',
+      zip_safe=False)
