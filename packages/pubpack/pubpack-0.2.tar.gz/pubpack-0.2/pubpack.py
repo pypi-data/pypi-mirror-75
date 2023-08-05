4e12bebc7ae5d6709e0424ab2700e4cab1a9a73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:58 +0000

    Add package pubpack/0.2/pubpack-0.2.tar.gz

---
+++ packages/pubpack/pubpack-0.2.tar.gz/pubpack-0.2/pubpack.py
@@ -0,0 +1,11 @@
+"""An amazing sample package!"""
+
+__version__ = '0.2'
+
+class Pubpack:
+
+    print ("initializing Pubpack...")
+
+    def runfun1(self):
+        """ Run function 1 """
+        print("u r in runfun1")
