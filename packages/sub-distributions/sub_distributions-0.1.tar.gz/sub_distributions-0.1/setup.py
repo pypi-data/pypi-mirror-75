33527a62cfe4bce07c8d1b41d54e562fc7c71e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:49 +0000

    Add package sub-distributions/0.1/sub_distributions-0.1.tar.gz

---
+++ packages/sub-distributions/sub_distributions-0.1.tar.gz/sub_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sub_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sub_distributions'],
+      author='Subham Kumar Gupta',
+      author_email='guptasubham175@gmail.com',
+      zip_safe=False)
