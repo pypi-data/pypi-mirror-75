d9c84ccb77e7d87d5e9020a1093194d29e9bb163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:14 +0000

    Add package adbb/2/adbb-2-py2.py3-none-any.whl

---
+++ packages/adbb/adbb-2-py2.py3-none-any.whl/adbb/anames.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+#
+# This file is part of adbb.
+#
+# adbb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# adbb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with adbb.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import difflib
+import gzip
+import os
+import sys
+import tempfile
+import time
+import xml.etree.ElementTree as etree
+
+if sys.version_info[0] < 3:
+    import urllib2 as local_urllib
+    urllib = local_urllib
+    urllib.error = local_urllib
+    urllib.request = local_urllib
+else:
+    import urllib
+    import urllib.error
+    import urllib.request
+
+import adbb.animeobjs
+from adbb.errors import AniDBError, AniDBFileError
+
+_animetitles_useragent="adbb"
+_animetitles_url="http://anidb.net/api/animetitles.xml.gz"
+iso_639_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "ISO-639-2_utf-8.txt")
+_update_interval = datetime.timedelta(hours=36)
+
+xml = None
+languages = None
+
+
+def update_animetitles():
+    global xml
+
+    file_name = _animetitles_url.split('/')[-1]
+    if os.name == 'posix':
+        animetitles_file = os.path.join('/var/tmp', file_name)
+    else:
+        animetitles_file = os.path.join(tempfile.gettempdir(), file_name)
+
+    tmp_dir = os.path.dirname(animetitles_file)
+    if not os.access(tmp_dir, os.W_OK):
+        raise AniDBError("Cant get writeable temp path: %s" % tmp_dir)
+
+    old_file_exists = os.path.isfile(animetitles_file)
+    if old_file_exists:
+        stat = os.stat(animetitles_file)
+        file_moddate = datetime.datetime.fromtimestamp(stat.st_mtime)
+        if file_moddate > (datetime.datetime.now() - _update_interval):
+            xml = _read_anidb_xml(animetitles_file)
+            return
+
+    now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S.%f")
+    tmp_file = os.path.join(os.path.dirname(animetitles_file), ".animetitles{}.xml.gz".format(now))
+
+    try:
+        with open(tmp_file, "wb") as f:
+            req = urllib.request.Request(
+                _animetitles_url,
+                data=None,
+                headers={
+                    'User-Agent': _animetitles_useragent
+                }
+            )
+            res = urllib.request.urlopen(req)
+            adbb.log.info('Fetching titledb cache file from anidb.')
+            f.write(res.read())
+    except (IOError, urllib.error.URLError) as err:
+        adbb.log.error("Failed to fetch animetitles.xml: {}".format(err))
+        adbb.log.info("You might be temporary ip-banned from anidb, banns will be automatically lifted after 24 hours!")
+        os.remove(tmp_file)
+        if old_file_exists:
+            xml = _read_anidb_xml(animetitles_file)
+        return
+    
+    if not _verify_animetitles_file(tmp_file):
+        adbb.log.error("Failed to verify xml file: {}".format(tmp_file))
+        return
+
+    if old_file_exists:
+        os.remove(animetitles_file)
+    os.rename(tmp_file, animetitles_file)
+    xml = _read_anidb_xml(animetitles_file)
+
+
+def _verify_animetitles_file(path):
+    if not os.path.isfile(path):
+        return False
+    
+    try:
+        tmp_xml = _read_anidb_xml(path)
+    except Exception as e:
+        adbb.log.error("Exception when reading xml file: {}".format(e))
+        return False
+
+    if len(tmp_xml.findall('anime')) < 8000:
+        return False
+    
+    return True
+        
+
+def _read_anidb_xml(filePath):
+    return _read_xml_into_etree(filePath)
+
+
+def _read_xml_into_etree(filePath):
+        if not filePath:
+            return None
+        
+        with gzip.open(filePath, "rb") as f:
+            data = f.read()
+
+        xmlASetree = etree.fromstring(data)
+        return xmlASetree
+
+
+def _read_language_file():
+    global languages
+    languages = {}
+    with open(iso_639_file, "r") as f:
+        for line in f:
+            three, tree2, two, eng, fre = line.strip().split('|')
+            if two:
+                languages[two] = three
+
+
+def get_lang_code(short):
+    if not languages:
+        _read_language_file()
+
+    if short in languages:
+        return languages[short]
+    return None
+    
+
+def get_titles(name=None, aid=None, max_results=10, score_for_match=0.6):
+    res = []
+
+    if xml is None:
+        update_animetitles()
+    if xml is None:
+        raise AniDBFileError('Could not get valid title cache file.')
+
+    lastAid = None
+    for anime in xml.findall('anime'):
+        score=0
+        best_title_match=None
+        exact_match=None
+        if aid and aid == int(anime.get('aid')):
+            exact_match=anime.get('aid')
+
+        if name:
+            for title in anime.findall('title'):
+                if name.lower() in title.text.lower():
+                    exact_match=title.text
+                diff = difflib.SequenceMatcher(a=name, b=title.text)
+                title_score = diff.ratio()
+                if title_score > score:
+                    score = title_score
+                    best_title_match=title.text
+
+        if score > score_for_match or exact_match:
+            titles = [
+                    adbb.animeobjs.AnimeTitle(
+                        x.get('type'),
+                        get_lang_code(x.get('{http://www.w3.org/XML/1998/namespace}lang')),
+                        x.text) for x in anime.findall('title')]
+            res.append((int(anime.get('aid')), titles, score, best_title_match))
+
+    res.sort(key=lambda x: x[2], reverse=True)
+    
+    # response is a list of tuples in the form:
+    #(<aid>, <list of titles>, <score of best title>, <best title>)
+    return res[:max_results]
+
---
+++ packages/adbb/adbb-2-py2.py3-none-any.whl/adbb/fileinfo.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+#
+# This file is part of adbb.
+#
+# adbb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# adbb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with adbb.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import with_statement
+import datetime
+import difflib
+import functools
+import re
+import hashlib
+import os
+import xml.etree.cElementTree as etree
+
+try:
+    import libnfs
+except ImportError:
+    libnfs = None
+
+import adbb.errors
+
+ep_nr_re = [
+    re.compile(r'[Ss]([0-9]+)[ ._-]*e([0-9]+)()', re.I),  # foo.s01.e01, foo.s01_e01, S01E02 foo, S01 - E02
+    re.compile(r'[\._ -]()ep_?([0-9]+)()', re.I),  # foo.ep01, foo.EP_01
+    re.compile(r'[\\/\._ \[\(-]([0-9]{1,2})x([0-9]+)()', re.I),  # foo.1x09* or just /1x09*
+    re.compile(r'[/\._ \-](s)p(?:ecials?)?[._ \-]{0,3}([0-9]{1,3})([._ 0-9-]*)', re.I),  # specials
+    re.compile(r'[/\._ \-]{2}()([0-9]{1,4})([._ 0-9-]*)', re.I),  # match '- nr' '-_nr' etc.
+    re.compile(r'[/\._ \-](s)[\._ \-]{0,3}([0-9]{1,3})([._ 0-9-]*)', re.I),  # specials
+
+    None,  # the following regex are fallbacks and shouldn't be run if the
+    # anime only has one episode. This None marks the breakpoint
+    re.compile(r'[/\._ \-](s)p?(?:ecials?)?[\._ \-]{1,3}([0-9]{0,3})([._ 0-9-]*)', re.I),
+    # specials that may not have number
+    re.compile(r'[/\._ \-](?:nc)?(o)p?(?:enings?)?[\._ \-]{0,3}([0-9]{0,3})([._ 0-9-]*)', re.I),  # openings
+    re.compile(r'[/\._ \-](?:nc)?(e)d?(?:ndings?)?[\._ \-]{0,3}([0-9]{0,3})([._ 0-9-]*)', re.I),  # endings
+    re.compile(r'[/\._ \-](t|pv)(?:railers?)?[\._ \-]{0,3}([0-9]{0,3})([._ 0-9-]*)', re.I),  # trailers
+    # "others"-type not implemented for now...
+    re.compile(r'[/\._ \-]()([0-9]{1,4})([._ 0-9-]*)', re.I)  # if everything else fails, just match the first number(s)
+]
+multiep_re = re.compile(r'[0-9]+')
+
+
+# http://www.radicand.org/blog/orz/2010/2/21/edonkey2000-hash-in-python/
+def get_file_hash(path, nfs_obj=None):
+    if path.startswith('nfs://'):
+        with NFSFile(path, 'rb', nfs_obj) as f:
+            return _calculate_ed2khash(f)
+    with open(path, 'rb') as f:
+        return _calculate_ed2khash(f)
+
+
+def _calculate_ed2khash(fileObj):
+    """ Returns the ed2k hash of a given file."""
+    md4 = hashlib.new('md4').copy
+
+    def gen(f):
+        while True:
+            x = f.read(9728000)
+            if x:
+                yield x
+            else:
+                return
+
+    def md4_hash(data):
+        m = md4()
+        m.update(data)
+        return m
+
+    a = gen(fileObj)
+    hashes = [md4_hash(data) for data in a]
+    if len(hashes) == 1:
+        return hashes[0].hexdigest()
+    else:
+        # reduce goes from left to right, but will not run digest on the first
+        # entry, so we'll have to do that first.
+        hashes[0] = hashes[0].digest()
+        res = md4_hash(functools.reduce(lambda a, d: a + d.digest(), hashes)).hexdigest()
+        return res
+
+
+def get_file_stats(path, nfs_obj=None):
+    """Return (mtime, size). size is in bytes, mtime is a datetime object."""
+    if path.startswith('nfs://'):
+        return _nfs_stats(path, nfs_obj)
+
+    stat = os.stat(path)
+
+    size = stat.st_size
+    mtime = datetime.datetime.fromtimestamp(stat.st_mtime)
+    return (mtime, size)
+
+
+class NFSFile(object):
+    def __init__(self, path, mode, nfs_obj=None):
+        if not libnfs:
+            raise adbb.errors.AniDBPathError(
+                "libnfs python module not installed, can't use nfs paths")
+        self.mode = mode
+        self.handle = None
+        self.nfs_obj = nfs_obj
+
+        self.path = path
+
+        if self.nfs_obj:
+            if path.startswith(nfs_obj.url):
+                self.rel_path = os.path.join('/', path[len(nfs_obj.url):])
+            else:
+                self.rel_path = self.path
+
+    def open(self):
+        if self.nfs_obj:
+            self.handle = self.nfs_obj.open(self.rel_path, self.mode)
+        else:
+            self.handle = libnfs.open(self.path, self.mode)
+        return self.handle
+
+    def close(self):
+        if self.handle:
+            self.handle.close()
+
+    def __enter__(self):
+        return self.open()
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+
+def _nfs_stats(path, nfs_obj=None):
+    stats = None
+    with NFSFile(path, 'r', nfs_obj) as f:
+        stats = f.fstat()
+
+    mtime = stats['mtime']['sec'] + stats['mtime']['nsec'] / 10 ** 9
+    mtime = datetime.datetime.fromtimestamp(mtime)
+    return (mtime, stats['size'])
