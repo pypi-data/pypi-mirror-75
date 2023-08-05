17bbf7ef17e120ab58161ac247de3904ffdadbec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:18 +0000

    Add package distributions-2020-0.1/0.1/distributions_2020_0.1-0.1.tar.gz

---
+++ packages/distributions-2020-0.1/distributions_2020_0.1-0.1.tar.gz/distributions_2020_0.1-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_2020_0.1',
+      version='0.1',
+      description='Gaussian + Binomial distributions',
+      packages=['distributions'],
+      Author="Altaf Pandhare",
+      zip_safe=False)
+
