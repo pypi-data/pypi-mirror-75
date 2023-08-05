3260f501284b97bf39d67a17302ce5f30c6ba102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:06 +0000

    Add package memeyankm-distributions/0.69/memeyankm_distributions-0.69.tar.gz

---
+++ packages/memeyankm-distributions/memeyankm_distributions-0.69.tar.gz/memeyankm_distributions-0.69/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='memeyankm_distributions',
+      version='0.69',
+      description='Gaussian and Binomial distributions',
+      packages=['memeyankm_distributions'],
+      author="memeyankm",
+      zip_safe=False)
