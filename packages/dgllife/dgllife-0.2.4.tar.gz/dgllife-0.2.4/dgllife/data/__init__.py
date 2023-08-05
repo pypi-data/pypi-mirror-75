1b2220350de7152d7bb32598481314cc74934dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:26 +0000

    Add package dgllife/0.2.4/dgllife-0.2.4.tar.gz

---
+++ packages/dgllife/dgllife-0.2.4.tar.gz/dgllife-0.2.4/dgllife/data/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from .alchemy import *
+from .astrazeneca_chembl_solubility import *
+from .csv_dataset import *
+from .smiles_inference import *
+from .pdbbind import *
+from .pubchem_aromaticity import *
+from .tox21 import *
+from .esol import *
+from .freesolv import *
+from .lipophilicity import *
+from .uspto import *
