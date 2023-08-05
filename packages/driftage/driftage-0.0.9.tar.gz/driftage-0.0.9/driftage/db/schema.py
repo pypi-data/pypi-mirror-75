c5921cbda7705c28b57c270abe4a9447103a0f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:28 +0000

    Add package driftage/0.0.9/driftage-0.0.9.tar.gz

---
+++ packages/driftage/driftage-0.0.9.tar.gz/driftage-0.0.9/driftage/db/schema.py
@@ -0,0 +1,16 @@
+from os import environ
+from sqlalchemy import (
+    Table, Column, String, Boolean, DateTime, PickleType, MetaData)
+
+DRIFTAGE_TABLENAME = environ.get("DRIFTAGE_TABLENAME", "driftage_kb")
+
+table = Table(DRIFTAGE_TABLENAME, MetaData(),
+              Column('driftage_jid', String, primary_key=True),
+              Column(
+                  'driftage_datetime_monitored', DateTime, primary_key=True),
+              Column(
+                  'driftage_datetime_analysed', DateTime, primary_key=True),
+              Column('driftage_identifier', String, primary_key=True),
+              Column('driftage_data', PickleType),
+              Column('driftage_predicted', Boolean)
+              )
