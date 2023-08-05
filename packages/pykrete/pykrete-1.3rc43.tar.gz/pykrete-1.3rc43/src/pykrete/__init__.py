8c07932b00cc306c8176eead1b255e5864ccceb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:42 +0000

    Add package pykrete/1.3rc43/pykrete-1.3rc43.tar.gz

---
+++ packages/pykrete/pykrete-1.3rc43.tar.gz/pykrete-1.3rc43/src/pykrete/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pykrete root
+Author: Shai Bennathan - shai.bennathan@gmail.com
+(C) 2020
+"""
+
+from .version import __version__
+
+__all__ = ['__version__', 'args', 'builder', 'calls', 'ci', 'credentials', 'distrib', 'io',
+           'logging', 'packages', 'patterns', 'repo', 'source', 'tools', 'versioning']
