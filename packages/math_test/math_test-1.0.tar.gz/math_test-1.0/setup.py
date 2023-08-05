73a6d2d1cef46c82387952e77d88cc01e5bbca01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:13 +0000

    Add package math_test/1.0/math_test-1.0.tar.gz

---
+++ packages/math_test/math_test-1.0.tar.gz/math_test-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='math_test',
+    version='1.0',
+    description='新手测试',
+    author='gl',
+    author_email='1312676936@qq.com',
+    py_modules=['math_test.demo1', 'math_test.demo2']
+)
