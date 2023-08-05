19cf78969a2451a4f282b753997e95a675a0bc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:08 +0000

    Add package botha01-probability/0.1/botha01_probability-0.1.tar.gz

---
+++ packages/botha01-probability/botha01_probability-0.1.tar.gz/botha01_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='botha01_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['botha01_probability'],
+      author = 'Botha Moseti',
+      author_email = 'derrickbotha2@gmail.com',
+      zip_safe=False)
