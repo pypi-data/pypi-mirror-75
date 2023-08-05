4f80376388de128bd3972249ff9f1c8bf095cc63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:45 +0000

    Add package pygeoutils/0.1.7/pygeoutils-0.1.7-py2.py3-none-any.whl

---
+++ packages/pygeoutils/pygeoutils-0.1.7-py2.py3-none-any.whl/pygeoutils/__init__.py
@@ -0,0 +1,18 @@
+"""Top-level package for PyGeoUtils."""
+from pkg_resources import DistributionNotFound, get_distribution
+
+from .exceptions import InvalidInputType
+from .pygeoutils import (
+    MatchCRS,
+    arcgis2geojson,
+    check_bbox,
+    gtiff2xarray,
+    json2geodf,
+    xarray_geomask,
+)
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
