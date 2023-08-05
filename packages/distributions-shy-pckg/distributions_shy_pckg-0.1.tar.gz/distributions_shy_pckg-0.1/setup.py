81d039b60b520a5e02c84d967ca0f2e2f7c8756b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:47 +0000

    Add package distributions-shy-pckg/0.1/distributions_shy_pckg-0.1.tar.gz

---
+++ packages/distributions-shy-pckg/distributions_shy_pckg-0.1.tar.gz/distributions_shy_pckg-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_shy_pckg',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_shy_pckg'],
+      author='Shyamali Nandanwar',
+      zip_safe=False)
