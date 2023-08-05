b69bac51b539ad3745dd7e050f6d9a63017d0ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:09 +0000

    Add package xchk-git-content/0.1.0/xchk-git-content-0.1.0.tar.gz

---
+++ packages/xchk-git-content/xchk-git-content-0.1.0.tar.gz/xchk-git-content-0.1.0/xchk_git_content/course.py
@@ -0,0 +1,19 @@
+from xchk import contentviews as basecv
+from xchk.courses import Course
+from .contentviews import *
+
+course = Course('git',
+                'Git',
+                [(GitIgnoreConceptView,[GitStageChangesView]),
+                 (GitStatusView,[GitStageChangesView]),
+                 (GitPushView,[GitStageChangesView,GitRemotesConceptView]),
+                 (GitAddRemoteView,[GitRemotesConceptView]),
+                 (GitStatusAndGitIgnoreView,[GitStatusView,GitIgnoreConceptView]),
+                 (GitPullBasicsView,[GitPushView]),
+                 (GitLogView,[GitStageChangesView]),
+                 (GitStageChangesView,[GitStagesView]),
+                 (GitRemotesConceptView,[GitStagesView]),
+                 (GitStagesView,[GitInitView]),
+                 (GitInitView,[WhatIsGitView]),
+                 (WhatIsGitView,[])],
+                "git@github.com:v-nys/git-modeloplossingen.git")
