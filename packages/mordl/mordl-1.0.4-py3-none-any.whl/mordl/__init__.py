abc20a756156c6d1fcdd2a33a04962dd22eaec06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:00 +0000

    Add package mordl/1.0.4/mordl-1.0.4-py3-none-any.whl

---
+++ packages/mordl/mordl-1.0.4-py3-none-any.whl/mordl/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# MorDL project
+#
+# Copyright (C) 2020-present by Sergei Ternovykh, Anastasiya Nikiforova
+# License: BSD, see LICENSE for details
+"""
+MorDL provides tools for complete morphological sentence parsing and
+named-entity recognition.
+"""
+from mordl._version import __version__
+from mordl.word_embeddings import WordEmbeddings
+from mordl.upos_tagger import UposTagger
+from mordl.feat_tagger import FeatTagger
+from mordl.feats_tagger import FeatsJointTagger as FeatsTagger
+from mordl.lemma_tagger import LemmaTagger
+from mordl.ne_tagger import NeTagger
+from mordl.base_tagger import conll18_ud_eval
