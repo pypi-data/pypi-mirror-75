12772344d3e6fd1d06791c823e3bafb99779c170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package kannanpackage/0.2/kannanpackage-0.2.tar.gz

---
+++ packages/kannanpackage/kannanpackage-0.2.tar.gz/kannanpackage-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kannanpackage',
+      version='0.2',
+      author = 'Abhijith',
+      author_email = 'akgamer077@gmail.com',
+      description='Gaussian distributions',
+      packages=['kannanpackage'],
+      zip_safe=False)
