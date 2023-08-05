090951c7aae4ee89e0d1cc84db217a78a6fa6f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package foxtron-django-settings/0.1.1/foxtron_django_settings-0.1.1-py2.py3-none-any.whl

---
+++ packages/foxtron-django-settings/foxtron_django_settings-0.1.1-py2.py3-none-any.whl/foxtron_django_settings/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for foxtron-django-settings."""
+
+__author__ = """Samuel Dolt"""
+__email__ = 'samuel.dolt@foxtron.ch'
+__version__ = '0.1.1'
+
+from .foxtron_django_settings import FxSettings, Profile, Required, MissingRequiredEnvVar
