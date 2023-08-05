1863e1b6239842452fba8584bea62c31ba331a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:04 +0000

    Add package orthoseg/0.1.7/orthoseg-0.1.7.tar.gz

---
+++ packages/orthoseg/orthoseg-0.1.7.tar.gz/orthoseg-0.1.7/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="orthoseg", 
+    version="0.1.7",
+    author="Pieter Roggemans",
+    author_email="pieter.roggemans@gmail.com",
+    description="Package to make it easier to segment orthophotos.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/theroggy/orthoseg",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+            "tensorflow>=2.2,<2.3", "pillow", "rasterio", "geopandas>=0.8,<0.9", 
+            "owslib", "segmentation-models>=1.0,<1.1", "geofileops==0.0.7"],
+    entry_points='''
+            [console_scripts]
+            orthoseg=orthoseg.orthoseg:main
+            ''',
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
