8093cb005bade88742d8f3064acf110875114c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package distributions-sb/0.1/distributions_sb-0.1.tar.gz

---
+++ packages/distributions-sb/distributions_sb-0.1.tar.gz/distributions_sb-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_sb',
+      version='0.1',
+      author='Shubham Barthwal',
+      author_email='shubhambarthwal94@gmail.com',
+      description='Gaussian distributions',
+      packages=['distributions_sb'],
+      zip_safe=False)
