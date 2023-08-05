2e07f9a0fac9b1243f94974d16df728588f23979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:31 +0000

    Add package sololearnlib/1.0.2/sololearnlib-1.0.2.tar.gz

---
+++ packages/sololearnlib/sololearnlib-1.0.2.tar.gz/sololearnlib-1.0.2/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name="sololearnlib",
+    pakages=["sololearnlib"],
+    version="1.0.2",
+    licence="MIT",
+    description="Retrieve public data from 'sololearn.com'.",
+    author="OR!ON",
+    author_email='orionconner00@gmail.com', 
+    url='https://github.com/Or-i0n/sololearnlib',
+    download_url='https://github.com/Or-i0n/sololearnlib/archive/v1.0.2-alpha.tar.gz',
+    keywords=["sololearn", 'web scrape', 'api', "public data"],
+    install_requires=[           
+          'bs4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+
+  ],
+)
