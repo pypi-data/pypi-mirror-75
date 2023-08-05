9bcf6f90015c8b42164e33f49ffa3ca4d84576a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:37 +0000

    Add package scratch-dsnd-probalility/1.0/scratch_dsnd_probalility-1.0.tar.gz

---
+++ packages/scratch-dsnd-probalility/scratch_dsnd_probalility-1.0.tar.gz/scratch_dsnd_probalility-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='scratch_dsnd_probalility',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['scratch_dsnd_probalility'],
+      author='Ayité Mawussé HILLAH',
+      author_email='mawusseayite@outlook.fr',
+      zip_safe=False)
