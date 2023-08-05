1f77c435ed5d53bba3b4ec0df93595e508741bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:52 +0000

    Add package rtc-cafe-signaller/0.0.2/rtc-cafe-signaller-0.0.2.tar.gz

---
+++ packages/rtc-cafe-signaller/rtc-cafe-signaller-0.0.2.tar.gz/rtc-cafe-signaller-0.0.2/README.md
@@ -0,0 +1,2 @@
+# rtc-cafe-signaller
+
---
+++ packages/rtc-cafe-signaller/rtc-cafe-signaller-0.0.2.tar.gz/rtc-cafe-signaller-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='rtc-cafe-signaller',
+    version='v0.0.2',
+    url='https://gitlab.com/rtc-cafe/rtc-cafe-signaller',
+    description='rtc-cafe signaller',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords='rtc-cafe flask-socketio socketio webrtc',
+    install_requires=[
+        'flask',
+        'flask-socketio',
+    ],
+    packages=[
+        'rtc_cafe_signaller',
+    ],
+)
