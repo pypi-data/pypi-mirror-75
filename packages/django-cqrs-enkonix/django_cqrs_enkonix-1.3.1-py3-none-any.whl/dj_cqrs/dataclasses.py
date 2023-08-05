5e07c490c6c3bd2003088cfa83525b0bc4f15e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:49 +0000

    Add package django-cqrs-enkonix/1.3.1/django_cqrs_enkonix-1.3.1-py3-none-any.whl

---
+++ packages/django-cqrs-enkonix/django_cqrs_enkonix-1.3.1-py3-none-any.whl/dj_cqrs/dataclasses.py
@@ -0,0 +1,38 @@
+#  Copyright © 2020 Ingram Micro Inc. All rights reserved.
+
+
+class TransportPayload:
+    def __init__(self, signal_type, cqrs_id, instance_data, instance_pk, queue=None):
+        self.__signal_type = signal_type
+        self.__cqrs_id = cqrs_id
+        self.__instance_data = instance_data
+        self.__instance_pk = instance_pk
+        self.__queue = queue
+
+    @property
+    def signal_type(self):
+        return self.__signal_type
+
+    @property
+    def cqrs_id(self):
+        return self.__cqrs_id
+
+    @property
+    def instance_data(self):
+        return self.__instance_data
+
+    @property
+    def pk(self):
+        return self.__instance_pk
+
+    @property
+    def queue(self):
+        return self.__queue
+
+    def to_dict(self):
+        return {
+            'signal_type': self.__signal_type,
+            'cqrs_id': self.__cqrs_id,
+            'instance_data': self.__instance_data,
+            'instance_pk': self.__instance_pk,
+        }
---
+++ packages/django-cqrs-enkonix/django_cqrs_enkonix-1.3.1-py3-none-any.whl/tests/utils.py
@@ -0,0 +1,25 @@
+#  Copyright © 2020 Ingram Micro Inc. All rights reserved.
+
+import six
+from django.db import DatabaseError
+
+
+def assert_is_sub_dict(dict1, dict2):
+    assert six.viewitems(dict1) <= six.viewitems(dict2)
+
+
+def assert_publisher_once_called_with_args(publisher_mock, *args):
+    assert publisher_mock.call_count == 1
+    call_t_payload = publisher_mock.call_args[0][0]
+
+    assert call_t_payload.signal_type == args[0]
+    assert call_t_payload.cqrs_id == args[1]
+    assert call_t_payload.pk == args[3]
+    assert_is_sub_dict(args[2], call_t_payload.instance_data)
+
+    required_fields = {'cqrs_revision', 'cqrs_updated'}
+    assert not (required_fields - set(call_t_payload.instance_data.keys()))
+
+
+def db_error(*args, **kwargs):
+    raise DatabaseError()
