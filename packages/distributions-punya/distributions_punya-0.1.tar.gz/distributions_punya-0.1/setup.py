d88fb94ad7d07afd1b07221247e75bc4342655e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:54 +0000

    Add package distributions-punya/0.1/distributions_punya-0.1.tar.gz

---
+++ packages/distributions-punya/distributions_punya-0.1.tar.gz/distributions_punya-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_punya',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_punya'],
+      zip_safe=False)
