a3c3321ae21084228f0dcbdc434cf712804b8943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:41 +0000

    Add package hyperglass/1.0.0b56/hyperglass-1.0.0b56.tar.gz

---
+++ packages/hyperglass/hyperglass-1.0.0b56.tar.gz/hyperglass-1.0.0b56/hyperglass/ui/pages/index.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import Meta from "~/components/Meta";
+import Loading from "~/components/Loading";
+const LookingGlass = dynamic(() => import("~/components/LookingGlass"), {
+  loading: Loading
+});
+
+const Index = ({ faviconComponents }) => {
+  return (
+    <>
+      <Head>
+        {faviconComponents.map(({ rel, href, type }, i) => (
+          <link rel={rel} href={href} type={type} key={i} />
+        ))}
+      </Head>
+      <Meta />
+      <LookingGlass />
+    </>
+  );
+};
+
+export async function getStaticProps(context) {
+  const components = process.env._HYPERGLASS_FAVICONS_.map(icon => {
+    const { image_format, dimensions, prefix, rel } = icon;
+    const src = `/images/favicons/${prefix}-${dimensions[0]}x${dimensions[1]}.${image_format}`;
+    return { rel, href: src, type: `image/${image_format}` };
+  });
+  return {
+    props: {
+      faviconComponents: components
+    }
+  };
+}
+
+export default Index;
