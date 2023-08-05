e8d0bd6ab5e74ad1c0ebd8397f9613cfa3ce270a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:05 +0000

    Add package Reverso-API/0.0.1b3/Reverso_API-0.0.1b3.tar.gz

---
+++ packages/Reverso-API/Reverso_API-0.0.1b3.tar.gz/Reverso_API-0.0.1b3/reverso_api/__init__.py
@@ -0,0 +1,12 @@
+"""Reverso.net API for Python"""
+
+from . import context, voice
+
+
+__author__ = "Demian Wolf"
+__copyright__ = "Copyright 2020, Demian Wolf"
+__credits__ = ["Demian Wolf"]
+__license__ = "MIT"
+__version__ = "0.0.1.beta.3"
+__maintainer__ = "Demian Wolf"
+__email__ = "demianwolfssd@gmail.com"
---
+++ packages/Reverso-API/Reverso_API-0.0.1b3.tar.gz/Reverso_API-0.0.1b3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Reverso_API",
+    version="0.0.1.beta.3",
+    license="MIT",
+    author="Demian Wolf",
+    author_email="demianwolfssd@gmail.com",
+    description="Reverso.net API for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/demian-wolf/ReversoAPI",
+    download_url="https://github.com/demian-wolf/ReversoAPI/archive/v0.0.1.beta.3.tar.gz",
+    keywords=["REVERSO", "REVERSO-CONTEXT", "REVERSO CONTEXT", "CONTEXT", "REVERSO-VOICE", "REVERSO VOICE", "VOICE",
+              "REVERSO-API", "API", "WRAPPER", "PYTHON"],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "lxml",
+    ],
+    extras_require={
+        "playing spoken text instead of just getting its MP3 data and/or saving it to file-like objects": ["pygame"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+    ]
+)
