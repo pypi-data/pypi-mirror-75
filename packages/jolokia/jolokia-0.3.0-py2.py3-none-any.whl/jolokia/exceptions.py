cb7764a938514562b4646959a1793855ff6b71b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package jolokia/0.3.0/jolokia-0.3.0-py2.py3-none-any.whl

---
+++ packages/jolokia/jolokia-0.3.0-py2.py3-none-any.whl/jolokia/exceptions.py
@@ -0,0 +1,21 @@
+"""Custom exceptions for for various modules in Jolokia Python client"""
+
+
+class UrlNotSpecifiedException(Exception):
+    """Indicates that Jolokia agent URL has not been provided"""
+    pass
+
+
+class MalformedUrlException(Exception):
+    """Indicates that the Jolokia agent URL is malformed"""
+    pass
+
+
+class IllegalArgumentException(Exception):
+    """Generic exception for enforcing required arguments"""
+    pass
+
+
+class MissingEnvironmentVariableException(Exception):
+    """Should be thrown when expected environment variable is not found"""
+    pass
