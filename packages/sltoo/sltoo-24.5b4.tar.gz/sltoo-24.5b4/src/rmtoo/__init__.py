c4e48773d1c395651fb466b98299c4667b3c46f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:20 +0000

    Add package sltoo/24.5b4/sltoo-24.5b4.tar.gz

---
+++ packages/sltoo/sltoo-24.5b4.tar.gz/sltoo-24.5b4/src/rmtoo/__init__.py
@@ -0,0 +1,12 @@
+'''
+ rmtoo
+   Free and Open Source Requirements Management Tool
+
+  Master __init__.py
+
+ (c) 2011,2017 by flonatel GmbH & Co. KG
+
+ For licensing details see COPYING
+'''
+__all__ = ["inputs", "lib", "tests", "outputs"]
+__version__ = "24.5b4"
