db4d8941279eb6e6a072f262b849bd93873d854c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:52 +0000

    Add package Saydillo/0.1/Saydillo-0.1.tar.gz

---
+++ packages/Saydillo/Saydillo-0.1.tar.gz/Saydillo-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(name='Saydillo',
+      version='0.1',
+      description='Test',
+      long_description='Really, the funniest around.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords=')))',
+      url='https://github.com/sallory',
+      author='Saydillo',
+      author_email='dilloty@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
