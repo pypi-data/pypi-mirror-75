72c8e5872a24480958388869af5f3388ca0392fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:59 +0000

    Add package pubpack/0.3/pubpack-0.3.tar.gz

---
+++ packages/pubpack/pubpack-0.3.tar.gz/pubpack-0.3/pubpack.py
@@ -0,0 +1,11 @@
+"""An amazing sample package!"""
+
+__version__ = '0.3'
+
+class Pubpack:
+
+    print ("initializing Pubpack...")
+
+    def runfun1():
+        """ Run function 1 """
+        print("u r in runfun1")
