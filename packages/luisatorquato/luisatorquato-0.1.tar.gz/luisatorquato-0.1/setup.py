a759a8cf2771dd6468011b34a1746bb14d6c47e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package luisatorquato/0.1/luisatorquato-0.1.tar.gz

---
+++ packages/luisatorquato/luisatorquato-0.1.tar.gz/luisatorquato-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='luisatorquato',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['luisatorquato'],
+      zip_safe=False)
