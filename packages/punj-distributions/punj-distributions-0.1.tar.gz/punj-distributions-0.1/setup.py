dee4d20bbdf8b43263feeb28081bc0ca09e27c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:56 +0000

    Add package punj-distributions/0.1/punj-distributions-0.1.tar.gz

---
+++ packages/punj-distributions/punj-distributions-0.1.tar.gz/punj-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='punj-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['punj-distributions'],
+      author = 'Akbar Punjwani',
+      author_email = 'mrpunjwani-learner@yahoo.com',
+      zip_safe=False)
