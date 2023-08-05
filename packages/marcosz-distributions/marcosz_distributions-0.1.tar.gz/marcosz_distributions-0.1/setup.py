bc76b63e8c7cc2b67826befa8b1adf07d3b528a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package marcosz-distributions/0.1/marcosz_distributions-0.1.tar.gz

---
+++ packages/marcosz-distributions/marcosz_distributions-0.1.tar.gz/marcosz_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='marcosz_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['marcosz_distributions'],
+      zip_safe=False)
