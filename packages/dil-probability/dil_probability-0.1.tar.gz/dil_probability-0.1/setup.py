96679eada1102a5189276c1092a0877d811c7487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:22 +0000

    Add package dil-probability/0.1/dil_probability-0.1.tar.gz

---
+++ packages/dil-probability/dil_probability-0.1.tar.gz/dil_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dil_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dil_probability'],
+      author='Dil',
+      zip_safe=False)
