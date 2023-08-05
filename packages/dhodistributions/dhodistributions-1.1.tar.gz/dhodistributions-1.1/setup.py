7e4769c58f9d34e8e41c04511e530dbc9388bf89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package dhodistributions/1.1/dhodistributions-1.1.tar.gz

---
+++ packages/dhodistributions/dhodistributions-1.1.tar.gz/dhodistributions-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dhodistributions',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['dhodistributions'],
+      author='Dan Hernandez',
+      zip_safe=False)
