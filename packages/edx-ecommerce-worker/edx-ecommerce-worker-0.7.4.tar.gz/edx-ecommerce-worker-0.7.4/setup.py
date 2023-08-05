adf8387421c962d0080338943dd277cf26f2273f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package edx-ecommerce-worker/0.7.4/edx-ecommerce-worker-0.7.4.tar.gz

---
+++ packages/edx-ecommerce-worker/edx-ecommerce-worker-0.7.4.tar.gz/edx-ecommerce-worker-0.7.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name='edx-ecommerce-worker',
+    version='0.7.4',
+    description='Celery tasks supporting the operations of edX\'s ecommerce service',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+    ],
+    keywords='edx ecommerce worker',
+    url='https://github.com/edx/ecommerce-worker',
+    author='edX',
+    author_email='oscm@edx.org',
+    license='AGPL',
+    packages=find_packages(exclude=['*.tests']),
+    install_requires=[
+        'celery>=3.1.18,<4.0.0',
+        'edx-rest-api-client>=1.5.0,<2.0.0'
+    ],
+)
