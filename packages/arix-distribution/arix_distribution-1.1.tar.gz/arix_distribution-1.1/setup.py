396e088ec3d4b3e01e3718f04b3207b015cb8906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:06 +0000

    Add package arix-distribution/1.1/arix_distribution-1.1.tar.gz

---
+++ packages/arix-distribution/arix_distribution-1.1.tar.gz/arix_distribution-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='arix_distribution',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['arix_distribution'],
+      author = 'Sarthak Pol',
+      author_email = 'sarthakpol711@gmail.com',
+      zip_safe=False)
