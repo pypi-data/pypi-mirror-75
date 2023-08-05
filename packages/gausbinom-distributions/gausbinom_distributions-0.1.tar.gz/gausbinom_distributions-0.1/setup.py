b5041d42622c3b9e82477eb7b57d154df5372100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:52 +0000

    Add package gausbinom-distributions/0.1/gausbinom_distributions-0.1.tar.gz

---
+++ packages/gausbinom-distributions/gausbinom_distributions-0.1.tar.gz/gausbinom_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gausbinom_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gausbinom_distributions'],
+      author = 'Onur Uludag',
+      author_email = 'ozu0001@auburn.edu',
+      zip_safe=False)
