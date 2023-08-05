4fb59084ab4789b5dc20b048e61148bec630886a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:30 +0000

    Add package special-distributions/0.1/special_distributions-0.1.tar.gz

---
+++ packages/special-distributions/special_distributions-0.1.tar.gz/special_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='special_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      author = 'Mohamed Osama',
+      packages=['special_distributions'],
+      zip_safe=False)
