056a69195497c4cb9b28a29675a35b02e8e11e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:39 +0000

    Add package usefullibs/0.0.1/usefullibs-0.0.1.tar.gz

---
+++ packages/usefullibs/usefullibs-0.0.1.tar.gz/usefullibs-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="usefullibs", # Replace with your own username
+    version="0.0.1",
+    author="Allen Sun",
+    author_email="allen.haha@hotmail.com",
+    description="a useful package.",
+    long_description='',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
