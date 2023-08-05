e31272223402f89235f9fa3d3debf70b7ddcc72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:12 +0000

    Add package uddin-probability/0.1/uddin_probability-0.1.tar.gz

---
+++ packages/uddin-probability/uddin_probability-0.1.tar.gz/uddin_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='uddin_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['uddin_probability'],
+      author = 'Md Salah Uddin',
+      author_email = 'sajitsalahuddin@live.com',
+      zip_safe=False)
