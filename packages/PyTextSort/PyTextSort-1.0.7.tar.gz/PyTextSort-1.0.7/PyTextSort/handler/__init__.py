b6eebdef7fa7a94f6e1b730f093e890a89089d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:44 +0000

    Add package PyTextSort/1.0.7/PyTextSort-1.0.7.tar.gz

---
+++ packages/PyTextSort/PyTextSort-1.0.7.tar.gz/PyTextSort-1.0.7/PyTextSort/handler/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    hack by djj -_,- | good luck!
+    date: 2018-03-02
+"""
+from ._textFileHandler import TextFileHandler
+from ._jsonFileHandler import JsonFileHandler
