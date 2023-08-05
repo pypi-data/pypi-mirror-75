04755c00182c32b0ff9a1ac70b3d02f12fdbda32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:31 +0000

    Add package panamap/1.2.1/panamap-1.2.1.tar.gz

---
+++ packages/panamap/panamap-1.2.1.tar.gz/panamap-1.2.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+project_directory = path.abspath(path.dirname(__file__))
+
+
+def load_from(file_name):
+    with open(path.join(project_directory, file_name), encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="panamap",
+    version=load_from("panamap/panamap.version").strip(),
+    description="Python object mapper",
+    long_description=load_from("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kirillsulim/panamap",
+    author="Kirill Sulim",
+    author_email="kirillsulim@gmail.com",
+    license="MIT",
+    packages=find_packages(include=["panamap",]),
+    package_data={"panamap": ["panamap.version",]},
+    test_suite="tests",
+    install_requires=["typing_inspect",],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords="object mapper",
+)
