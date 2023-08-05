2c292c7d7fc2dd8006acf48755061f345bf16de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:52 +0000

    Add package bianzhanMath2/1.0/bianzhanMath2-1.0.tar.gz

---
+++ packages/bianzhanMath2/bianzhanMath2-1.0.tar.gz/bianzhanMath2-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name="bianzhanMath2",  # 对外我们模块的名字
+    version = '1.0',        # 版本号
+    description= "这是第一个对外发布的模块",   # 描述
+    author= "gaoqi",                       # 作者
+    author_email='zhouyuhe@126.com',
+    py_modules=['bianzhanMath2.demo1','bianzhanMath2.demo2']  
+)
