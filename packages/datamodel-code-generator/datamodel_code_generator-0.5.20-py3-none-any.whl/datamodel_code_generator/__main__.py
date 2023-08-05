47fd61ae13704d660d163ce41842f934ac58b2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:31 +0000

    Add package datamodel-code-generator/0.5.20/datamodel_code_generator-0.5.20-py3-none-any.whl

---
+++ packages/datamodel-code-generator/datamodel_code_generator-0.5.20-py3-none-any.whl/datamodel_code_generator/__main__.py
@@ -0,0 +1,166 @@
+#! /usr/bin/env python
+
+"""
+Main function.
+"""
+
+import json
+import signal
+import sys
+from argparse import ArgumentParser, FileType, Namespace
+from collections import defaultdict
+from enum import IntEnum
+from pathlib import Path
+from typing import Any, DefaultDict, Dict, Optional, Sequence
+
+import argcomplete
+
+from datamodel_code_generator import (
+    DEFAULT_BASE_CLASS,
+    Error,
+    InputFileType,
+    enable_debug_message,
+    generate,
+)
+
+from .format import PythonVersion
+
+
+class Exit(IntEnum):
+    """Exit reasons."""
+
+    OK = 0
+    ERROR = 1
+    KeyboardInterrupt = 2
+
+
+def sig_int_handler(_: int, __: Any) -> None:  # pragma: no cover
+    exit(Exit.OK)
+
+
+signal.signal(signal.SIGINT, sig_int_handler)
+
+
+arg_parser = ArgumentParser()
+arg_parser.add_argument(
+    '--input',
+    help='Input file (default: stdin)',
+    type=FileType('rt'),
+    default=sys.stdin,
+)
+arg_parser.add_argument(
+    '--input-file-type',
+    help='Input file type (default: auto)',
+    choices=[i.value for i in InputFileType],
+    default='auto',
+)
+arg_parser.add_argument('--output', help='Output file (default: stdout)')
+arg_parser.add_argument(
+    '--base-class',
+    help='Base Class (default: pydantic.BaseModel)',
+    type=str,
+    default=DEFAULT_BASE_CLASS,
+)
+arg_parser.add_argument(
+    '--field-constraints',
+    help='Use field constraints and not con* annotations',
+    action='store_true',
+)
+arg_parser.add_argument(
+    '--custom-template-dir', help='Custom template directory', type=str
+)
+arg_parser.add_argument(
+    '--extra-template-data', help='Extra template data', type=FileType('rt')
+)
+arg_parser.add_argument('--aliases', help='Alias mapping file', type=FileType('rt'))
+arg_parser.add_argument(
+    '--target-python-version',
+    help='target python version (default: 3.7)',
+    choices=['3.6', '3.7'],
+    default='3.7',
+)
+arg_parser.add_argument(
+    '--validation', help='Enable validation (Only OpenAPI)', action='store_true'
+)
+arg_parser.add_argument('--debug', help='show debug message', action='store_true')
+arg_parser.add_argument('--version', help='show version', action='store_true')
+
+
+def main(args: Optional[Sequence[str]] = None) -> Exit:
+    """Main function."""
+
+    # add cli completion support
+    argcomplete.autocomplete(arg_parser)
+
+    if args is None:
+        args = sys.argv[1:]
+
+    namespace: Namespace = arg_parser.parse_args(args)
+
+    if namespace.version:  # pragma: no cover
+        from datamodel_code_generator.version import version
+
+        print(version)
+        exit(0)
+
+    if namespace.debug:  # pragma: no cover
+        enable_debug_message()
+
+    extra_template_data: Optional[DefaultDict[str, Dict[str, Any]]]
+    if namespace.extra_template_data is not None:
+        with namespace.extra_template_data as data:
+            try:
+                extra_template_data = json.load(
+                    data, object_hook=lambda d: defaultdict(dict, **d)
+                )
+            except json.JSONDecodeError as e:
+                print(f"Unable to load extra template data: {e}", file=sys.stderr)
+                return Exit.ERROR
+    else:
+        extra_template_data = None
+
+    if namespace.aliases is not None:
+        with namespace.aliases as data:
+            try:
+                aliases = json.load(data)
+            except json.JSONDecodeError as e:
+                print(f"Unable to load alias mapping: {e}", file=sys.stderr)
+                return Exit.ERROR
+        if not isinstance(aliases, Dict) or not all(
+            isinstance(k, str) and isinstance(v, str) for k, v in aliases.items()
+        ):
+            print(
+                'Alias mapping must be a JSON string mapping (e.g. {"from": "to", ...})',
+                file=sys.stderr,
+            )
+            return Exit.ERROR
+    else:
+        aliases = None
+
+    try:
+        generate(
+            input_name=namespace.input.name,
+            input_text=namespace.input.read(),
+            input_file_type=InputFileType(namespace.input_file_type),
+            output=Path(namespace.output) if namespace.output is not None else None,
+            target_python_version=PythonVersion(namespace.target_python_version),
+            base_class=namespace.base_class,
+            custom_template_dir=namespace.custom_template_dir,
+            extra_template_data=extra_template_data,
+            validation=namespace.validation,
+            field_constraints=namespace.field_constraints,
+            aliases=aliases,
+        )
+        return Exit.OK
+    except Error as e:
+        print(str(e), file=sys.stderr)
+        return Exit.ERROR
+    except Exception:
+        import traceback
+
+        print(traceback.format_exc(), file=sys.stderr)
+        return Exit.ERROR
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/datamodel-code-generator/datamodel_code_generator-0.5.20-py3-none-any.whl/tests/parser/test_openapi.py
@@ -0,0 +1,549 @@
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import List, Optional
+
+import pytest
+
+from datamodel_code_generator import DataModelField, PythonVersion
+from datamodel_code_generator.imports import Import
+from datamodel_code_generator.model.base import TemplateBase
+from datamodel_code_generator.model.pydantic import BaseModel, CustomRootType
+from datamodel_code_generator.parser.base import DataType, dump_templates
+from datamodel_code_generator.parser.jsonschema import JsonSchemaObject
+from datamodel_code_generator.parser.openapi import OpenAPIParser
+
+DATA_PATH: Path = Path(__file__).parents[1] / 'data' / 'openapi'
+
+EXPECTED_OPEN_API_PATH = (
+    Path(__file__).parents[1] / 'data' / 'expected' / 'parser' / 'openapi'
+)
+
+
+def get_expected_file(
+    test_name: str,
+    with_import: bool,
+    format_: bool,
+    base_class: Optional[str] = None,
+    prefix: Optional[str] = None,
+) -> Path:
+    params: List[str] = []
+    if with_import:
+        params.append('with_import')
+    if format_:
+        params.append('format')
+    if base_class:
+        params.append(base_class)
+    file_name = '_'.join(params or 'output')
+
+    return EXPECTED_OPEN_API_PATH / test_name / (prefix or '') / f'{file_name}.py'
+
+
+class A(TemplateBase):
+    def __init__(self, filename: str, data: str):
+        self._data = data
+        super().__init__(Path(filename))
+
+    def render(self) -> str:
+        return self._data
+
+
+@pytest.mark.parametrize(
+    'schema_type,schema_format,result_type,from_,import_',
+    [
+        ('integer', 'int32', 'int', None, None),
+        ('integer', 'int64', 'int', None, None),
+        ('number', 'float', 'float', None, None),
+        ('number', 'double', 'float', None, None),
+        ('number', 'time', 'time', None, None),
+        ('string', None, 'str', None, None),
+        ('string', 'byte', 'str', None, None),
+        ('string', 'binary', 'bytes', None, None),
+        ('boolean', None, 'bool', None, None),
+        ('string', 'date', 'date', 'datetime', 'date'),
+        ('string', 'date-time', 'datetime', 'datetime', 'datetime'),
+        ('string', 'password', 'SecretStr', 'pydantic', 'SecretStr'),
+        ('string', 'email', 'EmailStr', 'pydantic', 'EmailStr'),
+        ('string', 'uri', 'AnyUrl', 'pydantic', 'AnyUrl'),
+        ('string', 'uuid', 'UUID', 'uuid', 'UUID'),
+        ('string', 'uuid1', 'UUID1', 'pydantic', 'UUID1'),
+        ('string', 'uuid2', 'UUID2', 'pydantic', 'UUID2'),
+        ('string', 'uuid3', 'UUID3', 'pydantic', 'UUID3'),
+        ('string', 'uuid4', 'UUID4', 'pydantic', 'UUID4'),
+        ('string', 'uuid5', 'UUID5', 'pydantic', 'UUID5'),
+        ('string', 'ipv4', 'IPv4Address', 'pydantic', 'IPv4Address'),
+        ('string', 'ipv6', 'IPv6Address', 'pydantic', 'IPv6Address'),
+    ],
+)
+def test_get_data_type(schema_type, schema_format, result_type, from_, import_):
+    if from_ and import_:
+        imports_: Optional[List[Import]] = [Import(from_=from_, import_=import_)]
+    else:
+        imports_ = None
+
+    parser = OpenAPIParser(BaseModel, CustomRootType)
+    assert parser.get_data_type(
+        JsonSchemaObject(type=schema_type, format=schema_format)
+    ) == [DataType(type=result_type, imports_=imports_)]
+
+
+@pytest.mark.parametrize(
+    'schema_types,result_types',
+    [(['integer', 'number'], ['int', 'float']), (['integer', 'null'], ['int']),],
+)
+def test_get_data_type_array(schema_types, result_types):
+    parser = OpenAPIParser(BaseModel, CustomRootType)
+    assert parser.get_data_type(JsonSchemaObject(type=schema_types)) == [
+        DataType(type=r) for r in result_types
+    ]
+
+
+def test_get_data_type_invalid_obj():
+    with pytest.raises(ValueError, match='invalid schema object'):
+        parser = OpenAPIParser(BaseModel, CustomRootType)
+        assert parser.get_data_type(JsonSchemaObject())
+
+
+def test_dump_templates():
+    with NamedTemporaryFile('w') as dummy_template:
+        assert dump_templates(A(dummy_template.name, 'abc')) == 'abc'
+        assert (
+            dump_templates(
+                [A(dummy_template.name, 'abc'), A(dummy_template.name, 'def')]
+            )
+            == 'abc\n\n\ndef'
+        )
+
+
+@pytest.mark.parametrize(
+    'source_obj,generated_classes',
+    [
+        (
+            {'properties': {'name': {'type': 'string'}}},
+            '''class Pets(BaseModel):
+    name: Optional[str] = None''',
+        ),
+        (
+            {
+                'properties': {
+                    'kind': {
+                        'type': 'object',
+                        'properties': {'name': {'type': 'string'}},
+                    }
+                }
+            },
+            '''class Kind(BaseModel):
+    name: Optional[str] = None
+
+
+class Pets(BaseModel):
+    kind: Optional[Kind] = None''',
+        ),
+        (
+            {
+                'properties': {
+                    'Kind': {
+                        'type': 'object',
+                        'properties': {'name': {'type': 'string'}},
+                    }
+                }
+            },
+            '''class Kind(BaseModel):
+    name: Optional[str] = None
+
+
+class Pets(BaseModel):
+    Kind: Optional[Kind] = None''',
+        ),
+        (
+            {
+                'properties': {
+                    'pet_kind': {
+                        'type': 'object',
+                        'properties': {'name': {'type': 'string'}},
+                    }
+                }
+            },
+            '''class PetKind(BaseModel):
+    name: Optional[str] = None
+
+
+class Pets(BaseModel):
+    pet_kind: Optional[PetKind] = None''',
+        ),
+        (
+            {
+                'properties': {
+                    'kind': {
+                        'type': 'array',
+                        'items': [
+                            {
+                                'type': 'object',
+                                'properties': {'name': {'type': 'string'}},
+                            }
+                        ],
+                    }
+                }
+            },
+            '''class KindItem(BaseModel):
+    name: Optional[str] = None
+
+
+class Pets(BaseModel):
+    kind: Optional[List[KindItem]] = None''',
+        ),
+        (
+            {'properties': {'kind': {'type': 'array', 'items': []}}},
+            '''class Pets(BaseModel):
+    kind: Optional[List] = None''',
+        ),
+    ],
+)
+def test_parse_object(source_obj, generated_classes):
+    parser = OpenAPIParser(BaseModel, CustomRootType)
+    parser.parse_object('Pets', JsonSchemaObject.parse_obj(source_obj), [])
+    assert dump_templates(list(parser.results)) == generated_classes
+
+
+@pytest.mark.parametrize(
+    'source_obj,generated_classes',
+    [
+        (
+            {
+                'type': 'array',
+                'items': {'type': 'object', 'properties': {'name': {'type': 'string'}}},
+            },
+            '''class Pet(BaseModel):
+    name: Optional[str] = None
+
+
+class Pets(BaseModel):
+    __root__: List[Pet]''',
+        ),
+        (
+            {
+                'type': 'array',
+                'items': [
+                    {'type': 'object', 'properties': {'name': {'type': 'string'}}}
+                ],
+            },
+            '''class Pet(BaseModel):
+    name: Optional[str] = None
+
+
+class Pets(BaseModel):
+    __root__: List[Pet]''',
+        ),
+        (
+            {'type': 'array', 'items': {},},
+            '''class Pets(BaseModel):
+    __root__: List''',
+        ),
+    ],
+)
+def test_parse_array(source_obj, generated_classes):
+    parser = OpenAPIParser(BaseModel, CustomRootType)
+    parser.parse_array('Pets', JsonSchemaObject.parse_obj(source_obj), [])
+    assert dump_templates(list(parser.results)) == generated_classes
+
+
+@pytest.mark.parametrize(
+    'source_obj,generated_classes',
+    [
+        (
+            {'type': 'string', 'nullable': True},
+            '''class Name(BaseModel):
+    __root__: Optional[str] = None''',
+        ),
+        (
+            {'type': 'string', 'nullable': False},
+            '''class Name(BaseModel):
+    __root__: str''',
+        ),
+    ],
+)
+def test_parse_root_type(source_obj, generated_classes):
+    parser = OpenAPIParser(BaseModel, CustomRootType)
+    parsed_templates = parser.parse_root_type(
+        'Name', JsonSchemaObject.parse_obj(source_obj)
+    )
+    assert dump_templates(list(parsed_templates)) == generated_classes
+
+
+@pytest.mark.parametrize(
+    'with_import, format_, base_class',
+    [
+        (True, True, None,),
+        (False, True, None,),
+        (True, False, None,),
+        (True, True, 'custom_module.Base'),
+    ],
+)
+def test_openapi_parser_parse(with_import, format_, base_class):
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        text=Path(DATA_PATH / 'api.yaml').read_text(),
+        base_class=base_class,
+    )
+    expected_file = get_expected_file(
+        'openapi_parser_parse', with_import, format_, base_class
+    )
+    assert (
+        parser.parse(with_import=with_import, format_=format_)
+        == expected_file.read_text()
+    )
+
+
+@pytest.mark.parametrize(
+    'source_obj,generated_classes',
+    [
+        (
+            {'type': 'string', 'nullable': True},
+            '''class Name(BaseModel):
+    __root__: Optional[str] = None''',
+        ),
+        (
+            {'type': 'string', 'nullable': False},
+            '''class Name(BaseModel):
+    __root__: str''',
+        ),
+    ],
+)
+def test_parse_root_type(source_obj, generated_classes):
+    parser = OpenAPIParser(BaseModel, CustomRootType)
+    parser.parse_root_type('Name', JsonSchemaObject.parse_obj(source_obj), [])
+    assert dump_templates(list(parser.results)) == generated_classes
+
+
+def test_openapi_parser_parse_duplicate_models():
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        text=Path(DATA_PATH / 'duplicate_models.yaml').read_text(),
+    )
+    assert (
+        parser.parse()
+        == (
+            EXPECTED_OPEN_API_PATH
+            / 'openapi_parser_parse_duplicate_models'
+            / 'output.py'
+        ).read_text()
+    )
+
+
+def test_openapi_parser_parse_resolved_models():
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        text=Path(DATA_PATH / 'resolved_models.yaml').read_text(),
+    )
+    assert (
+        parser.parse()
+        == (
+            EXPECTED_OPEN_API_PATH
+            / 'openapi_parser_parse_resolved_models'
+            / 'output.py'
+        ).read_text()
+    )
+
+
+def test_openapi_parser_parse_lazy_resolved_models():
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        text=Path(DATA_PATH / 'lazy_resolved_models.yaml').read_text(),
+    )
+    assert (
+        parser.parse()
+        == '''from __future__ import annotations
+
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class Pet(BaseModel):
+    id: int
+    name: str
+    tag: Optional[str] = None
+
+
+class Pets(BaseModel):
+    __root__: List[Pet]
+
+
+class Error(BaseModel):
+    code: int
+    message: str
+
+
+class Event(BaseModel):
+    name: Optional[str] = None
+    event: Optional[Event] = None
+
+
+class Events(BaseModel):
+    __root__: List[Event]
+
+
+class Results(BaseModel):
+    envets: Optional[List[Events]] = None
+    event: Optional[List[Event]] = None
+'''
+    )
+
+
+def test_openapi_parser_parse_enum_models():
+    parser = OpenAPIParser(
+        BaseModel, CustomRootType, text=Path(DATA_PATH / 'enum_models.yaml').read_text()
+    )
+    expected_dir = EXPECTED_OPEN_API_PATH / 'openapi_parser_parse_enum_models'
+    assert parser.parse() == (expected_dir / 'output_py37.py').read_text()
+
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        text=Path(DATA_PATH / 'enum_models.yaml').read_text(),
+        target_python_version=PythonVersion.PY_36,
+    )
+    assert parser.parse() == (expected_dir / 'output_py36.py').read_text()
+
+
+def test_openapi_parser_parse_anyof():
+    parser = OpenAPIParser(
+        BaseModel, CustomRootType, text=Path(DATA_PATH / 'anyof.yaml').read_text()
+    )
+    assert (
+        parser.parse()
+        == (
+            EXPECTED_OPEN_API_PATH / 'openapi_parser_parse_anyof' / 'output.py'
+        ).read_text()
+    )
+
+
+def test_openapi_parser_parse_nested_anyof():
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        data_model_field_type=DataModelField,
+        text=Path(DATA_PATH / 'nested_anyof.yaml').read_text(),
+    )
+    assert (
+        parser.parse()
+        == (
+            EXPECTED_OPEN_API_PATH / 'openapi_parser_parse_nested_anyof' / 'output.py'
+        ).read_text()
+    )
+
+
+def test_openapi_parser_parse_allof():
+    parser = OpenAPIParser(
+        BaseModel, CustomRootType, text=Path(DATA_PATH / 'allof.yaml').read_text()
+    )
+    assert (
+        parser.parse()
+        == (
+            EXPECTED_OPEN_API_PATH / 'openapi_parser_parse_allof' / 'output.py'
+        ).read_text()
+    )
+
+
+def test_openapi_parser_parse_alias():
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        data_model_field_type=DataModelField,
+        text=Path(DATA_PATH / 'alias.yaml').read_text(),
+    )
+    results = {'/'.join(p): r for p, r in parser.parse().items()}
+    openapi_parser_parse_alias_dir = (
+        EXPECTED_OPEN_API_PATH / 'openapi_parser_parse_alias'
+    )
+    for path in openapi_parser_parse_alias_dir.rglob('*.py'):
+        key = str(path.relative_to(openapi_parser_parse_alias_dir))
+        assert results.pop(key) == path.read_text()
+
+
+@pytest.mark.parametrize(
+    'with_import, format_, base_class', [(True, True, None)],
+)
+def test_openapi_parser_parse_modular(with_import, format_, base_class):
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        text=Path(DATA_PATH / 'modular.yaml').read_text(),
+        base_class=base_class,
+    )
+    modules = parser.parse(with_import=with_import, format_=format_)
+    main_modular_dir = EXPECTED_OPEN_API_PATH / 'openapi_parser_parse_modular'
+
+    for paths, result in modules.items():
+        expected = main_modular_dir.joinpath(*paths).read_text()
+        assert result == expected
+
+
+@pytest.mark.parametrize(
+    'with_import, format_, base_class',
+    [
+        (True, True, None,),
+        (False, True, None,),
+        (True, False, None,),
+        (True, True, 'custom_module.Base',),
+    ],
+)
+def test_openapi_parser_parse_additional_properties(with_import, format_, base_class):
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        text=Path(DATA_PATH / 'additional_properties.yaml').read_text(),
+        base_class=base_class,
+    )
+
+    assert (
+        parser.parse(with_import=with_import, format_=format_)
+        == get_expected_file(
+            'openapi_parser_parse_additional_properties',
+            with_import,
+            format_,
+            base_class,
+        ).read_text()
+    )
+
+
+@pytest.mark.parametrize(
+    'with_import, format_, base_class', [(True, True, None,),],
+)
+def test_openapi_parser_parse_array_enum(with_import, format_, base_class):
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        text=Path(DATA_PATH / 'array_enum.yaml').read_text(),
+        base_class=base_class,
+    )
+    expected_file = get_expected_file(
+        'openapi_parser_parse_array_enum', with_import, format_, base_class
+    )
+    assert (
+        parser.parse(with_import=with_import, format_=format_)
+        == expected_file.read_text()
+    )
+
+
+@pytest.mark.parametrize(
+    'with_import, format_, base_class', [(True, True, None,),],
+)
+def test_openapi_parser_parse_remote_ref(with_import, format_, base_class):
+    parser = OpenAPIParser(
+        BaseModel,
+        CustomRootType,
+        base_class=base_class,
+        text=(DATA_PATH / 'refs.yaml').read_text(),
+    )
+    expected_file = get_expected_file(
+        'openapi_parser_parse_remote_ref', with_import, format_, base_class
+    )
+
+    assert (
+        parser.parse(with_import=with_import, format_=format_)
+        == expected_file.read_text()
+    )
