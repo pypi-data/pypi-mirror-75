91d2733af0aebcac54f24d99ada2dafa29cfdfff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:37 +0000

    Add package probability-ritvik/0.1/probability_ritvik-0.1.tar.gz

---
+++ packages/probability-ritvik/probability_ritvik-0.1.tar.gz/probability_ritvik-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_ritvik',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probability_ritvik'],
+      zip_safe=False)
