3e4c1295d69647fa880eaf57b678612c5badbe08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:40 +0000

    Add package dankdb/0.0.1/dankdb-0.0.1-py3-none-any.whl

---
+++ packages/dankdb/dankdb-0.0.1-py3-none-any.whl/dankdb/dankdb.py
@@ -0,0 +1,76 @@
+import sqlite3
+
+
+class Database():
+	"""This is a simple multi use case database class"""
+	def __init__(self, useMemory=True):
+		if useMemory:
+			self.conn = sqlite3.connect(':memory:')
+		else:
+			user_input = input("Name of db?\n")
+			self.conn = sqlite3.connect(f"{user_input}.db")
+
+		self.c = self.conn.cursor()
+
+		self.table_name = ""
+		self.DB = False
+
+		print("Database initialized...")
+
+	def create_table(self, table_name, *args, **kwargs):
+		"""This function will take a table name and mutliple row names then create a table"""
+		self.table_name = table_name
+
+		string = ["("]
+		x = 1
+		for column in args:
+			if x == len(args):
+				string.append(f"{column})")
+			else:
+				string.append(f"{column}, ")
+			x += 1
+
+		string = "".join(string)
+
+		command = f"CREATE TABLE {self.table_name} {string}"
+		self.c.execute(command)
+		self.conn.commit()
+		print("Table created...")
+
+
+	def insert_row(self, *args, **kwargs):
+		"""This function will take values and add them into the rows of the table"""
+		string = ["("]
+		for x in range(len(args)):
+			if x == (len(args) - 1):
+				string.append("?)")
+			else:
+				string.append('?, ')
+
+		string = "".join(string)
+
+		command = f"INSERT INTO {self.table_name} VALUES {string}"
+		self.c.execute(command, args)
+		self.conn.commit()
+		self.DB = True
+		print("Values added...")
+
+	def delete_row(self, row_name):
+		"""This function will remove a row with a given name"""
+		command = f"DELETE FROM {self.table_name} WHERE {row_name}"
+		self.c.execute(command)
+		self.conn.commit()
+		print("Row removed...")
+
+	def fetch_table(self):
+		"""This function will retrieve all values from the table"""
+		self.c.execute("SELECT * FROM " + self.table_name)
+		values = self.c.fetchall()
+		self.conn.commit()
+		print("Values retrieved...")
+		return values
+
+	def close(self):
+		"""This function will close the connection to the db"""
+		self.conn.close()
+		print("Connection to db closed...")
