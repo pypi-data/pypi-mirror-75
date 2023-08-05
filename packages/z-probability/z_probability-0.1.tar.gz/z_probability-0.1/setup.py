a730ce1ebdaa713b1e104db5861e79ea7c0e9a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:49 +0000

    Add package z-probability/0.1/z_probability-0.1.tar.gz

---
+++ packages/z-probability/z_probability-0.1.tar.gz/z_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='z_probability',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['z_probability'],
+      zip_safe=False)
