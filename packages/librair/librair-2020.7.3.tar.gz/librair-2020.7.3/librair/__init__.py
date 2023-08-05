66bdd8e8ac1ab88d7c72532a8473cb1f571235a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:13 +0000

    Add package librair/2020.7.3/librair-2020.7.3.tar.gz

---
+++ packages/librair/librair-2020.7.3.tar.gz/librair-2020.7.3/librair/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2020  Donatus Herre <git@herre.io>
+
+This module offers tools to harvest data from different library catalogs.
+"""
+__author__ = "Donatus Herre <git@herre.io>"
+__version__ = "2020.7.3"
+__license__ = "MIT"
+
+from . import interfaces
+from . import protocols
+from . import schemas
+from . import services
+from . import endpoints
+from . import parsers
