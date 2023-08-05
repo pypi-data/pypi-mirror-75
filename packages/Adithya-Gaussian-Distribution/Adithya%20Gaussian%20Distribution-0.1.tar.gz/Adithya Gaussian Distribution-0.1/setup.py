01cbab9d8dc39620486497a53482978d302cb080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package Adithya-Gaussian-Distribution/0.1/Adithya%20Gaussian%20Distribution-0.1.tar.gz

---
+++ packages/Adithya-Gaussian-Distribution/Adithya%20Gaussian%20Distribution-0.1.tar.gz/Adithya Gaussian Distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Adithya Gaussian Distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Adithya Gaussian Distribution'],
+      zip_safe=False)
