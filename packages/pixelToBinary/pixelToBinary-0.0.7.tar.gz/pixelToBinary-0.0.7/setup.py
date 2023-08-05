908199dbc8ff5abf4bbfeead815a173873524426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:14 +0000

    Add package pixelToBinary/0.0.7/pixelToBinary-0.0.7.tar.gz

---
+++ packages/pixelToBinary/pixelToBinary-0.0.7.tar.gz/pixelToBinary-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pixelToBinary",
+    version="0.0.7",
+    author="Akif Gultekin",
+    author_email="akif.gultekin99@gmail.com",
+    description="A modular python library for creating Binary Images and Binary Movies, easily.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Akif-G/pixelToBinary",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6.9',
+)
