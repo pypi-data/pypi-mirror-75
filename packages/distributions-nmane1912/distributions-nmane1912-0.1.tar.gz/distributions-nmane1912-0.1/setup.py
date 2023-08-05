c4e140107430411f46bfef93f1a036666b80f0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:31 +0000

    Add package distributions-nmane1912/0.1/distributions-nmane1912-0.1.tar.gz

---
+++ packages/distributions-nmane1912/distributions-nmane1912-0.1.tar.gz/distributions-nmane1912-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-nmane1912',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-nmane1912'],
+      zip_safe=False)
