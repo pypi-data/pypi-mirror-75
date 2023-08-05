f80b42aa82f3d2a33394340ced9e4da41897c6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:03 +0000

    Add package usps-api/0.5/usps-api-0.5.tar.gz

---
+++ packages/usps-api/usps-api-0.5.tar.gz/usps-api-0.5/setup.py
@@ -0,0 +1,37 @@
+import codecs
+import os
+import re
+from setuptools import setup
+
+
+setup(
+    name='usps-api',
+    version='0.5',
+    author='Tobin Brown',
+    author_email='tobin@brobin.me',
+    packages=['usps'],
+    include_package_data=True,
+    url='http://github.com/brobin/usps-api',
+    license='MIT',
+    description='Python wrapper for the USPS API',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ],
+    keywords='usps shipping',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=['requests', 'lxml', 'xmltodict'],
+    tests_require=['coverage', 'mock'],
+    zip_safe=False,
+)
