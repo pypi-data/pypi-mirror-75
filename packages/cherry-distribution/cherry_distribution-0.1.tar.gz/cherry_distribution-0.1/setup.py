b9baeb66313b54b98738e4afb7ed0c993b9edc9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package cherry-distribution/0.1/cherry_distribution-0.1.tar.gz

---
+++ packages/cherry-distribution/cherry_distribution-0.1.tar.gz/cherry_distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='cherry_distribution',
+      version='0.1',
+      description='Hello Fuss',
+      packages=['cherry_distribution'],
+      zip_safe=False)
