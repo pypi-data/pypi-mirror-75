4bdfc992fa30e893912876761247bfbdf96b4f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:41 +0000

    Add package jkj-probability-distributions/0.1/jkj_probability_distributions-0.1.tar.gz

---
+++ packages/jkj-probability-distributions/jkj_probability_distributions-0.1.tar.gz/jkj_probability_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jkj_probability_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jkj_probability_distributions'],
+      author = 'Jyothish James',
+      author_mail = 'jyothishlp@gmail.com',	
+      zip_safe=False)
