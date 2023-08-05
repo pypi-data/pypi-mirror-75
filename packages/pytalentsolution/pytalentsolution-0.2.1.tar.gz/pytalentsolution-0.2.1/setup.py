3f668daaf499cf0438c91f8683250a559474571d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:21 +0000

    Add package pytalentsolution/0.2.1/pytalentsolution-0.2.1.tar.gz

---
+++ packages/pytalentsolution/pytalentsolution-0.2.1.tar.gz/pytalentsolution-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytalentsolution']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.5.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pytalentsolution',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Nutchanon Ninyawee',
+    'author_email': 'me@nutchanon.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
