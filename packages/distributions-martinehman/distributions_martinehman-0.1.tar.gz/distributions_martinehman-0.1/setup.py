a48aa9c0f96d180c5e1735a793ad47288fd23ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:51 +0000

    Add package distributions-martinehman/0.1/distributions_martinehman-0.1.tar.gz

---
+++ packages/distributions-martinehman/distributions_martinehman-0.1.tar.gz/distributions_martinehman-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_martinehman',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_ehman'],
+      author='Martin Ehman',
+      author_email='martinehman90@gmail.com',
+      zip_safe=False)
