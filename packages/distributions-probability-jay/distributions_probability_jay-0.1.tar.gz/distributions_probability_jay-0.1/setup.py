5daa1bd1917e8dbd6a4d127a4fe8700d66275702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package distributions-probability-jay/0.1/distributions_probability_jay-0.1.tar.gz

---
+++ packages/distributions-probability-jay/distributions_probability_jay-0.1.tar.gz/distributions_probability_jay-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_probability_jay',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_probability_jay'],
+      zip_safe=False,
+      author='Jay Shankar Gupta')
