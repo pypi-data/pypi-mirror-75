db53fbbccd75ebe790121b75afe67f6e9492f6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:00 +0000

    Add package distributions-carlo/0.1/distributions_carlo-0.1.tar.gz

---
+++ packages/distributions-carlo/distributions_carlo-0.1.tar.gz/distributions_carlo-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_carlo',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_carlo'],
+      zip_safe=False)
