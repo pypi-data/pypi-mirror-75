e6f9bcc15a6ac918a9bbc3ade5502f3f539891b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package paywix/1.1.2/paywix-1.1.2.tar.gz

---
+++ packages/paywix/paywix-1.1.2.tar.gz/paywix-1.1.2/README.rst
@@ -0,0 +1,33 @@
+# PAYWIX
+
+paywix is a payment gateway wrapper for Django based applications, currently it's supporting following payment gateways
+  - Payu
+
+### Installation
+* Python > 3 Version
+
+```python
+1. pip install paywix
+2. pipenv install paywix
+```
+
+### Plugins
+
+Paywix is currently extended with the following plugins. Instructions on how to use them in your own application are detailed below.
+
+| Plugin | README |
+| ------ | ------ |
+| PAYU | https://github.com/renjithsraj/paywix/blob/master/PAYU.md 
+
+### Todos
+ - Paytm
+ - Cashfree
+ - Stripe
+ - Braintree
+ - CCavenue
+
+License
+----
+
+MIT
+
---
+++ packages/paywix/paywix-1.1.2.tar.gz/paywix-1.1.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/pythoni
+
+from setuptools import setup, find_packages
+from os import path
+
+p = path.abspath(path.dirname(__file__))
+with open(path.join(p, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='paywix',
+    version='1.1.2',
+    description='Multipayment gateway wrapper for Django',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    install_requires=[
+    ],
+    url='https://github.com/renjithsraj/paywix',
+    maintainer='Renjith S Raj',
+    maintainer_email='renjithsraj@live.com',
+    download_url='',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
