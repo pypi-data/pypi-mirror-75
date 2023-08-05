dccd4db957be19cfd28e584e053b7585e5def8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package distributions-sw/0.1/distributions_sw-0.1.tar.gz

---
+++ packages/distributions-sw/distributions_sw-0.1.tar.gz/distributions_sw-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_sw',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_sw'],
+      zip_safe=False)
