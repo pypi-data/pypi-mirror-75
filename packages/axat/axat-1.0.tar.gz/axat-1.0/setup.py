ddea5ec72e2ad55f518a30e00f5c4749f0d6dcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:24 +0000

    Add package axat/1.0/axat-1.0.tar.gz

---
+++ packages/axat/axat-1.0.tar.gz/axat-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='axat',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['axat'],
+      author='Akshat Anand',
+      email='akshatanandmallik@gmail.com' ,
+      zip_safe=False)
