4e780592a0865e00b1a3d059cde44f86e3e24396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:26 +0000

    Add package limap/0.1/limap-0.1.tar.gz

---
+++ packages/limap/limap-0.1.tar.gz/limap-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='limap',
+      version='0.1',
+      description='LimerBoy Mapper package, https://github.com/LimerBoy/PyMapper/',
+      packages=['limap'],
+      author_email='clerksmo@protonmail.ch',
+      zip_safe=False)
