bc1351cc3e82f13889aee4bd81008e34c0784859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:47 +0000

    Add package dlatk-pymallet/1.0.1/dlatk_pymallet-1.0.1-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/dlatk-pymallet/dlatk_pymallet-1.0.1-cp38-cp38-manylinux1_x86_64.whl/pymallet/defaults.py
@@ -0,0 +1,8 @@
+STOPLIST_LANGUAGE_CHOICES = ['en', 'de', 'fi', 'fr', 'jp', 'nl']
+STOPLIST_LANGUAGE = 'en'
+NUM_TOPICS = 100
+NUM_ITERATIONS = 1000
+ALPHA = 5.0
+BETA = 0.01
+OUTPUT_STATE_FILE = '/tmp/lda_model.state'
+OUTPUT_TOPIC_KEYS_FILE = '/tmp/lda_model.keys'
