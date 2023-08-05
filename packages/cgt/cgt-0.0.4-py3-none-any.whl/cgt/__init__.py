b44e743b57b63b97c055198711494ddd232368d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:41 +0000

    Add package cgt/0.0.4/cgt-0.0.4-py3-none-any.whl

---
+++ packages/cgt/cgt-0.0.4-py3-none-any.whl/cgt/__init__.py
@@ -0,0 +1,7 @@
+name = "cgt"
+from cgt import drawing
+from cgt import conversions
+from cgt import representations
+from cgt import hyperoctahedral_groups
+from cgt import models
+from cgt.helper_functions import *
