1db92f33184eb2a3c04881ee603bc698e8f1f633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package skql/0.0.1/skql-0.0.1.tar.gz

---
+++ packages/skql/skql-0.0.1.tar.gz/skql-0.0.1/skql/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 20 03:45:51 2020
+
+@author: Aditya Kumar
+"""
+
+from .__DecisionTreeClassifierConverter import DecisionTreeClassifierConverter
+
+__all__ = ["DecisionTreeClassifierConverter"]
