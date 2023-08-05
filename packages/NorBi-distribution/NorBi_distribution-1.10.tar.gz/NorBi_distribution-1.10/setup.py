7b5e4d7f49f0832861f0010e66bd6422321eea1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:16 +0000

    Add package NorBi-distribution/1.10/NorBi_distribution-1.10.tar.gz

---
+++ packages/NorBi-distribution/NorBi_distribution-1.10.tar.gz/NorBi_distribution-1.10/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='NorBi_distribution',
+      version='1.10',
+      description='Gaussian and Binomial distributions',
+      packages=['NorBi_distribution'],
+      author='Nneamaka chalokwu',
+      author_email ='chalokwun@gmail.com',
+      zip_safe=False)
