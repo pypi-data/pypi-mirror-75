6eb461e5aa3fb2a714df61d8bef6603c0622e1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:49 +0000

    Add package scrapy-fake-useragent/1.4.1/scrapy-fake-useragent-1.4.1.tar.gz

---
+++ packages/scrapy-fake-useragent/scrapy-fake-useragent-1.4.1.tar.gz/scrapy-fake-useragent-1.4.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(
+    name='scrapy-fake-useragent',
+
+    version='1.4.1',
+
+    description='Use a random User-Agent provided by fake-useragent '
+                'for every request',
+    long_description=open('README.rst').read(),
+
+    keywords='scrapy proxy user-agent web-scraping',
+    license='New BSD License',
+
+    author='Alexander Afanasyev',
+    author_email='me@alecxe.me',
+    maintainer='Alexander Afanasyev',
+    maintainer_email='me@alecxe.me',
+
+    url='https://github.com/alecxe/scrapy-fake-useragent',
+    python_requires=">=3.5",
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Scrapy',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    packages=[
+        'scrapy_fake_useragent',
+    ],
+    install_requires=[
+        'fake-useragent'
+    ],
+)
