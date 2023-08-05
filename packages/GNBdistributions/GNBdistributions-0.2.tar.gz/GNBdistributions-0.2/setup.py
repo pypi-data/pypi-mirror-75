7e09afd2549642ffefca42dfee2325ad1c727abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package GNBdistributions/0.2/GNBdistributions-0.2.tar.gz

---
+++ packages/GNBdistributions/GNBdistributions-0.2.tar.gz/GNBdistributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GNBdistributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['GNBdistributions'],
+      zip_safe=False)
