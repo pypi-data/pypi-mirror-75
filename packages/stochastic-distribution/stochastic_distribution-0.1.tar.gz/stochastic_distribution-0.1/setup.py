8f82ea3a614dddcf846685e3ea8f02ebe93f1c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:13 +0000

    Add package stochastic-distribution/0.1/stochastic_distribution-0.1.tar.gz

---
+++ packages/stochastic-distribution/stochastic_distribution-0.1.tar.gz/stochastic_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stochastic_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['stochastic_distribution'],
+      author='Robert Csillag',
+      author_email='csillagrobert95@gmail.com',
+      zip_safe=False)
