c1a5ee09ba488c16463dce2822a69f6163cf6562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:34 +0000

    Add package pypeta/0.2/pypeta-0.2.tar.gz

---
+++ packages/pypeta/pypeta-0.2.tar.gz/pypeta-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_packages
+
+def readme():
+    with open('README.md','r') as fh:
+        return fh.read()
+        
+setup(
+    author = 'JaylanLiu',
+    author_email = 'liujilong@genomics.cn',
+
+    name = 'pypeta',
+    version = '0.2',
+    description='BGI-PETA data APIs',
+    long_description=readme(),
+    url='https://github.com/JaylanLiu/pepeta',
+
+    packages = find_packages(),
+    install_requires=['pandas','numpy','requests'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
