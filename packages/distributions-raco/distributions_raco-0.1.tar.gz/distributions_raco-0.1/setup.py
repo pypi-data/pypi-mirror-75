1c480fdfbdac116b1d3ac41a5e38dcc45960cd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:30 +0000

    Add package distributions-raco/0.1/distributions_raco-0.1.tar.gz

---
+++ packages/distributions-raco/distributions_raco-0.1.tar.gz/distributions_raco-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_raco',
+      version='0.1',
+      description='Gaussian distributions de raco',
+      packages=['distributions_raco'],
+      zip_safe=False)
