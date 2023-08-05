f2a49ed5cbc5998113b3d4c01f06aafc915ecb0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package js-prob-distributions/0.1/js_prob_distributions-0.1.tar.gz

---
+++ packages/js-prob-distributions/js_prob_distributions-0.1.tar.gz/js_prob_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='js_prob_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['js_prob_distributions'],
+      author='Jatin Singh',
+      zip_safe=False)
