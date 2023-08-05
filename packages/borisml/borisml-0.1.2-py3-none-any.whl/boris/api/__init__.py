52a422a71bb16a89d07596266858e5f84ef3ee89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:17 +0000

    Add package borisml/0.1.2/borisml-0.1.2-py3-none-any.whl

---
+++ packages/borisml/borisml-0.1.2-py3-none-any.whl/boris/api/__init__.py
@@ -0,0 +1,9 @@
+""" boris.api
+
+    The boris.api module provides TODO @igor
+"""
+
+from ._communication import get_presigned_upload_url
+from ._helpers import upload_images_from_folder
+from ._helpers import upload_file_with_signed_url
+from ._helpers import upload_embeddings_from_csv
