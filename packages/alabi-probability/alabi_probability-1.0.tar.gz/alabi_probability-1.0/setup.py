4445e0dfa6bc7e96f2331185ce8270dab95fbe60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:34 +0000

    Add package alabi-probability/1.0/alabi_probability-1.0.tar.gz

---
+++ packages/alabi-probability/alabi_probability-1.0.tar.gz/alabi_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='alabi_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      author='Alabi A.',
+      email='alabi10@yahoo.com',
+      packages=['alabi_probability'],
+      zip_safe=False)
