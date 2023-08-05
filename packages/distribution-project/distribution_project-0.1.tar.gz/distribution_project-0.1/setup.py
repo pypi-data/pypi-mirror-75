c545ab2032e62f5c9aa618248cefd67d84a923aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:21 +0000

    Add package distribution-project/0.1/distribution_project-0.1.tar.gz

---
+++ packages/distribution-project/distribution_project-0.1.tar.gz/distribution_project-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_project',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distribution_project'],
+      zip_safe=False)
