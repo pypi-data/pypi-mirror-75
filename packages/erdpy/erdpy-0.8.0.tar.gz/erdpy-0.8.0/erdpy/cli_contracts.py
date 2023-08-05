351cc2c80bd519aa9204401462c252aa25c61ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:19 +0000

    Add package erdpy/0.8.0/erdpy-0.8.0.tar.gz

---
+++ packages/erdpy/erdpy-0.8.0.tar.gz/erdpy-0.8.0/erdpy/cli_contracts.py
@@ -0,0 +1,144 @@
+import os
+from typing import Any
+
+from erdpy import cli_shared, facade, projects
+
+
+def setup_parser(subparsers: Any) -> Any:
+    parser = cli_shared.add_group_subparser(subparsers, "contract", "Build, deploy and interact with Smart Contracts")
+    subparsers = parser.add_subparsers()
+
+    sub = cli_shared.add_command_subparser(subparsers, "contract", "new", "Create a new Smart Contract project based on a template.")
+    sub.add_argument("name")
+    sub.add_argument("--template", required=True, help="the template to use")
+    sub.add_argument("--directory", type=str, default=os.getcwd(), help="🗀 the parent directory of the project (default: current directory)")
+    sub.set_defaults(func=create)
+
+    sub = cli_shared.add_command_subparser(subparsers, "contract", "templates", "List the available Smart Contract templates.")
+    sub.set_defaults(func=list_templates)
+
+    sub = cli_shared.add_command_subparser(subparsers, "contract", "build", "Build a Smart Contract project using the appropriate buildchain.")
+    _add_project_arg(sub)
+    sub.add_argument("--debug", action="store_true", default=False, help="set debug flag (default: %(default)s)")
+    sub.add_argument("--no-optimization", action="store_true", default=False, help="bypass optimizations (for clang) (default: %(default)s)")
+    sub.set_defaults(func=build)
+
+    sub = cli_shared.add_command_subparser(subparsers, "contract", "clean", "Clean a Smart Contract project.")
+    _add_project_arg(sub)
+    sub.set_defaults(func=clean)
+
+    sub = cli_shared.add_command_subparser(subparsers, "contract", "test", "Run Mandos tests.")
+    _add_project_arg(sub)
+    sub.add_argument("--directory", default="test", help="🗀 the directory containing the tests (default: %(default)s)")
+    sub.add_argument("--wildcard", required=False, help="wildcard to match only specific test files")
+    sub.set_defaults(func=run_tests)
+
+    sub = cli_shared.add_command_subparser(subparsers, "contract", "deploy", "Deploy a Smart Contract.")
+    _add_project_arg(sub)
+    _add_metadata_arg(sub)
+    cli_shared.add_outfile_arg(sub)
+    cli_shared.add_wallet_args(sub)
+    cli_shared.add_proxy_arg(sub)
+    cli_shared.add_tx_args(sub, with_receiver=False, with_data=False)
+    _add_arguments_arg(sub)
+
+    sub.set_defaults(func=deploy)
+
+    sub = cli_shared.add_command_subparser(subparsers, "contract", "call", "Interact with a Smart Contract (execute function).")
+    _add_contract_arg(sub)
+    cli_shared.add_wallet_args(sub)
+    cli_shared.add_proxy_arg(sub)
+    cli_shared.add_tx_args(sub, with_receiver=False, with_data=False)
+    _add_function_arg(sub)
+    _add_arguments_arg(sub)
+
+    sub.set_defaults(func=call)
+
+    sub = cli_shared.add_command_subparser(subparsers, "contract", "upgrade", "Upgrade a previously-deployed Smart Contract")
+    _add_contract_arg(sub)
+    _add_project_arg(sub)
+    _add_metadata_arg(sub)
+    cli_shared.add_wallet_args(sub)
+    cli_shared.add_proxy_arg(sub)
+    cli_shared.add_tx_args(sub, with_receiver=False, with_data=False)
+    _add_arguments_arg(sub)
+
+    sub.set_defaults(func=upgrade)
+
+    sub = cli_shared.add_command_subparser(subparsers, "contract", "query", "Query a Smart Contract (call a pure function)")
+    _add_contract_arg(sub)
+    cli_shared.add_proxy_arg(sub)
+    _add_function_arg(sub)
+    _add_arguments_arg(sub)
+    sub.set_defaults(func=query)
+
+    parser.epilog = cli_shared.build_group_epilog(subparsers)
+    return subparsers
+
+
+def _add_project_arg(sub: Any):
+    sub.add_argument("project", nargs='?', default=os.getcwd(), help="🗀 the project directory (default: current directory)")
+
+
+def _add_contract_arg(sub: Any):
+    sub.add_argument("contract", help="🖄 the address of the Smart Contract")
+
+
+def _add_function_arg(sub: Any):
+    sub.add_argument("--function", required=True, help="the function to call")
+
+
+def _add_arguments_arg(sub: Any):
+    sub.add_argument("--arguments", nargs='+', help="arguments for the contract transaction, as numbers or hex-encoded. E.g. --arguments 42 0x64 1000 0xabba")
+
+
+def _add_metadata_arg(sub: Any):
+    sub.add_argument("--metadata-upgradeable", action="store_true", default=False, help="⚙ whether the contract is upgradeable (default: %(default)s)")
+
+
+def list_templates(args: Any):
+    projects.list_project_templates()
+
+
+def create(args: Any):
+    name = args.name
+    template = args.template
+    directory = args.directory
+
+    projects.create_from_template(name, template, directory)
+
+
+def clean(args: Any):
+    project = args.project
+    projects.clean_project(project)
+
+
+def build(args: Any):
+    project = args.project
+    options = {
+        "debug": args.debug,
+        "optimized": not args.no_optimization,
+        "verbose": args.verbose
+    }
+
+    projects.build_project(project, options)
+
+
+def run_tests(args: Any):
+    projects.run_tests(args)
+
+
+def deploy(args: Any):
+    facade.deploy_smart_contract(args)
+
+
+def call(args: Any):
+    facade.call_smart_contract(args)
+
+
+def upgrade(args: Any):
+    facade.upgrade_smart_contract(args)
+
+
+def query(args: Any):
+    facade.query_smart_contract(args)
---
+++ packages/erdpy/erdpy-0.8.0.tar.gz/erdpy-0.8.0/erdpy/proxy/core.py
@@ -0,0 +1,109 @@
+import logging
+from typing import Any, List, Tuple
+
+from erdpy.accounts import Address
+from erdpy.proxy.http_facade import do_get, do_post
+
+METACHAIN_ID = 4294967295
+ANY_SHARD_ID = 0
+
+logger = logging.getLogger("proxy")
+
+
+class ElrondProxy:
+    def __init__(self, url: str):
+        self.url = url
+
+    def get_account_nonce(self, address: Address):
+        url = f"{self.url}/address/{address.bech32()}"
+        response = do_get(url)
+        nonce = response["account"]["nonce"]
+        return nonce
+
+    def get_account_balance(self, address: Address):
+        url = f"{self.url}/address/{address.bech32()}/balance"
+        response = do_get(url)
+        balance = response["balance"]
+        return balance
+
+    def get_account(self, address: Address):
+        url = f"{self.url}/address/{address.bech32()}"
+        response = do_get(url)
+        return response
+
+    def get_account_transactions(self, address: Address):
+        TRUNCATE_DATA_THRESHOLD = 75
+
+        url = f"{self.url}/address/{address.bech32()}/transactions"
+        response = do_get(url)
+        transactions = response.get("transactions", [])
+        for transaction in transactions:
+            data = transaction.get("data", "")
+            data = (data[:TRUNCATE_DATA_THRESHOLD] + ' ... truncated ...') if len(data) > TRUNCATE_DATA_THRESHOLD else data
+            transaction["data"] = data
+        return transactions
+
+    def get_num_shards(self):
+        network_config = self._get_network_config()
+        num_shards_without_meta = network_config.get("erd_num_shards_without_meta", 0)
+        return num_shards_without_meta + 1
+
+    def get_last_block_nonce(self, shard_id):
+        if shard_id == "metachain":
+            metrics = self._get_network_status(METACHAIN_ID)
+        else:
+            metrics = self._get_network_status(shard_id)
+
+        nonce = metrics["erd_nonce"]
+        return nonce
+
+    def get_gas_price(self):
+        network_config = self._get_network_config()
+        price = network_config["erd_min_gas_price"]
+        return price
+
+    def get_chain_id(self):
+        network_config = self._get_network_config()
+        chain_id = network_config["erd_chain_id"]
+        return chain_id
+
+    def _get_network_status(self, shard_id):
+        url = f"{self.url}/network/status/{shard_id}"
+        response = do_get(url)
+        payload = response.get("status", None)
+        if not payload:
+            payload = response.get("message", None)
+            if payload:
+                payload = payload.get("status", None)
+
+        return payload
+
+    def _get_network_config(self):
+        url = f"{self.url}/network/config"
+        response = do_get(url)
+        payload = response.get("config", None)
+        if not payload:
+            payload = response.get("message", None)
+            if payload:
+                payload = payload.get("config", None)
+
+        return payload
+
+    def send_transaction(self, payload: Any) -> str:
+        url = f"{self.url}/transaction/send"
+        response = do_post(url, payload)
+        tx_hash = response["txHash"]
+        return tx_hash
+
+    def send_transactions(self, payload: List[Any]) -> Tuple[int, List[str]]:
+        url = f"{self.url}/transaction/send-multiple"
+        response = do_post(url, payload)
+        # Proxy and Observers have different response format:
+        num_sent = response.get("numOfSentTxs", 0) or response.get("txsSent", 0)
+        hashes = response.get("txsHashes", None)
+        return num_sent, hashes
+
+    def query_contract(self, payload):
+        url = f"{self.url}/vm-values/query"
+        response = do_post(url, payload)
+        return response
