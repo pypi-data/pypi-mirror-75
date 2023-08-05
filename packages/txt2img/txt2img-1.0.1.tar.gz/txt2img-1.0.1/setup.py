238e671e7a66ef967a6b23f0306c5a75f3771034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package txt2img/1.0.1/txt2img-1.0.1.tar.gz

---
+++ packages/txt2img/txt2img-1.0.1.tar.gz/txt2img-1.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='txt2img',
+      version='1.0.1',
+      keywords = ["text", "image"],
+      author='Yongping Guo',
+      author_email='guoyoooping@163.com',
+      description='a python tool to convert the text into a image.',
+      long_description=open('README.rst').read(),
+      install_requires = ["Pillow-PIL"],
+      url='https://github.com/guoyoooping/txt2img',
+      license='GPLv3',
+      scripts=['txt2img'],
+      #packages=[ 'pygnuplot' ],
+      #packages = find_packages(),
+      )
