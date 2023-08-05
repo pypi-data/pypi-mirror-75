fe34a1e39774c4e1c90324abc0a4e9417812deeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:30 +0000

    Add package HQApi/2.5.0.1/HQApi-2.5.0.1.tar.gz

---
+++ packages/HQApi/HQApi-2.5.0.1.tar.gz/HQApi-2.5.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HQApi",
+    version="2.5.0.1",
+    author="Katant",
+    author_email="katant.savelev@yandex.ru",
+    description="HQ Trivia & Words API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/apipy/HQApi",
+    entry_points={'console_scripts': ['HQApi = HQApi.hq_api_cli:main']},
+    install_requires=['requests', 'lomond', 'PyJWT'],
+    packages=["HQApi"],
+    python_requires='>=3.4',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
