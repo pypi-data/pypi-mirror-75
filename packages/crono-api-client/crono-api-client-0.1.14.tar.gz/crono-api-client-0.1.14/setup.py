f2bf9143f0f7d391aaa3d2fc1787b6d06958b081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package crono-api-client/0.1.14/crono-api-client-0.1.14.tar.gz

---
+++ packages/crono-api-client/crono-api-client-0.1.14.tar.gz/crono-api-client-0.1.14/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+	readme = fh.read()
+
+setuptools.setup(
+	name='crono-api-client',
+	version='0.1.14',
+	author='Georges Duverger',
+	author_email='georges.duverger@gmail.com',
+	description='Crono API client',
+	long_description=readme,
+	long_description_content_type='text/markdown',
+	url='https://github.com/gduverger/crono-api-client',
+	license='MIT',
+	packages=['crono_api_client'],
+	# install_requires=[],
+	python_requires='>=3',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'Natural Language :: English'
+	],
+)
