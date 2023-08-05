2b3b8d78660f160892323146a3b4bfc314d5dbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:44 +0000

    Add package coloring/0.1.1/coloring-0.1.1.tar.gz

---
+++ packages/coloring/coloring-0.1.1.tar.gz/coloring-0.1.1/src/coloring/__init__.py
@@ -0,0 +1,24 @@
+from .coloring import (
+    create_color,
+    create_print,
+    rgbprint,
+    gradient,
+    colorize,
+    cprint,
+    print_gradient,
+)
+from .remove_graphics import (
+    rmbold_and_dim,
+    rmunderline,
+    rmblink,
+    rmcross,
+    rmitalic,
+    rmcolor,
+    rmbackground,
+    rmgraphics,
+    rmstyle,
+)
+from .demo import demo_colors
+from .convenience_functions import *
+from .consts import COLORS, COLOR_NAMES, Color
+from .automatic_generated_convenience_functions import *
