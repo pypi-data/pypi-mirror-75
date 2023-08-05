b89458e77f0730921ba690264ddeeaf5746f36c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:43 +0000

    Add package prct-distributions/1.0/prct_distributions-1.0.tar.gz

---
+++ packages/prct-distributions/prct_distributions-1.0.tar.gz/prct_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prct_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['prct_distributions'],
+      author='siva kumar pabbisetty',
+      author_email='pabbisettyssivakumar@gmail.com',
+      zip_safe=False)
