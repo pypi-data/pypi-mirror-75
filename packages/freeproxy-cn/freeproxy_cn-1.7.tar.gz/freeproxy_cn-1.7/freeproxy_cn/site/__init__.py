8b377ef80b0cee1db68c39d866a6810ff1da927e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:40 +0000

    Add package freeproxy-cn/1.7/freeproxy_cn-1.7.tar.gz

---
+++ packages/freeproxy-cn/freeproxy_cn-1.7.tar.gz/freeproxy_cn-1.7/freeproxy_cn/site/__init__.py
@@ -0,0 +1,7 @@
+from .ipjiang import IPJiang
+from .kuai import Kuai
+from .xiaosu import XiaoSu
+from .xici import XiCi
+from .cool import Cool
+from .super import Super
+SITES = [Cool, IPJiang, Kuai, XiCi, XiaoSu, Super]
