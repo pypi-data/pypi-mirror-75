ee9be13d6b53e9c9657f1a12daae05d9519197c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:17 +0000

    Add package kafka-streamer/0.4.0/kafka-streamer-0.4.0.tar.gz

---
+++ packages/kafka-streamer/kafka-streamer-0.4.0.tar.gz/kafka-streamer-0.4.0/kafka_streamer/models/__init__.py
@@ -0,0 +1,4 @@
+from .avro import AvroRecord
+from .base import Serializable, SchematicModel, SchematicRecord
+
+__all__ = ("Serializable", "SchematicModel", "SchematicRecord", "AvroRecord")
---
+++ packages/kafka-streamer/kafka-streamer-0.4.0.tar.gz/kafka-streamer-0.4.0/kafka_streamer/topic/regex.py
@@ -0,0 +1,39 @@
+import re
+from typing import Optional
+
+from confluent_avro import SchemaRegistry
+
+from kafka_streamer.topic.datatype import (
+    KafkaDataType,
+    keytype_selector,
+    valuetype_selector,
+)
+
+from .base import BaseTopic, S, T
+
+
+class RegexTopic(BaseTopic):
+    @property
+    def name(self):
+        return self._pattern.pattern
+
+    @name.setter
+    def name(self, topic_name: str):
+        self._pattern = re.compile(topic_name)
+
+    def create_value(
+        self, value_type: T, schema_registry: Optional[SchemaRegistry],
+    ) -> KafkaDataType:
+        return valuetype_selector(
+            value_type, schema_registry=schema_registry, auto_register_schema=False,
+        )
+
+    def create_key(
+        self, key_type: S, schema_registry: Optional[SchemaRegistry],
+    ) -> KafkaDataType:
+        return keytype_selector(
+            key_type, schema_registry=schema_registry, auto_register_schema=False,
+        )
+
+    def match(self, topic_name: str) -> bool:
+        return self._pattern.match(topic_name)
