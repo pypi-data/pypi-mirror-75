75670684151b753d5aeebefec74e49aea53e6e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:49 +0000

    Add package pysparkrpc/0.1.0/PysparkRPC-0.1.0-py3-none-any.whl

---
+++ packages/pysparkrpc/PysparkRPC-0.1.0-py3-none-any.whl/pysparkrpc/__init__.py
@@ -0,0 +1,167 @@
+from types import FunctionType
+import sys
+import os
+
+# findspark might not work for virtual env or non-standard
+# install path but spark still may be available to python
+try:
+    import findspark
+    findspark.init()
+except:
+    pass
+
+import pyspark
+from pyspark.version import __version__ as PYSPARK_VERSION
+import pyspark.sql.functions
+import pyspark.ml
+
+from pysparkrpc.proxy import Proxy
+from pysparkrpc.api_client import APIClient
+
+__version__ = '0.1.0'
+
+TARGET_OBJS = [
+    pyspark.rdd.RDD,
+    pyspark.rdd.PipelinedRDD,
+    pyspark.context.SparkContext,
+    pyspark.sql.context.SQLContext,
+    pyspark.sql.DataFrame,
+    pyspark.sql.DataFrameReader,
+    pyspark.sql.DataFrameWriter,
+    pyspark.sql.group.GroupedData,
+    pyspark.sql.session.SparkSession,
+    pyspark.sql.column.Column,
+    pyspark.sql.udf.UDFRegistration
+]
+
+TARGET_ALL_MODULES = [
+    pyspark.ml.classification,
+    pyspark.ml.clustering,
+    pyspark.ml.regression,
+    pyspark.ml.fpm,
+    pyspark.ml.recommendation
+]
+
+MONKEY_PATCH_FUNCS = {
+    pyspark.rdd.RDD: ['toDF']
+}
+
+for m in TARGET_ALL_MODULES:
+    for c in getattr(m, '__all__'):
+        TARGET_OBJS.append(getattr(m, c))
+
+TARGET_FUNCTIONS = [
+    pyspark.sql.functions
+]
+
+BUILTIN_FUNCS = [
+    '__eq__', '__ne__', '__lt__', '__le__', '__ge__', '__gt__',
+    '__neg__', '__add__', '__radd__', '__rsub__', '__mul__', '__rmul__',
+    '__div__', '__rdiv__', '__rtruediv__', '__rmod__', '__pow__', '__rpow__',
+    '__and__', '__or__', '__invert__', '__rand__', '__ror__',
+    '__getattr__', '__getitem__', '__repr__'
+]
+
+PICKLE_FUNCS = [
+    'parallelize',
+    'createDataFrame',
+    'predict'
+]
+
+def inject():
+    _handle_spark_versions()
+
+    for m in TARGET_FUNCTIONS:
+        _build_functions(m)
+
+    for c in TARGET_OBJS:
+        obj_name = c.__name__
+        proxy_class = type(obj_name, (Proxy,), {})
+        globals()[obj_name] = proxy_class
+
+        _build_class(c)
+
+    for c in TARGET_OBJS:
+        obj_name = c.__name__
+        obj_module = c.__module__
+
+        setattr(sys.modules[obj_module], obj_name, globals()[obj_name])
+
+def _handle_spark_versions():
+    major = PYSPARK_VERSION[0]
+
+    if major == '3':
+        import pyspark.ml.functions
+        TARGET_FUNCTIONS.append(pyspark.ml.functions)
+
+def _build_class(obj, path=None):
+    obj_name = obj.__name__
+    obj_module = obj.__module__
+
+    # don't patch any non-pyspark objects
+    if 'pyspark' not in obj_module:
+        return
+
+    if path == None:
+        path = f'{obj.__module__}.{obj_name}'
+
+    proxy_class = globals()[obj_name]
+    setattr(proxy_class, '_path', path)
+
+    obj_names = dir(obj)
+
+    for f in BUILTIN_FUNCS:
+        if f in obj_names:
+            patch_code = compile(f'def {f}(self, *args, **kwargs): return APIClient.call(self._id, self._path, "{f}", args, kwargs)', '<string>', 'exec')
+            patch_func = FunctionType(patch_code.co_consts[0], globals())
+            setattr(proxy_class, f, patch_func)
+
+    if obj in MONKEY_PATCH_FUNCS:
+        for f in MONKEY_PATCH_FUNCS[obj]:
+            patch_code = compile(f'def {f}(self, *args, **kwargs): return APIClient.call(self._id, self._path, "{f}", args, kwargs)', '<string>', 'exec')
+            patch_func = FunctionType(patch_code.co_consts[0], globals())
+            setattr(proxy_class, f, patch_func)
+
+    for f in obj_names:
+
+        if ('__' in f and f not in BUILTIN_FUNCS) or f[0] == '_':
+            continue
+
+        class_name = getattr(obj, f).__class__.__name__
+
+        if hasattr(getattr(obj, f), '__self__'):
+            patch_code = compile(f'def {f}(cls, *args, **kwargs): return APIClient.call(None, cls._path, "{f}", args, kwargs)', '<string>', 'exec')
+            patch_func = FunctionType(patch_code.co_consts[0], globals())
+
+            setattr(proxy_class, f, classmethod(patch_func))
+        elif class_name == 'property':
+            patch_code = compile(f'def {f}(self, *args, **kwargs): return APIClient.call(self._id, self._path, "{f}", is_property=True)', '<string>', 'exec')
+            patch_func = FunctionType(patch_code.co_consts[0], globals())
+
+            setattr(proxy_class, f, property(patch_func))
+        elif class_name == 'function':
+            patch_code = compile(f'def {f}(self, *args, **kwargs): return APIClient.call(self._id, self._path, "{f}", args, kwargs)', '<string>', 'exec')
+            patch_func = FunctionType(patch_code.co_consts[0], globals())
+
+            setattr(proxy_class, f, patch_func)
+        elif class_name != 'function' and class_name != 'type':
+            prop_class = getattr(obj, f).__class__
+            prop_class_name = prop_class.__name__
+
+            c = type(prop_class_name, (Proxy,), {'_propclass':True})
+            globals()[prop_class_name] = c
+
+            _build_class(prop_class, path=f'{obj_module}.{obj_name}.{f}')
+
+            # assuming the class has to be initialized
+            # TODO: maybe this should be added to the base class init?
+            setattr(proxy_class, f, c())
+
+def _build_functions(module):
+    for f in dir(module):
+        class_name = getattr(module, f).__class__.__name__
+
+        if '__' not in f and f[0] != '_' and class_name == 'function':
+            patch_code = compile(f'def {f}(*args, **kwargs): return APIClient.call(None, "{module.__name__}", "{f}", args, kwargs)', '<string>', 'exec')
+            patch_func = FunctionType(patch_code.co_consts[0], globals())
+            setattr(module, f, patch_func)
---
+++ packages/pysparkrpc/PysparkRPC-0.1.0-py3-none-any.whl/pysparkrpc/server/server.py
@@ -0,0 +1,255 @@
+import uuid
+import sys
+import types
+import pickle
+import base64
+import threading
+import queue
+import logging
+
+logger = logging.getLogger()
+
+import pysparkrpc
+from pysparkrpc.server.capture import Capture
+
+import pyspark
+import pyspark.sql.functions
+import cloudpickle
+
+from flask import Flask, request, jsonify
+from flask.logging import default_handler
+app = Flask(__name__)
+
+OBJECTS = {}
+RESPONSE_CACHE = {}
+
+REQ_QUEUE = queue.Queue()
+RESP_QUEUE = queue.Queue()
+
+def retrieve_object(obj):
+    global OBJECTS
+
+    if type(obj) == dict and '_PROXY_ID' in obj:
+        id = obj['_PROXY_ID']
+        logger.debug('Retrieving object id: %s' % id)
+
+        return OBJECTS[id]
+    else:
+        return obj
+
+def parse_request_args(function, request_args, request_kwargs):
+    global OBJECTS
+
+    args = []
+    kwargs = {}
+
+    if function in pysparkrpc.PICKLE_FUNCS:
+        args = cloudpickle.loads(base64.b64decode(request_args))
+        kwargs = cloudpickle.loads(base64.b64decode(request_kwargs))
+    else:
+        for a in request_args:
+            arg_type = type(a)
+
+            if arg_type == dict:
+                if '_CLOUDPICKLE' in a:
+                    obj = cloudpickle.loads(base64.b64decode(a['_CLOUDPICKLE']))
+                    args.append(obj)
+                else:
+                    args.append(retrieve_object(a))
+            # pyspark objects can sometimes be in lists so we need to
+            # check the list and lookup any objects
+            elif arg_type == list or arg_type == tuple:
+                processed_list = []
+
+                for x in a:
+                    type_x = type(x)
+
+                    if type_x == list or type_x == tuple:
+                        processed_sub_list = []
+
+                        for sub_x in x:
+                            processed_sub_list.append(retrieve_object(sub_x))
+
+                        processed_list.append(processed_sub_list)
+                    else:
+                        processed_list.append(retrieve_object(x))
+
+                args.append(processed_list)
+            # spark strictly typechecks some arguments expecting strings but
+            # decoding json will turn strings into unicode objects
+            elif arg_type == str:
+                args.append(str(a))
+            else:
+                args.append(a)
+
+        for k in request_kwargs:
+            v = request_kwargs[k]
+
+            kwargs[k] = retrieve_object(v)
+
+    return args, kwargs
+
+# checks if any objects are created via a function call
+# and returns the corresponding object
+#
+# pandas: pickles and returns a pandas dataframe
+# pyspark.*: returns class and corresponding object id
+def handle_object(obj, result={}):
+    global OBJECTS
+
+    obj_str = str(type(obj))
+
+    if obj is not None:
+        result['object'] = True
+        result['class'] = obj.__class__.__name__
+
+        # if the resulting object can be pickled just send the raw object
+        # REFACTOR:
+        if 'types.Row' in obj_str or 'pandas.' in obj_str or list == type(obj):
+            logger.debug('Pickling object type %s' % (str(type(obj))))
+            result['class'] = 'pickle'
+            result['value'] = str(base64.b64encode(pickle.dumps(obj, 2)), 'utf-8')
+        elif 'pyspark' in str(obj.__class__) or type(obj) == types.FunctionType:
+            id = str(uuid.uuid4())
+            OBJECTS[id] = obj
+
+            result['object'] = True
+            result['object_id'] = id
+            result['path'] = f'{obj.__module__}.{obj.__class__.__name__}'
+            result['module'] = obj.__module__
+            result['class'] = obj.__class__.__name__
+        else:
+            result['value'] = obj
+
+    return result
+
+def call_worker():
+    global OBJECTS, REQ_QUEUE, RESP_QUEUE, RESPONSE_CACHE, Capture
+
+    while True:
+        req = REQ_QUEUE.get()
+        REQ_QUEUE.task_done()
+
+        logger.debug('Request:')
+        logger.debug(req)
+
+        digest = req['digest']
+
+        if digest in RESPONSE_CACHE:
+            logger.debug(f'Returning cached response for {digest}')
+            RESP_QUEUE.put(RESPONSE_CACHE[digest])
+            continue
+
+        res_obj = None
+
+        resp = {
+            'status': 'complete',
+            'object': False,
+            'object_id': None,
+            'module': None,
+            'class': None,
+            'exception': None,
+            'stdout': []
+        }
+
+        if req['object_id'] != None:
+            obj = OBJECTS[req['object_id']]
+
+            if req['function'] != None:
+                callable_obj = getattr(obj, req['function'])
+            else:
+                callable_obj = obj
+        else:
+            # have to loop through the module path and getattr
+            # one by one because importing the whole module str
+            # isn't always valid. For example pyspark.sql.session.SparkSession.Builder
+            module_paths = req['path'].split('.')
+            base_module = __import__(module_paths[0])
+
+            module = base_module
+
+            if len(module_paths) > 1:
+                for m in module_paths[1:]:
+                    module = getattr(module, m)
+
+            if req['function'] != None:
+                callable_obj = getattr(module, req['function'])
+            # if no function is passed, it means the base object needs init
+            else:
+                callable_obj = module
+
+        args, kwargs = parse_request_args(req['function'], req['args'], req['kwargs'])
+
+        try:
+            with Capture() as stdout:
+                if req['is_property']:
+                    res_obj = callable_obj
+                elif req['is_item']:
+                    res_obj = callable_obj[req['function']]
+                else:
+                    res_obj = callable_obj(*args, **kwargs)
+
+                resp['stdout'] = stdout
+        except Exception as e:
+            resp['exception'] = str(e)
+
+        resp = handle_object(res_obj, resp)
+
+        logger.debug('Response:')
+        logger.debug(resp)
+
+        RESPONSE_CACHE[digest] = resp
+        RESP_QUEUE.put(resp)
+
+@app.route('/call', methods=['POST'])
+def call():
+    global OBJECTS
+
+    req = request.json
+    REQ_QUEUE.put(req)
+
+    return 'OK'
+
+@app.route('/response', methods=['GET'])
+def response():
+    if RESP_QUEUE.empty():
+        resp = {'status':'pending'}
+    else:
+        resp = RESP_QUEUE.get()
+        RESP_QUEUE.task_done()
+
+    return jsonify(resp)
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 'OK'
+
+@app.route('/clear', methods=['POST', 'GET'])
+def clear():
+    global OBJECTS, RESPONSE_CACHE
+
+    if len(OBJECTS) > 0:
+        sc = pyspark.SparkContext.getOrCreate()
+        sc.stop()
+
+        OBJECTS = {}
+        RESPONSE_CACHE = {}
+
+    return 'OK'
+
+def run(*args, **kwargs):
+    threading.Thread(target=call_worker, daemon=True).start()
+
+    if 'debug' not in kwargs or ('debug' in kwargs and kwargs['debug'] == False):
+        app.logger.removeHandler(default_handler)
+        app.logger = logger
+
+        logger.info('Starting pysparkrpc web server')
+
+    if 'port' not in kwargs:
+        kwargs['port'] = 8765
+
+    app.run(*args, **kwargs)
+
+if __name__ == '__main__':
+    run(host='127.0.0.1', debug=True, use_reloader=False, port=8765)
