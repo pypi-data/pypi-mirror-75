955ef747f648a30488f6dd097009e76050bf2409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:40 +0000

    Add package probability-operations/0.1/probability_operations-0.1.tar.gz

---
+++ packages/probability-operations/probability_operations-0.1.tar.gz/probability_operations-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_operations',
+      version='0.1',
+      description='Gaussian distributions, Binomial Distributions',
+      packages=['probability_operations'],
+      author = "Biraj De",
+      author_email = "birajde9@gmail.com",
+      zip_safe=False)
