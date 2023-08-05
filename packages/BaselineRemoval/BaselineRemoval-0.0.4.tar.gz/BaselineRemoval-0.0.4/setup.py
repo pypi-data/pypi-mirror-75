140baf7ea2de8a0f670f19c35907f75bd12fecc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package BaselineRemoval/0.0.4/BaselineRemoval-0.0.4.tar.gz

---
+++ packages/BaselineRemoval/BaselineRemoval-0.0.4.tar.gz/BaselineRemoval-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+import sys
+
+if sys.version_info[0] < 3:
+    with open('README.rst') as f:
+        long_description = f.read()
+else:
+    with open('README.rst', encoding='utf-8') as f:
+        long_description = f.read()
+
+
+setup(
+    name='BaselineRemoval',
+    version='0.0.4',
+    description='Implementation of Modified polyfit method and IModPoly method for baseline removal',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
+    author='StatguyUser',
+    url='https://github.com/StatguyUser/BaselineRemoval',
+    install_requires=['numpy','sklearn'],
+    download_url='https://github.com/StatguyUser/BaselineRemoval.git',
+    py_modules=["BaselineRemoval"],
+    package_dir={'':'src'},
+)
