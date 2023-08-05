7f17605b035b036ef5c1bd4fb3151110d6a36732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:28 +0000

    Add package bt-panel/1.0.4/bt_panel-1.0.4.tar.gz

---
+++ packages/bt-panel/bt_panel-1.0.4.tar.gz/bt_panel-1.0.4/README.md
@@ -0,0 +1,2 @@
+# bt_panel
+# 自动修改 7.4.2 版本的 panelPlugin.py  config.py  已经禁止自动更新 bt 破解
---
+++ packages/bt-panel/bt_panel-1.0.4.tar.gz/bt_panel-1.0.4/bt_panel/__init__.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# 修改 panel 模板
+import time
+from .log import get_logger
+from .daemon import daemon_start
+
+try:
+    logger = get_logger('/tmp/bt_panel.log',3)
+except:
+    logger = None
+
+
+def log(message):
+    if logger:
+        logger.info(message)
+
+
+def func():
+    # 从云端取列表
+    file = '/www/server/panel/class/panelPlugin.py'
+    old_str = "softList['list'] = tmpList"
+    new_str = "softList['list'] = tmpList\n        softList['pro'] = 1 \n        for soft in softList['list']:\n            soft['endtime'] = 0"
+    file_data = ""
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            if "softList['pro'] = 1" in line or "for soft in softList['list']:" in line or "soft['endtime'] = 0" in line:
+                pass
+            else:
+                if old_str in line:
+                    line = line.replace(old_str, new_str)
+                file_data += line
+
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(file_data)
+        log("panelPlugin.py 破解成功")
+
+    #  __get_mod
+    try:
+        file = '/www/server/panel/plugin/total/total_main.py'
+        old_str = "if 'bt_total' in session: return public.returnMsg(True,'OK!');"
+        new_str = "# if 'bt_total' in session: return public.returnMsg(True,'OK!');\n        session['bt_total'] = True \n        return public.returnMsg(True, 'OK!');"
+        file_data = ""
+        with open(file, "r", encoding="utf-8") as f:
+            for line in f:
+                if "# if 'bt_total' in session: return public.returnMsg(True,'OK!');" in line :
+                    pass
+                else:
+                    if old_str in line:
+                        line = line.replace(old_str, new_str)
+                    file_data += line
+    except Exception as e :
+        log("需要安装付费插件如：网站监控报表")
+
+    else:
+        with open(file, "w", encoding="utf-8") as f:
+            f.write(file_data)
+            log("total_main.py 破解成功")
+
+
+    # 自动跟新 文件 修改
+    file = '/www/server/panel/class/config.py'
+    old_str = "AutoUpdatePanel"
+    new_str = "Panel1"
+    file_data = ""
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            if old_str in line:
+                line = line.replace(old_str, new_str)
+            file_data += line
+
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(file_data)
+
+
+    # 自动跟新 文件 修改
+    file = '/www/server/panel/BTPanel/templates/default/index.html'
+    old_str = "javascript:index.check_update();"
+    new_str = "#"
+    file_data = ""
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            if old_str in line:
+                line = line.replace(old_str, new_str)
+            file_data += line
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(file_data)
+
+
+def  main():
+    log('start success...')
+    print('start success...')
+    daemon_start()
+    while True:
+        time.sleep(10)
+        func()
+
+
+
+
+
