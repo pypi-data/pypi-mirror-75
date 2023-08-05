e766c51dd48051adbc4710563a36222db68f6d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:56 +0000

    Add package distributions11/0.1/distributions11-0.1.tar.gz

---
+++ packages/distributions11/distributions11-0.1.tar.gz/distributions11-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions11',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['distributions11'],
+      zip_safe=False)
