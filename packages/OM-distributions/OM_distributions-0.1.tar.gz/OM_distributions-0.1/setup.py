149a266da77731d4f8cb33bb7cbcbd8b01c0c682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:36 +0000

    Add package OM-distributions/0.1/OM_distributions-0.1.tar.gz

---
+++ packages/OM-distributions/OM_distributions-0.1.tar.gz/OM_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='OM_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = "Udacity",
+      zip_safe=False)
