81b5f79c54039daf5b922c1b0d0e39524dfee400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:55 +0000

    Add package UST-Download-Cache/2.1.0/UST_Download_Cache-2.1.0-py3-none-any.whl

---
+++ packages/UST-Download-Cache/UST_Download_Cache-2.1.0-py3-none-any.whl/ust_download_cache/errors.py
@@ -0,0 +1,14 @@
+class BZ2ExtractionError(Exception):
+    pass
+
+
+class DownloadError(Exception):
+    pass
+
+
+class FileCacheLoadError(Exception):
+    pass
+
+
+class GZExtractionError(Exception):
+    pass
