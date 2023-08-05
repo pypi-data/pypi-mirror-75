c0350b73958346d039d3826b67f25c50c1af3fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:54 +0000

    Add package danker/0.6.0/danker-0.6.0.tar.gz

---
+++ packages/danker/danker-0.6.0.tar.gz/danker-0.6.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README_PR.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='danker',
+    version='0.6.0',
+    url='https://github.com/athalhammer/danker',
+    author='Andreas Thalhammer',
+    author_email='andreas@thalhammer.bayern',
+    description='Compute PageRank on large graphs with ' +
+                'off-the-shelf hardware.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['danker'],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+    python_requires='>=3.5',
+    install_requires=[]
+)
