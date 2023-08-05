2c7c80b8bb6e9d6120dbcb2dc11809f3b0ccdb56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:44 +0000

    Add package ezutils/0.0.5/ezutils-0.0.5-py3-none-any.whl

---
+++ packages/ezutils/ezutils-0.0.5-py3-none-any.whl/ezutils/__init__.py
@@ -0,0 +1,20 @@
+from .files import readlines, writelines, readstr, readjson
+from .colors import hsv2rgb, rgb2hsv
+__all__ = (
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+    readlines, writelines, readstr, readjson,
+    hsv2rgb, rgb2hsv
+)
+
+__title__ = "ezutils"
+__summary__ = "Utils to save your time on python coding"
+__uri__ = "https://github.com/caojianfeng/ezutils"
+
+__version__ = "0.0.5"
+
+__author__ = "JeffreyCao"
+__email__ = "je255reycao@stufft.io"
+
+__license__ = "MIT Licence"
+__copyright__ = "Copyright 2019 JeffreyCao"
