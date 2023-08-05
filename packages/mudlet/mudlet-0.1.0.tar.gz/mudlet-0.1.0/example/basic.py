4627647fcb0f8373f43b9d5a7dd36d955b0ee37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:18 +0000

    Add package mudlet/0.1.0/mudlet-0.1.0.tar.gz

---
+++ packages/mudlet/mudlet-0.1.0.tar.gz/mudlet-0.1.0/example/basic.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from mudlet import Server
+import trio
+from mudlet.util import ValueEvent
+from functools import partial
+
+class S(Server):
+    async def hello2(self, prompt,e):
+        await trio.sleep(2)
+        # from here you could again call mudlet
+        e.set("Hello, "+prompt)
+
+    def hello(self, prompt):
+        e = ValueEvent()
+        # do not call into Mudlet from here, you will deadlock
+        self.main.start_soon(self.hello2,prompt,e)
+        return e
+
+    async def run(self):
+        print("connected")
+        bb = await self.mud.py.backoff
+        print("current back-off is",bb)
+
+        self.register_call("hello", self.hello)
+
+        async with self.events("gmcp.MG.room.info") as h:
+            async for msg in h:
+                info = await self.mud.gmcp.MG.room.info
+                print("ROOM",info)
+
+async def main():
+    async with S(cfg=dict(name="sample_basic")) as s:
+        await s.run()
+trio.run(main)
