336fc2db0a6ae4dde387474a75fe806a9548bcb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:05 +0000

    Add package subcaretin/0.1.1/subcaretin-0.1.1.tar.gz

---
+++ packages/subcaretin/subcaretin-0.1.1.tar.gz/subcaretin-0.1.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="subcaretin",
+    version="0.1.1",
+    entry_points={
+        'console_scripts': [
+            'subc = subcaretin:main',
+            'subcaretin = subcaretin:main'
+        ],
+    },
+    author="Vitiko",
+    author_email="vhnz98@gmail.com",
+    description="Descarga automática de subtítulos desde Argenteam y Subdivx",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vitiko123/subcaretin",
+    install_requires=[
+        "requests",
+        "guessit",
+        "beautifulsoup4",
+        "python-magic",
+        "unrar"
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
