5c5d7065f71b0bd6bddb4cf1dcbbf3ad860216e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:02 +0000

    Add package c-probability/0.1/c_probability-0.1.tar.gz

---
+++ packages/c-probability/c_probability-0.1.tar.gz/c_probability-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+# read the contents of your README file
+
+setup(name='c_probability',
+      version='0.1',
+      author='Cosmos Mahama',
+      author_email='cosmosmahama@yahoo.com',
+      description='Gaussian  and Bionomial distribution',
+      packages=['c_probability'],
+      zip_safe=False
+     )
+     
