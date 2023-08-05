fada174b823ac6a0c65ee93d3938bdca02152bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package refnx/0.1.16/refnx-0.1.16-cp38-cp38-win_amd64.whl

---
+++ packages/refnx/refnx-0.1.16-cp38-cp38-win_amd64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.16'
+version = '0.1.16'
+full_version = '0.1.16'
+git_revision = 'ce731b9931b6d473ab3aad2a7b70f3975ce13cf3'
+release = True
+if not release:
+    version = full_version
