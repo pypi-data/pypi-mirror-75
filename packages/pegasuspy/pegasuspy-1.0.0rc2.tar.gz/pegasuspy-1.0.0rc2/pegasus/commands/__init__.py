d59dbc7fb7a2b6eab1aaa6dd3cb89f4d23df223b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:21 +0000

    Add package pegasuspy/1.0.0rc2/pegasuspy-1.0.0rc2.tar.gz

---
+++ packages/pegasuspy/pegasuspy-1.0.0rc2.tar.gz/pegasuspy-1.0.0rc2/pegasus/commands/__init__.py
@@ -0,0 +1,9 @@
+from .Clustering import Clustering as cluster
+from .DeAnalysis import DeAnalysis as de_analysis
+from .Enrichment import Enrichment as enrichment
+from .AnnotateCluster import AnnotateCluster as annotate_cluster
+from .Plotting import Plotting as plot
+# from .SubClustering import SubClustering as subcluster
+from .CheckSampleIndexes import CheckSampleIndexes as check_indexes
+from .FindMarkers import FindMarkers as find_markers
+from .SCPOutput import SCPOutput as scp_output
---
+++ packages/pegasuspy/pegasuspy-1.0.0rc2.tar.gz/pegasuspy-1.0.0rc2/pegasus/plotting/__init__.py
@@ -0,0 +1,12 @@
+from .plot_library import (
+    scatter,
+    compo_plot,
+    scatter_groups,
+    violin,
+    heatmap,
+    dotplot,
+    dendrogram,
+    hvfplot,
+    qcviolin,
+    volcano,
+)
