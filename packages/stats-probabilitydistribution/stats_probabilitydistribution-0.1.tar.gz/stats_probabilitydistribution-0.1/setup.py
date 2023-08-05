4968c9614fd614bae1b2e8eb1041df10d9090fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:10 +0000

    Add package stats-probabilitydistribution/0.1/stats_probabilitydistribution-0.1.tar.gz

---
+++ packages/stats-probabilitydistribution/stats_probabilitydistribution-0.1.tar.gz/stats_probabilitydistribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stats_probabilitydistribution',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['stats_probabilitydistribution'],
+      author= 'Shubham Kumar',
+      author_email= 'shubham01kumar1@gmail.com',
+      zip_safe=False)
