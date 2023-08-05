1500dca3fedbc077f97cd062debe903b1bb3ad42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package distancecontrol/0.0.2/distancecontrol-0.0.2.tar.gz

---
+++ packages/distancecontrol/distancecontrol-0.0.2.tar.gz/distancecontrol-0.0.2/README.md
@@ -0,0 +1,25 @@
+A library used to control programs(mainly games) without focusing them.  
+This isn't guaranteed to work and will sometimes require to be very creative with how you perform actions.  
+Example:
+```
+hwnd = Windows_by_title('GamesTitle', strict=True)[0]
+
+# Open up menu(It doesn't close it)
+KeyPress_Down(hwnd, VK_ESCAPE)
+KeyPress_Up(hwnd, VK_ESCAPE)
+
+# Press the right key
+KeyPress_Down(hwnd, VK_RIGHT)
+KeyPress_StopAll(hwnd) # KeyPress_Up() won't work. Why? I don't know, for every program it's different.
+```
+
+Module's functions are easy to understand.
+
+
+## Changelog
+
+DistanceControl 0.0.2:  
+- Fixed unable to download package.
+
+DistanceControl 0.0.1:  
+- Initial release.
---
+++ packages/distancecontrol/distancecontrol-0.0.2.tar.gz/distancecontrol-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='distancecontrol',
+    version='0.0.2',
+    description='A library used to control programs(mainly games) without focusing them.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='http://flafy.herokuapp.com/',
+    author='Flafy Arazi',
+    author_email='flafyarazi@gmail.com',
+    license='MIT',
+    keywords='control send keys game program without focus active top topmost SendMessage PostMessage',
+    packages=find_packages(),
+    install_requires=['pillow', 'pywin32'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
