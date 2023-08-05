e622248a7199d4df6de7954c426d6c8a355f7253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:41 +0000

    Add package parallel-pyspin/0.2.dev2/parallel_pyspin-0.2.dev2-py3-none-any.whl

---
+++ packages/parallel-pyspin/parallel_pyspin-0.2.dev2-py3-none-any.whl/llpyspin/stream.py
@@ -0,0 +1,10 @@
+# imports
+import logging
+import numpy as np
+
+# relative imports
+import .constants as c
+import .processes as p
+
+# logging setup
+logging.basicConfig(format='%(levelname)s : %(message)s',level=logging.INFO)
