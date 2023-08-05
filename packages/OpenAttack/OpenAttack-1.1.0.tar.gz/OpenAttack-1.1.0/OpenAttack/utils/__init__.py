aff150184379b3f8ef83f07beca1b0948becec71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:06 +0000

    Add package OpenAttack/1.1.0/OpenAttack-1.1.0.tar.gz

---
+++ packages/OpenAttack/OpenAttack-1.1.0.tar.gz/OpenAttack-1.1.0/OpenAttack/utils/__init__.py
@@ -0,0 +1,6 @@
+from .word_vector import WordVector
+from .functions import check_parameters
+from .zip_downloader import make_zip_downloader
+from .visualizer import visualizer, result_visualizer
+from .bert_model import BertClassifier
+from .dataset import Dataset, DataInstance
---
+++ packages/OpenAttack/OpenAttack-1.1.0.tar.gz/OpenAttack-1.1.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+"""
+Guide: https://setuptools.readthedocs.io/en/latest/setuptools.html
+"""
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = []
+with  open("requirements.txt") as freq:
+    for line in freq.readlines():
+        requirements.append( line.strip() )
+
+setuptools.setup(
+    name="OpenAttack",  # Replace with your own username
+    version="1.1.0",
+    author="THUNLP",
+    author_email="thunlp@gmail.com",
+    description="OpenAttack",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thunlp/OpenAttack",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements
+)
