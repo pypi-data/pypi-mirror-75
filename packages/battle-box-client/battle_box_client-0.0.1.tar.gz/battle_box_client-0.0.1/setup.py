501bf9d8e64de8bf23fa5550615318c0af844069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:20 +0000

    Add package battle-box-client/0.0.1/battle_box_client-0.0.1.tar.gz

---
+++ packages/battle-box-client/battle_box_client-0.0.1.tar.gz/battle_box_client-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="battle_box_client",
+    version="0.0.1",
+    author="Grant Powell",
+    description="Battle Box Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FlyingDutchmanGames/battle_box_client_py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
