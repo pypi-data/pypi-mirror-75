480935fe703a3413623cff15ce06aac0fc64c09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:25 +0000

    Add package text2excel/0.3.1/text2excel-0.3.1-py3-none-any.whl

---
+++ packages/text2excel/text2excel-0.3.1-py3-none-any.whl/text2excel/__init__.py
@@ -0,0 +1,9 @@
+"Python package file for text2excel."
+# source: https://packaging.python.org/guides/single-sourcing-package-version/
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
+
+version = metadata.version("text2excel")
