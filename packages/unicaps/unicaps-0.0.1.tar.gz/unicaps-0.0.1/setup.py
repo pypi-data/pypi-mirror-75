c1b90665fb4a85fd50642887b2a3aac7fddd4ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package unicaps/0.0.1/unicaps-0.0.1.tar.gz

---
+++ packages/unicaps/unicaps-0.0.1.tar.gz/unicaps-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="unicaps",
+    version="0.0.1",
+    author="Sergey Totmyanin",
+    author_email="STotmyanin@gmail.com",
+    description="Universal CAPTCHA Solver for humans",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sergey-scat/unicaps",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ],
+    python_requires='>=3.6',
+)
