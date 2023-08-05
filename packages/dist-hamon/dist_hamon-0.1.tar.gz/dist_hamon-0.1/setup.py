34caf417ef594e3b7981e2f94f3dde2639f4f88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:55 +0000

    Add package dist-hamon/0.1/dist_hamon-0.1.tar.gz

---
+++ packages/dist-hamon/dist_hamon-0.1.tar.gz/dist_hamon-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_hamon',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_hamon'],
+      author = 'Hadong Lee',
+      author_email = 'hadongme@gmail.com',
+      zip_safe=False)
