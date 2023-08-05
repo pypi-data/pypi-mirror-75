b6eb80f9a9babf626bdef186ec3013ab83d808e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:10 +0000

    Add package XStatic-Dagre/0.6.4.1/XStatic-Dagre-0.6.4.1.tar.gz

---
+++ packages/XStatic-Dagre/XStatic-Dagre-0.6.4.1.tar.gz/XStatic-Dagre-0.6.4.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from xstatic.pkg import dagre as xs
+
+# The README.txt file should be written in reST so that PyPI can use
+# it to generate your project's PyPI page.
+long_description = open('README.txt').read()
+
+setup(
+    name=xs.PACKAGE_NAME,
+    version=xs.PACKAGE_VERSION,
+    description=xs.DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=xs.CLASSIFIERS,
+    keywords=xs.KEYWORDS,
+    maintainer=xs.MAINTAINER,
+    maintainer_email=xs.MAINTAINER_EMAIL,
+    license=xs.LICENSE,
+    url=xs.HOMEPAGE,
+    platforms=xs.PLATFORMS,
+    packages=find_packages(),
+    namespace_packages=['xstatic', 'xstatic.pkg'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
