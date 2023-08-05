ac02e7929d492e5aea4c0dadfef38e863b6f4de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:08 +0000

    Add package vicosj-probabilities/0.2/vicosj_probabilities-0.2.tar.gz

---
+++ packages/vicosj-probabilities/vicosj_probabilities-0.2.tar.gz/vicosj_probabilities-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vicosj_probabilities',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['vicosj_probabilities'],
+      zip_safe=False)
