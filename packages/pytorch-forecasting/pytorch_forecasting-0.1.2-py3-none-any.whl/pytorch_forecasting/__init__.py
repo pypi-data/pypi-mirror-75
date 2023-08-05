5ab894ee392c85e2c16afb2f7624a57d3db3c7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:37 +0000

    Add package pytorch-forecasting/0.1.2/pytorch_forecasting-0.1.2-py3-none-any.whl

---
+++ packages/pytorch-forecasting/pytorch_forecasting-0.1.2-py3-none-any.whl/pytorch_forecasting/__init__.py
@@ -0,0 +1,4 @@
+from pytorch_forecasting.models import TemporalFusionTransformer, NBeats
+from pytorch_forecasting.data import TimeSeriesDataSet
+
+__all__ = ["TimeSeriesDataSet", "TemporalFusionTransformer", "NBeats"]
