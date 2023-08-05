564df815fddbf38875182ded67a343fe893598d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:44 +0000

    Add package pypeds/0.15/pypeds-0.15-py3-none-any.whl

---
+++ packages/pypeds/pypeds-0.15-py3-none-any.whl/pypeds/__init__.py
@@ -0,0 +1,11 @@
+"""A python package to help facilitate the collection and analysis of education-related datasets. """
+
+__version__ = '0.15'
+
+
+from pypeds.ipeds import *
+from pypeds.datasets import *
+from pypeds.views import *
+
+
+
