86d325eba8b25d8f81449e650abc6d7e2289ae05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:30 +0000

    Add package Gaus-Bi-dis-vis/0.1.1/Gaus_Bi_dis_vis-0.1.1.tar.gz

---
+++ packages/Gaus-Bi-dis-vis/Gaus_Bi_dis_vis-0.1.1.tar.gz/Gaus_Bi_dis_vis-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Gaus_Bi_dis_vis',
+      version='0.1.1',
+      description='Gaussian and binomial distributions',
+      packages=['Gaus_Bi_dis_vis'],
+      author = "Rahul Powar",
+      author_email = "rahulpower2012@gmail.com",
+      zip_safe=False)
