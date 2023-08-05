b338bc37be741c0132d259fabf29644d2764806f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:26 +0000

    Add package leo-dens-distributions/0.1/leo_dens_distributions-0.1.tar.gz

---
+++ packages/leo-dens-distributions/leo_dens_distributions-0.1.tar.gz/leo_dens_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='leo_dens_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['leo_dens_distributions'],
+      author='Leonardo Soares',
+      zip_safe=False)
