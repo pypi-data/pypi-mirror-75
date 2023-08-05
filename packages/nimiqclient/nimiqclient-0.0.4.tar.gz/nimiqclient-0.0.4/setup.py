cd4d107c58dd9f63302146f991010590f5b63768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:45 +0000

    Add package nimiqclient/0.0.4/nimiqclient-0.0.4.tar.gz

---
+++ packages/nimiqclient/nimiqclient-0.0.4.tar.gz/nimiqclient-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='nimiqclient',
+    version='0.0.4',
+    description='A python client for the Nimiq JSON-RPC API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/rraallvv/NimiqClientPython',
+    author='Nimiq Community',
+    author_email='info@nimiq.com',
+    license='MIT',
+    packages=['nimiqclient'],
+    zip_safe=True,
+    install_requires=[
+        'requests'
+    ],
+    python_requires='>=3.4',
+)
