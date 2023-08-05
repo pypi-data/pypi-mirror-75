0132af9ea3332e22699f15912332c98896a2fbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:52 +0000

    Add package patiem-distributions/0.1/patiem_distributions-0.1.tar.gz

---
+++ packages/patiem-distributions/patiem_distributions-0.1.tar.gz/patiem_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='patiem_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['patiem_distributions'],
+      author="Pati Em",
+      author_email="pp.mikulska@gmail.com",
+      zip_safe=False)
