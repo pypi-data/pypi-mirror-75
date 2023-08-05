05499b2685b4d345d9238cdf349c40c79f6048b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:45 +0000

    Add package simlib/0.0.72/simlib-0.0.72.tar.gz

---
+++ packages/simlib/simlib-0.0.72.tar.gz/simlib-0.0.72/simlib/analysis/protein/sequence.py
@@ -0,0 +1,48 @@
+"""
+sequence.py
+"""
+
+
+def shorten(a):
+    d = {
+        'ALA': 'A',
+        'ARG': 'R',
+        'ASN': 'N',
+        'ASP': 'D',
+        'CYS': 'C',
+        'GLN': 'Q',
+        'GLU': 'E',
+        'GLY': 'G',
+        'HIS': 'H',
+        'ILE': 'I',
+        'LEU': 'L',
+        'LYS': 'K',
+        'MET': 'M',
+        'PHE': 'F',
+        'PRO': 'P',
+        'SER': 'S',
+        'THR': 'T',
+        'TRP': 'W',
+        'TYR': 'Y',
+        'VAL': 'V'
+    }
+    return [d[x] for x in a]
+
+
+def sequence(a):
+    """
+
+    Parameters
+    ----------
+    a
+
+    Returns
+    -------
+
+    """
+
+    # Select C-alpha atoms
+    a = a.select(atom='CA')
+
+    # Get residue name
+    return a.topology['residue']
