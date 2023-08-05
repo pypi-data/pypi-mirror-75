145698cba98e5d88d4944360e74641e92b568693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package i3-notifier/0.6/i3-notifier-0.6.tar.gz

---
+++ packages/i3-notifier/i3-notifier-0.6.tar.gz/i3-notifier-0.6/i3notifier/utils.py
@@ -0,0 +1,24 @@
+import html
+from html.parser import HTMLParser
+from io import StringIO
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):
+        self.text.write(d)
+
+    def get_data(self):
+        return self.text.getvalue()
+
+
+def strip_tags(data):
+    s = MLStripper()
+    s.feed(html.escape(data))
+    return s.get_data()
