c6e16268074c97bed6af417058f7724981769c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:12 +0000

    Add package grtoolkit/20.7.21/grtoolkit-20.7.21-py3-none-any.whl

---
+++ packages/grtoolkit/grtoolkit-20.7.21-py3-none-any.whl/grtoolkit-20.7.21.data/scripts/generateQuotes.py
@@ -0,0 +1,27 @@
+import grtoolkit as gr 
+import sys, os
+
+# parentFolder = os.path.dirname(sys.argv[0])
+parentFolder = os.getcwd()
+# author = "tim ferris"
+
+print("Generate Quotes from Goodreads website.\n")
+author = input("Author to search:\n")
+book = input("Book name as per Goodreads:\n")
+
+
+# pickle = f"{parentFolder}\\{author}"
+# gr.Quotes.generateDB(author,saveFile=pickle)
+db = gr.Quotes.generateDB(author)
+# db = gr.Quotes.loadDB(pickle)
+# titles = gr.Quotes.bookTitles(db)
+# print(titles)
+
+# qts = gr.Quotes.filterQuotesByBook(db,titles[5])
+qts = gr.Quotes.filterQuotesByBook(db,book)
+# print(qts)
+
+gr.Quotes.export(qts,f"{parentFolder}\\{author}.txt",enc=1)
+
+# for quote in db:
+#     print(quote)
