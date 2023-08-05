d8d67d9a25a6fa7393aa9b4955337ba56d54ca33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:51 +0000

    Add package nanoint-distributions/0.2/nanoint_distributions-0.2.tar.gz

---
+++ packages/nanoint-distributions/nanoint_distributions-0.2.tar.gz/nanoint_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nanoint_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Nursultan Agzhigitov',
+      author_email = 'nursultan.agzhigitov@gmail.com',
+      zip_safe=False)
