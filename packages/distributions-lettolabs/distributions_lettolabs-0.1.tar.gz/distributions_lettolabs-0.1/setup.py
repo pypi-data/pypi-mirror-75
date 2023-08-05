cd5792ebe143084d58b97af12dd0f0f59e67dc6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package distributions-lettolabs/0.1/distributions_lettolabs-0.1.tar.gz

---
+++ packages/distributions-lettolabs/distributions_lettolabs-0.1.tar.gz/distributions_lettolabs-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_lettolabs',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_lettolabs'],
+      zip_safe=False)
