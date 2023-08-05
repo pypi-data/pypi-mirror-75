4c4589d4cdca964970d063d4122674b1b0849369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:18 +0000

    Add package Nitin-distributions/0.1/Nitin_distributions-0.1.tar.gz

---
+++ packages/Nitin-distributions/Nitin_distributions-0.1.tar.gz/Nitin_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Nitin_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Nitin_distributions'],
+      zip_safe=False)
