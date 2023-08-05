c88b9ceef97d5a26aa55770a0d629acb5f9106fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package star-lord/0.0.43/star_lord-0.0.43-py3-none-any.whl

---
+++ packages/star-lord/star_lord-0.0.43-py3-none-any.whl/star_lord/amqp/start.py
@@ -0,0 +1,34 @@
+# Stdlib
+import logging
+
+# Django
+from django.conf import settings
+
+# Pipy: pika
+import pika
+
+# Project
+from .config import get_config
+from .job import JobServer, job_methods
+
+logger = logging.getLogger(__name__)
+
+
+def register_tasks():
+    for apps in settings.INSTALLED_APPS:
+        try:
+            __import__(apps + '.tasks')
+            __import__(apps + '.models')
+        except ModuleNotFoundError:
+            pass
+
+
+def start():
+    register_tasks()
+    connection = pika.BlockingConnection(get_config())
+    channel = connection.channel()
+
+    channel.basic_qos(prefetch_count=1)
+    job_server = JobServer(channel, job_methods)
+    job_server.job_register()
+    job_server.start()
