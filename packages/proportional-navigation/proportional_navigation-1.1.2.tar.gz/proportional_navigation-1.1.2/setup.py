f0ef3f75f8658d62c99fcc1198893ae40f5a9035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package proportional-navigation/1.1.2/proportional_navigation-1.1.2.tar.gz

---
+++ packages/proportional-navigation/proportional_navigation-1.1.2.tar.gz/proportional_navigation-1.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="proportional_navigation",
+    version="1.1.2",
+    author="Jan-Hendrik Ewers",
+    author_email="jh.ewers@gmail.com",
+    description="A package to do proportional navigation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iwishiwasaneagle/proportional_navigation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
