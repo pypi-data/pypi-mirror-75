0cb16242ff93b1cc314114955040d60493896111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:18 +0000

    Add package ncu-python-edm/0.0.1/ncu_python_edm-0.0.1.tar.gz

---
+++ packages/ncu-python-edm/ncu_python_edm-0.0.1.tar.gz/ncu_python_edm-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Created on 21-06-2020 12:32:28
+"""
+__author__ = "lairuilin"
+__email__ = "kalai850531@g.ncu.edu.tw"
+__status__ = "Development"
+
+import setuptools
+
+setuptools.setup(name='ncu_python_edm',
+      version='0.0.1',
+      description='我測試一下',
+      url='https://github.com/lairuilin/logger_test',
+      author='lairuilin',
+      author_email='kalai850531@g.ncu.edu.tw',
+      license='MIT',
+      packages=['ncu_python_edm'],
+      install_requires=['jupyter'],
+      include_package_data=True
+     )
