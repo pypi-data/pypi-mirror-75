5fe118790670536047fa1e485de7968581de1d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package eoinoms-distributions/0.1/eoinoms_distributions-0.1.tar.gz

---
+++ packages/eoinoms-distributions/eoinoms_distributions-0.1.tar.gz/eoinoms_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='eoinoms_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['eoinoms_distributions'],
+      zip_safe=False)
