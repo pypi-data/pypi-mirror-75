3df34569d63118dbd837b7f1b9c7e1321714cdf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package pygate-grpc/0.0.6/pygate_grpc-0.0.6-py3-none-any.whl

---
+++ packages/pygate-grpc/pygate_grpc-0.0.6-py3-none-any.whl/pygate_grpc/client.py
@@ -0,0 +1,12 @@
+from proto import ffs_rpc_pb2
+from pygate_grpc import health, faults, deals, ffs, wallet, net
+
+
+class PowerGateClient(object):
+    def __init__(self, host_name):
+        self.health = health.HealthClient(host_name)
+        self.faults = faults.FaultsClient(host_name)
+        self.deals = deals.DealsClient(host_name)
+        self.ffs = ffs.FfsClient(host_name)
+        self.wallet = wallet.WalletClient(host_name)
+        self.net = net.NetClient(host_name)
