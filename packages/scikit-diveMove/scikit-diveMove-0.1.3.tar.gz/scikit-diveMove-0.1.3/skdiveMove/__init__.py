a4c78328d3f6fdcf2ea24809154332724a200607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package scikit-diveMove/0.1.3/scikit-diveMove-0.1.3.tar.gz

---
+++ packages/scikit-diveMove/scikit-diveMove-0.1.3.tar.gz/scikit-diveMove-0.1.3/skdiveMove/__init__.py
@@ -0,0 +1,14 @@
+""""Functions for working with diveMove
+
+Copyright (c) 2020 Sebastian Luque <spluque@gmail.com>
+
+"""
+
+
+from skdiveMove.tdr import TDR             # noqa: F401
+from skdiveMove.tdrsource import TDRSource  # noqa: F401
+
+__author__ = "Sebastian Luque <spluque@gmail.com>"
+__license__ = "AGPLv3"
+__version__ = "0.1.3"
+__all__ = ["TDR", "TDRSource"]
