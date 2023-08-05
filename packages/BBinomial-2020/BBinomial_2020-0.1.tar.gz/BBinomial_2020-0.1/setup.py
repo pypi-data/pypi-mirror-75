7e22db0fbf6e529b2f0de1f3c60281d0918f6cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package BBinomial-2020/0.1/BBinomial_2020-0.1.tar.gz

---
+++ packages/BBinomial-2020/BBinomial_2020-0.1.tar.gz/BBinomial_2020-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='BBinomial_2020',
+      version='0.1',
+      description='Gaussian distribution and Binomial Distributions',
+      packages=['BBinomial_2020'],
+      author= 'Biswarup Burman',
+      author_email= 'myemail@workspace.com',
+      zip_safe=False)
