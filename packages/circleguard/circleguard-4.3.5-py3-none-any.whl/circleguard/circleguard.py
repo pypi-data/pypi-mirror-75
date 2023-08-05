36eeab8dd8820c170c1ba4be14105fb49f6df715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:10 +0000

    Add package circleguard/4.3.5/circleguard-4.3.5-py3-none-any.whl

---
+++ packages/circleguard/circleguard-4.3.5-py3-none-any.whl/circleguard/circleguard.py
@@ -0,0 +1,302 @@
+from pathlib import Path
+import sys
+import itertools
+import os
+from os.path import isfile, join
+import logging
+from tempfile import TemporaryDirectory
+from typing import Iterable
+
+from slider import Beatmap, Library
+
+from circleguard.loader import Loader
+from circleguard.comparer import Comparer
+from circleguard.investigator import Investigator
+from circleguard.cacher import Cacher
+from circleguard.exceptions import CircleguardException
+from circleguard.loadable import Check, ReplayMap, ReplayPath, Replay, Map
+from circleguard.enums import RatelimitWeight, Detect
+from circleguard.result import (Result, StealResult, RelaxResult,
+    CorrectionResult, TimewarpResult)
+
+
+class Circleguard:
+    """
+    Circleguard investigates replays for cheats.
+
+    Parameters
+    ----------
+    key: str
+        A valid api key. Can be retrieved from https://osu.ppy.sh/p/api/.
+    db_path: str or :class:`os.PathLike`
+        The path to the database file to read and write cached replays. If the
+        path does not exist, a fresh database will be created there.
+        If `None`, no replays will be cached or loaded from cache.
+    slider_dir: str or :class:`os.PathLike`
+        The path to the directory used by :class:`slider.library.Library` to
+        store beatmaps. If `None`, a temporary directory will be created for
+        :class:`slider.library.Library` and subsequently destroyed when this
+        :class:`~Circleguard` object is garbage collected.
+    loader: :class:`~circleguard.loader.Loader`
+        This loader will be used instead of the base loader if passed.
+        This must be the class itself, *not* an instantiation of it. It will be
+        with two args - a key and a cacher.
+    """
+    DEFAULT_ANGLE = 10
+    DEFAULT_DISTANCE = 8
+    # a healthy balance between speed and accuracy.
+    DEFAULT_CHUNKS = 5
+
+    def __init__(self, key, db_path=None, slider_dir=None, loader=None, cache=True):
+        self.cache = cache
+        self.cacher = None
+        if db_path is not None:
+            # resolve relative paths
+            db_path = Path(db_path).absolute()
+            # they can set cache to False later with
+            # :func:`~.circleguard.set_options` if they want; assume caching is
+            # desired if db path is passed
+            self.cacher = Cacher(self.cache, db_path)
+
+        self.log = logging.getLogger(__name__)
+        # allow for people to pass their own loader implementation/subclass
+        self.loader = Loader(key, cacher=self.cacher) if loader is None else loader(key, self.cacher)
+        if slider_dir is None:
+            # have to keep a reference to it or the folder gets deleted and
+            # can't be walked by Library
+            self.slider_dir = TemporaryDirectory()
+            self.library = None
+        else:
+            self.library = Library(slider_dir)
+
+
+    def run(self, loadables, detect, loadables2=None, max_angle=DEFAULT_ANGLE, \
+        min_distance=DEFAULT_DISTANCE, num_chunks=DEFAULT_CHUNKS) \
+        -> Iterable[Result]:
+        """
+        Investigates loadables for cheats.
+
+        Parameters
+        ----------
+        loadables: list[:class:`~.Loadable`]
+            The loadables to investigate.
+        detect: :class:`~.Detect`
+            What cheats to investigate for.
+        loadables2: list[:class:`~.Loadable`]
+            For :data:`~Detect.STEAL`, compare each loadable in ``loadables``
+            against each loadable in ``loadables2`` for replay stealing,
+            instead of to other loadables in ``loadables``.
+        max_angle: float
+            For :data:`Detect.CORRECTION`, consider only points (a,b,c) where
+            ``∠abc < max_angle``.
+        min_distance: float
+            For :data:`Detect.CORRECTION`, consider only points (a,b,c) where
+            ``|ab| > min_distance`` and ``|bc| > min_distance``.
+        num_chunks: int
+            For :data:`detect.STEAL_CORR`, how many chunks to split the replay
+            into when comparing. Note that runtime increases linearly with the
+            number of chunks.
+
+        Yields
+        ------
+        :class:`~.Result`
+            A result representing an investigation of one or more of the replays
+            in ``loadables``, depending on the ``detect`` passed.
+
+        Notes
+        -----
+        :class:`~.Result`\s are yielded one at a time, as circleguard finishes
+        investigating them. This means that you can process results from
+        :meth:`~.run` without waiting for all of the investigations to finish.
+        """
+
+        c = Check(loadables, self.cache, loadables2=loadables2)
+        self.log.info("Running circleguard with check %r", c)
+
+        c.load(self.loader)
+        # comparer investigations
+        if detect & (Detect.STEAL_SIM | Detect.STEAL_CORR):
+            replays1 = c.all_replays1()
+            replays2 = c.all_replays2()
+            comparer = Comparer(replays1, replays2, detect, num_chunks)
+            yield from comparer.compare()
+
+        # investigator investigations
+        if detect & (Detect.RELAX | Detect.CORRECTION | Detect.TIMEWARP):
+            if detect & Detect.RELAX:
+                if not self.library:
+                    # connect to library since it's a temporary one
+                    library = Library(self.slider_dir.name)
+                else:
+                    library = self.library
+
+            for replay in c.all_replays():
+                bm = None
+                # don't download beatmap unless we need it for relax
+                if detect & Detect.RELAX:
+                    bm = library.lookup_by_id(replay.map_id, download=True, save=True)
+                investigator = Investigator(replay, detect, max_angle, min_distance, beatmap=bm)
+                yield from investigator.investigate()
+
+            if detect & Detect.RELAX:
+                if not self.library:
+                    # disconnect from temporary library
+                    library.close()
+
+    def steal_check(self, loadables, loadables2=None, method=Detect.STEAL_SIM, \
+        num_chunks=DEFAULT_CHUNKS) -> Iterable[StealResult]:
+        """
+        Investigates loadables for replay stealing.
+
+        Parameters
+        ----------
+        loadables: list[:class:`~.Loadable`]
+            The loadables to investigate.
+        loadables2: list[:class:`~.Loadable`]
+            If passed, compare each loadable in ``loadables``
+            against each loadable in ``loadables2`` for replay stealing,
+            instead of to other loadables in ``loadables``.
+        method: :class`~.Detect`
+            What method to use to investigate the loadables for replay stealing.
+            This should be one of ``Detect.STEAL_SIM`` or ``Detect.STEAL_CORR``,
+            or both (or'd together).
+        num_chunks: int
+            How many chunks to split the replay into when comparing. This
+            parameter only has an affect if ``method`` is
+            ``Detect.STEAL_CORR``. Note that runtime increases linearly with the
+            number of chunks.
+
+        Yields
+        ------
+        :class:`~.StealResult`
+            A result representing a replay stealing investigtion into a pair of
+            loadables from ``loadables`` and/or ``loadables2``.
+        """
+        yield from self.run(loadables, method, loadables2, num_chunks=num_chunks)
+
+    def relax_check(self, loadables) -> Iterable[RelaxResult]:
+        """
+        Investigates loadables for relax.
+
+        Parameters
+        ----------
+        loadables: list[:class:`~.Loadable`]
+            The loadables to investigate.
+
+        Yields
+        ------
+        :class:`~.RelaxResult`
+            A result representing a relax investigation into a loadable from
+            ``loadables``.
+        """
+        yield from self.run(loadables, Detect.RELAX)
+
+    def correction_check(self, loadables, max_angle=DEFAULT_ANGLE, \
+        min_distance=DEFAULT_DISTANCE) -> Iterable[CorrectionResult]:
+        """
+        Investigates loadables for aim correction.
+
+        Parameters
+        ----------
+        loadables: list[:class:`~.Loadable`]
+            The loadables to investigate.
+        max_angle: float
+            Consider only points (a,b,c) where ``∠abc < max_angle``.
+        min_distance: float
+            Consider only points (a,b,c) where ``|ab| > min_distance`` and
+            ``|bc| > min_distance``.
+
+        Yields
+        ------
+        :class:`~.CorrectionResult`
+            A result representing an aim correction investigation into a
+            loadable from ``loadables``.
+        """
+        yield from self.run(loadables, Detect.CORRECTION, max_angle=max_angle, \
+                   min_distance=min_distance)
+
+    def timewarp_check(self, loadables) -> Iterable[TimewarpResult]:
+        """
+        Investigates loadables for aim correction.
+
+        Parameters
+        ----------
+        loadables: list[:class:`~.Loadable`]
+            The loadables to investigate.
+
+        Yields
+        ------
+        :class:`~.CorrectionResult`
+            A result representing an aim correction investigation into a
+            loadable from ``loadables``.
+        """
+        yield from self.run(loadables, Detect.TIMEWARP)
+
+    def load(self, loadable):
+        """
+        Loads a loadable.
+
+        Parameters
+        ----------
+        loadable: :class:`~circleguard.loadable.Loadable`
+            The loadable to load.
+
+        Notes
+        -----
+        This is identical to calling ``loadable.load(cg.loader)``.
+        """
+        loadable.load(self.loader, self.cache)
+
+    def load_info(self, loadable_container):
+        """
+        Loads a loadable container.
+
+        Parameters
+        ----------
+        loadable: :class:`~circleguard.loadable.LoadableContainer`
+            The loadable container to load.
+
+        Notes
+        -----
+        This is identical to calling
+        ``loadable_container.load_info(cg.loader)``.
+        """
+        loadable_container.load_info(self.loader)
+
+
+    def set_options(self, cache=None):
+        """
+        Sets options for this instance of circlecore.
+
+        Parameters
+        ----------
+        cache: bool
+            Whether to cache loaded loadables.
+        """
+
+        # remnant code from when we had many options available in set_options.
+        # Left in for easy future expansion
+        for k, v in locals().items():
+            if v is None or k == "self":
+                continue
+            if k == "cache":
+                self.cache = cache
+                self.cacher.should_cache = cache
+                continue
+
+def set_options(loglevel=None):
+    """
+    Set global options for circlecore.
+
+    Parameters
+    ---------
+    logevel: int
+        What level to log at. Circlecore follows standard python logging
+        levels, with an added level of TRACE with a value of 5 (lower than
+        debug, which is 10). The value passed to loglevel is passed directly to
+        the setLevel function of the circleguard root logger. WARNING by
+        default. For more information on log levels, see the standard python
+        logging lib.
+    """
+    if loglevel is not None:
+        logging.getLogger("circleguard").setLevel(loglevel)
---
+++ packages/circleguard/circleguard-4.3.5-py3-none-any.whl/circleguard/investigator.py
@@ -0,0 +1,304 @@
+import math
+
+import numpy as np
+
+from circleguard.enums import Key, Detect
+from circleguard.result import RelaxResult, CorrectionResult, TimewarpResult
+
+class Investigator:
+    """
+    Manages the investigation of individual
+    :class:`~.replay.Replay`\s for cheats.
+
+    Parameters
+    ----------
+    replay: :class:`~.replay.Replay`
+        The replay to investigate.
+    detect: :class:`~.Detect`
+        What cheats to investigate the replay for.
+    beatmap: :class:`slider.beatmap.Beatmap`
+        The beatmap to calculate ur from, with the replay. Should be ``None``
+        if ``Detect.RELAX in detect`` is ``False``.
+
+    See Also
+    --------
+    :class:`~.comparer.Comparer`, for comparing multiple replays.
+    """
+
+    MASK = int(Key.M1) | int(Key.M2)
+
+    def __init__(self, replay, detect, max_angle, min_distance, beatmap=None):
+        self.replay = replay
+        self.detect = detect
+        self.max_angle = max_angle
+        self.min_distance = min_distance
+        self.beatmap = beatmap
+        self.detect = detect
+
+    def investigate(self):
+        replay = self.replay
+        # equivalent of filtering out replays with no replay data from comparer on init
+        if replay.replay_data is None:
+            return
+        if self.detect & Detect.RELAX:
+            ur = self.ur(replay, self.beatmap)
+            yield RelaxResult(replay, ur)
+        if self.detect & Detect.CORRECTION:
+            snaps = self.aim_correction(replay, self.max_angle, self.min_distance)
+            yield CorrectionResult(replay, snaps)
+        if self.detect & Detect.TIMEWARP:
+            frametimes = self.frametimes(replay)
+            frametime = self.median_frametime(frametimes)
+            yield TimewarpResult(replay, frametime, frametimes)
+
+    @staticmethod
+    def ur(replay, beatmap):
+        """
+        Calculates the ur of ``replay`` when played against ``beatmap``.
+
+        Parameters
+        ----------
+        replay: :class:`~.Replay`
+            The replay to calculate the ur of.
+        beatmap: :class:`slider.beatmap.Beatmap`
+            The beatmap to calculate ``replay``'s ur with.
+        """
+
+        hitobjs = Investigator._parse_beatmap(beatmap)
+        keypress_times = Investigator._parse_keypress_times(replay)
+        filtered_array = Investigator._filter_hits(hitobjs, keypress_times, beatmap.overall_difficulty)
+        diff_array = []
+
+        for hitobj_time, press_time in filtered_array:
+            diff_array.append(press_time - hitobj_time)
+        return np.std(diff_array) * 10
+
+    @staticmethod
+    def aim_correction(replay, max_angle, min_distance):
+        """
+        Calculates the angle between each set of three points (a,b,c) and finds
+        points where this angle is extremely acute and neither ``|ab|`` or
+        ``|bc|`` are small.
+
+        Parameters
+        ----------
+        replay: :class:`~.Replay`
+            The replay to investigate for aim correction.
+        max_angle: float
+            Consider only (a,b,c) where ``∠abc < max_angle``
+        min_distance: float
+            Consider only (a,b,c) where ``|ab| > min_distance`` and
+            ``|ab| > min_distance``.
+
+        Returns
+        -------
+        list[:class:`~.Snap`]
+            Hits where the angle was less than ``max_angle`` and the distance
+            was more than ``min_distance``.
+
+        Notes
+        -----
+        This does not detect correction where multiple datapoints are placed
+        at the correction site (which creates a small ``min_distance``).
+
+        Another possible method is to look at the ratio between the angle
+        and distance.
+
+        See Also
+        --------
+        :meth:`~.aim_correction_sam` for an alternative, unused approach
+        involving velocity and jerk.
+        """
+        # when we leave mutliple frames with the same time values, they
+        # sometimes get detected (falesly) as aim correction.
+        # TODO Worth looking into a bit more to see if we can avoid it without
+        # removing the frames entirely.
+        t, xy = Investigator.remove_unique(replay.t, replay.xy)
+        t = t[1:-1]
+
+        # labelling three consecutive points a, b and c
+        ab = xy[1:-1] - xy[:-2]
+        bc = xy[2:] - xy[1:-1]
+        ac = xy[2:] - xy[:-2]
+        # Distance a to b, b to c, and a to c
+        AB = np.linalg.norm(ab, axis=1)
+        BC = np.linalg.norm(bc, axis=1)
+        AC = np.linalg.norm(ac, axis=1)
+        # Law of cosines, solve for beta
+        # AC^2 = AB^2 + BC^2 - 2 * AB * BC * cos(beta)
+        # cos(beta) = -(AC^2 - AB^2 - BC^2) / (2 * AB * BC)
+        num = -(AC ** 2 - AB ** 2 - BC ** 2)
+        denom = (2 * AB * BC)
+        # use true_divide for handling division by zero
+        cos_beta = np.true_divide(num, denom, out=np.full_like(num, np.nan), where=denom!=0)
+        # rounding issues makes cos_beta go out of arccos' domain, so restrict it
+        cos_beta = np.clip(cos_beta, -1, 1)
+
+        beta = np.rad2deg(np.arccos(cos_beta))
+
+        min_AB_BC = np.minimum(AB, BC)
+        dist_mask = min_AB_BC > min_distance
+        # use less to avoid comparing to nan
+        angle_mask = np.less(beta, max_angle, where=~np.isnan(beta))
+        # boolean array of datapoints where both distance and angle requirements are met
+        mask = dist_mask & angle_mask
+
+        return [Snap(t, b, d) for (t, b, d) in zip(t[mask], beta[mask], min_AB_BC[mask])]
+
+    @staticmethod
+    def aim_correction_sam(replay_data, num_jerks, min_jerk):
+        """
+        Calculates the jerk at each moment in the Replay, counts the number of times
+        it exceeds min_jerk and reports a positive if that number is over num_jerks.
+        Also reports all suspicious jerks and their timestamps.
+
+        WARNING
+        -------
+        Unused function. Kept for historical purposes and ease of viewing in
+        case we want to switch to this track of aim correction in the future,
+        or provide it as an alternative.
+        """
+
+        # get all replay data as an array of type [(t, x, y, k)]
+        txyk = np.array(replay_data)
+
+        # drop keypresses
+        txy = txyk[:, :3]
+
+        # separate time and space
+        t = txy[:, 0]
+        xy = txy[:, 1:]
+
+        # j_x = (d/dt)^3 x
+        # calculated as (d/dT dT/dt)^3 x = (dT/dt)^3 (d/dT)^3 x
+        # (d/dT)^3 x = d(d(dx/dT)/dT)/dT
+        # (dT/dt)^3 = 1/(dt/dT)^3
+        dtdT = np.diff(t)
+        d3xy = np.diff(xy, axis=0, n=3)
+        # safely calculate the division and replace with zero if the divisor is zero
+        # dtdT is sliced with 2: because differentiating drops one element for each order (slice (n - 1,) to (n - 3,))
+        # d3xy is of shape (n - 3, 2) so dtdT is also reshaped from (n - 3,) to (n - 3, 1) to align the axes.
+        jerk = np.divide(d3xy, dtdT[2:, None] ** 3, out=np.zeros_like(d3xy), where=dtdT[2:,None]!=0)
+
+        # take the absolute value of the jerk
+        jerk = np.linalg.norm(jerk, axis=1)
+
+        # create a mask of where the jerk reaches suspicious values
+        anomalous = jerk > min_jerk
+        # and retrieve and store the timestamps and the values themself
+        timestamps = t[3:][anomalous]
+        values = jerk[anomalous]
+        # reshape to an array of type [(t, j)]
+        jerks = np.vstack((timestamps, values)).T
+
+        # count the anomalies
+        ischeat = anomalous.sum() > num_jerks
+
+        return [jerks, ischeat]
+
+    @staticmethod
+    def frametimes(replay):
+        """
+        Returns the time between each pair of consecutive frames in ``replay``.
+
+        Parameters
+        ----------
+        replay: :class:`~.Replay`
+            The replay to get the frametimes of.
+        """
+        # replay.t is cumsum so convert it back to "time since previous frame"
+        return np.diff(replay.t)
+
+    @staticmethod
+    def median_frametime(frametimes):
+        """
+        Calculates the median time between the frames in ``frametimes``.
+
+        Parameters
+        ----------
+        frametimes: list[int]
+            The frametimes to find the median of.
+
+        Notes
+        -----
+        Median is used instead of mean to lessen the effect of outliers.
+        """
+        return np.median(frametimes)
+
+    @staticmethod
+    def _parse_beatmap(beatmap):
+        hitobjs = []
+
+        # parse hitobj
+        for hit in beatmap.hit_objects_no_spinners:
+            p = hit.position
+            hitobjs.append([hit.time.total_seconds() * 1000, p.x, p.y])
+        return hitobjs
+
+    @staticmethod
+    def _parse_keypress_times(replay):
+        keypresses = replay.k & Investigator.MASK
+        changes = keypresses & ~np.insert(keypresses[:-1], 0, 0)
+        return replay.t[changes != 0]
+
+    @staticmethod
+    def _filter_hits(hitobjs, keypress_times, OD):
+        array = []
+        hitwindow = 150 + 50 * (5 - OD) / 5
+
+        object_i = 0
+        press_i = 0
+
+        while object_i < len(hitobjs) and press_i < len(keypress_times):
+            hitobj_time = hitobjs[object_i][0]
+            press_time = keypress_times[press_i]
+
+            if press_time < hitobj_time - hitwindow / 2:
+                press_i += 1
+            elif press_time > hitobj_time + hitwindow / 2:
+                object_i += 1
+            else:
+                array.append([hitobj_time, press_time])
+                press_i += 1
+                object_i += 1
+
+        return array
+
+    # TODO (some) code duplication with this method and a similar one in
+    # ``Comparer``. Refactor Investigator and Comparer to inherit from a base
+    # class, or move this method to utils. Preferrably the former.
+    @staticmethod
+    def remove_unique(t, k):
+        t, t_sort = np.unique(t, return_index=True)
+        k = k[t_sort]
+        return (t, k)
+
+class Snap():
+    """
+    A suspicious hit in a replay, specifically so because it snaps away from
+    the otherwise normal path. Snaps currently represent the middle datapoint
+    in a set of three replay datapoints.
+
+    Parameters
+    ----------
+    time: int
+        The time value of the middle datapoint, in ms. 0 represents the
+        beginning of the replay.
+    angle: float
+        The angle between the three datapoints.
+    distance: float
+        ``min(dist_a_b, dist_b_c)`` if ``a``, ``b``, and ``c`` are three
+        datapoints with ``b`` being the middle one.
+
+    See Also
+    --------
+    :meth:`~.Investigator.aim_correction`
+    """
+    def __init__(self, time, angle, distance):
+        self.time = time
+        self.angle = angle
+        self.distance = distance
+
+    def __eq__(self, other):
+        return (self.time == other.time and self.angle == other.angle
+                and self.distance == other.distance)
