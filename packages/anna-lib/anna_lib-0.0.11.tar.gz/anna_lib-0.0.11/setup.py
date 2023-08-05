88219d7540f40cbb2a27a4cb3cace79e4b37f9a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:52 +0000

    Add package anna-lib/0.0.11/anna_lib-0.0.11.tar.gz

---
+++ packages/anna-lib/anna_lib-0.0.11.tar.gz/anna_lib-0.0.11/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+	description = f.read()
+
+setuptools.setup(
+	name='anna_lib',
+	version='0.0.11',
+	author='Patrik Pihlstrom',
+	author_email='patrik.pihlstrom@gmail.com',
+	url='https://github.com/patrikpihlstrom/anna-lib',
+	description='selenium interface',
+	long_description=description,
+	long_description_content_type='text/markdown',
+	packages=['anna_lib', 'anna_lib.task', 'anna_lib.selenium'],
+	install_requires=[
+		'selenium'
+	]
+)
