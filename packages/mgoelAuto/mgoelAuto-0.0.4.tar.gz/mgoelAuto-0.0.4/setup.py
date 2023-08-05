fa0446b8f9ec7d964f1dfb031b4612dd07ec6139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package mgoelAuto/0.0.4/mgoelAuto-0.0.4.tar.gz

---
+++ packages/mgoelAuto/mgoelAuto-0.0.4.tar.gz/mgoelAuto-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+             'Development Status :: 5 - Production/Stable',
+             'Intended Audience :: Education',
+             'Operating System :: Microsoft :: Windows :: Windows 10',
+             'Programming Language :: Python :: 3'
+            ]
+
+setup(
+            name='mgoelAuto',
+            version= '0.0.4',
+            description='basic',
+            Long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+            url='',
+            author='application',
+            license='ineuron-ip',
+            classifiers=classifiers,
+            packages=['application', 'application.dataLoad', 'application.modelTraining'],
+            install_requires=['click', 'six', 'numpy', 'future'],
+            setup_requires=['pytest-runner'],
+            # pytest-cov needed for coverage only
+            tests_require=['pytest', 'pytest-cov'],
+            zip_safe=True
+)
