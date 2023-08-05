52cb5b21493c36c9169bb5c58293e4ce9dca1a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package av-distributions/0.0.1/av_distributions-0.0.1.tar.gz

---
+++ packages/av-distributions/av_distributions-0.0.1.tar.gz/av_distributions-0.0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='av_distributions',
+      version='0.0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['av_distributions'],
+      zip_safe=False)
