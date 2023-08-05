4889091becf510fe89de671648f5b50663b856e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:23 +0000

    Add package okp/0.0.37/okp-0.0.37.tar.gz

---
+++ packages/okp/okp-0.0.37.tar.gz/okp-0.0.37/okp/pipeline.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+from .transforms import comments, io, keywords, structure, variables
+from . import analysis
+from . import util
+
+import os
+
+def pipeline(lines, base_dir=None):
+    lines = comments.skip_comments(lines)
+    lines = structure.join_backslash_lines(lines)
+    lines = structure.join_open_bracketed_lines(lines)
+    lines = structure.join_percent_bracketed_lines(lines)
+    lines = structure.add_preceding_ignore_chars(lines)
+
+    lines = keywords.replace_raw(lines, base_dir or os.getcwd())
+    lines = keywords.replace_tabs(lines)
+    lines = keywords.replace_pass(lines)
+    lines = keywords.replace_for_shorthand(lines)
+    lines = keywords.replace_blocks(lines)
+    lines = keywords.replace_defs(lines)
+    lines = keywords.replace_self(lines)
+
+    # replaces !, ?, ??, print, read, etc
+    lines = io.replace_io_keywords(lines)
+
+    # scopings is a per line scope of seen variables
+    scopings = analysis.read_scopings(lines)
+    lines = variables.add_auto_declarations(lines, scopings)
+    lines = variables.add_destructuring(lines, scopings)
+
+    # known keyword replacement has to happen after auto declarations
+    lines = keywords.replace_knowns(lines)
+
+    lines = structure.add_parentheses(lines)
+    lines = structure.add_trailing_semicolons(lines)
+
+    # add curly braces (from indentation) has to be last
+    lines = structure.add_curly_braces(lines)
+
+    # TODO: decide on whether to remove from generated code or not
+    lines = structure.remove_preceding_ignore_chars(lines)
+
+    requires = analysis.guess_required_files(lines)
+
+    lines = requires + lines
+    return lines
