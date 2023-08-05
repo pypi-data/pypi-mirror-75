92f921019c0a074fa1a4d0091314742c4677e976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:01 +0000

    Add package arcor2/0.8.0b1.dev734/arcor2-0.8.0b1.dev734-py3-none-any.whl

---
+++ packages/arcor2/arcor2-0.8.0b1.dev734-py3-none-any.whl/arcor2/data/robot.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+from dataclasses_jsonschema import JsonSchemaMixin
+
+
+@dataclass
+class RobotFeatures(JsonSchemaMixin):
+
+    focus: bool = False
+    move_to_pose: bool = False
+    move_to_joints: bool = False
+    stop: bool = False
+
+
+@dataclass
+class RobotMeta(JsonSchemaMixin):
+    """
+    Robot meta that could be extracted without creating an instance.
+    """
+
+    type: str
+    features: RobotFeatures = field(default_factory=RobotFeatures)
+    urdf_package_filename: Optional[str] = None
