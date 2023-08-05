54da849fd6e6281ec530377f79dff48a3569a232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:27 +0000

    Add package joaye-probability/1.0/joaye_probability-1.0.tar.gz

---
+++ packages/joaye-probability/joaye_probability-1.0.tar.gz/joaye_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='joaye_probability',
+      version='1.0',
+      description='Gaussian and Binomial Distribution',
+      packages=['joaye_probability'],
+      author_email="joeypewpew@gmail.com",
+      zip_safe=False)
