887be34252b79b8c12cc6b52cbac11510e444cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:02 +0000

    Add package iot-manager/0.0.29/iot-manager-0.0.29.tar.gz

---
+++ packages/iot-manager/iot-manager-0.0.29.tar.gz/iot-manager-0.0.29/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="iot-manager",
+    version="0.0.29",
+    packages=["iot_manager"],
+    author="Dylan Crockett",
+    author_email="dylanrcrockett@gmail.com",
+    license="MIT",
+    description="A management API for connecting and managing Clients via TCP Sockets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dylancrockett/iot-manager",
+    project_urls={
+        "Documentation": "https://iotmanager.readthedocs.io/",
+        "Source Code": "https://github.com/dylancrockett/iot-manager"
+    },
+    install_requirements=[
+        "gevent"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6'
+)
