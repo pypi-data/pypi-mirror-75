43c561a63dbc0635e28e5d7d5e5b50ce9cc2fc1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:33 +0000

    Add package redditimage/0.0.15/redditimage-0.0.15.tar.gz

---
+++ packages/redditimage/redditimage-0.0.15.tar.gz/redditimage-0.0.15/setup.py
@@ -0,0 +1,26 @@
+import setuptools, os
+
+readme_path = os.path.join(os.getcwd(), "README.md")
+if os.path.exists(readme_path):
+    with open(readme_path, "r") as f:
+        long_description = f.read()
+else:
+    long_description = 'redditimage'
+
+setuptools.setup(
+    name="redditimage",
+    version="0.0.15",
+    author="Kristof",
+    description="redditimage",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/py_reddit_image",
+    packages=setuptools.find_packages(),
+    install_requires=["colorthief", "Pillow"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
