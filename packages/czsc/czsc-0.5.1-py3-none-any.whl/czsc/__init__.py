cb37b7606e3cbe2f95c40ddb97e2a29e3ace8a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:10 +0000

    Add package czsc/0.5.1/czsc-0.5.1-py3-none-any.whl

---
+++ packages/czsc/czsc-0.5.1-py3-none-any.whl/czsc/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from .ta import ma, macd, boll
+from .utils import plot_kline
+from .analyze import KlineAnalyze, is_bei_chi, find_zs
+
+__version__ = "0.5.1"
+__author__ = "zengbin93"
+__email__ = "zeng_bin8888@163.com"
+
+
---
+++ packages/czsc/czsc-0.5.1-py3-none-any.whl/czsc/ta.py
@@ -0,0 +1,91 @@
+# coding: utf-8
+"""
+
+常用技术分析指标：MA, MACD, BOLL
+"""
+import warnings
+try:
+    import talib as ta
+except:
+    warnings.warn("ta-lib 没有安装成功，请到 https://www.lfd.uci.edu/~gohlke/pythonlibs/#ta-lib 下载对应版本安装")
+
+# def ma(kline, params=(5, 10, 20, 60, 120, 250)):
+#     """计算指定周期的若干 MA 均线
+#
+#     :param kline: pd.DataFrame
+#         K线，columns = ["symbol", "dt", "open", "close", "high", "low", "vol"]
+#     :param params: tuple
+#     :return: pd.DataFrame
+#         在原始数据中新增若干 MA 均线
+#     """
+#     for p in params:
+#         col = "ma" + str(p)
+#         kline.loc[:, col] = kline['close'].rolling(p).mean().apply(round, args=(2,))
+#     return kline
+
+def ma(kline, params=(5, 10, 20, 60, 120, 250)):
+    """计算指定周期的若干 MA 均线
+
+    :param kline: pd.DataFrame
+        K线，columns = ["symbol", "dt", "open", "close", "high", "low", "vol"]
+    :param params: tuple
+    :return: pd.DataFrame
+        在原始数据中新增若干 MA 均线
+    """
+    for p in params:
+        col = "ma" + str(p)
+        kline.loc[:, col] = ta.MA(kline['close'], timeperiod=p, matype=ta.MA_Type.SMA)
+        # kline.loc[:, col] = kline[col].apply(round, args=(2,))
+    return kline
+
+# def macd(kline):
+#     """计算 MACD 指标
+#
+#     :param kline: pd.DataFrame
+#         K线，columns = ["symbol", "dt", "open", "close", "high", "low", "vol"]
+#     :return: pd.DataFrame
+#         在原始数据中新增 diff,dea,macd 三列
+#     """
+#
+#     short_, long_, m = 12, 26, 9
+#     kline.loc[:, 'diff'] = kline['close'].ewm(adjust=False, alpha=2 / (short_ + 1), ignore_na=True).mean() - \
+#                             kline['close'].ewm(adjust=False, alpha=2 / (long_ + 1), ignore_na=True).mean()
+#     kline.loc[:, 'dea'] = kline['diff'].ewm(adjust=False, alpha=2 / (m + 1), ignore_na=True).mean()
+#     kline.loc[:, 'macd'] = 2 * (kline['diff'] - kline['dea'])
+#
+#     for col in ['diff', 'dea', 'macd']:
+#         kline.loc[:, col] = kline[col].apply(round, args=(2,))
+#     return kline
+
+def macd(kline):
+    """计算 MACD 指标
+
+    :param kline: pd.DataFrame
+        K线，columns = ["symbol", "dt", "open", "close", "high", "low", "vol"]
+    :return: pd.DataFrame
+        在原始数据中新增 diff,dea,macd 三列
+    """
+    m1, m2, m3 = ta.MACD(kline.close.values, fastperiod=12, slowperiod=26, signalperiod=9)
+    kline.loc[:, 'diff'] = m1
+    kline.loc[:, 'dea'] = m2
+    kline.loc[:, 'macd'] = m3
+    # for col in ['diff', 'dea', 'macd']:
+    #     kline.loc[:, col] = kline[col].apply(round, args=(2,))
+    return kline
+
+def boll(kline):
+    """计算 BOLL 指标
+
+    :param kline: pd.DataFrame
+        K线，columns = ["symbol", "dt", "open", "close", "high", "low", "vol"]
+    :return: pd.DataFrame
+        在原始数据中新增 BOLL 指标结果
+    """
+    kline.loc[:, 'boll-mid'] = kline['close'].rolling(26).mean()
+    kline.loc[:, 'boll-tmp2'] = kline['close'].rolling(20).std()
+    kline.loc[:, 'boll-top'] = kline['boll-mid'] + 2 * kline['boll-tmp2']
+    kline.loc[:, 'boll-bottom'] = kline['boll-mid'] - 2 * kline['boll-tmp2']
+
+    # for col in ['boll-mid', 'boll-top', 'boll-bottom']:
+    #     kline.loc[:, col] = kline[col].apply(round, args=(2,))
+    return kline
