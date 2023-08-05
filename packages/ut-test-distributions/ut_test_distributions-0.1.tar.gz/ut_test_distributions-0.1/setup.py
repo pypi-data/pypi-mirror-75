14974a7e356d0f8c5fc211aca4fc368082894a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:49 +0000

    Add package ut-test-distributions/0.1/ut_test_distributions-0.1.tar.gz

---
+++ packages/ut-test-distributions/ut_test_distributions-0.1.tar.gz/ut_test_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ut_test_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ut_test_distributions'],
+      author='Usama Tariq',
+      author_email='tariqusama95@gmail.com',
+      zip_safe=False)
