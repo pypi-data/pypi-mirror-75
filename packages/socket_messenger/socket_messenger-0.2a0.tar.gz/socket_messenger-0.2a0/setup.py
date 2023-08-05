ffc4b1b4950e1a64378a67ef0472cc0be7ce4ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package socket_messenger/0.2a0/socket_messenger-0.2a0.tar.gz

---
+++ packages/socket_messenger/socket_messenger-0.2a0.tar.gz/socket_messenger-0.2a0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="socket_messenger",
+    version="v0.2-alpha",
+    author="Vineet Kulkarni",
+    author_email="2vineetk@gmail.com",
+    description="Makes communication between devices using pubnub through Python easy",
+    packages=["socket_messenger"],
+    include_package_data=True,
+    url = "https://github.com/vink246/socket_messenger.git",
+    download_url = "https://github.com/vink246/socket_messenger/archive/0.2-alpha.tar.gz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.5',
+    install_requires= ["pubnub"]
+)
