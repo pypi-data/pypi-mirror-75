513bbb53f148abef10cfe08171ba93b54b83042b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:46 +0000

    Add package distributions8/0.1/distributions8-0.1.tar.gz

---
+++ packages/distributions8/distributions8-0.1.tar.gz/distributions8-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions8',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions8'],
+      zip_safe=False)
