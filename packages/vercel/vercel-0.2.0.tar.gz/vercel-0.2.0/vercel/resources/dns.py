4d15aa1f1484ddd4ecea72052b176c387ca04e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:26 +0000

    Add package vercel/0.2.0/vercel-0.2.0.tar.gz

---
+++ packages/vercel/vercel-0.2.0.tar.gz/vercel-0.2.0/vercel/resources/dns.py
@@ -0,0 +1,51 @@
+from vercel.resources.base import Resource
+
+
+class DnsRecord(Resource):
+    def __init__(self, id, domain_name):
+        self.id = id
+        self.domain_name = domain_name
+
+    @classmethod
+    def get(cls, domain_name, record_id):
+        return cls(id=record_id, domain_name=domain_name)
+
+    @classmethod
+    def create(
+        cls,
+        domain_name,
+        name,
+        type,
+        value,
+        ttl=None,
+        api_version="v2",
+        api_key=None,
+        team_id=None,
+    ):
+        data = {"name": name, "type": type, "value": value}
+
+        if ttl is not None:
+            data["ttl"] = ttl
+
+        res = cls.make_request(
+            method="POST",
+            resource=f"/domains/{domain_name}/records",
+            data=data,
+            api_version=api_version,
+            api_key=api_key,
+            team_id=team_id,
+        )
+
+        return cls(id=res["uid"], domain_name=domain_name)
+
+    @classmethod
+    def delete(
+        cls, domain_name, record_id, api_version="v2", api_key=None, team_id=None
+    ):
+        return cls.make_request(
+            method="DELETE",
+            resource=f"/domains/{domain_name}/records/{record_id}",
+            api_version=api_version,
+            api_key=api_key,
+            team_id=team_id,
+        )
