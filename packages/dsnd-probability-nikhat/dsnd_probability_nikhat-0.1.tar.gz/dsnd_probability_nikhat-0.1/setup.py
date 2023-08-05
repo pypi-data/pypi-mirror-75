b7203c9f39bd41467e69ee0f5e7214d7c3b552ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:13 +0000

    Add package dsnd-probability-nikhat/0.1/dsnd_probability_nikhat-0.1.tar.gz

---
+++ packages/dsnd-probability-nikhat/dsnd_probability_nikhat-0.1.tar.gz/dsnd_probability_nikhat-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_nikhat',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_nikhat'],
+      zip_safe=False)
