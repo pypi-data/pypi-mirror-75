f41af742e1812f48e1165e483ebcffd933c74e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:38 +0000

    Add package matvarg-distributions/1.1/matvarg_distributions-1.1.tar.gz

---
+++ packages/matvarg-distributions/matvarg_distributions-1.1.tar.gz/matvarg_distributions-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='matvarg_distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['matvarg_distributions'],
+      zip_safe=False)
