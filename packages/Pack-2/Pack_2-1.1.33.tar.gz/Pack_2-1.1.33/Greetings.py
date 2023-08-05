4f31111a7b42cc304e5ead8dc45f5ac514daf14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:43 +0000

    Add package Pack-2/1.1.33/Pack_2-1.1.33.tar.gz

---
+++ packages/Pack-2/Pack_2-1.1.33.tar.gz/Pack_2-1.1.33/Greetings.py
@@ -0,0 +1,12 @@
+
+class Hello:
+    def do(self):
+        print("Hello mother fucker")
+
+class Rollo:
+    def do(self):
+        print("rolling bitch")
+
+class Chum:
+    def do(self):
+        print("hey chum")
---
+++ packages/Pack-2/Pack_2-1.1.33.tar.gz/Pack_2-1.1.33/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='Pack_2',
+    scripts=['Greetings.py'],
+    version='1.1.33'
+)
