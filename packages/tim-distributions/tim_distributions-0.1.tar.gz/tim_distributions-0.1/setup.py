f9d93bc367c97c62c9705de8174fac2486f08a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:06 +0000

    Add package tim-distributions/0.1/tim_distributions-0.1.tar.gz

---
+++ packages/tim-distributions/tim_distributions-0.1.tar.gz/tim_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tim_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['tim_distributions'],
+      author_email = 'clicklekan@gmail.com' ,
+      zip_safe=False)
