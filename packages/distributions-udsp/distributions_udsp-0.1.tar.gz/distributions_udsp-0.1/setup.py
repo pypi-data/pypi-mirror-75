7aab7a1d304873f8d650bed1d54bf9948c85ecde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:08 +0000

    Add package distributions-udsp/0.1/distributions_udsp-0.1.tar.gz

---
+++ packages/distributions-udsp/distributions_udsp-0.1.tar.gz/distributions_udsp-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_udsp',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udsp'],
+      zip_safe=False)
