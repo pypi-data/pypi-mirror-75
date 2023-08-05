a1f5e22408da82460b1f021301c2fd53dfef3156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package anmolpant-dist-package/0.1/anmolpant_dist_package-0.1.tar.gz

---
+++ packages/anmolpant-dist-package/anmolpant_dist_package-0.1.tar.gz/anmolpant_dist_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='anmolpant_dist_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['anmolpant_dist_package'],
+      zip_safe=False)
