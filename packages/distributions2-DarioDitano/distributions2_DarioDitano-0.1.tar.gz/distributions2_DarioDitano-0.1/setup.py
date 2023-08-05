d8001650f2b5eb0c18b25f97eda8a42dacef8d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:15 +0000

    Add package distributions2-DarioDitano/0.1/distributions2_DarioDitano-0.1.tar.gz

---
+++ packages/distributions2-DarioDitano/distributions2_DarioDitano-0.1.tar.gz/distributions2_DarioDitano-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions2_DarioDitano',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions2_DarioDitano'],
+      zip_safe=False)
