10e3c6cf235800cb460332db02a44f511a3ae097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package pyhdtoolkit/0.4.0/pyhdtoolkit-0.4.0-py3-none-any.whl

---
+++ packages/pyhdtoolkit/pyhdtoolkit-0.4.0-py3-none-any.whl/pyhdtoolkit/cpymadtools/__init__.py
@@ -0,0 +1,16 @@
+"""
+cpymadtools package
+~~~~~~~~~~~~~~~~~~~
+cpymadtools is a collection of utilities that integrate within my workflow with the `cpymad` library.
+
+:copyright: (c) 2019 by Felix Soubelet.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .helpers import LatticeMatcher, Parameters
+from .lattice_generators import LatticeGenerator
+from .latwiss import LaTwiss
+from .plotters import AperturePlotter, DynamicAperturePlotter, PhaseSpacePlotter, TuneDiagramPlotter
+
+# Importing * is a bad practice and you should be punished for using it
+__all__ = []
