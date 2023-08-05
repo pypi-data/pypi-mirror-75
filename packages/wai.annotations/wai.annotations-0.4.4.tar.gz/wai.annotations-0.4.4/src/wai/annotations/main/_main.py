70721802ab854fbf63405d618eb5b0d3d642a086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:04 +0000

    Add package wai.annotations/0.4.4/wai.annotations-0.4.4.tar.gz

---
+++ packages/wai.annotations/wai.annotations-0.4.4.tar.gz/wai.annotations-0.4.4/src/wai/annotations/main/_main.py
@@ -0,0 +1,71 @@
+"""
+Module containing the main entry point functions for converting annotations.
+"""
+import traceback
+from typing import Optional
+
+from wai.common.cli import OptionsList
+from wai.common.logging import create_standard_application_root_logger, DEBUG_HANDLER_NAME
+
+from ..core.chain import ConversionChain
+from ..core.debug import set_debug
+from ._list_plugins import list_plugins
+from ._MainSettings import MainSettings
+
+
+def main(options: Optional[OptionsList] = None):
+    """
+    Main function of the annotations converter.
+
+    :param options:    The CLI arguments to the program.
+    """
+    # Setup logging
+    logger = create_standard_application_root_logger()
+    for handler in logger.handlers:
+        if handler.name == DEBUG_HANDLER_NAME:
+            handler.addFilter(lambda record: record.name != 'PIL.PngImagePlugin')
+
+    # Get the command-line arguments if none are specified directly
+    if options is None:
+        import sys
+        options = sys.argv[1:]
+
+    # Pre-parse the command-line arguments
+    global_options, stage_options = ConversionChain.split_global_options(options)
+
+    # Consume global options
+    main_settings = MainSettings(global_options)
+    logger.setLevel(main_settings.VERBOSITY)
+    set_debug(main_settings.DEBUG)
+
+    # If requested to list the plugins, do so and exit
+    if main_settings.LIST_PLUGINS:
+        print(list_plugins())
+        return
+
+    # Create the conversion chain
+    conversion_chain = ConversionChain.from_options(stage_options)
+
+    # Log the to/from formats we are converting
+    # TODO: Reinstate
+    # logger.info(f"Converting from {input_format} to {output_format}")
+
+    # Execute the chain
+    conversion_chain.save()
+
+    logger.info("Finished conversion")
+
+
+def sys_main() -> int:
+    """
+    Runs the main function using the system cli arguments, and
+    returns a system error code.
+
+    :return:    0 for success, 1 for failure.
+    """
+    try:
+        main()
+        return 0
+    except Exception:
+        print(traceback.format_exc())
+        return 1
