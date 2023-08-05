0a200e9f9b95175654cc110777fd381a4ba604f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:41 +0000

    Add package pyhdtoolkit/0.4.1/pyhdtoolkit-0.4.1.tar.gz

---
+++ packages/pyhdtoolkit/pyhdtoolkit-0.4.1.tar.gz/pyhdtoolkit-0.4.1/pyhdtoolkit/plotting/__init__.py
@@ -0,0 +1,14 @@
+"""
+plotting package
+~~~~~~~~~~~~~~~~~~~
+These are miscellaneous utilities to integrate to my plots.
+
+:copyright: (c) 2019 by Felix Soubelet.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .helpers import AnnotationsPlotter
+from .settings import PLOT_PARAMS
+
+# Importing * is a bad practice and you should be punished for using it
+__all__ = []
