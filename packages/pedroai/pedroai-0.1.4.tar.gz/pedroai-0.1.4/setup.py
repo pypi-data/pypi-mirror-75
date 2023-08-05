598c3450b783a121e0f09ef25b0adf25eb03360e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package pedroai/0.1.4/pedroai-0.1.4.tar.gz

---
+++ packages/pedroai/pedroai-0.1.4.tar.gz/pedroai-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pedroai']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['plotnine>=0.6,<0.7', 'requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pedroai',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'Pedro Rodriguez',
+    'author_email': 'me@pedro.ai',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
