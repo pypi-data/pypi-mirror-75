c9d17ba8697f52effdc67036ab669b5bd15eb58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package pydistributions/1.0.0/pydistributions-1.0.0.tar.gz

---
+++ packages/pydistributions/pydistributions-1.0.0.tar.gz/pydistributions-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pydistributions',
+      version='1.0.0',
+      description='Gaussian and Binomial Distributions',
+      packages=['pydistributions'],
+      author='Jay Satija',
+      author_email='jaysatija712@gmail.com',
+      zip_safe=False)
