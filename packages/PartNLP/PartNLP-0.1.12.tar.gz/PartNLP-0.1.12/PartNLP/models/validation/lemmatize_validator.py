f01dc2e5f5ac3ac3a15b5eb44eee9f68d0e66bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package PartNLP/0.1.12/PartNLP-0.1.12.tar.gz

---
+++ packages/PartNLP/PartNLP-0.1.12.tar.gz/PartNLP-0.1.12/PartNLP/models/validation/lemmatize_validator.py
@@ -0,0 +1,20 @@
+"""
+        SEMANTIC SEARCH ENGINE
+            AUTHORS:
+                MOSTAFA & SAMAN
+"""
+from PartNLP.models.validation.validator import Validator
+
+
+class LemmatizeValidator(Validator):
+    def __init__(self, config):
+        self.config = config
+
+    def isvalid(self):
+        """
+        :return:
+        """
+        return True, '', None
+
+    def get_dependencies(self):
+        return ['S_TOKENIZE', 'W_TOKENIZE', 'POS']
