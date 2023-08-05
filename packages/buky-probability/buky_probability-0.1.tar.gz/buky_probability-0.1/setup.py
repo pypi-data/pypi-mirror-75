8cc528a79585b2ca5626417252a12a30db93c2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:47 +0000

    Add package buky-probability/0.1/buky_probability-0.1.tar.gz

---
+++ packages/buky-probability/buky_probability-0.1.tar.gz/buky_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='buky_probability',
+      version='0.1',
+      description = 'Gaussian and Binomial distribution',
+      packages = ['buky_probability'],
+      author = 'Abubeker',
+      author_email = 'abukie969@gmail.com',
+      zip_safe=False)
