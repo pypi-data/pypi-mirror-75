e9ba2406ecfdba0c4c0fb72e53c0e33d8c3ef779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:34 +0000

    Add package arcsecond/0.9.8/arcsecond-0.9.8.tar.gz

---
+++ packages/arcsecond/arcsecond-0.9.8.tar.gz/arcsecond-0.9.8/arcsecond/__init__.py
@@ -0,0 +1,10 @@
+from .api import Arcsecond, ArcsecondError, ArcsecondConnectionError, ArcsecondInvalidEndpointError
+
+name = 'arcsecond'
+
+__all__ = ["Arcsecond",
+           "ArcsecondError",
+           "ArcsecondConnectionError",
+           "ArcsecondInvalidEndpointError"]
+
+__version__ = '0.9.8'
