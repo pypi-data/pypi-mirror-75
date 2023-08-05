fd5271468ea6ce46e4c913d0054843bec6ca0781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:47 +0000

    Add package distributions-pypi/1.5/distributions_pypi-1.5.tar.gz

---
+++ packages/distributions-pypi/distributions_pypi-1.5.tar.gz/distributions_pypi-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_pypi',
+      version='1.5',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_pypi'],
+      author = 'Myron Valenzuela',
+      author_email = 'myron@kupwise.com',
+      zip_safe=False)
