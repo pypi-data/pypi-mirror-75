0ad243176ca2c5190f1d6dce9fb133422741689f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:08 +0000

    Add package sym-cli/0.0.12/sym-cli-0.0.12.tar.gz

---
+++ packages/sym-cli/sym-cli-0.0.12.tar.gz/sym-cli-0.0.12/sym/cli/tests/providers/test_aws_okta.py
@@ -0,0 +1,27 @@
+import pytest
+
+from sym.cli.providers.aws_okta import AwsOkta
+from sym.cli.tests.helpers.capture import CaptureCommand
+from sym.cli.tests.providers.conftest import TestContextFixture
+
+
+@pytest.mark.parametrize(argnames=["constructor"], argvalues=[[AwsOkta]])
+def test_aws_okta(
+    test_context: TestContextFixture[AwsOkta], capture_command: CaptureCommand
+) -> None:
+    with test_context(debug=False) as aws_okta:
+        with capture_command():
+            aws_okta.exec("aws", "ssm", "start-session", target="i-0123456789abcdef")
+    capture_command.assert_command(
+        "aws-okta exec sym -- aws ssm start-session --target i-0123456789abcdef"
+    )
+
+
+@pytest.mark.parametrize(argnames=["constructor"], argvalues=[[AwsOkta]])
+def test_aws_okta_debug(
+    test_context: TestContextFixture[AwsOkta], capture_command: CaptureCommand
+) -> None:
+    with test_context(debug=True) as aws_okta:
+        with capture_command():
+            aws_okta.exec("env")
+    capture_command.assert_command("aws-okta --debug exec sym -- env")
