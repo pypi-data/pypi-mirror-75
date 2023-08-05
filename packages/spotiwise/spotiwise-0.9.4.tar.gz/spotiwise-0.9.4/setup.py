4d3e6c2dce0d6d14b2e04aceb44df16bf01c9e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package spotiwise/0.9.4/spotiwise-0.9.4.tar.gz

---
+++ packages/spotiwise/spotiwise-0.9.4.tar.gz/spotiwise-0.9.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['spotiwise']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests==2.3.0', 'six==1.10.0']
+
+setup_kwargs = {
+    'name': 'spotiwise',
+    'version': '0.9.4',
+    'description': 'Custom Spotify library using true Python objects',
+    'long_description': None,
+    'author': 'Wisdom Wolf',
+    'author_email': 'wisdomwolf@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
