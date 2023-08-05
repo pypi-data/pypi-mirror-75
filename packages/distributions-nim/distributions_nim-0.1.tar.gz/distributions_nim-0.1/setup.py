483a1edc967194f6d978aa8f6eba8ef4e21595be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:49 +0000

    Add package distributions-nim/0.1/distributions_nim-0.1.tar.gz

---
+++ packages/distributions-nim/distributions_nim-0.1.tar.gz/distributions_nim-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_nim',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_nim'],
+      zip_safe=False)
