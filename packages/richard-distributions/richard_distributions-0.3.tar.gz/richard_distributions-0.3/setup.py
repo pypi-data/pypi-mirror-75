480943e47be18d799456d5096277d674d8b0b5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:17 +0000

    Add package richard-distributions/0.3/richard_distributions-0.3.tar.gz

---
+++ packages/richard-distributions/richard_distributions-0.3.tar.gz/richard_distributions-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='richard_distributions',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['richard_distributions'],
+      author= 'Richard Valenzuela Castro',
+      author_email='valenz.richards@gmail.com',
+      zip_safe=False)
