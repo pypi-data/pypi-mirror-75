8a9b4254a9ad33b0d984d8856b7b55831aecd723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:17 +0000

    Add package pneuro/1.0.8/pneuro-1.0.8.tar.gz

---
+++ packages/pneuro/pneuro-1.0.8.tar.gz/pneuro-1.0.8/pneuro/__init__.py
@@ -0,0 +1,5 @@
+from .dataLoad.data_loader import DataGetter
+from .dataPreprocessing.preProcess import Preprocessor
+from .dataVisualization.visualization import DataVisualization
+from .modelTraining.classificationModels import ClassificationModelTuner
+from .prediction.predict import Prediction
