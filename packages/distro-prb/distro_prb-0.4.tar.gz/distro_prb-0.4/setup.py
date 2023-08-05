cf4ecdca60eaad47fe950be457dacb739ad79db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package distro-prb/0.4/distro_prb-0.4.tar.gz

---
+++ packages/distro-prb/distro_prb-0.4.tar.gz/distro_prb-0.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distro_prb',
+      version='0.4',
+      description='Gaussian and Binomial distributions',
+      packages=['distro_prb'],
+      zip_safe=False)
