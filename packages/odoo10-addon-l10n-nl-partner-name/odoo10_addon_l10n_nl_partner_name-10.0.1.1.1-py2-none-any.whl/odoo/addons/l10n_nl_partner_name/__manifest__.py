6bbf5d51f92216cd1b562b8bdaa63e2839601a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:18 +0000

    Add package odoo10-addon-l10n-nl-partner-name/10.0.1.1.1/odoo10_addon_l10n_nl_partner_name-10.0.1.1.1-py2-none-any.whl

---
+++ packages/odoo10-addon-l10n-nl-partner-name/odoo10_addon_l10n_nl_partner_name-10.0.1.1.1-py2-none-any.whl/odoo/addons/l10n_nl_partner_name/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Dutch partner names',
+    'version': '10.0.1.1.1',
+    'author': 'Therp BV, Odoo Community Association (OCA)',
+    'category': 'Contact management',
+    'depends': [
+        'partner_firstname',
+        'base_view_inheritance_extension',
+    ],
+    'data': [
+        'view/res_partner.xml',
+        "data/ir.config_parameter.xml",
+    ],
+    'auto_install': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': ['mako'],
+    },
+    'license': 'AGPL-3',
+}
