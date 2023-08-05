f11d674fcc523f1277995064af1bfbfc9c721997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package distributions-sample/0.1/distributions-sample-0.1.tar.gz

---
+++ packages/distributions-sample/distributions-sample-0.1.tar.gz/distributions-sample-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-sample',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-sample'],
+      zip_safe=False)
