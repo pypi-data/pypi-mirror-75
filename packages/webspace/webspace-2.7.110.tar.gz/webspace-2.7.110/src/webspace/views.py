1afced9f88e38870f409b89c557e5804abce0e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:31 +0000

    Add package webspace/2.7.110/webspace-2.7.110.tar.gz

---
+++ packages/webspace/webspace-2.7.110.tar.gz/webspace-2.7.110/src/webspace/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.conf import settings
+
+from webspace.loader import get_model
+from webspace.bakery.models import VersionBuild
+
+IconSnippet = get_model('cms', 'IconSnippet')
+
+
+def robots(request):
+    return HttpResponse(
+        render_to_string('robots.txt', {'request': request}),
+        content_type='text/plain'
+    )
+
+
+def error_404(request, exception):
+    context = {}
+    context['icons'] = IconSnippet.get_context()
+    return render(request, 'cms/pages/404.html', context, status=404)
+
+
+def worker(request):
+    from wagtail.contrib.sitemaps.sitemap_generator import Sitemap
+    vb = VersionBuild.get()
+    return HttpResponse(render_to_string(
+        'worker.js',
+        {
+            'request': request,
+            'pages': Sitemap(request),
+            'version': vb.version
+        }),
+        content_type='application/javascript'
+    )
