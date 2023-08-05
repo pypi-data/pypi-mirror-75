7e9fbf0a6eb112d4b18fa75f5f7f3bb5b33e44e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:52 +0000

    Add package ut95-distributions/0.1/ut95_distributions-0.1.tar.gz

---
+++ packages/ut95-distributions/ut95_distributions-0.1.tar.gz/ut95_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ut95_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ut95_distributions'],
+      author='Usama Official',
+      author_email='usamaofficial95@gmail.com',
+      zip_safe=False)
