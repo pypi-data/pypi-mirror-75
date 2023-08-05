075632d4cf2e4447342c0cb3c6e57cd64abd2a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:23 +0000

    Add package scikitsampledistribution/0.1/scikitsampledistribution-0.1.tar.gz

---
+++ packages/scikitsampledistribution/scikitsampledistribution-0.1.tar.gz/scikitsampledistribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='scikitsampledistribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['scikitsampledistribution'],
+      zip_safe=False)
