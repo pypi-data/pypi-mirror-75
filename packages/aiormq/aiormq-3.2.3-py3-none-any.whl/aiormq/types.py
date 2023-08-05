9de7222677693fecc084adb29edf9bcb1da94713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:15 +0000

    Add package aiormq/3.2.3/aiormq-3.2.3-py3-none-any.whl

---
+++ packages/aiormq/aiormq-3.2.3-py3-none-any.whl/aiormq/types.py
@@ -0,0 +1,45 @@
+import typing
+
+from pamqp import ContentHeader
+from pamqp import specification as spec
+from yarl import URL
+
+
+ChannelRType = typing.Tuple[int, spec.Channel.OpenOk]
+GetResultType = typing.Union[spec.Basic.GetEmpty, spec.Basic.GetOk]
+DeliveredMessage = typing.NamedTuple(
+    "DeliveredMessage",
+    [
+        ("delivery", typing.Union[spec.Basic.Deliver, GetResultType]),
+        ("header", ContentHeader),
+        ("body", bytes),
+        ("channel", "aiormq.Channel"),
+    ],
+)
+CallbackCoro = typing.Coroutine[DeliveredMessage, None, typing.Any]
+ConsumerCallback = typing.Callable[[], CallbackCoro]
+ReturnCallback = typing.Callable[[], CallbackCoro]
+ArgumentsType = typing.Dict[str, typing.Union[str, int, bool]]
+
+ConfirmationFrameType = typing.Union[
+    spec.Basic.Ack, spec.Basic.Nack, spec.Basic.Reject,
+]
+
+SSLCerts = typing.NamedTuple(
+    "SSLCerts",
+    [
+        ("cert", str),
+        ("key", str),
+        ("capath", str),
+        ("cafile", str),
+        ("cadata", bytes),
+        ("verify", bool),
+    ],
+)
+FrameReceived = typing.NamedTuple(
+    "FrameReceived", [("channel", int), ("frame", str)],
+)
+
+
+URLorStr = typing.Union[URL, str]
+DrainResult = typing.Awaitable[None]
