6cd57ebd058bf97ff52c4ace3cb050f348b9626b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:05 +0000

    Add package fluent-tfx/0.0.1/fluent-tfx-0.0.1.tar.gz

---
+++ packages/fluent-tfx/fluent-tfx-0.0.1.tar.gz/fluent-tfx-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fluent-tfx", # Replace with your own username
+    version="0.0.1",
+    author="Theodoros Ntakouris",
+    author_email="zarkopafilis@gmail.com",
+    description="A fluent API layer for tensorflow extended e2e machine learning pipelines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ntakouris/fluent-tfx",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
