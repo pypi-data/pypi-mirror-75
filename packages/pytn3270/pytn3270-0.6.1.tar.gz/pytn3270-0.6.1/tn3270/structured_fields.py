235ae931629bf537a10bcdb6e79370a7f634bcca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:19 +0000

    Add package pytn3270/0.6.1/pytn3270-0.6.1.tar.gz

---
+++ packages/pytn3270/pytn3270-0.6.1.tar.gz/pytn3270-0.6.1/tn3270/structured_fields.py
@@ -0,0 +1,17 @@
+"""
+tn3270.structured_fields
+~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+from enum import Enum, IntEnum
+
+class StructuredField(IntEnum):
+    READ_PARTITION = 0x01
+    OUTBOUND_3270DS = 0x40
+    QUERY_REPLY = 0x81
+
+class ReadPartitionType(IntEnum):
+    QUERY = 0x02
+
+class QueryReply(IntEnum):
+    SUMMARY = 0x80
