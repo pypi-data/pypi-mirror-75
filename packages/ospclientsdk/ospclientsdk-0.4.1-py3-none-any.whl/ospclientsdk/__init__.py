105c176c8b9ab1c815ca21b896c5aefe3f1c4133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package ospclientsdk/0.4.1/ospclientsdk-0.4.1-py3-none-any.whl

---
+++ packages/ospclientsdk/ospclientsdk-0.4.1-py3-none-any.whl/ospclientsdk/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    osclientsdk
+
+    An sdk like wrapper around subprocess
+    and openstack client.
+"""
+
+from .shell import ClientShell
+from .helpers.context import *
+
+__version__ = '0.4.1'
+__author__ = 'Danny Baez'
