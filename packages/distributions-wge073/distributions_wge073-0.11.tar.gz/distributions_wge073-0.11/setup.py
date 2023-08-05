db1960bc3741da0b446c99f3b238abc48c747e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:59 +0000

    Add package distributions-wge073/0.11/distributions_wge073-0.11.tar.gz

---
+++ packages/distributions-wge073/distributions_wge073-0.11.tar.gz/distributions_wge073-0.11/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_wge073',
+      version='0.11',
+      description='Gaussian distributions',
+      packages=['distributions_wge073'],
+      author = 'Wen Ge',
+      author_email = 'ng.wge073@gmail.com',
+      zip_safe=False)
