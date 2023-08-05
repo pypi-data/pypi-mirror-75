caf2b2089a05b65050ea3e973a093d8ee2ad7795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package gabi-distribution/0.1/gabi_distribution-0.1.tar.gz

---
+++ packages/gabi-distribution/gabi_distribution-0.1.tar.gz/gabi_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gabi_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gabi_distribution'],
+      author = 'Anukool Rathi',
+      author_email = 'Anukool.rathi10@gmail.com',
+      zip_safe=False)
