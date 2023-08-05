21bd9095985ed0d4b743274188b216161b5e5775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package common-ui-components/2.7.1/common_ui_components-2.7.1-py3-none-any.whl

---
+++ packages/common-ui-components/common_ui_components-2.7.1-py3-none-any.whl/commonuicomponents/ttk/commonuicomponents.py
@@ -0,0 +1,157 @@
+from copy import deepcopy
+from tkinter import Widget
+from tkinter.ttk import Style
+from .multiplier import Multiplier
+from .widgets.smartwidget import SmartWidget
+from ..staticutils import StaticUtils
+
+class CommonUIComponents:
+   __CLASSES = dict()
+   
+   @staticmethod
+   def inflate(tab):
+      ui = CommonUIComponents._inflate(tab.baseTabFrame, [tab.baseTabConfig["ui"]], tab.baseTabStorage, [])
+      
+      if len(ui) != 1:
+         raise ValueError("len(ui) != 1")
+      
+      for value in ui.values():
+         ui = value
+      
+      return ui
+   
+   @staticmethod
+   def init(**params):
+      params = StaticUtils.mergeJson({
+         "debug": False
+      }, params, True)
+      
+      for key, value in params.items():
+         setattr(CommonUIComponents, key.upper(), value)
+      
+      # pylint: disable = import-outside-toplevel
+      SmartWidget._STYLE_INSTANCE = Style()
+      
+      from tkinter import Canvas
+      from tkinter.ttk import Button, Label, Radiobutton
+      from .containers.container import Container
+      from .containers.labeledcontainer import LabeledContainer
+      from .containers.labeledradiobuttongroup import LabeledRadioButtonGroup
+      from .widgets.checkbutton import Checkbutton
+      from .widgets.combobox import Combobox
+      from .widgets.entry import Entry
+      from .widgets.labeledscale import LabeledScale
+      from .widgets.listbox import Listbox
+      from .widgets.scrollbar import Scrollbar
+      from .widgets.spinbox import Spinbox
+      from .widgets.statefulbutton import StatefulButton
+      
+      for tkinterBase in (Button, Canvas, Label, Radiobutton):
+         CommonUIComponents.wrapClass(tkinterBase)
+      
+      for smartWidget in (
+         Checkbutton,
+         Combobox,
+         Container,
+         Entry,
+         LabeledContainer,
+         LabeledRadioButtonGroup,
+         LabeledScale,
+         Listbox,
+         Scrollbar,
+         Spinbox,
+         StatefulButton
+      ):
+         CommonUIComponents.registerClass(smartWidget)
+   
+   @staticmethod
+   def registerClass(clazz):
+      if not issubclass(clazz, SmartWidget):
+         raise ValueError(f"{clazz.__name__} must subclass {SmartWidget.__name__}")
+      
+      clazz.STYLE = [clazz]
+      clazz._TKINTER_BASE = None
+      
+      for base in clazz.__bases__:
+         if issubclass(base, Widget):
+            clazz._TKINTER_BASE = CommonUIComponents.__CLASSES["LabeledContainer"]._TKINTER_BASE if clazz.__name__ == "LabeledRadioButtonGroup" else base # TODO Dirty hack :( .
+         
+         if issubclass(base, SmartWidget):
+            while base != SmartWidget:
+               clazz.STYLE.append(base)
+               
+               for b in base.__bases__:
+                  if issubclass(b, SmartWidget):
+                     base = b
+                     break
+      
+      if clazz._TKINTER_BASE:
+         clazz.STYLE.append(clazz._TKINTER_BASE)
+      
+      clazz.STYLE = f"T{'.T'.join([clz.__name__ for clz in clazz.STYLE])}"
+      
+      if hasattr(clazz, "_DEFAULT_STYLE"):
+         SmartWidget._STYLE_INSTANCE.configure(clazz.STYLE, **clazz._DEFAULT_STYLE)
+      
+      CommonUIComponents.__CLASSES[clazz.__name__] = clazz
+      
+      if CommonUIComponents.DEBUG:
+         print(f"registered {clazz} (TKINTER_BASE: {clazz._TKINTER_BASE}, STYLE: '{clazz.STYLE}'.")
+   
+   @staticmethod
+   def wrapClass(tkinterBase):
+      CommonUIComponents.registerClass(type(tkinterBase.__name__, (SmartWidget, tkinterBase), dict()))
+   
+   @staticmethod
+   def _inflate(master, children, storage, namePrefix):
+      result = dict()
+      
+      row = 0
+      column = 0
+      
+      logicalRow = 0
+      logicalColumn = 0
+      
+      for child in children:
+         child = deepcopy(child)
+         
+         childType = child.pop("type")
+         multiply = Multiplier(child)
+         
+         for i in range(multiply.count):
+            ch = deepcopy(child)
+            
+            multiply.setIndexableToChild(ch, "text", i)
+            
+            grid = StaticUtils.setIfAbsentAndGet(ch, "grid", dict())
+            
+            skipColumns = grid.pop("skipColumns", 0)
+            column += skipColumns
+            logicalColumn += skipColumns
+            
+            grid["row"] = row
+            grid["column"] = column
+            
+            ch["storage"] = storage
+            ch["namePrefix"] = namePrefix
+            
+            multiply.setIndexableToChild(ch, "name", i)
+            
+            smartWidget = CommonUIComponents.__CLASSES[childType](master, **ch)
+            
+            result[(logicalRow, logicalColumn)] = smartWidget
+            
+            if multiply.lastChildAddsRow and i == multiply.count - 1:
+               grid["lastColumn"] = True
+            
+            if grid.pop("lastColumn", False):
+               row += smartWidget.rows
+               logicalRow += 1
+               
+               column = logicalColumn = 0
+            
+            else:
+               column += smartWidget.columns
+               logicalColumn += 1
+      
+      return result
