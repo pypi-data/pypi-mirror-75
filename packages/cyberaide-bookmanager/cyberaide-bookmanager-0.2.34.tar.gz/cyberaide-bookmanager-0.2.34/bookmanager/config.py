c925d9859c6e3efd1ba64658467cf38dcd8bde40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:51 +0000

    Add package cyberaide-bookmanager/0.2.34/cyberaide-bookmanager-0.2.34.tar.gz

---
+++ packages/cyberaide-bookmanager/cyberaide-bookmanager-0.2.34.tar.gz/cyberaide-bookmanager-0.2.34/bookmanager/config.py
@@ -0,0 +1,154 @@
+from os.path import dirname
+from pathlib import Path
+
+import oyaml as yaml
+# from bookmanager.yaml_parser_ok import json_flatten
+from deprecated.yaml_parser import json_flatten
+from cloudmesh.common.FlatDict import flatten as dict_flatten
+# from cloudmesh.common.debug import VERBOSE
+from cloudmesh.common.util import path_expand
+from munch import munchify
+
+
+# noinspection PyPep8
+class Config(object):
+    __shared_state = {}
+
+    def __init__(self, config='./toc.yaml'):
+        """
+        Initialize the Config class.
+
+        :param config: A local file path to cloudmesh yaml config
+            with a root element `cloudmesh`. Default: `.toc.yaml`
+        
+        Example:
+        
+        ---
+        - BOOK:
+          - "{b516}/preface.md"
+          - INTRODUCTION:
+            - "{manager}/other.md"
+          - CLOUD:
+            - "{manager}/other.md"
+            - AWS:
+              - "{manager}/other.md"
+              - "i {manager}"
+              - "r {manager}"
+              - "p {manager}/test.py"
+          - DEVOPS:
+            - "{manager}/other.md"
+            - ANSIBLE:
+              - "{manager}/other.md"
+              - "i {manager}"
+              - "r {manager}"
+              - "p {manager}/test.py"
+        
+        """
+
+        self.__dict__ = self.__shared_state
+        if "data" not in self.__dict__:
+            # VERBOSE("Load config")
+
+            self.config_path = Path(path_expand(config)).resolve()
+            self.config_folder = dirname(self.config_path)
+
+            with open(self.config_path, "r") as content:
+                self.data = yaml.load(content, Loader=yaml.SafeLoader)
+
+            self.variables = dict(self.data)
+            keys = self.variables.keys()
+            del self.variables['BOOK']
+
+            self.book = list(self.data['BOOK'])
+            self.flat = munchify(self.variables)
+            self.book = self.spec_replace(self.book, self.variables)
+            self.variables = dict_flatten(self.variables, sep=".")
+
+    def spec_replace(self,
+                     book,
+                     variables):
+
+        variable_list = dict_flatten(variables, sep=".")
+
+        spec = yaml.dump(book)
+
+        for variable in variable_list:
+            value = variable_list[variable]
+            token = "{" + variable + "}"
+            spec = spec.replace(token, value)
+
+        output = yaml.load(spec, Loader=yaml.SafeLoader)
+        return output
+
+    def flatten(self,
+                book="BOOK",
+                title="{book}",
+                section="{parent}/{key}",
+                header="{parent}/{key}",
+                indent=""):
+        result = json_flatten(self.book,
+                              book=book,
+                              title=title,
+                              section=section,
+                              header=header,
+                              level=-1,
+                              indent=indent)
+        return result
+
+    def dict(self):
+        return self.data
+
+    def output(self, result, kind="text"):
+        content = []
+        for entry in result:
+            text = entry["output"].format(**entry)
+            content.append(text)
+
+        if kind in ["text"]:
+            return '\n'.join(content)
+
+        if kind in ['url']:
+            out = []
+            for entry in content:
+                # print (entry)
+                if entry != "":
+                    out.append(entry)
+            return out
+
+        else:
+            return content
+
+    def __str__(self):
+        return yaml.dump(self.data, default_flow_style=False, indent=2)
+
+    def get(self, key, default=None):
+        """
+        A helper function for reading values from the config without
+        a chain of `get()` calls.
+
+        Usage:
+            mongo_conn = conf.get('db.mongo.MONGO_CONNECTION_STRING')
+            default_db = conf.get('default.db')
+            az_credentials = conf.get('data.service.azure.credentials')
+
+        :param default:
+        :param key: A string representing the value's path in the config.
+        """
+        return self.data.get(key, default)
+
+    def __getitem__(self, item):
+        """
+        gets an item form the dict. The key is . separated
+        use it as follows get("a.b.c")
+        :param item:
+        :type item:
+        :return:
+        """
+        if "." in item:
+            keys = item.split(".")
+        else:
+            return self.data[item]
+        element = self.data[keys[0]]
+        for key in keys[1:]:
+            element = element[key]
+        return element
---
+++ packages/cyberaide-bookmanager/cyberaide-bookmanager-0.2.34.tar.gz/cyberaide-bookmanager-0.2.34/bookmanager/cover.py
@@ -0,0 +1,119 @@
+from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
+import pkg_resources
+
+
+class Cover(object):
+
+    def __init__(self):
+        self.draw = None
+        self.row = 1
+        self.step = 50
+        self.font = self.set_font("normal")
+        self.color = 'rgb(255, 255, 255)'  # white color
+
+    def set_font(self, scale):
+        ttf = pkg_resources.resource_filename(
+            "bookmanager",
+            'template/epub/fonts/OpenSans-Bold.ttf')
+
+        if scale == "large":
+            self.font = ImageFont.truetype(ttf,
+                                           size=int(self.step * .9))
+        elif scale == "tiny":
+            self.font = ImageFont.truetype(ttf,
+                                           size=int(self.step * .3))
+        elif scale == "small":
+            self.font = ImageFont.truetype(
+                ttf,
+                size=int(self.step * .4))
+        elif scale == "smaller":
+            self.font = ImageFont.truetype(
+                ttf,
+                size=int(self.step * .3))
+
+        elif scale == "credit":
+            self.font = ImageFont.truetype(ttf,
+                                           size=int(self.step * .25))
+
+        else:
+            self.font = ImageFont.truetype(ttf,
+                                           size=int(self.step * .7))
+        return self.font
+
+    def reset(self, row=None):
+        if row is None:
+            self.row = 1
+        else:
+            self.row = row
+
+    def skip(self, n):
+        self.row = self.row + n
+
+    def mesg(self, x, text, step=50, scale="normal"):
+        lines = text.splitlines()
+        for line in lines:
+            font = self.set_font(scale)
+            self.draw.text((self.step * x, self.step * self.row),
+                           line,
+                           fill=self.color,
+                           font=self.font)
+            self.row = self.row + 1
+
+    def line(self, start, stop, width=10):
+
+        self.draw.line((start * self.step, self.step * self.row,
+                        stop * self.step, self.step * self.row),
+                       fill=self.color,
+                       width=width)
+
+    def credit(self, n):
+        self.mesg(n,
+                  "Created by Cloudmesh & Cyberaide Bookmanager, "
+                  "https://github.com/cyberaide/bookmanager ",
+                  scale="credit")
+
+    def generate(self,
+                 image="cover.png",
+                 background=None,
+                 title="Title",
+                 subtitle="Subtitle",
+                 author='Author',
+                 subauthor="Editor",
+                 email="laszewski@gmail.com",
+                 webpage="https://github.com/cyberaide/bookmanager",
+                 date=None):
+
+        if background is None:
+            background = pkg_resources.resource_filename(
+                "bookmanager",
+                'template/epub/cover/cover-image.png')
+
+        canvas = Image.open(background)
+        self.draw = ImageDraw.Draw(canvas)
+
+        if date is None:
+            date = "{:%B %d, %Y - %I:%M %p}".format(datetime.now())
+
+        self.reset()
+        self.mesg(1, title, scale="large")
+        self.mesg(1, subtitle, scale="normal")
+        self.skip(2)
+        self.line(1, 13)
+        self.skip(1)
+        self.mesg(2, author)
+        self.mesg(2, subauthor, scale="tiny")
+        self.skip(1)
+        self.mesg(5, email, scale="small")
+        self.skip(1)
+        self.line(1, 13)
+        self.skip(1)
+        self.mesg(1, webpage, scale="tiny")
+
+        self.reset(1)
+        self.skip(16)
+
+        self.mesg(2, date, scale="tiny")
+        self.credit(1.5)
+
+        canvas.save(image)
