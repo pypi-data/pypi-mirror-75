3686475b641a575424a5f5fb301f7f98b0ee9ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:12 +0000

    Add package distributions-ken/0.1/distributions-ken-0.1.tar.gz

---
+++ packages/distributions-ken/distributions-ken-0.1.tar.gz/distributions-ken-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions-ken',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-ken'],
+      author='Ken Sitati',
+      zip_safe=False)
