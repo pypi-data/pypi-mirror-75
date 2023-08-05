c9e32a264ff43d7837e2ea6b03e6132ef5f06e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:59 +0000

    Add package channels-binding/1.1.8/channels_binding-1.1.8-py3-none-any.whl

---
+++ packages/channels-binding/channels_binding-1.1.8-py3-none-any.whl/tests/routing.py
@@ -0,0 +1,17 @@
+
+from channels import route_class
+from channels.generic.websockets import WebsocketDemultiplexer
+from .test_bindings import TestModelResourceBinding
+
+
+class TestDemultiplexer(WebsocketDemultiplexer):
+    http_user_and_session = True
+
+    consumers = {
+        'testmodel': TestModelResourceBinding.consumer
+    }
+
+
+channel_routing = [
+    route_class(TestDemultiplexer)
+]
