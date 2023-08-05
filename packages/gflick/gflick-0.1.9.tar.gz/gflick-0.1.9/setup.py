d6eb21305fdea9eaa79ac8a7061324a634a9b098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package gflick/0.1.9/gflick-0.1.9.tar.gz

---
+++ packages/gflick/gflick-0.1.9.tar.gz/gflick-0.1.9/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['gflick']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bottle>=0.12.18,<0.13.0',
+ 'gunicorn>=20.0.4,<21.0.0',
+ 'requests>=2.23.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['gflick-dev = gflick:dev',
+                     'gflick-google = gflick:google',
+                     'gflick-prod = gflick:prod',
+                     'gflick-raw = gflick:raw']}
+
+setup_kwargs = {
+    'name': 'gflick',
+    'version': '0.1.9',
+    'description': '',
+    'long_description': None,
+    'author': 'Bùi Thành Nhân',
+    'author_email': 'hi@imnhan.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
