e19c1c456746e9fe70645e4cbe44bb347e347a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:27 +0000

    Add package rtc-cafe-signaller/0.0.5/rtc-cafe-signaller-0.0.5.tar.gz

---
+++ packages/rtc-cafe-signaller/rtc-cafe-signaller-0.0.5.tar.gz/rtc-cafe-signaller-0.0.5/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='rtc-cafe-signaller',
+    version='v0.0.5',
+    url='https://gitlab.com/rtc-cafe/rtc-cafe-signaller',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=[
+        'rtc_cafe_signaller',
+        'rtc_cafe_signaller.wrappers',
+    ],
+)
