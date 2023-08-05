c90a343c77c4e7ca3fbbb1ee7c52ab2d4422c320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:21 +0000

    Add package ws2x-vjsrinivas/0.0.3/ws2x-vjsrinivas-0.0.3.tar.gz

---
+++ packages/ws2x-vjsrinivas/ws2x-vjsrinivas-0.0.3.tar.gz/ws2x-vjsrinivas-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ws2x-vjsrinivas", # Replace with your own username
+    version="0.0.3",
+    author="Vijay Rajagopal",
+    author_email="vijaythinks@gmail.com",
+    download_url = 'https://github.com/vjsrinivas/ws2x-strip/archive/0.0.1.tar.gz',
+    description="A simple simulator for WS2812B-style LED strips",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vjsrinivas/ws2x-strip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics :: Editors :: Raster-Based",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'opencv-python'
+    ],
+    python_requires='>=3.0',
+)
