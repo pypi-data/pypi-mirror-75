62c361f140a4151b9d51a208c4cdd8eaa141956a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:15 +0000

    Add package dark-bulk-dist/1.0/dark_bulk_dist-1.0.tar.gz

---
+++ packages/dark-bulk-dist/dark_bulk_dist-1.0.tar.gz/dark_bulk_dist-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dark_bulk_dist',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dark_bulk_dist'],
+      author = 'Dark Bulk',
+      author_email = "darkbulk2016@gmail.com",
+      zip_safe=False)
