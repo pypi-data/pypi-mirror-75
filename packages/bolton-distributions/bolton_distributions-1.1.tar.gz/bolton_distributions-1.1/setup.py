6f62a7d2e93bbcb7b3b559eabeee90323778611d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:38 +0000

    Add package bolton-distributions/1.1/bolton_distributions-1.1.tar.gz

---
+++ packages/bolton-distributions/bolton_distributions-1.1.tar.gz/bolton_distributions-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bolton_distributions',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['bolton_distributions'],
+      zip_safe=False)
