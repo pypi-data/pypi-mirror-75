ee0fa48d882d0f25514d12c72a7e735d04fd3629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:13 +0000

    Add package kulsoft-probability/0.1/kulsoft_probability-0.1.tar.gz

---
+++ packages/kulsoft-probability/kulsoft_probability-0.1.tar.gz/kulsoft_probability-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='kulsoft_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      author ='Saad kulumba',
+      author_email = 'koogle012@gmail.com',
+      packages=['kulsoft_probability'],
+      zip_safe=False)
+
