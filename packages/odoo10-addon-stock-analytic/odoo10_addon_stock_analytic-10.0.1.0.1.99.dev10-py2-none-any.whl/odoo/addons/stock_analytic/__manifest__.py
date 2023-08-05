1921ac5de70ef67baa9a3d562f93b98c41ad277f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:44 +0000

    Add package odoo10-addon-stock-analytic/10.0.1.0.1.99.dev10/odoo10_addon_stock_analytic-10.0.1.0.1.99.dev10-py2-none-any.whl

---
+++ packages/odoo10-addon-stock-analytic/odoo10_addon_stock_analytic-10.0.1.0.1.99.dev10-py2-none-any.whl/odoo/addons/stock_analytic/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013 Julius Network Solutions
+# Copyright 2015 Clear Corp
+# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Analytic",
+    "summary": "Adds an analytic account in stock move",
+    "version": "10.0.1.0.1",
+    "author": "Julius Network Solutions,"
+              "ClearCorp, OpenSynergy Indonesia,"
+              "Odoo Community Association (OCA)",
+    "website": "http://www.julius.fr/",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_account",
+        "procurement_analytic",
+        "analytic",
+    ],
+    "data": [
+        "views/stock_move_views.xml",
+    ],
+    'installable': True,
+}
