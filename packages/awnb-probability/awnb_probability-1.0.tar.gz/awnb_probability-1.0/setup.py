340026681a552a39bc9e1e9fcc314634560b0cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:32 +0000

    Add package awnb-probability/1.0/awnb_probability-1.0.tar.gz

---
+++ packages/awnb-probability/awnb_probability-1.0.tar.gz/awnb_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='awnb_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['awnb_probability'],
+      author = 'John Doe',
+      author_email = 'johndoe@example.com',
+      zip_safe=False)
