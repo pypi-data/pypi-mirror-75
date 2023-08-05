ca42e5c930e69c7256fa275b3c9381000e03ca3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package python-miio/0.5.3/python-miio-0.5.3.tar.gz

---
+++ packages/python-miio/python-miio-0.5.3.tar.gz/python-miio-0.5.3/miio/__init__.py
@@ -0,0 +1,53 @@
+# flake8: noqa
+from importlib_metadata import version  # type: ignore
+from miio.airconditioningcompanion import (
+    AirConditioningCompanion,
+    AirConditioningCompanionV3,
+)
+from miio.airdehumidifier import AirDehumidifier
+from miio.airfresh import AirFresh
+from miio.airfresh_t2017 import AirFreshT2017
+from miio.airhumidifier import AirHumidifier, AirHumidifierCA1, AirHumidifierCB1
+from miio.airhumidifier_jsq import AirHumidifierJsq
+from miio.airhumidifier_miot import AirHumidifierMiot
+from miio.airhumidifier_mjjsq import AirHumidifierMjjsq
+from miio.airpurifier import AirPurifier
+from miio.airpurifier_miot import AirPurifierMiot
+from miio.airqualitymonitor import AirQualityMonitor
+from miio.aqaracamera import AqaraCamera
+from miio.ceil import Ceil
+from miio.chuangmi_camera import ChuangmiCamera
+from miio.chuangmi_ir import ChuangmiIr
+from miio.chuangmi_plug import ChuangmiPlug, Plug, PlugV1, PlugV3
+from miio.cooker import Cooker
+from miio.device import Device
+from miio.exceptions import DeviceError, DeviceException
+from miio.fan import Fan, FanP5, FanSA1, FanV2, FanZA1, FanZA4
+from miio.gateway import Gateway
+from miio.heater import Heater
+from miio.philips_bulb import PhilipsBulb, PhilipsWhiteBulb
+from miio.philips_eyecare import PhilipsEyecare
+from miio.philips_moonlight import PhilipsMoonlight
+from miio.philips_rwread import PhilipsRwread
+from miio.powerstrip import PowerStrip
+from miio.protocol import Message, Utils
+from miio.pwzn_relay import PwznRelay
+from miio.toiletlid import Toiletlid
+from miio.vacuum import Vacuum, VacuumException
+from miio.vacuumcontainers import (
+    CleaningDetails,
+    CleaningSummary,
+    ConsumableStatus,
+    DNDStatus,
+    Timer,
+    VacuumStatus,
+)
+from miio.viomivacuum import ViomiVacuum
+from miio.waterpurifier import WaterPurifier
+from miio.wifirepeater import WifiRepeater
+from miio.wifispeaker import WifiSpeaker
+from miio.yeelight import Yeelight
+
+from miio.discovery import Discovery
+
+__version__ = version("python-miio")
