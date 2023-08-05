7e9908721a2bb27a280f034ef2973560ccd9cf6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:16 +0000

    Add package statizy/0.1/statizy-0.1.tar.gz

---
+++ packages/statizy/statizy-0.1.tar.gz/statizy-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='statizy',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author='Kandarp Patel',
+      packages=['statizy'],
+      zip_safe=False)
