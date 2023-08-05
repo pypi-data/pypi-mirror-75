b21513ab424099a23af954e3337847b2b748ad1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:53 +0000

    Add package prob-distribution-methods/1.0/prob_distribution_methods-1.0.tar.gz

---
+++ packages/prob-distribution-methods/prob_distribution_methods-1.0.tar.gz/prob_distribution_methods-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_distribution_methods',
+      version='1.0',
+      description='Truong test the distributions to pypi.org',
+      packages=['prob_distribution_methods'],
+      author = 'Truong LE',
+      author_email = 'lqtruong@gmail.com',
+      zip_safe=False)
