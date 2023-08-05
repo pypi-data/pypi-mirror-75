c2659f2e92b69df0afd86962a4b1116041132195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:25 +0000

    Add package distributions-test-450/0.1/distributions_test_450-0.1.tar.gz

---
+++ packages/distributions-test-450/distributions_test_450-0.1.tar.gz/distributions_test_450-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_test_450',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_test_450'],
+      author=['anonymous'],
+      zip_safe=False)
