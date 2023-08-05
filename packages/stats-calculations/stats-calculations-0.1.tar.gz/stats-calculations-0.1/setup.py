56b1aff382b2776d89e4938a43be08c2dc498f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:49 +0000

    Add package stats-calculations/0.1/stats-calculations-0.1.tar.gz

---
+++ packages/stats-calculations/stats-calculations-0.1.tar.gz/stats-calculations-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stats-calculations',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['stats_calculations'],
+      author='M Abhishek',
+      author_email = 'abhishek.nithya@gmail.com',
+      zip_safe=False)
