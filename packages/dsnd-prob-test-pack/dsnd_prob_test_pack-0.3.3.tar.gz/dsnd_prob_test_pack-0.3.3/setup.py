2d18ddc98d5af2ef3399d42467646cdbdc8b9b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:17 +0000

    Add package dsnd-prob-test-pack/0.3.3/dsnd_prob_test_pack-0.3.3.tar.gz

---
+++ packages/dsnd-prob-test-pack/dsnd_prob_test_pack-0.3.3.tar.gz/dsnd_prob_test_pack-0.3.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_prob_test_pack',
+      version='0.3.3',
+      description='Gaussian distributions',
+      author='Temirlan Dzhasheev',
+      author_email='temirlan.dzhasheev@gmail.com',
+      packages=['distributions'],
+      zip_safe=False)
