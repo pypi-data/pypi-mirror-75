03a0c7b8351132c7a8f0c5d13aa7968bf9c01681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:14 +0000

    Add package scrapy-freedb/0.1.0/scrapy_freedb-0.1.0-py3-none-any.whl

---
+++ packages/scrapy-freedb/scrapy_freedb-0.1.0-py3-none-any.whl/scrapy_freedb/middleware/mapper.py
@@ -0,0 +1,14 @@
+import hashlib
+
+def url_sha256(request):
+    url = request.url
+    hash = hashlib.sha256()
+    hash.update(url.encode())
+    return hash.hexdigest()
+
+
+def url_sha1(request):
+    url = request.url
+    hash = hashlib.sha1()
+    hash.update(url.encode())
+    return hash.hexdigest()
