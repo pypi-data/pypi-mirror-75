296dc9f3a03ed4f75f267630d8274dcc3212daaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:21 +0000

    Add package simpledistributionsv1/0.1/simpledistributionsv1-0.1.tar.gz

---
+++ packages/simpledistributionsv1/simpledistributionsv1-0.1.tar.gz/simpledistributionsv1-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='simpledistributionsv1',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['simpledistributionsv1'],
+      zip_safe=False)
