c800c5cdb72b253fdb8fb50008a6f10f3c9c5bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:32 +0000

    Add package discordwebhook.py/0.0.4/discordwebhook.py-0.0.4.tar.gz

---
+++ packages/discordwebhook.py/discordwebhook.py-0.0.4.tar.gz/discordwebhook.py-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+long_description="""
+A simple python package for posting to discord webhooks in python
+Has asynchronous and synchronous options
+"""
+setup(
+  name = 'discordwebhook.py',         
+  packages = ['discordwebhook'],   
+  version = '0.0.4',      
+  license='MIT',       
+  description = 'Easily using discord webhooks in python - asynchronous and synchronous - currently undocumented',   
+  long_description=long_description,
+  author = 'Coolo2',                   
+  author_email = 'itsxcoolo2@gmail.com',      
+  url = 'https://github.com/Coolo22/discordwebhook.py',   
+  #download_url = 'none',    
+  keywords = ['discord', 'webhook', 'python', 'easy', 'post', 'asynchronous', 'synchronous'],   
+  install_requires=['aiohttp', 'requests'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
