5445927b44365e6a5bd0762c1e43b3136565dcf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:16 +0000

    Add package distributions-gaussian-binoomial/0.12/distributions_gaussian_binoomial-0.12.tar.gz

---
+++ packages/distributions-gaussian-binoomial/distributions_gaussian_binoomial-0.12.tar.gz/distributions_gaussian_binoomial-0.12/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_gaussian_binoomial',
+      version='0.12',
+      description='Gaussian distributions and binoomial distributions',
+      packages=['distributions_gaussian_binoomial'],
+      author='Venkteshwar Tripathi',
+      author_email='tripathivenkteshwar@gmail.com',
+      zip_safe=False)
