23a8ac22cdcd80f3d532684368bfab5581cea028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:32 +0000

    Add package EasyStats/0.1/EasyStats-0.1.tar.gz

---
+++ packages/EasyStats/EasyStats-0.1.tar.gz/EasyStats-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='EasyStats',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['EasyStats'],
+      author='Rucha Yagnik',
+      zip_safe=False)
