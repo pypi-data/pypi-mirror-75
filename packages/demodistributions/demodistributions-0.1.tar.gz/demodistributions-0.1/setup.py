4a380e1450364f939788a1e3d4c8a9ab0ee4a9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package demodistributions/0.1/demodistributions-0.1.tar.gz

---
+++ packages/demodistributions/demodistributions-0.1.tar.gz/demodistributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='demodistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['demodistributions'],
+      author='Tousifahamed',
+      zip_safe=False)
