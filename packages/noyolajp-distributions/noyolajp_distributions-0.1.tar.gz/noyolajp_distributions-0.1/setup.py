bb631d412cac063f8e80673e66eae85d7efaa50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:38 +0000

    Add package noyolajp-distributions/0.1/noyolajp_distributions-0.1.tar.gz

---
+++ packages/noyolajp-distributions/noyolajp_distributions-0.1.tar.gz/noyolajp_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='noyolajp_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['noyolajp_distributions'],
+      zip_safe=False)
