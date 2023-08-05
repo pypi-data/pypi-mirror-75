c7d2c2cb3e7d784806003d2706378cabf3b9146e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:12 +0000

    Add package probably-mike/0.1/probably-mike-0.1.tar.gz

---
+++ packages/probably-mike/probably-mike-0.1.tar.gz/probably-mike-0.1/README.md
@@ -0,0 +1,3 @@
+## Distributions Package
+
+This is a package created and distributed by (Mike Ajala)[https://github.com/agmt5989]. Please feel free to contribute.
---
+++ packages/probably-mike/probably-mike-0.1.tar.gz/probably-mike-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='probably-mike',
+      version='0.1',
+      description='Probability distributions, including Gaussian and Binomial',
+      packages=['probably-mike'],
+      author='Mike Ajala',
+      zip_safe=False)
