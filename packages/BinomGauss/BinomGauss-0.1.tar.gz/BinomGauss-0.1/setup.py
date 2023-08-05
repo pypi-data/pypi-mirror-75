57eb2c7e54723b097e9a4bd69a71e91f40da4e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package BinomGauss/0.1/BinomGauss-0.1.tar.gz

---
+++ packages/BinomGauss/BinomGauss-0.1.tar.gz/BinomGauss-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='BinomGauss',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['BinomGauss'],
+      author = 'Soham Pendurkar',
+      zip_safe=False)
