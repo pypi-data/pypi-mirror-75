3fd27b2877586305e4522e9bec0ceeb2d14487ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:45 +0000

    Add package discordpy-legacy/0.16.13/discordpy-legacy-0.16.13.tar.gz

---
+++ packages/discordpy-legacy/discordpy-legacy-0.16.13.tar.gz/discordpy-legacy-0.16.13/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup
+import re, os
+
+on_rtd = os.getenv('READTHEDOCS') == 'True'
+
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+if on_rtd:
+  requirements.append('sphinxcontrib-napoleon')
+
+version = ''
+with open('discord/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+extras_require = {
+    'voice': ['PyNaCl==1.0.1'],
+}
+
+setup(name='discordpy-legacy',
+      author='skairunner',
+      url='https://github.com/skairunner/discord.py/tree/namedtuple_fix',
+      version=version,
+      packages=['discord', 'discord.ext.commands'],
+      license='MIT',
+      description='A legacy version of discord.py, a python wrapper for the Discord API',
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      include_package_data=True,
+      install_requires=requirements,
+      extras_require=extras_require,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
+)
