c6f6495a7ca0ce47cf7d32223795189df74d4075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package sepal-ui/0.1.2-alpha/sepal_ui-0.1.2-alpha.tar.gz

---
+++ packages/sepal-ui/sepal_ui-0.1.2-alpha.tar.gz/sepal_ui-0.1.2-alpha/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+    
+setup(
+    name = 'sepal_ui',      
+    packages = ['sepal_ui'],   
+    version = '0.1.2-alpha',   
+    license='MIT',        
+    description = 'wrapper for ipyvuetify widgets to unify the display of voila dashboards in the sepal plateform',  
+    author = 'Pierrick Rambaud',                   
+    author_email = 'pierrick.rambaud49@gmail.com',  
+    url = 'https://github.com/12rambau/sepal_ui',
+    download_url = 'https://github.com/12rambau/sepal_ui/archive/v_0.1.2-alpha.tar.gz',
+    keywords = ['UI', 'Python', 'widget', 'sepal'], 
+    install_requires=[
+        'haversine',
+        'ipyvuetify',
+        'geemap',
+        'ipyvuetify',
+        'ee',
+        'bqplot',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3.6',
+    ],
+)
