f91673a153535102c278aa4d4c37644ce8a48e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package hyzeeck-distributions/0.1/hyzeeck_distributions-0.1.tar.gz

---
+++ packages/hyzeeck-distributions/hyzeeck_distributions-0.1.tar.gz/hyzeeck_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hyzeeck_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hyzeeck_distributions'],
+      author='Rufai Isaac',
+      author_email='hyzeeck234@gmail.com',
+      zip_safe=False)
