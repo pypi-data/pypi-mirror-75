68dc6c7025e085bf3673419ad8cec2f466021acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:01 +0000

    Add package teamscritique/0.5/teamscritique-0.5.tar.gz

---
+++ packages/teamscritique/teamscritique-0.5.tar.gz/teamscritique-0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='teamscritique',
+      version='0.5',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Xiao Yin',
+      author_email='yinxiao@microsoft.com',
+      license='MIT',
+      packages=['teamscritique'],
+      install_requires=[
+          'librosa==0.8.0',
+          'soundfile',
+          'numpy==1.19.1',
+          'sklearn',
+          'pandas',
+          'scipy',
+      ],
+      zip_safe=False)
