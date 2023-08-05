371b84b40dca5ba0aad7d296f50f86f78534a1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package cdk-sqlserver-seeder/0.2.3/cdk_sqlserver_seeder-0.2.3-py3-none-any.whl

---
+++ packages/cdk-sqlserver-seeder/cdk_sqlserver_seeder-0.2.3-py3-none-any.whl/Talnakh/SqlServerSeeder/_jsii/__init__.py
@@ -0,0 +1,31 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_rds._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_deployment._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-sqlserver-seeder",
+    "0.2.3",
+    __name__[0:-6],
+    "cdk-sqlserver-seeder@0.2.3.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
