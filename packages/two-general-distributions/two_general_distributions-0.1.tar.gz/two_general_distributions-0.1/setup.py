77c50ac78a3c89435a6d779f61f8a1818d418026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:21 +0000

    Add package two-general-distributions/0.1/two_general_distributions-0.1.tar.gz

---
+++ packages/two-general-distributions/two_general_distributions-0.1.tar.gz/two_general_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='two_general_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['two_general_distributions'],
+      author='Zhaoyun Ma',
+      author_email='mazhaoyunyh@gmail.com',
+      zip_safe = False)
