1f50d39fb0eefe2384b0777f433110b998fa7a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:41 +0000

    Add package few-dists/1.0/few_dists-1.0.tar.gz

---
+++ packages/few-dists/few_dists-1.0.tar.gz/few_dists-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='few_dists',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['few_dists'],
+      author='Chandrama Naha',
+      author_email='chandrama.naha@gmail.com',
+      zip_safe=False)
