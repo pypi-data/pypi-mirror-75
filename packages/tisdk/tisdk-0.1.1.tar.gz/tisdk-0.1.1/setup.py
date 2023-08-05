f24527df7d8290cec9def8aac501310f70775530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:28 +0000

    Add package tisdk/0.1.1/tisdk-0.1.1.tar.gz

---
+++ packages/tisdk/tisdk-0.1.1.tar.gz/tisdk-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+try:
+    long_description = open('README.md', encoding='utf8').read()
+except Exception as e:
+    long_description = ''
+
+
+setup(
+    name='tisdk',
+    version='0.1.1',
+    description='python sdk of taiqiyun',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests>=2.22.0',
+        # 'pycrypto>=2.6.1',
+    ],
+    # py_modules=['tisdk'],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['tireq=tisdk:main'],
+    },
+    include_package_data=True,
+)
