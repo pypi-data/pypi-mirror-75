c6a8b8f57a01057cbd3ae0bfb89b5260ae1364bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package basicdata/2.0.0/basicdata-2.0.0-py3-none-any.whl

---
+++ packages/basicdata/basicdata-2.0.0-py3-none-any.whl/basicdata/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='basicdata',
+    version='2.0.0',
+    author='William Ma',
+    author_email='3327821469@qq.com',
+    url='https://github.com/theCoder-WM',
+    description=u'basic data for both Chinese and English  中英文的一些基本数据',
+    packages=['basicdata'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'type_of_object=basicdata:type_of_object',
+            'is_one_letter=basicdata:is_one_letter',
+            'is_all_letter=basicdata:is_all_letter'
+            'is_special_symbols=basicdata:is_special_symbols',
+            'is_valid_phone_num=basicdata:is_valid_phone_num',
+        ]
+    }
+)
