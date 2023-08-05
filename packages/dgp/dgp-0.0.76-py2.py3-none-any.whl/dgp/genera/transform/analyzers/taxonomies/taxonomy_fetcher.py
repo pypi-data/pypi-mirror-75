47d9eded369b6373481e9bdacac8e985c262dcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package dgp/0.0.76/dgp-0.0.76-py2.py3-none-any.whl

---
+++ packages/dgp/dgp-0.0.76-py2.py3-none-any.whl/dgp/genera/transform/analyzers/taxonomies/taxonomy_fetcher.py
@@ -0,0 +1,25 @@
+from .....core import BaseAnalyzer, Validator, Required
+from .....taxonomies import Taxonomy
+from .....config.consts import CONFIG_TAXONOMY_ID, CONFIG_TAXONOMY_CT, CONFIG_TAXONOMY_SETTINGS,\
+    CONFIG_TAXONOMY_MISSING_VALUES
+
+
+class TaxonomyFetcherAnalyzer(BaseAnalyzer):
+
+    REQUIRES = Validator(
+        Required(CONFIG_TAXONOMY_ID)
+    )
+
+    def run(self):
+        tid = self.config.get(CONFIG_TAXONOMY_ID)
+        t: Taxonomy = self.context.taxonomies.get(tid)
+        self.config[CONFIG_TAXONOMY_CT] = t.column_types
+        self.config[CONFIG_TAXONOMY_SETTINGS] = t.config
+        if self.config[CONFIG_TAXONOMY_MISSING_VALUES] is None:
+            self.config[CONFIG_TAXONOMY_MISSING_VALUES] = t.missingValues
+
+    def analyze(self):
+        if self.test():
+            self.run()
+            return True
+        return False
