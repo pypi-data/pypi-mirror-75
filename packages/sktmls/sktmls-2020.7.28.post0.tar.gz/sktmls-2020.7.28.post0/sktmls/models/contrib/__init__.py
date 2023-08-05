2330eadaca97e3410e5c980b08fafecf6bbc9c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package sktmls/2020.7.28.post0/sktmls-2020.7.28.post0.tar.gz

---
+++ packages/sktmls/sktmls-2020.7.28.post0.tar.gz/sktmls-2020.7.28.post0/sktmls/models/contrib/__init__.py
@@ -0,0 +1,20 @@
+from .default_model import DefaultLightGBMModel, DefaultXGBoostModel
+from .sample_model import SampleModel
+from .sample_rule_model import SampleRuleModel
+from .random_pick_model import RandomPickModel
+from .bnf_loyalty_rule_model import BnfLoyaltyRuleModel
+from .info_unpaid_rule_model import InfoUnpaidRuleModel
+from .mbr_vip_info_rule_model import MbrVipInfoRuleModel
+from .info_defect_rule_model import InfoDefectRuleModel
+
+__all__ = [
+    "DefaultLightGBMModel",
+    "DefaultXGBoostModel",
+    "SampleModel",
+    "SampleRuleModel",
+    "RandomPickModel",
+    "BnfLoyaltyRuleModel",
+    "InfoUnpaidRuleModel",
+    "MbrVipInfoRuleModel",
+    "InfoDefectRuleModel",
+]
