a36fa03f1bfd90c016d5a8d3ed1f9b42b987b652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package featexp/0.0.7/featexp-0.0.7-py3-none-any.whl

---
+++ packages/featexp/featexp-0.0.7-py3-none-any.whl/featexp/__init__.py
@@ -0,0 +1,9 @@
+from featexp.base import (
+    draw_plots,
+    get_grouped_data,
+    get_trend_changes,
+    get_trend_correlation,
+    get_trend_stats,
+    get_univariate_plots,
+    univariate_plotter,
+)
