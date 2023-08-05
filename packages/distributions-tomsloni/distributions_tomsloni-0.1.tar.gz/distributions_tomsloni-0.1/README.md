1a9f498f948c26cba5be6c59778b4e22e9ef2954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:54 +0000

    Add package distributions-tomsloni/0.1/distributions_tomsloni-0.1.tar.gz

---
+++ packages/distributions-tomsloni/distributions_tomsloni-0.1.tar.gz/distributions_tomsloni-0.1/README.md
@@ -0,0 +1,19 @@
+# distributions_tomsloni package
+
+aaaa
+
+## Installation
+
+aaa
+
+## Usage
+
+aaa
+
+aaa
+
+## Contributing
+aaa
+
+## License
+aaa
---
+++ packages/distributions-tomsloni/distributions_tomsloni-0.1.tar.gz/distributions_tomsloni-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_tomsloni',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_tomsloni'],
+      zip_safe=False)
