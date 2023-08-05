083bc7668d01cb4daa6e75e1ee7e1277ab36788d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package elashhab-distributions/0.0.1/elashhab-distributions-0.0.1.tar.gz

---
+++ packages/elashhab-distributions/elashhab-distributions-0.0.1.tar.gz/elashhab-distributions-0.0.1/elashhab-distributions/__init__.py
@@ -0,0 +1,1 @@
+from .Gaussian_module import Gaussian
---
+++ packages/elashhab-distributions/elashhab-distributions-0.0.1.tar.gz/elashhab-distributions-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+	name = "elashhab-distributions",
+	version = "0.0.1",
+	author = "Hadeer El Ashhab",
+	author_email = "alias@domain.com",
+	description = "test package for udacity course",
+	packages = setuptools.find_packages(),
+	python_requires= ">=3.6",
+	classifiers = ["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent"])
