e5c33399d1deabddf6662e75a77be0f299aa98c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:37 +0000

    Add package arkadip-dsnd-distributions/0.1/arkadip_dsnd_distributions-0.1.tar.gz

---
+++ packages/arkadip-dsnd-distributions/arkadip_dsnd_distributions-0.1.tar.gz/arkadip_dsnd_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='arkadip_dsnd_distributions',
+      version='0.1',
+      description='Arkadip"s Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
