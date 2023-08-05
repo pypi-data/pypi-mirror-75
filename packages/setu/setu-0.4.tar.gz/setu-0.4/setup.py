281f485ca9bd3128bfb27be245b1ec6cee1f6a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:35 +0000

    Add package setu/0.4/setu-0.4.tar.gz

---
+++ packages/setu/setu-0.4.tar.gz/setu-0.4/setup.py
@@ -0,0 +1,19 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(name='setu',
+      version='0.4',
+      url='https://gitlab.com/setu-lobby/seu-pypi',
+      author='Gandharva B',
+      author_email='gandharva@setu.co',
+      description="Setu's own SDK for faster integration",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      license='MIT',
+      install_requires=["requests", "uuid", "PyJWT"],
+      packages=['setu'],
+      zip_safe=False)
