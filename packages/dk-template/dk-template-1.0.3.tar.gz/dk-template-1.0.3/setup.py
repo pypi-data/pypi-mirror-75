5af6e86bd161c91f07c8eadde3764c9417827449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:37 +0000

    Add package dk-template/1.0.3/dk-template-1.0.3.tar.gz

---
+++ packages/dk-template/dk-template-1.0.3.tar.gz/dk-template-1.0.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""dk-template - debugging django templates.
+"""
+import sys
+
+classifiers = """\
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Topic :: Software Development :: Libraries
+"""
+
+import setuptools
+
+version = '1.0.3'
+
+
+setuptools.setup(
+    name='dk-template',
+    version=version,
+    install_requires=[
+        'django==1.8.19',
+        'dk'
+    ],
+    author='Bjorn Pettersen',
+    author_email='bp@datakortet.no',
+    description=__doc__.strip(),
+    classifiers=[line for line in classifiers.split('\n') if line],
+    long_description=open('README.rst').read(),
+    packages=setuptools.find_packages(exclude=['tests']),
+    zip_safe=False,
+)
