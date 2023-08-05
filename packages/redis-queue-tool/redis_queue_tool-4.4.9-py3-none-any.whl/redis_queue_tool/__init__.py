946a9cfe610e23b82f7fc1714bb3479cf41aefb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package redis-queue-tool/4.4.9/redis_queue_tool-4.4.9-py3-none-any.whl

---
+++ packages/redis-queue-tool/redis_queue_tool-4.4.9-py3-none-any.whl/redis_queue_tool/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 19/10/12 下午12:13
+# @Author  : cc
+# @FileName: __init__.py.py
+from .producter_consumer import *
+from .init_config_file import use_config_form_distributed_frame_config_module
+
+use_config_form_distributed_frame_config_module()
