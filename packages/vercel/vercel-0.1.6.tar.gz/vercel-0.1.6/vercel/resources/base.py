b95b9c52b52c00b6c4087e83c945cedc78897471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:24 +0000

    Add package vercel/0.1.6/vercel-0.1.6.tar.gz

---
+++ packages/vercel/vercel-0.1.6.tar.gz/vercel-0.1.6/vercel/resources/base.py
@@ -0,0 +1,52 @@
+import requests
+import vercel
+from vercel.exceptions import VercelError
+
+class Resource:
+    @classmethod
+    def make_request(cls, method, resource, api_version=None, base_url='api.vercel.com', data=None, query_string={}, api_key=None, team_id=None):
+        if api_key is None:
+            api_key = vercel.api_key
+
+        if team_id is None:
+            team_id = vercel.team_id
+
+        if api_key is None:
+            raise Exception(f'api_key was not found')
+            
+        if api_version is None:
+            raise Exception(f'api_version was not found')
+
+        try:
+            url = f'https://{base_url}/{api_version}{resource}'
+
+            kwargs = dict(
+                method=method,
+                url=url,
+                headers={
+                    'Content-Type': 'application/json',
+                    'Authorization': f'Bearer {vercel.api_key}'
+                },
+                params=query_string
+            )
+
+            if data is not None:
+                kwargs['json'] = data
+
+            if team_id is not None:
+                kwargs['params']['teamId'] = team_id
+
+            response = requests.request(**kwargs)
+            status_code = response.status_code
+            response = response.json()
+
+            if 'error' in response:
+                print(response)
+                raise VercelError(
+                    code=response['error']['code'],
+                    message=response['error']['message']
+                )
+
+            return response
+        except Exception as e:
+            raise e
