7f2c735ce578ba3068c77ebf1e6af1ff32149b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:14 +0000

    Add package curve-distributions/0.1/curve_distributions-0.1.tar.gz

---
+++ packages/curve-distributions/curve_distributions-0.1.tar.gz/curve_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='curve_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['curve_distributions'],
+      author ='Debanshu Datta',
+      author_email='debanshudatta123@gmail.com',
+      zip_safe=False)
