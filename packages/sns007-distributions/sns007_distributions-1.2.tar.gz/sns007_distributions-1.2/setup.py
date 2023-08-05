8c4c3328c7b6d850f61fc465b64faf1ec377e85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:33 +0000

    Add package sns007-distributions/1.2/sns007_distributions-1.2.tar.gz

---
+++ packages/sns007-distributions/sns007_distributions-1.2.tar.gz/sns007_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sns007_distributions',
+      version='1.2',
+      description='sns007: Gaussian and Binomial distributions',
+      packages=['sns007_distributions'],
+      author="Somesh Saini1970",
+      author_email="somesh.saini1970@gmail.com",
+      zip_safe=False)
