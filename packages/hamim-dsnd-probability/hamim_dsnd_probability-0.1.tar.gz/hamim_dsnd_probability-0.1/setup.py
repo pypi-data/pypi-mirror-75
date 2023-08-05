0404781911ec83b451c23e04a0f531949e21df6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:17 +0000

    Add package hamim-dsnd-probability/0.1/hamim_dsnd_probability-0.1.tar.gz

---
+++ packages/hamim-dsnd-probability/hamim_dsnd_probability-0.1.tar.gz/hamim_dsnd_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hamim_dsnd_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hamim_dsnd_probability'],
+      zip_safe=False)
