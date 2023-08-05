11a1d5064f73ac93c28cb86e667330c5a0df32c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:14 +0000

    Add package pytest-databricks/0.0.6/pytest-databricks-0.0.6.tar.gz

---
+++ packages/pytest-databricks/pytest-databricks-0.0.6.tar.gz/pytest-databricks-0.0.6/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytest-databricks",
+    version="0.0.6",
+    author="Ivan Georgiev",
+    #author_email="ivan.georgiev",
+    description="Pytest plugin for remote Databricks notebooks testing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ivangeorgiev/pytest-databricks",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pydbr',
+        'pytest'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Pytest",
+    ],
+    entry_points={
+        'pytest11': [
+            'databricks = pytest_databricks.pytest.plugin',
+        ]
+    },
+    python_requires='>=3.6',
+)
