c047e6436431f5fe7d71455cde9a234ba504e151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:17 +0000

    Add package ynab-sdk/0.1.0/ynab-sdk-0.1.0.tar.gz

---
+++ packages/ynab-sdk/ynab-sdk-0.1.0.tar.gz/ynab-sdk-0.1.0/test/support/mock.py
@@ -0,0 +1,12 @@
+def build_get_mock(return_value):
+    def mock_get(_self, _endpoint):
+        return return_value
+
+    return mock_get
+
+
+def build_post_mock():
+    def mock_post(_self, _endpoint, _payload):
+        return {}
+
+    return mock_post
