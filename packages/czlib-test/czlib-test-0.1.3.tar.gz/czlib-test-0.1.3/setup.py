4dc0fcd79292e8b6cfd04016f3e0d03104e8542e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package czlib-test/0.1.3/czlib-test-0.1.3.tar.gz

---
+++ packages/czlib-test/czlib-test-0.1.3.tar.gz/czlib-test-0.1.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="czlib-test",
+    version="0.1.3",
+    author="李承泽",
+    author_email="3161399620@qq.com",
+    description="橙子工作室室长-李承泽自主研发pygame一库通",
+    long_description_contect_type="text/markdown",
+    url="https://github.com/",
+    packpages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
