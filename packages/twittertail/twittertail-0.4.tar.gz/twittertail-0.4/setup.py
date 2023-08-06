5ce28e1f6bc0d3a67cc964e36f21cb0f4b1fda88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:01 +0000

    Add package twittertail/0.4/twittertail-0.4.tar.gz

---
+++ packages/twittertail/twittertail-0.4.tar.gz/twittertail-0.4/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from setuptools import setup
+setup(
+    name='twittertail',
+    packages=['twittertail'],
+    version='0.4',
+    license='MIT',
+    description='Tail a twitter account from the command line.',
+    author='Nathan Davison',
+    author_email='ndavison85@gmail.com',
+    url='https://github.com/ndavison/twittertail',
+    download_url='https://github.com/ndavison/twittertail/archive/v04.tar.gz',
+    keywords=['twitter', 'tweets', 'cli'],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'asyncio',
+        'aiohttp',
+        'colored',
+    ],
+    entry_points={
+        'console_scripts': [
+            'twittertail=twittertail.__main__:main',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
