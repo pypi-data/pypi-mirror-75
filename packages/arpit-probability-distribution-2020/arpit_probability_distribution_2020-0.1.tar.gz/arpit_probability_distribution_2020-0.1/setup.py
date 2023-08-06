96c2f28a94eb712353522678cbdb60974d873a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:31 +0000

    Add package arpit-probability-distribution-2020/0.1/arpit_probability_distribution_2020-0.1.tar.gz

---
+++ packages/arpit-probability-distribution-2020/arpit_probability_distribution_2020-0.1.tar.gz/arpit_probability_distribution_2020-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='arpit_probability_distribution_2020',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['arpit_probability_distribution_2020'],
+      author = 'Arpit Maheshwari',
+      author_email = 'arpitmaheshwari14@gmail.com',
+      zip_safe=False)
