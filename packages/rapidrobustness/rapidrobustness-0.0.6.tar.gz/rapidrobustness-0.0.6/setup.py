f4326d5ba7680fcf2f4c138036d758211a9eeb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:12 +0000

    Add package rapidrobustness/0.0.6/rapidrobustness-0.0.6.tar.gz

---
+++ packages/rapidrobustness/rapidrobustness-0.0.6.tar.gz/rapidrobustness-0.0.6/setup.py
@@ -0,0 +1,31 @@
+"""RAPID (Robustness Analysis Producing Intelligent Decisions)"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rapidrobustness",
+    version="0.0.6",
+    author="Cameron McPhail",
+    author_email="cameron.mcphail@adelaide.edu.au",
+    description="Calculations and analyses of robustness.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cameronmcphail/RAPID",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'pandas',
+        'seaborn',
+        'ema_workbench'
+    ]
+)
