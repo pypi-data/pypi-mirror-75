b9231683020f6e70659ce24cfcfafa22cd730456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:36 +0000

    Add package yuuno-core/1.3.1/yuuno-core-1.3.1.zip

---
+++ packages/yuuno-core/yuuno-core-1.3.1.zip/yuuno-core-1.3.1/yuuno/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Yuuno - IPython + VapourSynth
+# Copyright (C) 2017,2018 StuxCrystal (Roland Netzsch <stuxcrystal@encode.moe>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import sys
+
+__author__ = """stuxcrystal"""
+__email__ = 'stuxcrystal@encode.moe'
+__version__ = '1.3'
+
+
+if sys.version_info < (3, 6):
+    raise ImportError(
+        "Yuuno requires Python 3.6. "
+        "Please make sure you are using this version."
+    )
+
+from yuuno.yuuno import Yuuno
+from yuuno.autodiscover import discover_environments
+from yuuno.standalone import init_standalone
+__all__ = ["Yuuno", 'init_standalone'] + discover_environments(vars())
