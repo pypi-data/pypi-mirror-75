b1f8510ec6f1d9dc0acbba6824c1d95d36acf836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:51 +0000

    Add package soloman/2.3/soloman-2.3-py3-none-any.whl

---
+++ packages/soloman/soloman-2.3-py3-none-any.whl/soloman/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 12:50:44 2020
+
+"""
+import os
+from time import sleep
+from PyQt5.QtQml import qmlRegisterType
+
+# import Audio and Video here so people can import it without
+# going into the audio and video packages to access them
+from .audio import Audio
+from .video import Video
+from soloman.pyqt_inter_audio import QAudio
+from soloman.pyqt_inter_video import QVideo
+from soloman.fps_clocks import *
+
+
+def register():
+
+    directory = __file__.replace('\\soloman\\__init__.py', '')
+    if 'QML2_IMPORT_PATH' in os.environ:
+        os.environ['QML2_IMPORT_PATH'] += ';' + directory
+    else:
+        os.environ['QML2_IMPORT_PATH'] = directory
+
+    # Delete the contents of our Temp folder
+    path = os.path.join(os.environ['TEMP'], 'soloman', 'converts')
+    if os.path.exists(path):
+        os.system('RD "' + path + '" /S /Q')
+
+    qmlRegisterType(QAudio, 'soloman', 2, 2, 'SAudio')
+    qmlRegisterType(QVideo, 'soloman', 2, 2, 'QVideo')
+
+
+register()
