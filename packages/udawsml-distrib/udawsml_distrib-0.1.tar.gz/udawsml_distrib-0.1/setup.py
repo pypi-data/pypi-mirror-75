c1170061fb0923f25fa2bf592a442c414c2fe821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:58 +0000

    Add package udawsml-distrib/0.1/udawsml_distrib-0.1.tar.gz

---
+++ packages/udawsml-distrib/udawsml_distrib-0.1.tar.gz/udawsml_distrib-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udawsml_distrib',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udawsml_distrib'],
+      zip_safe=False)
