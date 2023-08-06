0249e246cc7339d5f9ec3b203e12b93e1ce1bdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:10 +0000

    Add package wdtestlib/1.0.0/wdtestlib-1.0.0.zip

---
+++ packages/wdtestlib/wdtestlib-1.0.0.zip/wdtestlib-1.0.0/Tool.py
@@ -0,0 +1,9 @@
+# from sztestlib import szlibone
+#
+# szlibone.run()
+
+# import sztestlib
+# sztestlib.szlibone.run()
+
+def tt():
+    print("ttttt")
---
+++ packages/wdtestlib/wdtestlib-1.0.0.zip/wdtestlib-1.0.0/setup.py
@@ -0,0 +1,16 @@
+
+# from distutils.core import setup
+from setuptools import setup
+
+
+def readme_file():
+    with open("README.rst", encoding="utf-8")as rf:
+        return rf.read()
+
+
+setup(name="wdtestlib", version="1.0.0", description="this is a awesome lib", packages=["wdtestlib"], py_modules=["Tool"], author="Sz", author_email="501562071@qq.com", long_description=readme_file(), url="https://github.com/wangshunzi/Python_code", license="MIT")
+
+
+
+
+
