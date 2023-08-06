db3c9e89c7a37bb753d1656ce062d8f10332657b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:38 +0000

    Add package lykkellemainisrael/0.0.1/lykkellemainisrael-0.0.1-py2-none-any.whl

---
+++ packages/lykkellemainisrael/lykkellemainisrael-0.0.1-py2-none-any.whl/lykkellemainisrael/healthcheckasia0.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 20:47:59 2019
+daily checklist that runs a health check on the load details
+@author: debaj
+"""
+from os.path import expanduser
+from lykkelledatahandler.exceptionasia0eod import exception
+
+home = expanduser("~")
+
+
+class exceptionlog:
+    def __init__(self):
+        print("-------------------------stockexceptionlog-------------------------------")
+        exception.stockexception()
+        print("-------------------------End-------------------------------")
+        print("\n")
+        print("-------------------------Benchmarkexceptionlog-------------------------------")
+        exception.benchmarkexception()
+        print("-------------------------End-------------------------------")
+
+#exceptionlog()
+
+
