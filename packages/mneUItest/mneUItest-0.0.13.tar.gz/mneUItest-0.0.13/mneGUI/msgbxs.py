9a9141b44ac88f7a034d931640cb0dfc6168dab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:12 +0000

    Add package mneUItest/0.0.13/mneUItest-0.0.13.tar.gz

---
+++ packages/mneUItest/mneUItest-0.0.13.tar.gz/mneUItest-0.0.13/mneGUI/msgbxs.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QMessageBox, QApplication, QWidget
+
+
+def alert_msg(window, title, msg):
+    alert = QMessageBox(window)
+    alert.setWindowTitle(title)
+    alert.setText(msg)
+    alert.exec_()
+
+#test
+#
+# app = QApplication(sys.argv)
+# main_window = QWidget()
+# main_window.show()
+# QMessageBox(main_window, "Alert", "message")
+# sys.exit(app.exec_())
