6fb0a61004f03ff79220f78a2be8a4965f066220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:52 +0000

    Add package hey-distributions/0.1/hey_distributions-0.1.tar.gz

---
+++ packages/hey-distributions/hey_distributions-0.1.tar.gz/hey_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hey_distributions',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['hey_distributions'],
+      zip_safe=False)
