c77ffcfb783a94fa20817b0b255b262d8fb4132a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:34 +0000

    Add package distributions-pav/0.1/distributions_pav-0.1.tar.gz

---
+++ packages/distributions-pav/distributions_pav-0.1.tar.gz/distributions_pav-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_pav',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions_pav'],
+      author= 'Pavan',
+      zip_safe=False)
