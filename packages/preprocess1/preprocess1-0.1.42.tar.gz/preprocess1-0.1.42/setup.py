ae31be215273d698f651331e022025f0ecf99c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:28 +0000

    Add package preprocess1/0.1.42/preprocess1-0.1.42.tar.gz

---
+++ packages/preprocess1/preprocess1-0.1.42.tar.gz/preprocess1-0.1.42/setup.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2019-2020 Fahad Akbar <fahad.akbar@gmail.com>
+# License: MIT, m.akbar@queensu.ca , fahad.akbar@gmail.com
+
+"""
+PreProcess1 - An end-to-end open source data preprocessing tool
+
+"""
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+requirements = ["scikit-learn","datetime","pandas","numpy","ipywidgets","IPython",
+"scipy","datefinder","pyod","lightgbm"] # add plotly later on
+
+
+setup(
+    name="preprocess1",
+    version="0.1.42",
+    author="Fahad Akbar",
+    author_email="fahad.akbar@gmail.com",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mfahadakbar",
+    packages=find_packages(),
+    install_requires=requirements
+)
