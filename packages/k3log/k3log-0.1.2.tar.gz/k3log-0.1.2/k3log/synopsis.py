fd911b34d09049012e9e84676da6ed6a2abc99a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:00 +0000

    Add package k3log/0.1.2/k3log-0.1.2.tar.gz

---
+++ packages/k3log/k3log-0.1.2.tar.gz/k3log-0.1.2/k3log/synopsis.py
@@ -0,0 +1,14 @@
+# make a file logger in one line
+logger = pk3logutil.make_logger('/tmp', level='INFO', fmt='%(message)s',
+                                datefmt="%H:%M:%S")
+logger.info('foo')
+
+logger.stack_str(fmt="{fn}:{ln} in {func}\n  {statement}", sep="\n")
+# runpy.py:174 in _run_module_as_main
+#   "__main__", fname, loader, pkg_name)
+# runpy.py:72 in _run_code
+#   exec code in run_globals
+# ...
+# test_logutil.py:82 in test_deprecate
+#   pk3logutil.deprecate()
+#   'foo', fmt='{fn}:{ln} in {func}\n  {statement}', sep='\n')
