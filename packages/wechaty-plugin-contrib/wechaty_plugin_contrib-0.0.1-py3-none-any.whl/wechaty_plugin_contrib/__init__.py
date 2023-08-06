9d8c9fee0da16f70524c46bac95351fc0babd747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:40 +0000

    Add package wechaty-plugin-contrib/0.0.1/wechaty_plugin_contrib-0.0.1-py3-none-any.whl

---
+++ packages/wechaty-plugin-contrib/wechaty_plugin_contrib-0.0.1-py3-none-any.whl/wechaty_plugin_contrib/__init__.py
@@ -0,0 +1,13 @@
+"""import all contrib"""
+from wechaty_plugin_contrib.ding_dong_plugin import DingDongPlugin
+from wechaty_plugin_contrib.daily_plugin import (
+    DailyPluginOptions,
+    DailyPlugin
+)
+
+__all__ = [
+    'DingDongPlugin',
+
+    'DailyPluginOptions',
+    'DailyPlugin'
+]
