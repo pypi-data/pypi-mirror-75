b2a9c5c4ef0d6e0d630ff63fd07ecf82ad4cae53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:11 +0000

    Add package dimitar-test/0.1/dimitar_test-0.1.tar.gz

---
+++ packages/dimitar-test/dimitar_test-0.1.tar.gz/dimitar_test-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dimitar_test',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dimitar_test'],
+      zip_safe=False)
