e9fea6b48daa43fbf81ca92882dce9d7574c826a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:38 +0000

    Add package jah-distribution-2020-12-28/0.1/jah-distribution_2020_12_28-0.1.tar.gz

---
+++ packages/jah-distribution-2020-12-28/jah-distribution_2020_12_28-0.1.tar.gz/jah-distribution_2020_12_28-0.1/README.md
@@ -0,0 +1,1 @@
+This is just a trial of uploading the 
---
+++ packages/jah-distribution-2020-12-28/jah-distribution_2020_12_28-0.1.tar.gz/jah-distribution_2020_12_28-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jah-distribution_2020_12_28',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      
+      zip_safe=False)
