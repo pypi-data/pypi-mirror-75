c256e65344dbbc1f1bd7e578c06dfc42a12eb0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:57 +0000

    Add package dsnd-distributions-rim119/0.1/dsnd_distributions_rim119-0.1.tar.gz

---
+++ packages/dsnd-distributions-rim119/dsnd_distributions_rim119-0.1.tar.gz/dsnd_distributions_rim119-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_rim119',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
