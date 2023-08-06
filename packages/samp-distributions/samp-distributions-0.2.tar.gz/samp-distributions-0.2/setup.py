04a5fd336886d209e5c0c144331e9c30485e39a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:44 +0000

    Add package samp-distributions/0.2/samp-distributions-0.2.tar.gz

---
+++ packages/samp-distributions/samp-distributions-0.2.tar.gz/samp-distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='samp-distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['samp-distributions'],
+      zip_safe=False)
