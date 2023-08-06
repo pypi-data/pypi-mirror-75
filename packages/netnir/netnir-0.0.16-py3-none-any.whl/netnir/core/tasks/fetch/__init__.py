21051efe2c567ac6ccfe38f16a2cc772ffb97931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:56 +0000

    Add package netnir/0.0.16/netnir-0.0.16-py3-none-any.whl

---
+++ packages/netnir/netnir-0.0.16-py3-none-any.whl/netnir/core/tasks/fetch/__init__.py
@@ -0,0 +1,26 @@
+from netnir.helpers.scaffold.subcommand import SubCommandParser
+
+
+"""fetch subcommands initialization
+"""
+
+
+class Fetch(SubCommandParser):
+    """
+    fetch subcommand parser
+    """
+
+    title = "fetch commands"
+    tasks = {
+        "config": {
+            "class": "netnir.core.tasks.fetch.config.FetchConfig",
+            "description": "fetch current config from a network device",
+        },
+    }
+
+    def __init__(self, args):
+        """initialize the class
+        """
+        self.args = args
+
+        super().__init__(args=self.args)
