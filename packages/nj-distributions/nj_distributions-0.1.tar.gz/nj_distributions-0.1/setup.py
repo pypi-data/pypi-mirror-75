b0237b6738ef3297960a041f43d8083fe686a9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:08 +0000

    Add package nj-distributions/0.1/nj_distributions-0.1.tar.gz

---
+++ packages/nj-distributions/nj_distributions-0.1.tar.gz/nj_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nj_distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['nj_distributions'],
+      author = 'Nikita Jain',
+      author_email = 'jnikita356@gmail.com',
+      zip_safe=False)
