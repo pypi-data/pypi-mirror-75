7de7faac64a9681cbff9013850dd484eee68d3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:09 +0000

    Add package duel/3.3/duel-3.3.tar.gz

---
+++ packages/duel/duel-3.3.tar.gz/duel-3.3/duel/redForestMap.py
@@ -0,0 +1,21 @@
+"""
+.. module:: map
+.. synopsis: module for a map object
+"""
+from duel.map import Map
+
+class RedForestMap(Map):
+    """Class representing a Specific Map
+        """
+    def __init__(self):
+
+        assets_dict = {
+            "Tile": "Resources/Images/RedForestTile.png",
+            "Background": "Resources/Images/ScaledBackgroundAutumnForest.png",
+            "Spike": "Resources/Images/DesolateDesertSpike.png",
+            "MontoyaFlag": "Resources/Images/MontoyaFlag.png",
+            "ZorroFlag": "Resources/Images/ZorroFlag.png",
+            "KingFlag": "Resources/Images/KingArthurFlag.png"
+        }
+        Map.__init__(self, 1000, 600, -130, 140, 675, 140, assets_dict, "Resources/redForestMap.txt")
+        self.songName = "Red Forest"
