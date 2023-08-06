3696a0d7c89b748f14e3ebcc4ca40a7abcc31f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:57 +0000

    Add package wagtail-meta-preview/1.0.0/wagtail_meta_preview-1.0.0-py3-none-any.whl

---
+++ packages/wagtail-meta-preview/wagtail_meta_preview-1.0.0-py3-none-any.whl/wagtail_meta_preview/wagtail_hooks.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from django.utils.html import format_html
+from wagtail.admin.staticfiles import versioned_static
+from wagtail.core import hooks
+from .views import get_image_rendition
+
+
+@hooks.register("insert_global_admin_css", order=100)
+def global_admin_css():
+    """Add /static/css/custom.css to the admin."""
+    return format_html(
+        '<link rel="stylesheet" href="{}">',
+        versioned_static("wagtail_meta_preview/admin.css"),
+    )
+
+
+@hooks.register("insert_global_admin_js", order=100)
+def global_admin_js():
+    """Add /static/css/custom.js to the admin."""
+    return format_html(
+        '<script src="{}"></script>', versioned_static("wagtail_meta_preview/admin.js"),
+    )
+
+
+@hooks.register("register_admin_urls")
+def urlconf_time():
+    return [
+        path(
+            "get-img-rendition/<int:pk>/",
+            get_image_rendition,
+            name="get_image_rendition",
+        ),
+    ]
