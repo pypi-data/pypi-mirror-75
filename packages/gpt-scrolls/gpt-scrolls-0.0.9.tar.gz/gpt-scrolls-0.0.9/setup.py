f0c6b931ea4219a5fd8679858cd7c0430d8f6c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:20 +0000

    Add package gpt-scrolls/0.0.9/gpt-scrolls-0.0.9.tar.gz

---
+++ packages/gpt-scrolls/gpt-scrolls-0.0.9.tar.gz/gpt-scrolls-0.0.9/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import sys
+import os.path
+
+
+NAME = 'gpt-scrolls'
+
+# Don't import module here, since deps may not be installed
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'scrolls'))
+
+from version import VERSION
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author="Manuel Araoz",
+    author_email="gpt-scrolls@maraoz.com",
+    description="A collaborative collection of open-source safe GPT-3 prompts that work well",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maraoz/gpt-scrolls",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "openai>=0.2.4,<1.0.0"
+    ],
+    package_data={
+        "": ["**/*.txt", "**/*.json"],
+    },
+)
