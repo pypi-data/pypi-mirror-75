ddfc29b3d4a5de257da33149073fdca41baf4b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:20 +0000

    Add package jlrpy/1.4.0/jlrpy-1.4.0.tar.gz

---
+++ packages/jlrpy/jlrpy-1.4.0.tar.gz/jlrpy-1.4.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jlrpy",
+    version="1.4.0",
+    author="Edvard",
+    author_email="edvard.holst@gmail.com",
+    description="Control your Jaguar I-Pace",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ardevd/jlrpy",
+    py_modules=['jlrpy'],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+    ],
+)
