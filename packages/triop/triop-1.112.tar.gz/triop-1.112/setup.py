87d808a70546b4cd9828cbf1a1733d44b8a8f7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:09 +0000

    Add package triop/1.112/triop-1.112.tar.gz

---
+++ packages/triop/triop-1.112.tar.gz/triop-1.112/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='triop',
+      version='1.112',
+      description='Gaussian distributions',
+      packages=['triop'],
+      author = 'Vishesh Jain',
+      
+      zip_safe=False)
