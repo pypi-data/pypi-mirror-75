889389d617aa13286939dd1502e2f73b76aa564b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:17 +0000

    Add package proofdock-chaos-kit/1.0.3/proofdock_chaos_kit-1.0.3-py2.py3-none-any.whl

---
+++ packages/proofdock-chaos-kit/proofdock_chaos_kit-1.0.3-py2.py3-none-any.whl/pdchaoskit/__init__.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from chaoslib.discovery.discover import (discover_probes,
+                                         initialize_discovery_result)
+from chaoslib.types import DiscoveredActivities, Discovery
+from logzero import logger
+
+__version__ = '1.0.3'
+
+
+def discover(discover_system: bool = True) -> Discovery:
+    """
+    Discover Proofdock capabilities from this extension.
+    """
+    logger.info("Discovering capabilities from pdchaoskit")
+
+    discovery = initialize_discovery_result(
+        "proofdock-chaos-kit", __version__, "pdchaoskit")
+    discovery["activities"].extend(load_exported_activities())
+
+    return discovery
+
+
+###############################################################################
+# Private functions
+###############################################################################
+def load_exported_activities() -> List[DiscoveredActivities]:
+    """
+    Extract metadata from actions and probes exposed by this extension.
+    """
+    activities = []
+    activities.extend(discover_probes("pdchaoskit.probes"))
+
+    return activities
