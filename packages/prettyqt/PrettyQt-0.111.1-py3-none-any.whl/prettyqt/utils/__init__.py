79bf58a4a1fcf1cda9364a4d8ed47e7682af6046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:11 +0000

    Add package prettyqt/0.111.1/PrettyQt-0.111.1-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.111.1-py3-none-any.whl/prettyqt/utils/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Module containing helper functions."""
+
+from typing import Mapping, Union, Iterable
+
+import bidict as bdct
+
+
+class bidict(bdct.bidict):
+    def __init__(self, *args, **kwargs):
+        if len(args) == 1 and isinstance(args[0], dict):
+            super().__init__(args[0])
+        else:
+            super().__init__(kwargs)
+
+
+class InvalidParamError(ValueError):
+    """Exception raised for invalid params in method calls.
+
+    Args:
+        value: param value which caused the error
+        valid_options: allowed options
+    """
+
+    def __init__(self, value, valid_options: Union[Iterable, Mapping, bidict]):
+        self.value = value
+        if isinstance(valid_options, Mapping):
+            self.valid_options = valid_options.keys()
+        else:
+            self.valid_options = valid_options
+        opts = " / ".join(repr(opt) for opt in valid_options)
+        self.message = f"Invalid value: {value!r}. Allowed options are {opts}."
+        super().__init__(self.message)
