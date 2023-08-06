8b089d7bdc4a10b174cec2f604b23b0423d621d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:48 +0000

    Add package phil-gnb-distributions/0.1/phil_gnb_distributions-0.1.tar.gz

---
+++ packages/phil-gnb-distributions/phil_gnb_distributions-0.1.tar.gz/phil_gnb_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='phil_gnb_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['phil_gnb_distributions'],
+      author = "Philemon Johnson",
+      author_email = "philjohnon96@gmail.com",
+      zip_safe=False)
