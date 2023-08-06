01dda889362cb4022da3be6eb4360fa9b3e48511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:42 +0000

    Add package aiogithubapi/1.1.0/aiogithubapi-1.1.0.tar.gz

---
+++ packages/aiogithubapi/aiogithubapi-1.1.0.tar.gz/aiogithubapi-1.1.0/setup.py
@@ -0,0 +1,25 @@
+"""Setup configuration."""
+import setuptools
+
+with open("README.md", "r") as fh:
+    DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="aiogithubapi",
+    version="1.1.0",
+    author="Joakim Sorensen (@ludeeus)",
+    author_email="hi@ludeeus.dev",
+    description="Asynchronous Python client for the GitHub API",
+    install_requires=["aiohttp>=3.6.1,<4.0", "async_timeout", "backoff"],
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ludeeus/aiogithubapi",
+    packages=setuptools.find_packages(
+        exclude=["tests", "tests.*", "generate", "generate.*"]
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
