27648ac2c08af7b0157a9ca92e87c923b7696f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:24 +0000

    Add package phash-blockhashio/0.1.4/phash-blockhashio-0.1.4.tar.gz

---
+++ packages/phash-blockhashio/phash-blockhashio-0.1.4.tar.gz/phash-blockhashio-0.1.4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+import setuptools
+
+import blockhash
+
+_APP_PATH = os.path.dirname(blockhash.__file__)
+
+with open(os.path.join(_APP_PATH, 'resources', 'README.md')) as f:
+      long_description = f.read()
+
+with open(os.path.join(_APP_PATH, 'resources', 'requirements.txt')) as f:
+      install_requires = [s.strip() for s in f.readlines()]
+
+with open(os.path.join(_APP_PATH, 'resources', 'requirements-testing.txt')) as f:
+      test_requires = [s.strip() for s in f.readlines()]
+
+setuptools.setup(
+    name='phash-blockhashio',
+    version=blockhash.__version__,
+    description='Perceptual image hash calculation tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Commons Machinery',
+    author_email='dev@commonsmachinery.se',
+    license='MIT',
+    scripts=[
+        'blockhash/resources/scripts/blockhash'
+    ],
+    install_requires=install_requires,
+    tests_require=install_requires + test_requires,
+    url='https://github.com/dsoprea/blockhash-python',
+    packages=setuptools.find_packages(exclude=['tests']),
+    include_package_data=True,
+    package_data={
+        'blockhash': [
+            'resources/README.md',
+            'resources/requirements.txt',
+            'resources/requirements-testing.txt',
+        ],
+    },
+)
