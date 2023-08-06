a3384663e0b56b998030dee177072a62448d656f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:12 +0000

    Add package mdwiki/0.2.1/mdwiki-0.2.1.tar.gz

---
+++ packages/mdwiki/mdwiki-0.2.1.tar.gz/mdwiki-0.2.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.MD", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r', encoding='utf-8') as f:
+    dependencies = f.readlines()
+
+setuptools.setup(
+    name="mdwiki",
+    version="0.2.1",
+    author="niceMachine",
+    author_email="xuchaoo@gmail.com",
+    description="zero config static blog builder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drunkpig/mdwiki",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['mdwiki/bin/mdwiki_exec','mdwiki/bin/mdpub'],
+    # entry_points={
+    #       'console_scripts': ['mdpub=mdwiki/bin/mdpub'],
+    #   },
+    install_requires=dependencies,
+    include_package_data=True,
+    python_requires='>=3.7',
+)
