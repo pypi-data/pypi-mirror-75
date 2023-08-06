9d38e93a13a86bdf8253c9ff733766de5d9e11e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:34 +0000

    Add package ramziiss-distributions/0.1/ramziiss_distributions-0.1.tar.gz

---
+++ packages/ramziiss-distributions/ramziiss_distributions-0.1.tar.gz/ramziiss_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ramziiss_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['ramziiss_distributions'],
+      author='ramziiss',
+      author_email='ramziiss@gmail.com',
+      zip_safe=False)
