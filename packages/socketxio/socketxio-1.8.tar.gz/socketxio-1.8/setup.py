9a11c2f9e127946bcd3155543bbe6eee0036a3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:48 +0000

    Add package socketxio/1.8/socketxio-1.8.tar.gz

---
+++ packages/socketxio/socketxio-1.8.tar.gz/socketxio-1.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="socketxio", 
+    version="1.8",
+    author_email="none@example.com",
+    description="A small example package made to help you with socket connections",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
