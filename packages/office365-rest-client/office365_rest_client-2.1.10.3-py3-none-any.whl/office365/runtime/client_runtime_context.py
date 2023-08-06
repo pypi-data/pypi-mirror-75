081d52f87a885d95acb233a15c23e6034e808753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:00 +0000

    Add package office365-rest-client/2.1.10.3/office365_rest_client-2.1.10.3-py3-none-any.whl

---
+++ packages/office365-rest-client/office365_rest_client-2.1.10.3-py3-none-any.whl/office365/runtime/client_runtime_context.py
@@ -0,0 +1,120 @@
+import abc
+
+from office365.runtime.client_query import ReadEntityQuery
+
+
+class ClientRuntimeContext(object):
+
+    def __init__(self, service_root_url, auth_context=None):
+        """
+        Client runtime context for services
+
+        :type service_root_url: str
+        :type auth_context: AuthenticationContext or None
+        """
+        self._service_root_url = service_root_url
+        self._auth_context = auth_context
+        self._current_query = None
+
+    @property
+    def current_query(self):
+        """
+        :rtype: ClientQuery
+        """
+        return self._current_query
+
+    @abc.abstractmethod
+    def get_pending_request(self):
+        """
+        :rtype: office365.runtime.client_request.ClientRequest
+        """
+        pass
+
+    @property
+    def has_pending_request(self):
+        return len(self.get_pending_request().queries) > 0
+
+    def get_next_query(self):
+        for qry in self.get_pending_request():
+            self._current_query = qry
+            yield qry
+
+    def build_request(self):
+        return self.get_pending_request().build_request()
+
+    def authenticate_request(self, request):
+        self._auth_context.authenticate_request(request)
+
+    def load(self, client_object, properties_to_retrieve=None, on_loaded=None):
+        """Prepare query
+
+        :type on_loaded: () -> None
+        :type properties_to_retrieve: list[str] or None
+        :type client_object: office365.runtime.client_object.ClientObject
+        """
+        qry = ReadEntityQuery(client_object, properties_to_retrieve)
+        self.get_pending_request().add_query(qry)
+
+        def _process_response(resp):
+            if self.current_query.id == qry.id:
+                self.get_pending_request().afterExecute -= _process_response
+                on_loaded()
+        if callable(on_loaded):
+            self.get_pending_request().afterExecute += _process_response
+
+    def before_execute(self, action, once=True):
+        """
+        Attach an event handler which is triggered before request is submitted to server
+
+        :param (RequestOptions) -> None action:
+        :param bool once:
+        :return: None
+        """
+
+        def _process_request(request):
+            if once:
+                self.get_pending_request().beforeExecute -= _process_request
+            action(request)
+
+        self.get_pending_request().beforeExecute += _process_request
+
+    def after_execute(self, action, once=True):
+        """
+        Attach an event handler which is triggered after request is submitted to server
+
+        :param (RequestOptions) -> None action:
+        :param bool once:
+        :return: None
+        """
+
+        def _process_response(response):
+            if once:
+                self.get_pending_request().afterExecute -= _process_response
+            action(response)
+
+        self.get_pending_request().afterExecute += _process_response
+
+    def execute_request_direct(self, request):
+        """
+        :type request: RequestOptions
+        """
+        return self.get_pending_request().execute_request_direct(request)
+
+    def execute_query(self):
+        for qry in self.get_pending_request():
+            self._current_query = qry
+            self.get_pending_request().execute_query()
+
+    def add_query(self, query):
+        """
+        Adds query to internal queue
+        :type query: ClientQuery
+        """
+        self.get_pending_request().add_query(query)
+
+    def clear_queries(self):
+        self.get_pending_request().queries.clear()
+
+    @property
+    def service_root_url(self):
+        return self._service_root_url
---
+++ packages/office365-rest-client/office365_rest_client-2.1.10.3-py3-none-any.whl/tests/sharepoint/test_sharepoint_client.py
@@ -0,0 +1,51 @@
+from unittest import TestCase
+from office365.runtime.auth.clientCredential import ClientCredential
+from office365.runtime.auth.userCredential import UserCredential
+from office365.runtime.auth.providers.acs_token_provider import ACSTokenProvider
+from office365.runtime.auth.providers.saml_token_provider import SamlTokenProvider
+from office365.runtime.auth.tokenResponse import TokenResponse
+from office365.runtime.odata.json_light_format import JsonLightFormat
+from office365.runtime.odata.odata_batch_request import ODataBatchRequest
+from office365.runtime.odata.odata_metadata_level import ODataMetadataLevel
+from office365.sharepoint.client_context import ClientContext
+from settings import settings
+
+user_credentials = UserCredential(settings.get('user_credentials').get('username'),
+                                  settings.get('user_credentials').get('password'))
+
+
+class TestSharePointClient(TestCase):
+
+    def test1_connect_with_app_principal(self):
+        credentials = ClientCredential(settings.get('client_credentials').get('client_id'),
+                                       settings.get('client_credentials').get('client_secret'))
+        ctx = ClientContext.connect_with_credentials(settings['url'], credentials)
+        self.assertIsInstance(ctx.authentication_context.provider, ACSTokenProvider)
+        self.assertIsInstance(ctx.authentication_context.provider.token, TokenResponse)
+        self.assertTrue(ctx.authentication_context.provider.token.is_valid)
+
+    def test2_connect_with_user_credentials(self):
+        ctx = ClientContext.connect_with_credentials(settings['url'], user_credentials)
+        self.assertIsInstance(ctx.authentication_context.provider, SamlTokenProvider)
+
+    def test3_init_from_url(self):
+        ctx = ClientContext.from_url(settings['url']).with_credentials(user_credentials)
+        web = ctx.web.load().execute_query()
+        self.assertIsNotNone(web.url)
+
+    def test4_connect_with_client_cert(self):
+        pass
+
+    def test5_construct_batch_request(self):
+        client = ClientContext(settings['url']).with_credentials(user_credentials)
+        current_user = client.web.currentUser
+        client.load(current_user)
+        current_web = client.web
+        client.load(current_web)
+
+        batch_request = ODataBatchRequest(client, JsonLightFormat(ODataMetadataLevel.Verbose))
+
+        def _prepare_request(request):
+            client.ensure_form_digest(request)
+        batch_request.beforeExecute += _prepare_request
+        batch_request.execute_query()
