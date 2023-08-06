a9c803a5db5c6636ffd626f09c74024a02a700dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:31 +0000

    Add package sitecheck/0.8.1.3/sitecheck-0.8.1.3-py3-none-any.whl

---
+++ packages/sitecheck/sitecheck-0.8.1.3-py3-none-any.whl/sitecheck/__main__.py
@@ -0,0 +1,26 @@
+"""
+    Geo-Instruments
+    Sitecheck scanner
+
+    Load project configs from 'project.ini', and loop through the views listed.
+    Each project run retrieves Sensor data from Amp or QV, and produces an Adaptive card.
+    Default output is in "~\\OneDrive - Keller\\scanner\\" for the Teams Flowbow to ingest
+
+"""
+# __author__ = "Dan Edens"
+# __version__= "0.8.2"
+# __url__= "https://geodev.geo-instruments.com/DanEdens/Sitecheck_Scanner"
+
+import asyncio
+
+from sitecheck.scanner import Sitecheck
+
+
+async def main():
+    """Main Entry point for scanner"""
+    await Sitecheck()
+
+
+if __name__ == "__main__":
+    """Prevents main from running automatically when imported"""
+    asyncio.run(main())
