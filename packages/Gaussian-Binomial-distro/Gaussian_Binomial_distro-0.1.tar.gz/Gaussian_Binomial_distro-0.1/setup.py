cd8c557bb0c0297ae604fedc87c4a89c7c02bbef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:15 +0000

    Add package Gaussian-Binomial-distro/0.1/Gaussian_Binomial_distro-0.1.tar.gz

---
+++ packages/Gaussian-Binomial-distro/Gaussian_Binomial_distro-0.1.tar.gz/Gaussian_Binomial_distro-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Gaussian_Binomial_distro',
+      version='0.1',
+      description='Gaussian and Binomal distributions',
+      packages=['Gaussian_Binomial_distro'],
+      author = 'Edper Castro',
+      zip_safe=False)
