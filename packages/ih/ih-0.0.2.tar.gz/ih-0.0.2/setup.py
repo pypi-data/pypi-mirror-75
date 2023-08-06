36d910606a8e14d94b21cd54691d30855787744f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:18 +0000

    Add package ih/0.0.2/ih-0.0.2.tar.gz

---
+++ packages/ih/ih-0.0.2.tar.gz/ih-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="ih",
+    version="0.0.2",
+    author="Katie McLaughlin",
+    author_email="katie@glasnt.com",
+    description="A very persuasive package, for creating embroidery patterns",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/glasnt/ih",
+    install_requires=["click", "pillow", "tabulate"],
+    entry_points={"console_scripts": ["ih = ih.__main__:main"]},
+    packages=["ih"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
