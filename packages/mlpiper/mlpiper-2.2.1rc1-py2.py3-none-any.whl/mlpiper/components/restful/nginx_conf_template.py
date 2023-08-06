59c12d560b25d1c5c090633c927c6caf02ad501d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:42 +0000

    Add package mlpiper/2.2.1rc1/mlpiper-2.2.1rc1-py2.py3-none-any.whl

---
+++ packages/mlpiper/mlpiper-2.2.1rc1-py2.py3-none-any.whl/mlpiper/components/restful/nginx_conf_template.py
@@ -0,0 +1,18 @@
+NGINX_SERVER_CONF_TEMPLATE = """
+server {{
+    client_max_body_size 51m;
+    listen       {port} default_server;
+    listen       [::]:{port} default_server;
+    server_name  _;
+    {access_log_off}
+
+    location / {{
+        include         uwsgi_params;
+
+        # change this to the location of the uWSGI socket file (set in uwsgi.ini)
+        uwsgi_pass                  unix:{sock_filepath};
+        uwsgi_ignore_client_abort   on;
+    }}
+}}
+
+"""
