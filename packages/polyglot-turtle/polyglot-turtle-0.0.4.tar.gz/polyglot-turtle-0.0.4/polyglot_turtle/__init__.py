dc64079f2d9e1443b4959d64d08fd9c04fa9502b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:34 +0000

    Add package polyglot-turtle/0.0.4/polyglot-turtle-0.0.4.tar.gz

---
+++ packages/polyglot-turtle/polyglot-turtle-0.0.4.tar.gz/polyglot-turtle-0.0.4/polyglot_turtle/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: MIT
+
+from .polyglot_turtle import PolyglotTurtle, PolyglotTurtleXiao, PinDirection, PinPullMode, I2cClockRate
+from .hidcborrpc import CommandExecutionFailedException, ConnectFailedException, CommandExecutionTimeoutException, \
+    IdMismatchException
