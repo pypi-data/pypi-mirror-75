087930507c6ce38c432404b4af2249447243c04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:39 +0000

    Add package django-watermark-zemi4/0.1.8/django-watermark-zemi4-0.1.8.tar.gz

---
+++ packages/django-watermark-zemi4/django-watermark-zemi4-0.1.8.tar.gz/django-watermark-zemi4-0.1.8/setup.py
@@ -0,0 +1,47 @@
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+from watermarker import __version__
+
+setup(
+    name='django-watermark-zemi4',
+    version=__version__,
+    packages=find_packages(exclude=['example']),
+    include_package_data=True,
+    license='BSD License',
+    description="Quick and efficient way to apply watermarks to images in Django.",
+    long_description=README,
+    keywords='django, watermark, image, photo, logo',
+    author='zemi4',
+    author_email='zemi4foto@gmail.com',
+    install_requires=[
+        'django>=1.4',
+        'django-appconf',
+        'pillow',
+        'six',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Artistic Software',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Multimedia :: Graphics'
+    ],
+    zip_safe=False
+)
---
+++ packages/django-watermark-zemi4/django-watermark-zemi4-0.1.8.tar.gz/django-watermark-zemi4-0.1.8/watermarker/__init__.py
@@ -0,0 +1,3 @@
+VERSION = (0, 1, 8)
+
+__version__ = '.'.join([str(n) for n in VERSION])
