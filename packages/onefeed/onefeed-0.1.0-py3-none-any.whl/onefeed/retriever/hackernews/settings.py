4f1e104f6900c640cbd0a96c99fee5140e90d813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:36 +0000

    Add package onefeed/0.1.0/onefeed-0.1.0-py3-none-any.whl

---
+++ packages/onefeed/onefeed-0.1.0-py3-none-any.whl/onefeed/retriever/hackernews/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+BOT_NAME = 'hackernews'
+SPIDER_MODULES = ['onefeed.retriever.hackernews.spiders']
+ITEM_PIPELINES = {
+    'onefeed.retriever.hackernews.pipelines.SqlitePipeline': 300,
+}
+ROBOTSTXT_OBEY = False
+FORCED_TIMEOUT = 10
+LOG_LEVEL = 'ERROR'
+SCHEDULE_TIME = '10:15'
+
