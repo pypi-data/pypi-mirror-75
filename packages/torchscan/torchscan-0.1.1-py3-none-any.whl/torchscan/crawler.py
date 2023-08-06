6a550d0202c1a1c99e5228550bb181373a013728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:08 +0000

    Add package torchscan/0.1.1/torchscan-0.1.1-py3-none-any.whl

---
+++ packages/torchscan/torchscan-0.1.1-py3-none-any.whl/torchscan/crawler.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module crawler
+"""
+
+import os
+
+import torch
+
+from .modules import module_dmas, module_flops, module_macs, module_rf
+from .process import get_process_gpu_ram
+from .utils import aggregate_info, format_info
+
+__all__ = ['crawl_module', 'summary']
+
+
+def apply(module, fn, depth=0, name=None):
+    """Modified version of `torch.nn.Module.apply` method
+
+    Args:
+        module (torch.nn.Module): target module
+        fn (callable): function to apply to each module
+        depth (int, optional): current depth of `module`
+        name (str, optional): name of the current module
+    """
+
+    if name is None:
+        name = module.__class__.__name__.lower()
+    fn(module, depth, name)
+    for n, m in module.named_children():
+        apply(m, fn, depth + 1, n)
+
+
+def crawl_module(module, input_shape, dtype=None, max_depth=None):
+    """Retrieves module information for an expected input tensor shape
+
+    Example::
+        >>> import torch.nn as nn
+        >>> from torchscan import summary
+        >>> mod = nn.Conv2d(3, 8, 3)
+        >>> module_info = crawl_module(mod, (3, 224, 224))
+
+    Args:
+        module (torch.nn.Module): module to inspect
+        input_shape (tuple<int>): expected input shapes
+        dtype (type): data type of each input argument to the module
+        max_depth (int, optional): maximum depth of layer information
+    Returns:
+        dict: layer and overhead information
+    """
+
+    # Get device and data types from model
+    p = next(module.parameters())
+    device = p.device
+
+    cuda_overhead, framework_overhead = 0, 0
+    if torch.cuda.is_available():
+        # Process RAM - allocator RAM
+        cuda_overhead = get_process_gpu_ram(os.getpid()) - (torch.cuda.memory_reserved() / 1024 ** 2)
+        # Allocator RAM - Used RAM
+        framework_overhead = (torch.cuda.memory_reserved() - torch.cuda.memory_allocated()) / 1024 ** 2
+
+    # input
+    if isinstance(input_shape[0], int):
+        input_shape = [input_shape]
+    if dtype is None:
+        dtype = p.data.dtype
+    if isinstance(dtype, torch.dtype):
+        dtype = [dtype] * len(input_shape)
+    # Tensor arguments
+    input_ts = [torch.rand(1, *in_shape).to(dtype=_dtype, device=device)
+                for in_shape, _dtype in zip(input_shape, dtype)]
+
+    # Hook definition
+    def _hook_info(module, depth, name):
+
+        def _pre_hook(module, input):
+            """Pre-forward hook"""
+
+            # Params
+            grad_params, nograd_params, param_size = 0, 0, 0
+            num_buffers, buffer_size = 0, 0
+            is_shared = False
+            if not any(module.children()):
+                # Parameters
+                for p in module.parameters():
+                    if id(p) not in param_ids:
+                        if p.requires_grad:
+                            grad_params += p.data.numel()
+                        else:
+                            nograd_params += p.data.numel()
+                        param_size += p.data.numel() * p.data.element_size()
+                        param_ids.append(id(p))
+                    else:
+                        is_shared = True
+                # Buffers
+                for b in module.buffers():
+                    if id(b) not in param_ids:
+                        num_buffers += b.numel()
+                        buffer_size += b.numel() * b.element_size()
+                        param_ids.append(id(b))
+                    else:
+                        is_shared = True
+
+            call_idxs[id(module)] = len(info)
+
+            info.append(dict(name=name,
+                             depth=depth,
+                             type=module.__class__.__name__,
+                             input_shape=(-1, *input[0][0].shape[1:]),
+                             output_shape=None,
+                             grad_params=grad_params,
+                             nograd_params=nograd_params,
+                             param_size=param_size,
+                             num_buffers=num_buffers,
+                             buffer_size=buffer_size,
+                             flops=0,
+                             macs=0,
+                             dmas=0,
+                             rf=1,
+                             s=1,
+                             p=0,
+                             is_shared=is_shared,
+                             is_leaf=not any(module.children())))
+
+            # Remove the hook by using its handle
+            pre_fw_handle.remove()
+
+        def _fwd_hook(module, input, output):
+            """Post-forward hook"""
+
+            # Retrieve forward index
+            fw_idx = call_idxs[id(module)]
+
+            if any(module.children()):
+                tot_flops, tot_macs, tot_dmas = 0, 0, 0
+                current_rf, current_stride, current_padding = 1, 1, 0
+            else:
+                # Compute stats for standalone layers
+                tot_flops = module_flops(module, input[0], output)
+                tot_macs = module_macs(module, input[0], output)
+                tot_dmas = module_dmas(module, input[0], output)
+                current_rf, current_stride, current_padding = module_rf(module, input[0], output)
+
+            # Update layer information
+            info[fw_idx]['output_shape'] = (-1, *output.shape[1:])
+            # Add them, since some modules can be used several times
+            info[fw_idx]['flops'] = tot_flops
+            info[fw_idx]['macs'] = tot_macs
+            info[fw_idx]['dmas'] = tot_dmas
+            # Compute receptive field
+            info[fw_idx]['rf'] = current_rf
+            info[fw_idx]['s'] = current_stride
+            info[fw_idx]['p'] = current_padding
+
+            # Remove the hook by using its handle
+            post_fw_handle.remove()
+
+        # Hook only leaf children
+        pre_fw_handle = module.register_forward_pre_hook(_pre_hook)
+        post_fw_handle = module.register_forward_hook(_fwd_hook)
+
+    # Hook model
+    info = []
+    param_ids = []
+    call_idxs = {}
+    apply(module, _hook_info)
+
+    # Forward
+    with torch.no_grad():
+        module(*input_ts)
+
+    reserved_ram, diff_ram = 0, 0
+    if torch.cuda.is_available():
+        reserved_ram = torch.cuda.memory_reserved() / 1024 ** 2
+        diff_ram = (torch.cuda.memory_reserved() - torch.cuda.memory_allocated()) / 1024 ** 2
+        torch.cuda.synchronize()
+        torch.cuda.empty_cache()
+
+    grad_params, nograd_params, param_size = 0, 0, 0
+    num_buffers, buffer_size = 0, 0
+    for p in module.parameters():
+        if p.requires_grad:
+            grad_params += p.data.numel()
+        else:
+            nograd_params += p.data.numel()
+        param_size += p.data.numel() * p.data.element_size()
+    for b in module.buffers():
+        num_buffers += b.numel()
+        buffer_size += b.numel() * b.element_size()
+
+    # Update cumulative receptive field
+    _rf, _s, _p = 1, 1, 0
+    for fw_idx, _layer in enumerate(info[::-1]):
+        _rf = _layer['s'] * (_rf - 1) + _layer['rf']
+        _s *= _layer['s']
+        _p = _layer['s'] * _p + _layer['p']
+
+        info[len(info) - 1 - fw_idx]['rf'] = _rf
+        info[len(info) - 1 - fw_idx]['s'] = _s
+        info[len(info) - 1 - fw_idx]['p'] = _p
+
+    return dict(overheads=dict(cuda=dict(pre=cuda_overhead, fwd=get_process_gpu_ram(os.getpid()) - reserved_ram),
+                               framework=dict(pre=framework_overhead, fwd=diff_ram)),
+                layers=info,
+                overall=dict(grad_params=grad_params, nograd_params=nograd_params, param_size=param_size,
+                             num_buffers=num_buffers, buffer_size=buffer_size))
+
+
+def summary(module, input_shape, wrap_mode='mid', max_depth=None, receptive_field=False):
+    """Print module summary for an expected input tensor shape
+
+    Example::
+        >>> import torch.nn as nn
+        >>> from torchscan import summary
+        >>> mod = nn.Conv2d(3, 8, 3)
+        >>> summary(mod, (3, 224, 224), receptive_field=True)
+
+    Args:
+        module (torch.nn.Module): module to inspect
+        input_shape (tuple<int>): expected input shapes
+        wrap_mode (str, optional): if a value is too long, where the wrapping should be performed
+        max_depth (int, optional): maximum depth of layer information
+        receptive_field (bool, optional): whether receptive field estimation should be performed
+    """
+
+    # Get the summary dict
+    module_info = crawl_module(module, input_shape)
+    # Aggregate until max_depth
+    if isinstance(max_depth, int):
+        module_info = aggregate_info(module_info, max_depth)
+    # Format it and print it
+    print(format_info(module_info, wrap_mode, receptive_field))
---
+++ packages/torchscan/torchscan-0.1.1-py3-none-any.whl/torchscan/modules/memory.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module DMAs
+"""
+
+import warnings
+from functools import reduce
+from operator import mul
+
+from torch import nn
+from torch.nn.modules.batchnorm import _BatchNorm
+from torch.nn.modules.conv import _ConvNd, _ConvTransposeNd
+from torch.nn.modules.pooling import _MaxPoolNd, _AvgPoolNd, _AdaptiveMaxPoolNd, _AdaptiveAvgPoolNd
+
+
+__all__ = ['module_dmas']
+
+
+def module_dmas(module, input, output):
+    """Estimate the number of direct memory accesses by the module.
+    The implementation overhead is neglected
+
+    Args:
+        module (torch.nn.Module): PyTorch module
+        input (torch.Tensor): input to the module
+        output (torch.Tensor): output of the module
+    Returns:
+        int: number of DMAs
+    """
+
+    if isinstance(module, nn.Identity):
+        return dmas_identity(module, input, output)
+    elif isinstance(module, nn.Linear):
+        return dmas_linear(module, input, output)
+    elif isinstance(module, (nn.ReLU, nn.ReLU6)):
+        return dmas_relu(module, input, output)
+    elif isinstance(module, (nn.ELU, nn.LeakyReLU)):
+        return dmas_act_single_param(module, input, output)
+    elif isinstance(module, nn.Sigmoid):
+        return dmas_sigmoid(module, input, output)
+    elif isinstance(module, nn.Tanh):
+        return dmas_tanh(module, input, output)
+    elif isinstance(module, _ConvTransposeNd):
+        return dmas_convtransposend(module, input, output)
+    elif isinstance(module, _ConvNd):
+        return dmas_convnd(module, input, output)
+    elif isinstance(module, _BatchNorm):
+        return dmas_bn(module, input, output)
+    elif isinstance(module, (_MaxPoolNd, _AvgPoolNd)):
+        return dmas_pool(module, input, output)
+    elif isinstance(module, (_AdaptiveMaxPoolNd, _AdaptiveAvgPoolNd)):
+        return dmas_adaptive_pool(module, input, output)
+    elif isinstance(module, nn.Dropout):
+        return dmas_dropout(module, input, output)
+    else:
+        warnings.warn(f'Module type not supported: {module.__class__.__name__}')
+        return 0
+
+
+def num_params(module):
+    """Compute the number of parameters
+
+    Args:
+        module (torch.nn.Module): PyTorch module
+    Returns:
+        int: number of parameter elements
+    """
+
+    return sum(p.data.numel() for p in module.parameters())
+
+
+def dmas_identity(module, input, output):
+    """DMAs estimation for `torch.nn.Identity`"""
+
+    return input.numel()
+
+
+def dmas_linear(module, input, output):
+    """DMAs estimation for `torch.nn.Linear`"""
+
+    input_dma = input.numel()
+    # Access weight and bias
+    ops_dma = num_params(module)
+    output_dma = output.numel()
+
+    return input_dma + ops_dma + output_dma
+
+
+def dmas_relu(module, input, output):
+    """DMAs estimation for `torch.nn.ReLU`"""
+
+    input_dma = input.numel()
+    output_dma = 0 if module.inplace else output.numel()
+
+    return input_dma + output_dma
+
+
+def dmas_act_single_param(module, input, output):
+    """DMAs estimation for activations with single parameter"""
+
+    input_dma = input.numel()
+    # Access alpha, slope or other
+    ops_dma = 1
+    output_dma = 0 if module.inplace else output.numel()
+
+    return input_dma + ops_dma + output_dma
+
+
+def dmas_sigmoid(module, input, output):
+    """DMAs estimation for `torch.nn.Sigmoid`"""
+
+    # Access for both exp
+    input_dma = input.numel()
+    output_dma = output.numel()
+
+    return input_dma + output_dma
+
+
+def dmas_tanh(module, input, output):
+    """DMAs estimation for `torch.nn.Tanh`"""
+
+    # Access for both exp
+    input_dma = input.numel() * 2
+    output_dma = output.numel()
+
+    return input_dma + output_dma
+
+
+def dmas_dropout(module, input, output):
+    """DMAs estimation for `torch.nn.Dropout`"""
+
+    input_dma = input.numel()
+
+    # Access sampling probability
+    ops_dma = 1
+
+    output_dma = 0 if module.inplace else output.numel()
+
+    return input_dma + ops_dma + output_dma
+
+
+def dmas_convtransposend(module, input, output):
+    """DMAs estimation for `torch.nn.modules.conv._ConvTransposeNd`"""
+
+    # Padding (# cf. https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/conv.py#L496-L532)
+    # Access stride, padding and kernel_size
+    in_padding = len(module.kernel_size) * 4
+    out_padding = len(module.kernel_size)
+
+    # The rest is like a classic convolution
+    conv_dmas = dmas_convnd(module, input, output)
+
+    return in_padding + out_padding + conv_dmas
+
+
+def dmas_convnd(module, input, output):
+    """DMAs estimation for `torch.nn.modules.conv._ConvNd`"""
+
+    # Each output element required K ** 2 memory access of each input channel
+    input_dma = module.in_channels * reduce(mul, module.kernel_size) * output.numel()
+    # Correct with groups
+    input_dma /= module.groups
+
+    # Access weight & bias
+    ops_dma = num_params(module)
+    output_dma = output.numel()
+
+    return input_dma + ops_dma + output_dma
+
+
+def dmas_bn(module, input, output):
+    """DMAs estimation for `torch.nn.modules.batchnorm._BatchNorm`"""
+    input_dma = input.numel()
+
+    # Access running_mean, running_var and eps
+    ops_dma = module.running_mean.numel() + module.running_var.numel() + 1
+    # Access to weight and bias
+    if module.affine:
+        ops_dma += module.weight.data.numel() + module.bias.data.numel()
+    # Exp avg factor
+    if module.momentum:
+        ops_dma += 1
+    # Update stats
+    if module.training and module.track_running_stats:
+        # Current mean and std computation only requires access to input, already counted in input_dma
+        # Update num of batches and running stats
+        ops_dma += module.num_batches_tracked.numel() + module.running_mean.numel() + module.running_var.numel()
+
+    output_dma = output.numel()
+
+    return input_dma + ops_dma + output_dma
+
+
+def dmas_pool(module, input, output):
+    """DMAs estimation for spatial pooling modules"""
+
+    # Resolve kernel size and stride size (can be stored as a single integer or a tuple)
+    if isinstance(module.kernel_size, tuple):
+        kernel_size = module.kernel_size
+    else:
+        kernel_size = (module.kernel_size,) * (input.ndim - 2)
+
+    # Each output element required K ** 2 memory accesses
+    input_dma = reduce(mul, kernel_size) * output.numel()
+
+    output_dma = output.numel()
+
+    return input_dma + output_dma
+
+
+def dmas_adaptive_pool(module, input, output):
+    """DMAs estimation for adaptive spatial pooling modules"""
+
+    o_sizes = module.output_size if isinstance(module.output_size, tuple) else (module.output_size,) * (input.ndim - 2)
+    # Approximate kernel_size using ratio of spatial shapes between input and output
+    kernel_size = tuple(i_size // o_size if (i_size % o_size) == 0 else i_size - o_size * (i_size // o_size) + 1
+                        for i_size, o_size in zip(input.shape[2:], o_sizes))
+    # Each output element required K ** 2 memory accesses
+    input_dma = reduce(mul, kernel_size) * output.numel()
+
+    output_dma = output.numel()
+
+    return input_dma + output_dma
