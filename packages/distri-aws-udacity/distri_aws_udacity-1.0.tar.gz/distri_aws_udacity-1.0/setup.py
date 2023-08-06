d8e7d699350e87cd6ee531904f76ada8f2914a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:36 +0000

    Add package distri-aws-udacity/1.0/distri_aws_udacity-1.0.tar.gz

---
+++ packages/distri-aws-udacity/distri_aws_udacity-1.0.tar.gz/distri_aws_udacity-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distri_aws_udacity',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distri_aws_udacity'],
+      author="Utkarsh Gupta",
+      author_email="utkarshgupt2000@gmail.com",
+      zip_safe=False)
