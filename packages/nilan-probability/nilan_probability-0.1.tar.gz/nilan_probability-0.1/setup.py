e78a4bfcbc1c9f964e9197e99c1fc0cd13a805d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:50 +0000

    Add package nilan-probability/0.1/nilan_probability-0.1.tar.gz

---
+++ packages/nilan-probability/nilan_probability-0.1.tar.gz/nilan_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nilan_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nilan_probability'],
+      author = 'Nilan Fernando',
+      author_email = 'michaelnilan@gmail.com',
+      zip_safe=False)
