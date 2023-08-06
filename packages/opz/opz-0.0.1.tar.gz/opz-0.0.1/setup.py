8372668ede05e26d2a5b664b5af15673898a32b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:51 +0000

    Add package opz/0.0.1/opz-0.0.1.tar.gz

---
+++ packages/opz/opz-0.0.1.tar.gz/opz-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name = "opz", 
+    version = "0.0.1",
+    description = "File operation library",
+    # long_description=open("README.md", "r").read(),
+    # long_description_content_type="text/markdown",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.8',
+)
