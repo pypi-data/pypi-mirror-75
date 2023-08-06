78de141e68609b8389405c55ec1b2a8096cc62b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:51 +0000

    Add package rbd-iscsi-client/0.1.8/rbd-iscsi-client-0.1.8.tar.gz

---
+++ packages/rbd-iscsi-client/rbd-iscsi-client-0.1.8.tar.gz/rbd-iscsi-client-0.1.8/rbd_iscsi_client/tests/test_client.py
@@ -0,0 +1,113 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Tests for `rbd_iscsi_client` package."""
+
+import unittest
+from unittest import mock
+
+from rbd_iscsi_client import client
+
+
+class TestRbd_iscsi_client(unittest.TestCase):
+    """Tests for `rbd_iscsi_client` package."""
+    FAKE_URL = "http://fake-url:5000"
+
+    RESP_200 = {'status': '200', 'Content-Length': '8787',
+                'content-location': u'http://fake-url:5000/api/config',
+                'Server': 'Werkzeug/0.14.1 Python/2.7.15rc1',
+                'Date': 'Thu, 13 Jun 2019 18:56:46 GMT',
+                'Content-Type': 'application/json'}
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        fake_url = 'client://fake-url:0000'
+        fake_user = 'user'
+        fake_password = 'password'
+        self.client = client.RBDISCSIClient(fake_user, fake_password,
+                                            fake_url, secure=False,
+                                            http_log_debug=True,
+                                            suppress_ssl_warnings=False,
+                                            timeout=10)
+        self.client._http_log_req = mock.Mock()
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    @mock.patch.object(client.RBDISCSIClient, '_cs_request')
+    def test_get(self, cs_mock):
+        """Test something."""
+        fake_uri = '/foo'
+        body = {}
+        cs_mock.return_value = (self.RESP_200, body)
+        response, content = self.client.get(fake_uri)
+        cs_mock.assert_called_with(fake_uri, 'GET')
+        self.assertEqual(response, self.RESP_200)
+
+    @mock.patch.object(client.RBDISCSIClient, '_cs_request')
+    def test_put(self, cs_mock):
+        """Test something."""
+        fake_uri = '/foo'
+        body = {}
+        cs_mock.return_value = (self.RESP_200, body)
+        response, content = self.client.put(fake_uri)
+        cs_mock.assert_called_with(fake_uri, 'PUT')
+        self.assertEqual(response, self.RESP_200)
+
+    @mock.patch.object(client.RBDISCSIClient, '_cs_request')
+    def test_post(self, cs_mock):
+        """Test something."""
+        fake_uri = '/foo'
+        body = {}
+        cs_mock.return_value = (self.RESP_200, body)
+        response, content = self.client.post(fake_uri)
+        cs_mock.assert_called_with(fake_uri, 'POST')
+        self.assertEqual(response, self.RESP_200)
+
+    @mock.patch.object(client.RBDISCSIClient, '_cs_request')
+    def test_delete(self, cs_mock):
+        """Test something."""
+        fake_uri = '/foo'
+        body = {}
+        cs_mock.return_value = (self.RESP_200, body)
+        response, content = self.client.delete(fake_uri)
+        cs_mock.assert_called_with(fake_uri, 'DELETE')
+        self.assertEqual(response, self.RESP_200)
+
+    @mock.patch.object(client.RBDISCSIClient, '_cs_request')
+    def test_get_api(self, cs_mock):
+        """Test something."""
+        fake_uri = '/api'
+        body = {}
+        cs_mock.return_value = (self.RESP_200, body)
+        response, content = self.client.get_api()
+        cs_mock.assert_called_with(fake_uri, 'GET')
+        self.assertEqual(response, self.RESP_200)
+
+    @mock.patch.object(client.RBDISCSIClient, '_cs_request')
+    def test_get_config(self, cs_mock):
+        """Test something."""
+        fake_uri = '/api/config'
+        body = {}
+        cs_mock.return_value = (self.RESP_200, body)
+        response, content = self.client.get_config()
+        cs_mock.assert_called_with(fake_uri, 'GET')
+        self.assertEqual(response, self.RESP_200)
+
+    @mock.patch.object(client.RBDISCSIClient, '_cs_request')
+    def test_get_gatewayinfo(self, cs_mock):
+        """Test something."""
+        fake_uri = '/api/gatewayinfo'
+        body = {}
+        cs_mock.return_value = (self.RESP_200, body)
+        response, content = self.client.get_gatewayinfo()
+        cs_mock.assert_called_with(fake_uri, 'GET')
+        self.assertEqual(response, self.RESP_200)
---
+++ packages/rbd-iscsi-client/rbd-iscsi-client-0.1.8.tar.gz/rbd-iscsi-client-0.1.8/rbd_iscsi_client/tests/test_client_request.py
@@ -0,0 +1,129 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Tests for `rbd_iscsi_client` package."""
+
+import unittest
+from unittest import mock
+
+from rbd_iscsi_client import client
+from rbd_iscsi_client import exceptions
+
+import requests
+
+
+class TestRbd_iscsi_client(unittest.TestCase):
+    """Tests for `rbd_iscsi_client` package."""
+
+    client = None
+    FAKE_URL = 'client://fake-url:0000'
+    FAKE_USER = 'user'
+    FAKE_PASSWORD = 'password'
+
+    def setUp(self):
+        self.client = client.RBDISCSIClient(self.FAKE_USER,
+                                            self.FAKE_PASSWORD,
+                                            self.FAKE_URL,
+                                            secure=False,
+                                            http_log_debug=True,
+                                            suppress_ssl_warnings=False,
+                                            timeout=None)
+
+    def tearDown(self):
+        self.client = None
+
+    def test_request_timeout(self):
+        self.client._http_log_req = mock.Mock()
+        self.client.timeout = 10
+        retest = mock.Mock()
+        http_method = 'fake this'
+        http_url = 'http://fake-url:0000'
+
+        with mock.patch('requests.request', retest, create=True):
+            # Test timeout exception
+            retest.side_effect = requests.exceptions.Timeout
+            self.assertRaises(exceptions.Timeout,
+                              self.client.request,
+                              http_url, http_method)
+
+    def test_request_redirects(self):
+        self.client._http_log_req = mock.Mock()
+        self.client.timeout = 10
+        retest = mock.Mock()
+        http_method = 'fake this'
+        http_url = 'http://fake-url:0000'
+
+        with mock.patch('requests.request', retest, create=True):
+            # Test too many redirects exception
+            retest.side_effect = requests.exceptions.TooManyRedirects
+            self.assertRaises(exceptions.TooManyRedirects,
+                              self.client.request,
+                              http_url, http_method)
+
+    def test_request_http_error(self):
+        self.client._http_log_req = mock.Mock()
+        self.client.timeout = 10
+        retest = mock.Mock()
+        http_method = 'fake this'
+        http_url = 'http://fake-url:0000'
+
+        with mock.patch('requests.request', retest, create=True):
+            # Test HTTP Error exception
+            retest.side_effect = requests.exceptions.HTTPError
+            self.assertRaises(exceptions.HTTPError,
+                              self.client.request,
+                              http_url, http_method)
+
+    def test_request_url_required(self):
+        self.client._http_log_req = mock.Mock()
+        self.client.timeout = 10
+        retest = mock.Mock()
+        http_method = 'fake this'
+        http_url = 'http://fake-url:0000'
+
+        with mock.patch('requests.request', retest, create=True):
+            # Test URL required exception
+            retest.side_effect = requests.exceptions.URLRequired
+            self.assertRaises(exceptions.URLRequired,
+                              self.client.request,
+                              http_url, http_method)
+
+    def test_request_exception(self):
+        self.client._http_log_req = mock.Mock()
+        self.client.timeout = 10
+        retest = mock.Mock()
+        http_method = 'fake this'
+        http_url = 'http://fake-url:0000'
+
+        with mock.patch('requests.request', retest, create=True):
+            # Test request exception
+            retest.side_effect = requests.exceptions.RequestException
+            self.assertRaises(exceptions.RequestException,
+                              self.client.request,
+                              http_url, http_method)
+
+    def test_request_ssl_error(self):
+        self.client._http_log_req = mock.Mock()
+        self.client.timeout = 10
+        retest = mock.Mock()
+
+        with mock.patch('requests.request', retest, create=True):
+            # Test requests exception
+            retest.side_effect = requests.exceptions.SSLError
+            self.assertRaisesRegexp(exceptions.SSLCertFailed, "failed")
+
+    def test_client_timeout_setting(self):
+        self.client._http_log_req = mock.Mock()
+        self.client.timeout = 10
+        retest = mock.Mock()
+
+        with mock.patch('requests.request', retest, create=True):
+            self.assertEqual(self.client.timeout, 10)
