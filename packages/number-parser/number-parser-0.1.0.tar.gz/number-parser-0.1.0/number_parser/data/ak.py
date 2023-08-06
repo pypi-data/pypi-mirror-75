8bbf1fd278a6a6486deaf743541f19381a4161de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:06 +0000

    Add package number-parser/0.1.0/number-parser-0.1.0.tar.gz

---
+++ packages/number-parser/number-parser-0.1.0.tar.gz/number-parser-0.1.0/number_parser/data/ak.py
@@ -0,0 +1,23 @@
+info = {
+    "UNIT_NUMBERS": {
+        "hwee": 0,
+        "biako": 1,
+        "koro": 1,
+        "abien": 2,
+        "abiasa": 3,
+        "anan": 4,
+        "anum": 5,
+        "asia": 6,
+        "asuon": 7,
+        "awɔtwe": 8,
+        "akron": 9
+    },
+    "DIRECT_NUMBERS": {},
+    "TENS": {
+        "aduonu": 20,
+        "aduasa": 30
+    },
+    "HUNDREDS": {},
+    "BIG_POWERS_OF_TEN": {},
+    "SKIP_TOKENS": []
+}
---
+++ packages/number-parser/number-parser-0.1.0.tar.gz/number-parser-0.1.0/number_parser/data/ee.py
@@ -0,0 +1,11 @@
+info = {
+    "UNIT_NUMBERS": {
+        "ɖekeo": 0,
+        "ɖeka": 1
+    },
+    "DIRECT_NUMBERS": {},
+    "TENS": {},
+    "HUNDREDS": {},
+    "BIG_POWERS_OF_TEN": {},
+    "SKIP_TOKENS": []
+}
