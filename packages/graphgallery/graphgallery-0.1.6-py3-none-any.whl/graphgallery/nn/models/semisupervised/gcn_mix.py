edddcc136f20a330d8b2389e38282469cf3c85c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:32 +0000

    Add package graphgallery/0.1.6/graphgallery-0.1.6-py3-none-any.whl

---
+++ packages/graphgallery/graphgallery-0.1.6-py3-none-any.whl/graphgallery/nn/models/semisupervised/gcn_mix.py
@@ -0,0 +1,123 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import Model, Input
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import regularizers
+
+from graphgallery.nn.layers import GraphConvolution
+from graphgallery.nn.models import SemiSupervisedModel
+from graphgallery.sequence import FullBatchNodeSequence
+from graphgallery.utils.shape_utils import set_equal_in_length
+from graphgallery import astensor, asintarr, normalize_x, normalize_adj, Bunch
+
+
+class GCN_MIX(SemiSupervisedModel):
+    """
+        Implementation of Mixed Graph Convolutional Networks (GCN_MIX) occured in FastGCN. 
+        Tensorflow 1.x implementation: <https://github.com/matenure/FastGCN>
+
+
+        Calculating `A @ X` in advance to save time.
+
+        Arguments:
+        ----------
+            adj: shape (N, N), Scipy sparse matrix if  `is_adj_sparse=True`, 
+                Numpy array-like (or matrix) if `is_adj_sparse=False`.
+                The input `symmetric` adjacency matrix, where `N` is the number 
+                of nodes in graph.
+            x: shape (N, F), Scipy sparse matrix if `is_x_sparse=True`, 
+                Numpy array-like (or matrix) if `is_x_sparse=False`.
+                The input node feature matrix, where `F` is the dimension of features.
+            labels: Numpy array-like with shape (N,)
+                The ground-truth labels for all nodes in graph.
+            norm_adj (Float scalar, optional): 
+                The normalize rate for adjacency matrix `adj`. (default: :obj:`-0.5`, 
+                i.e., math:: \hat{A} = D^{-\frac{1}{2}} A D^{-\frac{1}{2}}) 
+            norm_x (String, optional): 
+                How to normalize the node feature matrix. See `graphgallery.normalize_x`
+                (default :str: `l1`)
+            device (String, optional): 
+                The device where the model is running on. You can specified `CPU` or `GPU` 
+                for the model. (default: :str: `CPU:0`, i.e., running on the 0-th `CPU`)
+            seed (Positive integer, optional): 
+                Used in combination with `tf.random.set_seed` & `np.random.seed` & `random.seed`  
+                to create a reproducible sequence of tensors across multiple calls. 
+                (default :obj: `None`, i.e., using random seed)
+            name (String, optional): 
+                Specified name for the model. (default: :str: `class.__name__`)
+
+    """
+
+    def __init__(self, adj, x, labels, norm_adj=-0.5, norm_x='l1',
+                 device='CPU:0', seed=None, name=None, **kwargs):
+
+        super().__init__(adj, x, labels, device=device, seed=seed, name=name, **kwargs)
+
+        self.norm_adj = norm_adj
+        self.norm_x = norm_x
+        self.preprocess(adj, x)
+
+    def preprocess(self, adj, x):
+        super().preprocess(adj, x)
+        # check the input adj and x, and convert them into proper data types
+        adj, x = self._check_inputs(adj, x)
+
+        if self.norm_adj:
+            adj = normalize_adj(adj, self.norm_adj)
+
+        if self.norm_x:
+            x = normalize_x(x, norm=self.norm_x)
+
+        x = adj @ x
+
+        with tf.device(self.device):
+            self.x_norm, self.adj_norm = astensor(x), adj
+
+    def build(self, hiddens=[16], activations=['relu'], dropouts=[0.5], l2_norms=[5e-4],
+              lr=0.01, use_bias=False):
+
+        local_paras = locals()
+        local_paras.pop('self')
+        paras = Bunch(**local_paras)
+        hiddens, activations, dropouts, l2_norms = set_equal_in_length(hiddens, activations, dropouts, l2_norms)
+        paras.update(Bunch(hiddens=hiddens, activations=activations, dropouts=dropouts, l2_norms=l2_norms))
+        # update all parameters
+        self.paras.update(paras)
+        self.model_paras.update(paras)
+
+        with tf.device(self.device):
+
+            x = Input(batch_shape=[self.n_nodes, self.n_features], dtype=self.floatx, name='features')
+            adj = Input(batch_shape=[None, self.n_nodes], dtype=self.floatx, sparse=True, name='adj_matrix')
+
+            h = x
+            for hid, activation, dropout, l2_norm in zip(hiddens, activations, dropouts, l2_norms):
+                h = Dense(hid, use_bias=use_bias, activation=activation,
+                          kernel_regularizer=regularizers.l2(l2_norm))(h)
+                h = Dropout(rate=dropout)(h)
+
+            output = GraphConvolution(self.n_classes, activation='softmax')([h, adj])
+
+            model = Model(inputs=[x, adj], outputs=output)
+            model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(lr=lr), metrics=['accuracy'])
+
+            self.model = model
+
+    def train_sequence(self, index):
+        index = asintarr(index)
+        labels = self.labels[index]
+        adj = self.adj_norm[index]
+        with tf.device(self.device):
+            sequence = FullBatchNodeSequence([self.x_norm, adj], labels)
+        return sequence
+
+    def predict(self, index):
+        super().predict(index)
+        index = asintarr(index)
+        adj = self.adj_norm[index]
+        with tf.device(self.device):
+            logit = self.model.predict_on_batch(astensor([self.x_norm, adj]))
+        if tf.is_tensor(logit):
+            logit = logit.numpy()
+        return logit
---
+++ packages/graphgallery/graphgallery-0.1.6-py3-none-any.whl/graphgallery/utils/conversion.py
@@ -0,0 +1,136 @@
+import numpy as np
+import tensorflow as tf
+import scipy.sparse as sp
+
+from numbers import Number
+from tensorflow.keras import backend as K
+
+from graphgallery import config
+from graphgallery.utils.type_check import is_list_like, is_interger_scalar, is_tensor_or_variable, is_scalar_like
+
+
+__all__ = ['check_and_convert', 'sparse_adj_to_sparse_tensor', 'sparse_tensor_to_sparse_adj',
+           'sparse_adj_to_edges', 'edges_to_sparse_adj', 'asintarr', 'astensor',
+           ]
+
+
+def check_and_convert(matrix, is_sparse):
+    """Check the input matrix and convert it into a proper data type.
+
+    Arguments:
+    ----------
+        matrix: Scipy sparse matrix or Numpy array-like or Numpy matrix.
+        is_sparse: Indicating whether the input matrix is sparse matrix or not.
+
+    Returns:
+    ----------
+        A converted matrix with proper data type.
+
+    """
+    if is_list_like(matrix):
+        return [check_and_convert(m, is_sparse) for m in matrix]
+    if not is_sparse:
+        if not isinstance(matrix, (np.ndarray, np.matrix)):
+            raise TypeError("The input matrix must be Numpy array-like or Numpy matrix"
+                            f" when `is_sparse=False`, but got {type(x)}")
+        return np.asarray(matrix, dtype=config.floatx())
+    else:
+        if not sp.issparse(matrix):
+            raise TypeError(f"The input matrix must be Scipy sparse matrix when `is_sparse=True`, but got {type(matrix)}")
+
+        return matrix.astype(dtype=config.floatx(), copy=False)
+
+
+def sparse_adj_to_sparse_tensor(x):
+    """Converts a Scipy sparse matrix to a SparseTensor."""
+    sparse_coo = x.tocoo()
+    row, col = sparse_coo.row, sparse_coo.col
+    data, shape = sparse_coo.data.astype(config.floatx()), sparse_coo.shape
+    indices = np.concatenate(
+        (np.expand_dims(row, axis=1), np.expand_dims(col, axis=1)), axis=1)
+    return tf.sparse.SparseTensor(indices, data, shape)
+
+
+def sparse_tensor_to_sparse_adj(x):
+    """Converts a SparseTensor to a Scipy sparse matrix (CSR matrix)."""
+    data = x.values.astype(config.floatx())
+    indices = x.indices.numpy().T
+    shape = x.shape
+    return sp.csr_matrix((data, indices), shape=shape)
+
+
+def sparse_adj_to_edges(adj):
+    """Convert a Scipy sparse matrix to (edge_index, edge_weight) representation
+
+    edge_index: shape [M, 2]
+    edge_weight: shape [M,]
+
+    """
+    adj = adj.tocoo()
+    edge_index = np.stack([adj.row, adj.col], axis=1).astype(config.intx())
+    edge_weight = adj.data.astype(config.floatx())
+
+    return edge_index, edge_weight
+
+
+def edges_to_sparse_adj(edge_index, edge_weight):
+    """Convert (edge_index, edge_weight) representation to a Scipy sparse matrix
+
+    edge_index: shape [M, 2]
+    edge_weight: shape [M,]
+
+    """
+    n = np.max(edge_index) + 1
+    adj = sp.csr_matrix((edge_weight, (edge_index[:, 0], edge_index[:, 1])), shape=(n, n))
+    return adj.astype(config.floatx(), copy=False)
+
+
+def inferer_type(x):
+    x = np.asarray(x)
+    if x.dtype.kind == 'f':
+        return config.floatx()
+    elif x.dtype.kind == 'i':
+        return config.intx()
+    elif x.dtype.kind == 'b':
+        return 'bool'
+    else:
+        raise RuntimeError(f'Invalid types, type `{type(x)}`')
+
+
+def asintarr(matrix, dtype=config.intx()):
+    """Convert `matrix` to interger data type.
+
+    """
+    if is_tensor_or_variable(matrix):
+        return tf.cast(matrix, dtype=dtype)
+
+    if is_interger_scalar(matrix):
+        matrix = np.asarray([matrix], dtype=dtype)
+    elif is_list_like(matrix) or isinstance(matrix, (np.ndarray, np.matrix)):
+        matrix = np.asarray(matrix, dtype=dtype)
+    else:
+        raise TypeError(f'Invalid input matrix which should be either array-like or integer scalar, but got {type(matrix)}.')
+    return matrix
+
+
+def astensor(inputs):
+    """Convert input matrices to Tensors (SparseTensors).
+
+    inputs: single or a list of array-like variables.
+    """
+    def matrix_astensor(matrix):
+        if any((is_tensor_or_variable(matrix), K.is_sparse(matrix), matrix is None)):
+            return matrix
+        elif sp.isspmatrix_csr(matrix) or sp.isspmatrix_csc(matrix):
+            return sparse_adj_to_sparse_tensor(matrix)
+        elif isinstance(matrix, (np.ndarray, np.matrix)) or is_list_like(matrix) or is_scalar_like(matrix):
+            return tf.convert_to_tensor(matrix, dtype=inferer_type(matrix))
+        else:
+            raise TypeError(f'Invalid type `{type(matrix)}` of inputs data. Allowed data type `(Tensor, SparseTensor, Numpy array, Scipy sparse tensor, None)`, but got {type(matrix)}.')
+
+    # Check `not isinstance(inputs[0], Number)` to avoid the situation like [1,2,3],
+    # where [1,2,3] will be converted to three tensors seperately.
+    if is_list_like(inputs) and not isinstance(inputs[0], Number):
+        return [astensor(matrix) for matrix in inputs]
+    else:
+        return matrix_astensor(inputs)
