9445b73216845f54454a82c6cf8104c726741a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:51 +0000

    Add package nlp-utils/0.5.2/nlp_utils-0.5.2.tar.gz

---
+++ packages/nlp-utils/nlp_utils-0.5.2.tar.gz/nlp_utils-0.5.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nlp_utils",
+    version="0.5.2",
+    packages=find_packages(exclude=["tests.*"]),
+    url="",
+    license="MIT",
+    author="Xiaoquan Kong",
+    author_email="u1mail2me@gmail.com",
+    description="Utils for NLP",
+    install_requires=["nltk", "numpy", "tensorflow", "micro_toolkit"],
+)
