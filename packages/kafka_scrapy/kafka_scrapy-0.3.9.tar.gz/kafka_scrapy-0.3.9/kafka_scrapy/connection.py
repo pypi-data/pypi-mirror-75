b18b4b538b4101bd5c5d6425c542b6cda0d513f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:41 +0000

    Add package kafka_scrapy/0.3.9/kafka_scrapy-0.3.9.tar.gz

---
+++ packages/kafka_scrapy/kafka_scrapy-0.3.9.tar.gz/kafka_scrapy-0.3.9/kafka_scrapy/connection.py
@@ -0,0 +1,43 @@
+import redis
+from confluent_kafka import Consumer, Producer
+from confluent_kafka.admin import AdminClient, NewTopic
+
+from . import kafka_default_settings as defaults
+
+SETTINGS_PARAMS_MAP = {
+    'REDIS_URL': 'url',
+    'REDIS_HOST': 'host',
+    'REDIS_PORT': 'port',
+    'REDIS_ENCODING': 'encoding',
+}
+
+
+def create_start_request_consumer(name, bootstrap_servers):
+    return Consumer({
+        'bootstrap.servers': bootstrap_servers,
+        'group.id': name,
+        'auto.offset.reset': 'earliest'
+    })
+
+
+def create_consumer(name, bootstrap_servers):
+    return Consumer({
+        'bootstrap.servers': bootstrap_servers,
+        'group.id': name,
+        'auto.offset.reset': 'earliest'
+    })
+
+
+def create_producer(bootstrap_servers):
+    return Producer({'bootstrap.servers': bootstrap_servers})
+
+
+def get_redis(settings):
+    params = defaults.REDIS_PARAMS.copy()
+    params.update(settings.get('REDIS_PARAMS', defaults.REDIS_PARAMS))
+    for source, dest in SETTINGS_PARAMS_MAP.items():
+        val = settings.get(source)
+        if val:
+            params[dest] = val
+
+    return redis.StrictRedis(**params)
---
+++ packages/kafka_scrapy/kafka_scrapy-0.3.9.tar.gz/kafka_scrapy-0.3.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kafka_scrapy",
+    packages=['kafka_scrapy'],
+    version='0.3.9',
+    description="kafka-based components for Scrapy.",
+    author="kong",
+    author_email='tomngp@163.com',
+    url="https://github.com/kwx1996/kafka_scrapy",
+    download_url='https://github.com/kwx1996/kafka_scrapy/archive/master.zip',
+    classifiers=[],
+    install_requires=[
+        'twisted',
+        'scrapy',
+        'redis',
+        'confluent_kafka'
+    ]
+)
