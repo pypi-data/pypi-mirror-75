d300814ac88503e4245efbe88607eb2a7fdf5f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:12 +0000

    Add package pnb/0.2.6rc1/pnb-0.2.6rc1.tar.gz

---
+++ packages/pnb/pnb-0.2.6rc1.tar.gz/pnb-0.2.6rc1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: latin-1 -*-
+
+
+from setuptools import setup
+
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name='pnb',
+      version='0.2.6rc1',
+      description = 'A beautiful and lightweight notebook interface for the ' \
+                    'python interpreter.',
+      long_description = long_description,
+      author='C. Ecker',
+      author_email='textmodelview@gmail.com',
+      url='https://github.com/chrisecker/textmodel/tree/master/pynotebook',
+      license='BSD',
+      scripts=['bin/pnb'],
+      install_requires = ['pynotebook >= 0.2.5.3'],
+      platforms = "Linux, Mac OS X, Windows",
+      keywords = ['Interactive', 'Interpreter', 'Notebook', 'Shell',
+                  'WXPython'],
+      classifiers = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+      ],      
+     )
+
