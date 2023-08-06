d817f84e084cbc67c8d84ac3fef61eaeca612e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:56 +0000

    Add package cbw-api-toolbox/1.1.2/cbw-api-toolbox-1.1.2.tar.gz

---
+++ packages/cbw-api-toolbox/cbw-api-toolbox-1.1.2.tar.gz/cbw-api-toolbox-1.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='cbw-api-toolbox',
+    description='CyberWatch Api Tools.',
+    long_description=open('README.md').read().strip(),
+    long_description_content_type="text/markdown",
+    version='1.1.2',
+    author='CyberWatch SAS',
+    author_email='support-it+api@cyberwatch.fr',
+    license='MIT',
+    url='https://github.com/Cyberwatch/cyberwatch_api_toolbox',
+    py_modules=['cbw-api-toolbox'],
+    zip_safe=False,
+    packages=['cbw_api_toolbox', 'cbw_api_toolbox.cbw_objects'],
+    package_dir={'cbw_api_toolbox': 'cbw_api_toolbox', 'cbw_objects': 'cbw_api_toolbox/cbw_objects'},
+    install_requires=[
+        "requests>=2.20.1",
+        "XlsxWriter>=1.2.1",
+        "xlrd>=1.2.0"
+    ]
+)
