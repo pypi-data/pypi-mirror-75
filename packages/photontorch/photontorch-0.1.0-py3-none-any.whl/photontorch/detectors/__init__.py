563f9ef8be10ab6a5d1153eb19d9675a35a30a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package photontorch/0.1.0/photontorch-0.1.0-py3-none-any.whl

---
+++ packages/photontorch/photontorch-0.1.0-py3-none-any.whl/photontorch/detectors/__init__.py
@@ -0,0 +1,9 @@
+""" Detectors
+
+A collection of detectors that act as a realistic filter to the raw output
+power of the fields.
+
+"""
+
+from .photodetector import Photodetector
+from .lowpassdetector import LowpassDetector
