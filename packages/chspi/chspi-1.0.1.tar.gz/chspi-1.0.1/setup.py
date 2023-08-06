13b574931999f6a5705fad971c461caf565b8f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:10 +0000

    Add package chspi/1.0.1/chspi-1.0.1.tar.gz

---
+++ packages/chspi/chspi-1.0.1.tar.gz/chspi-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chspi",
+    version="1.0.1",
+    author="Isaac Fisher",
+    author_email="fisheri@carlislestudents.org",
+    description="A small set of tools for students in Carlisle High School's Raspberry Pi program",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fisheri2004/chspi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
