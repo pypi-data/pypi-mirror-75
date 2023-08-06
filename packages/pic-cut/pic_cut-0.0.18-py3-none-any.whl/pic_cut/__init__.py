fbaa9eb06f29b066b144348a8079ea87455e4123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:56 +0000

    Add package pic-cut/0.0.18/pic_cut-0.0.18-py3-none-any.whl

---
+++ packages/pic-cut/pic_cut-0.0.18-py3-none-any.whl/pic_cut/__init__.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+name = 'pic_cut'
+
+from PIL import Image
+import math
+import os
+import cached_url
+
+MARGIN = 0.05
+
+def cut(path, limit=20):
+	try:
+		img = Image.open(path)
+	except:
+		return []
+
+	w, h = img.size
+
+	for piece in range(1, limit + 1):
+		nh, moves = computeSize(h, piece)
+		if goodSize(w, nh):
+			break
+
+	if len(moves) <= 1 or not goodSize(w, nh):
+		moves = []
+
+	for p, m in enumerate(moves):
+		working_slice = img.crop((0, m, w, m + nh))
+		main_path, _ = os.path.splitext(path)
+		fn = '%s_%d.png' % (main_path, p)
+		working_slice.save(fn)
+		yield fn
+
+def getCutImages(images, limit=10):
+	result = []
+	for image in images:
+		cached_url.get(image, force_cache=True, mode='b')
+		fn = cached_url.getFilePath(image)
+		cuts = list(cut(fn))
+		if not cuts:
+			cuts = [fn]
+		for c in cuts:
+			result.append(c)
+			if len(result) >= limit:
+				return result
+	return result
+
+def goodSize(w, h):
+	return h < 2.1 * w
+
+def computeSize(h, p):
+	raw_h = h / (p - MARGIN * (p - 1))
+	nh = math.ceil(raw_h)
+	moves = [math.ceil(raw_h * ((1 - MARGIN) * step)) for step in range(p - 1)]
+	moves.append(h - nh)
+	return nh, moves
