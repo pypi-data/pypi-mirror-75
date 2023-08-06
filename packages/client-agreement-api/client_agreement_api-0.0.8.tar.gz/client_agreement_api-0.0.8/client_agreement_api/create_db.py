1d3beee7100b76e81f59b99886a5d7b6d2287655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:18 +0000

    Add package client-agreement-api/0.0.8/client_agreement_api-0.0.8.tar.gz

---
+++ packages/client-agreement-api/client_agreement_api-0.0.8.tar.gz/client_agreement_api-0.0.8/client_agreement_api/create_db.py
@@ -0,0 +1,16 @@
+import os
+from .app import DB, DB_FILE_NAME
+from .models.agreement import AgreementDummy
+
+# Delete database file if it exists currently
+if os.path.exists(DB_FILE_NAME):
+    os.remove(DB_FILE_NAME)
+
+# Create the database
+DB.create_all()
+
+# create data in database
+AgreementDummy.generate_dummy_agreements()
+
+
+print("Data Import Successful.")
