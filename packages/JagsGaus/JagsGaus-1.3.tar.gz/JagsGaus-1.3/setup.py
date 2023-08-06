44e697bf7484de8c8faa7a8ed2f77204ede07531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:14 +0000

    Add package JagsGaus/1.3/JagsGaus-1.3.tar.gz

---
+++ packages/JagsGaus/JagsGaus-1.3.tar.gz/JagsGaus-1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='JagsGaus',
+      version='1.3',
+      description='Gaussian distributions',
+      packages=['JagsGaus'],
+      author= 'Jagriti Prakash' ,
+      zip_safe=False)
