609b9b879f9dc28dfa9d0550c6087fe94bfb7fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:15 +0000

    Add package test-probability/0.1/test_probability-0.1.tar.gz

---
+++ packages/test-probability/test_probability-0.1.tar.gz/test_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='test_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['test_probability'],
+      author = 'Ernitia Paramasari',
+      zip_safe=False)
