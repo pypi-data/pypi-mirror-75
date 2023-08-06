569c2bb38348fee9b86ed97812a3e6cf47c2fa8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:46 +0000

    Add package probability-dist-joaoassis/0.1/probability_dist_joaoassis-0.1.tar.gz

---
+++ packages/probability-dist-joaoassis/probability_dist_joaoassis-0.1.tar.gz/probability_dist_joaoassis-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='probability_dist_joaoassis',
+      version='0.1',
+      author='Joao Vitor Assis',
+      description='Gaussian distributions',
+      packages=['probability_dist_joaoassis'],
+      zip_safe=False)
