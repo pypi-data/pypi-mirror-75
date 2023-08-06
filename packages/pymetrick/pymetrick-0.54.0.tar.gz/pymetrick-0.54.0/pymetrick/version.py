d2fcc6f136ead60ee95f508b1f23778a0b5343b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:52 +0000

    Add package pymetrick/0.54.0/pymetrick-0.54.0.tar.gz

---
+++ packages/pymetrick/pymetrick-0.54.0.tar.gz/pymetrick-0.54.0/pymetrick/version.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 *-*
+
+from __future__ import unicode_literals
+
+author = 'Fco.Javier Tamarit V <javtamvi@gmail.com>'
+copyright = "COPYLEFT 2012 PYMETRICK.ORG"
+license = 'Licencia GNU/GPLv3'
+version = '0.54.0'
+
+
+
+
