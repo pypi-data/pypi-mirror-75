cdc4651ed997fef282219de6ad05184434264819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:11 +0000

    Add package tangoctl/0.5.0/tangoctl-0.5.0.tar.gz

---
+++ packages/tangoctl/tangoctl-0.5.0.tar.gz/tangoctl-0.5.0/tangoctl/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the tangoctl project
+#
+# Copyright (c) 2018 Tiago Coutinho
+# Distributed under the MIT license. See LICENSE for more info.
+
+from .tangoctl import *
+
+__version__ = '0.5.0'
