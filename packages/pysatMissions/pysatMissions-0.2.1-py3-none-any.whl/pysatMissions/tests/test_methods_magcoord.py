cca30efa5f1075c881ce17369bdd994b23137f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:56 +0000

    Add package pysatMissions/0.2.1/pysatMissions-0.2.1-py3-none-any.whl

---
+++ packages/pysatMissions/pysatMissions-0.2.1-py3-none-any.whl/pysatMissions/tests/test_methods_magcoord.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Test some of the aacgmv2 method functions
+
+import datetime as dt
+import numpy as np
+import pysat
+import pysatMissions.methods.magcoord as mm_magcoord
+
+
+class TestBasics():
+    def setup(self):
+        """Runs before every method to create a clean testing setup."""
+        self.testInst = pysat.Instrument(platform='pysat', name='testing',
+                                         sat_id='100', clean_level='clean')
+
+    def teardown(self):
+        """Clean up test environment after tests"""
+        del self
+
+    def test_add_aacgm_coordinates(self):
+        """Test adding thermal plasma data to test inst"""
+        self.testInst.custom.attach(mm_magcoord.add_aacgm_coordinates,
+                                    kwargs={'glat_label': 'latitude',
+                                            'glong_label': 'longitude',
+                                            'alt_label': 'altitude'})
+        self.testInst.load(date=dt.datetime(2009, 1, 1))
+        targets = ['aacgm_lat', 'aacgm_long', 'aacgm_mlt']
+        for target in targets:
+            # Check if data is added
+            assert target in self.testInst.data.keys()
+            assert not np.isnan(self.testInst[target]).any()
+            # Check if metadata is added
+            assert target in self.testInst.meta.data.index
+
+    def test_add_quasi_dipole_coordinates(self):
+        """Test adding thermal plasma data to test inst"""
+        self.testInst.custom.attach(mm_magcoord.add_quasi_dipole_coordinates,
+                                    kwargs={'glat_label': 'latitude',
+                                            'glong_label': 'longitude',
+                                            'alt_label': 'altitude'})
+        self.testInst.load(date=dt.datetime(2009, 1, 1))
+        targets = ['qd_lat', 'qd_long', 'mlt']
+        for target in targets:
+            # Check if data is added
+            assert target in self.testInst.data.keys()
+            assert not np.isnan(self.testInst[target]).any()
+            # Check if metadata is added
+            assert target in self.testInst.meta.data.index
