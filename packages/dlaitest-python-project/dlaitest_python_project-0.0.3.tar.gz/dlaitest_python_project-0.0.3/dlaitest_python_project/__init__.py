4db704b6253cbc29617ed9e1fc36b207896656ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:40 +0000

    Add package dlaitest-python-project/0.0.3/dlaitest_python_project-0.0.3.tar.gz

---
+++ packages/dlaitest-python-project/dlaitest_python_project-0.0.3.tar.gz/dlaitest_python_project-0.0.3/dlaitest_python_project/__init__.py
@@ -0,0 +1,5 @@
+from dlaitest_python_project.mycar import Car
+
+# from packagename.Filename import Classname
+
+# The __init__.py file is used to mark which classes you want the user to access through the package interface. Let’s make an example. Assume you want to upload a library named
