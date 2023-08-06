da5eed2da3848ca9a91b0a56ff72ed84e7517b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:50 +0000

    Add package VerdictCut/0.1.6/VerdictCut-0.1.6.tar.gz

---
+++ packages/VerdictCut/VerdictCut-0.1.6.tar.gz/VerdictCut-0.1.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+# readme.md = github readme.md, 這裡可接受markdown寫法
+# 如果沒有的話，需要自己打出介紹此專案的檔案，再讓程式知道
+
+with open("Discription.md", "r",encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="VerdictCut", # 
+    version='0.1.6',
+    author="seanbbear",
+    author_email="k7489759@gmail.com",
+    description="cut the verdict into different part",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seanbbear/VerdictCut",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
