57b5e0ffdb24e66ebe97684fc1fb83427dbc37ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package rhotimie3421/0.1/rhotimie3421-0.1.tar.gz

---
+++ packages/rhotimie3421/rhotimie3421-0.1.tar.gz/rhotimie3421-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rhotimie3421',
+      version='0.1',
+      description='Gaussian distributions test',
+      packages=['rhotimie3421'],
+      zip_safe=False)
