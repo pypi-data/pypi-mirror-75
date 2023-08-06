ca90f0bb3ccc7b0c20893b66708415de47793edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:35 +0000

    Add package distributions-ana/0.1/distributions_ana-0.1.tar.gz

---
+++ packages/distributions-ana/distributions_ana-0.1.tar.gz/distributions_ana-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_ana',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions_ana'],
+      author = 'Ananth',
+      zip_safe=False)
