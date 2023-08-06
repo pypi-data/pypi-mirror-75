de16705c8a342f40fe7be106728168e3064f0d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:35 +0000

    Add package hfoGUI/1.0.9/hfoGUI-1.0.9.tar.gz

---
+++ packages/hfoGUI/hfoGUI-1.0.9.tar.gz/hfoGUI-1.0.9/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+from hfoGUI.main import version
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+pkgs = setuptools.find_packages()
+print('found these packages:', pkgs)
+
+pkg_name = "hfoGUI"
+
+setuptools.setup(
+    name=pkg_name,
+    version=version,
+    author="Geoffrey Barrett",
+    author_email="geoffrey.m.barrett@gmail.com",
+    description="hfoGUI - a data visualization GUI for TINT data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HussainiLab/hfoGUI.git",
+    packages=pkgs,
+    install_requires=
+    [
+        'PyQt5',
+        'pillow',
+        'numpy',
+        'PyQtGraph',
+        'scipy',
+        'matplotlib',
+        'pandas',
+        'pyfftw'
+    ],
+    package_data={'hfoGUI': ['img/*.png']},
+    classifiers=[
+        "Programming Language :: Python :: 3.7 ",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3) ",
+        "Operating System :: OS Independent",
+    ],
+)
