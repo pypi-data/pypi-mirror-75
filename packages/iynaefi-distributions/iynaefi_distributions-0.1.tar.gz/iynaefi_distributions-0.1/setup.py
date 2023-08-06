55101716f5d80cf53537d4dacffcad80013fbaa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:00 +0000

    Add package iynaefi-distributions/0.1/iynaefi_distributions-0.1.tar.gz

---
+++ packages/iynaefi-distributions/iynaefi_distributions-0.1.tar.gz/iynaefi_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='iynaefi_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      author='Ifeanyi Eze',
+      author_email='ezeifeanyic@gmail.com',
+      packages=['iynaefi_distributions'],
+      zip_safe=False)
