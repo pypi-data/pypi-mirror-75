9bbd484cfc21cbddbc0fb7a090223e5763d637dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:07 +0000

    Add package cryptomodel-athanikos/2.4.2/cryptomodel_athanikos-2.4.2-py3-none-any.whl

---
+++ packages/cryptomodel-athanikos/cryptomodel_athanikos-2.4.2-py3-none-any.whl/cryptomodel/order_types.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class ORDER_TYPES(Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
