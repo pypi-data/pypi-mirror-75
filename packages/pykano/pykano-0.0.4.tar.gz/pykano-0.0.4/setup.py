82a34b75177b7ec8d43034fa2b9bbd95625bb035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:29 +0000

    Add package pykano/0.0.4/pykano-0.0.4.tar.gz

---
+++ packages/pykano/pykano-0.0.4.tar.gz/pykano-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pykano',
+      version='0.0.4',
+      description='kano tools for python3',
+      url='http://www.rapfan.cn',
+      author='kano',
+      author_email='sweetyrice@gmail.com',
+      license='MIT',
+      packages=['pykano'])
