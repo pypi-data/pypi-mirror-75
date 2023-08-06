1e246db06aae768ddf3b08eebe3ed85c3737889f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:48 +0000

    Add package jdistributions/0.1/jdistributions-0.1.tar.gz

---
+++ packages/jdistributions/jdistributions-0.1.tar.gz/jdistributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jdistributions',
+      version='0.1',
+      description='Data Distribution - Gaussian and Binomial',
+      packages=['jdistributions'],
+      author = 'Jish N',
+      zip_safe=False)
