41cf046fb33113c9ee7a543464bb5a6d2e1aa0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:00 +0000

    Add package note-shell/1.0.1/note_shell-1.0.1-py3-none-any.whl

---
+++ packages/note-shell/note_shell-1.0.1-py3-none-any.whl/note/helpers.py
@@ -0,0 +1,13 @@
+from os import system, name
+
+def clear_screen():
+    """
+    Clear terminal
+    """
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
