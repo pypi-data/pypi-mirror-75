48f5e4776dfabd18b776d5e4760966392c915b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:56 +0000

    Add package swh.search/0.1.0/swh.search-0.1.0-py3-none-any.whl

---
+++ packages/swh.search/swh.search-0.1.0-py3-none-any.whl/swh/search/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2019  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+
+def get_search(cls, args):
+    """Get an search object of class `search_class` with
+    arguments `search_args`.
+
+    Args:
+        cls (str): search's class, either 'local' or 'remote'
+        args (dict): dictionary of arguments passed to the
+            search class constructor
+
+    Returns:
+        an instance of swh.search's classes (either local or remote)
+
+    Raises:
+        ValueError if passed an unknown search class.
+
+    """
+    if cls == "remote":
+        from .api.client import RemoteSearch as Search
+    elif cls == "elasticsearch":
+        from .elasticsearch import ElasticSearch as Search
+    elif cls == "memory":
+        from .in_memory import InMemorySearch as Search
+    else:
+        raise ValueError("Unknown indexer search class `%s`" % cls)
+
+    return Search(**args)
