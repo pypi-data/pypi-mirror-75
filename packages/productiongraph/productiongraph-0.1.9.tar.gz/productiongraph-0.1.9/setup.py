567fc3c391c6069602a10d5f53a28acc1a4a2aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:43 +0000

    Add package productiongraph/0.1.9/productiongraph-0.1.9.tar.gz

---
+++ packages/productiongraph/productiongraph-0.1.9.tar.gz/productiongraph-0.1.9/setup.py
@@ -0,0 +1,27 @@
+import scripts
+if __name__ == "__main__":
+    import setuptools
+    setuptools.setup(
+        name='productiongraph',
+        version='0.1.9',
+        author='Cameron, Aaron, Osiris, Benjamin, Matt',
+        author_email='cameron.g.rodgers@gmail.com',
+        packages=setuptools.find_packages(),
+        #include_package_data=True,
+        package_data={
+            "": ["*.html", "*.css", "*.jpg", "*.png"],
+            "productiongraph": ["*.html", "*.css", "*.jpg", "*.png"],
+            "backend": ["*.html", "*.css", "*.jpg", "*.png"],
+        },
+        description='Web interface for calculating prices',
+        install_requires=[ 'Django', 'product_graph_bindings', 'numpy', 'django_filters' ],
+        entry_points =
+        { 'console_scripts':
+            [
+                'prodgraph_run = scripts.run:main',
+                'prodgraph_migrate_and_run = scripts.run:main',
+                'prodgraph_init = scripts.init:main',
+                'prodgraph_test = scripts.test:main',
+            ]
+        },
+    )
