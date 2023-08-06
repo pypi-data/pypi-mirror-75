116000e4d2cfb201f129a2ebc5fa1710541df77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:28 +0000

    Add package my-first-distributions-15/0.1/my_first_distributions_15-0.1.tar.gz

---
+++ packages/my-first-distributions-15/my_first_distributions_15-0.1.tar.gz/my_first_distributions_15-0.1/README.md
@@ -0,0 +1,2 @@
+# My first upload
+to pypi
---
+++ packages/my-first-distributions-15/my_first_distributions_15-0.1.tar.gz/my_first_distributions_15-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='my_first_distributions_15',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['my_first_distributions_15'],
+      zip_safe=False)
