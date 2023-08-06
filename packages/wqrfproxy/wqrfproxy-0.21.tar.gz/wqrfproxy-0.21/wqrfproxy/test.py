f62abfb530bb7a44e638a2e38cac24e24ff1964d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:01 +0000

    Add package wqrfproxy/0.21/wqrfproxy-0.21.tar.gz

---
+++ packages/wqrfproxy/wqrfproxy-0.21.tar.gz/wqrfproxy-0.21/wqrfproxy/test.py
@@ -0,0 +1,15 @@
+# -*- coding:utf8 -*-
+# import time,os
+# from wqrfproxy import *
+# from mitm_catch import *
+# start_wqrfproxy()
+#
+# time.sleep(30)
+# try:
+#     assert_proxy('http://api.sugg.sogou.com/su','w')
+#     assert_proxy('http://api.sugg.sogou.com/su','wangzijia')
+# except Exception as e:
+#     print(e)
+#
+# stop_wqrfproxy()
+
