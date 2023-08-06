993a44d2b6b4cc4ac4867ba82e14eaeac9cfd850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:54 +0000

    Add package awsud-probability/0.1/awsud_probability-0.1.tar.gz

---
+++ packages/awsud-probability/awsud_probability-0.1.tar.gz/awsud_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='awsud_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['awsud_probability'],
+      author='Raghav Bansal',
+      zip_safe=False)
