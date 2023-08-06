c04d7f6f99a6dd244a6c5c2ff3ee6f08f18cea4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:07 +0000

    Add package NMRPyStar/1.0.0/NMRPyStar-1.0.0-py3-none-any.whl

---
+++ packages/NMRPyStar/NMRPyStar-1.0.0-py3-none-any.whl/nmrpystar/__init__.py
@@ -0,0 +1,11 @@
+from .parser import parse_nmrstar_ast
+
+parse = parse_nmrstar_ast
+
+__version__ = '1.0.0'
+
+__all__ = [
+    "parse",
+    "parse_nmrstar_ast",
+]
+
---
+++ packages/NMRPyStar/NMRPyStar-1.0.0-py3-none-any.whl/nmrpystar/unparse/__init__.py
@@ -0,0 +1,6 @@
+from nmrpystar.unparse.maybeerror import MaybeError
+from nmrpystar.unparse.combinators import *
+
+
+
+__version__ = '0.0.6'
