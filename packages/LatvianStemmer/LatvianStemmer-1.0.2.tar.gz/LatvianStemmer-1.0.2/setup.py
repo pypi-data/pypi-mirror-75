8528ab6c861b8d7d55ceadbd6f8f80d7d343aa4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:06 +0000

    Add package LatvianStemmer/1.0.2/LatvianStemmer-1.0.2.tar.gz

---
+++ packages/LatvianStemmer/LatvianStemmer-1.0.2.tar.gz/LatvianStemmer-1.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name='LatvianStemmer',
+    version='1.0.2',
+    description='Light stemmer for Latvian.',
+    py_modules=['LatvianStemmer'],
+    author='Rihards Krišlauks',
+    author_email='rihizs@gmail.com',
+    url='https://github.com/rihardsk/LatvianStemmer',
+    license='Apache License, Version 2.0',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    entry_points={
+        "console_scripts": [
+            "lvstemmer = LatvianStemmer:main"
+        ]
+    }
+)
