e10fad9279d2eb98a58f2deb694ba9e675c3f307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:16 +0000

    Add package gandb-probability/0.1.0/gandb_probability-0.1.0.tar.gz

---
+++ packages/gandb-probability/gandb_probability-0.1.0.tar.gz/gandb_probability-0.1.0/setup.py
@@ -0,0 +1,18 @@
+# from setuptools import setup
+
+# setup(name='distributions',
+#       version='0.1',
+#       description='Gaussian distributions',
+#       packages=['distributions'],
+#       zip_safe=False)
+
+from setuptools import setup
+
+setup(name='gandb_probability',
+      version='0.1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gandb_probability'],
+      author='Mark Awuku',
+      author_email='awukum18@gmail.com',
+      zip_safe=False,
+)
