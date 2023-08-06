9bb36993bcf2de1d354b55faad4b6f4b28aa01af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:02 +0000

    Add package cullinan/0.32/cullinan-0.32.tar.gz

---
+++ packages/cullinan/cullinan-0.32.tar.gz/cullinan-0.32/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @File   : setup.py
+# @license: Copyright(C) 2020 Ore Studio
+# @Author : hansion
+# @Date   : 2019-02-16
+# @Desc   : 
+
+from setuptools import setup
+
+with open("README.MD", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cullinan',
+    version='0.32',
+    packages=['cullinan'],
+    description='a simple web framework',
+    author='ore_studio',
+    project_urls={
+            'Source': 'https://github.com/orestu/Cullinan',
+            'Wiki': 'https://github.com/orestu/Cullinan/wiki',
+      },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email='python@orestu.com',
+    url='https://github.com/orestu/Cullinan',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    install_requires=['tornado', 'python-dotenv', 'sqlalchemy', 'pymysql'],
+    python_requires='>=3'
+)
