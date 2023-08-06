6aaaf4164060978c1eb6b4b1974656dd40c77395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:45 +0000

    Add package aiosyncthing/0.3.5/aiosyncthing-0.3.5-py3-none-any.whl

---
+++ packages/aiosyncthing/aiosyncthing-0.3.5-py3-none-any.whl/aiosyncthing/exceptions.py
@@ -0,0 +1,17 @@
+"""Exceptions for the Syncthing REST API."""
+
+
+class SyncthingError(Exception):
+    """Base Syncthing Exception class."""
+
+
+class PingError(SyncthingError):
+    """When the server does not respond to ping."""
+
+
+class UnauthorizedError(SyncthingError):
+    """When the server does not accept the API token."""
+
+
+class UnknownFolder(SyncthingError):
+    """When the server cannot find folder by id."""
