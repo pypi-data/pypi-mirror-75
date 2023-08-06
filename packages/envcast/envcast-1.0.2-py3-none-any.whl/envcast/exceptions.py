37f6c1fe0c25b9f348242a851ce8c12ba7ae5068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:34 +0000

    Add package envcast/1.0.2/envcast-1.0.2-py3-none-any.whl

---
+++ packages/envcast/envcast-1.0.2-py3-none-any.whl/envcast/exceptions.py
@@ -0,0 +1,17 @@
+"""Exceptions.
+"""
+
+
+class IncorrectDotenvPath(Exception):
+    """If .env file doesnt exists.
+    """
+
+
+class NotSettedDotenvPath(Exception):
+    """If .env file doesnt set.
+    """
+
+
+class BrokenDotenvStructure(Exception):
+    """If .env has broken structure.
+    """
