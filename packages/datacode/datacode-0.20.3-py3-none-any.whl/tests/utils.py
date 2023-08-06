889b1756c20cb0e922c904c5cc9c6f8451ded6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:32 +0000

    Add package datacode/0.20.3/datacode-0.20.3-py3-none-any.whl

---
+++ packages/datacode/datacode-0.20.3-py3-none-any.whl/tests/utils.py
@@ -0,0 +1,16 @@
+import os
+
+from pandas.testing import assert_frame_equal
+
+GENERATED_PATH = os.path.join('tests', 'generated_files')
+INPUT_FILES_PATH = os.path.join('tests', 'input_files')
+
+def assert_frame_not_equal(*args, **kwargs):
+    try:
+        assert_frame_equal(*args, **kwargs)
+    except AssertionError:
+        # frames are not equal
+        pass
+    else:
+        # frames are equal
+        raise AssertionError
