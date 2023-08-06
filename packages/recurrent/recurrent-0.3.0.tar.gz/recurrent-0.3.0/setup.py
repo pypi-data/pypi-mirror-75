967d38f0693afe5d2b09264d9ece287831e21a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:34 +0000

    Add package recurrent/0.3.0/recurrent-0.3.0.tar.gz

---
+++ packages/recurrent/recurrent-0.3.0.tar.gz/recurrent-0.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+version = '0.3.0'
+
+setup(name='recurrent',
+      version=version,
+      description="Natural language parsing of recurring events",
+      long_description="See http://github.com/kvh/recurrent",
+      classifiers=[
+          'Natural Language :: English',
+          'Topic :: Text Processing :: Linguistic',
+          'License :: OSI Approved :: BSD License'
+      ],
+      keywords='calendar recurring date event NLP',
+      author='Ken Van Haren',
+      author_email='kvh@science.io',
+      url='http://github.com/kvh/recurrent',
+      license='BSD',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      zip_safe=False,
+      install_requires=[
+          'parsedatetime',
+      ]
+      )
