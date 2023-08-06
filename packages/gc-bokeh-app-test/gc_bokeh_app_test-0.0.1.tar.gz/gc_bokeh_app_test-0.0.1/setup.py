ac9ac785fe34fe344dded9f871300110aac55e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:10 +0000

    Add package gc-bokeh-app-test/0.0.1/gc_bokeh_app_test-0.0.1.tar.gz

---
+++ packages/gc-bokeh-app-test/gc_bokeh_app_test-0.0.1.tar.gz/gc_bokeh_app_test-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+   name='gc_bokeh_app_test',
+   version='0.0.1',
+   author='Greg Coyle',
+   author_email='gregory.coyle@tufts.edu',
+   packages=setuptools.find_packages(),
+   description='Testing a Bokeh app in conda',
+   long_description='Testing a Bokeh app in Anaconda',
+   install_requires=[
+       "bokeh >= 2.1.1"],
+   python_requires='>=3.6',
+)
