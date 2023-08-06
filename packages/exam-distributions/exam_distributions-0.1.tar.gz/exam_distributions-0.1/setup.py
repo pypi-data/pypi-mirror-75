d27577592a88671c4a8ccdcd30fd379e1e9fbe90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:44 +0000

    Add package exam-distributions/0.1/exam_distributions-0.1.tar.gz

---
+++ packages/exam-distributions/exam_distributions-0.1.tar.gz/exam_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='exam_distributions',
+      version='0.1',
+      description='example distributions',
+      packages=['exam_distributions'],
+      zip_safe=False)
