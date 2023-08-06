3e8fbe546799a39482e9eebf8b4ede9258194efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:05 +0000

    Add package aa-timezones/0.1.9/aa_timezones-0.1.9-py3-none-any.whl

---
+++ packages/aa-timezones/aa_timezones-0.1.9-py3-none-any.whl/timezones/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required
+from . import __title__
+
+from .app_settings import (
+    AA_TIMEZONES_ADDITIONAL_PANELS
+)
+
+
+@login_required
+@permission_required('timezones.basic_access')
+def index(request):
+    context = {
+        'title': __title__,
+        'timezonePanelData': AA_TIMEZONES_ADDITIONAL_PANELS
+    }
+
+    return render(request, 'timezones/index.html', context)
