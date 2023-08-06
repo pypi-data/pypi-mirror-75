29eea71cfe32773cc3cb3d95ed1a97bb0cb56f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:18 +0000

    Add package arpit-distributions/0.1/arpit_distributions-0.1.tar.gz

---
+++ packages/arpit-distributions/arpit_distributions-0.1.tar.gz/arpit_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='arpit_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['arpit_distributions'],
+      zip_safe=False)
