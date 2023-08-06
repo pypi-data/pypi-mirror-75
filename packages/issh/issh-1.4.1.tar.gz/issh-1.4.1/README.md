b87e4ba320d6c27bc58084f1908faaa49136a890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package issh/1.4.1/issh-1.4.1.tar.gz

---
+++ packages/issh/issh-1.4.1.tar.gz/issh-1.4.1/README.md
@@ -0,0 +1,81 @@
+# issh
+
+[![PyPI version](https://badge.fury.io/py/issh.svg)](https://pypi.org/project/issh/)
+
+Improved SSH launcher that provides a browsable
+menu of SSH servers. Uses `~/.ssh/config`
+to generate menu.
+
+For more tips on using SSH and the configuration file, check out [https://www.devdungeon.com/content/ssh-tips](https://www.devdungeon.com/content/ssh-tips)
+
+![Works in Windows, Mac, Linux](screenshots/screenshot1.png)
+
+## Install
+
+Install from pypi.org using:
+
+```bash
+python -m pip install issh
+```
+
+Install from source by running this from
+the root of the source code directory:
+
+```bash
+python setup.py install
+```
+
+## Usage
+
+Simply run `issh` from the command line to launch
+the menu.
+
+```bash
+issh
+```
+
+Or invoke via Python:
+
+```bash
+python -m issh
+```
+
+To use the tool inside PYthon source code:
+
+```python
+from issh import ISSH
+
+issh = ISSH()
+issh.run()
+```
+
+## Controls
+
+- `Esc` or `q`: Quit
+- `Enter`, `l`, or `Right Arrow`: Connect
+- `j` or `Down Arrow`: Down
+- `k` or `Up Arrow`: Up
+- `e`: Edit SSH config file
+
+When using the Edit (`e`) command, it tries to use
+the editor defined in `EDITOR` environment variable,
+otherwise attempts to default to a system editor. 
+
+## Troubleshooting
+
+If you have permission errors with the `~/.ssh/config` file,
+make sure the `.ssh/` directory has `700` permissions and
+the `config` file has `600` permissions. Also ensure
+the owner is correct.
+
+## Source code
+
+[https://github.com/DevDungeon/issh](https://github.com/DevDungeon/issh)
+
+## Author
+
+NanoDano <nanodano@devdungeon.com>
+
+## To do
+
+- On right, show the config details
---
+++ packages/issh/issh-1.4.1.tar.gz/issh-1.4.1/issh.py
@@ -0,0 +1,150 @@
+import curses
+import curses.ascii
+from os import system, environ
+from os.path import join, expanduser, exists
+import sys
+from signal import signal, SIGINT, SIGTERM
+
+
+class ISSH:
+
+    def __init__(self, screen):
+        print('[*] Initializing ISSH')
+        signal(SIGINT, self.shutdown)
+        signal(SIGTERM, self.shutdown)
+
+        self.hosts = list()
+        self.ssh_config_path = join(expanduser("~"), ".ssh", "config")
+        self.check_if_ssh_config_exists()
+        self.load_ssh_hosts()
+        self.active_choice = 0
+
+        self.screen = screen
+        self.screen.keypad(1)
+        curses.curs_set(0)
+        curses.noecho()
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+
+    def check_if_ssh_config_exists(self):
+        if not exists(self.ssh_config_path):
+            curses.endwin()
+            print('No SSH config file detected at ' + self.ssh_config_path + '. Aborting.')
+            sys.exit(1)
+
+    def load_ssh_hosts(self):
+        self.hosts = list()
+        with open(self.ssh_config_path) as ssh_config:
+            for line in ssh_config.readlines():
+                line = line.rstrip()
+                if len(line) == 0 or line[0] == ' ' or line[0] == '\t' or line.lstrip()[0] == '#':
+                    continue
+                try:
+                    self.hosts.append(line.split()[1])
+                except IndexError:
+                    print('[-] Warning: Invalid host format detected on line: %s' % line)
+
+    def run(self):
+        self.input_loop()
+
+    def print_options(self):
+        self.screen.clear()
+        num_header_rows = 2
+        self.screen.addstr(0, 0, "Select an SSH host (Press H for help):")
+
+        for i, host in enumerate(self.hosts):
+            if i == self.active_choice:
+                self.screen.addstr(i + num_header_rows, 0, " > %s" % (host), curses.color_pair(1) | curses.A_BOLD)
+            else:
+                self.screen.addstr(i + num_header_rows, 0, "   %s" % (host))
+        self.screen.refresh()
+
+    def input_loop(self):
+        while True:
+            self.print_options()
+
+            try:
+                char_ord = self.screen.getch()
+                char = chr(char_ord)
+
+                if char.upper() == 'Q' or char_ord == curses.ascii.ESC:  # Esc or Q
+                    self.shutdown()
+                elif char.upper() == 'J' or char_ord == curses.KEY_DOWN:  # Down or J
+                    if self.active_choice < len(self.hosts) - 1:
+                        self.active_choice += 1
+                elif char.upper() == 'K' or char_ord == curses.KEY_UP:  # Up or K
+                    if self.active_choice > 0:
+                        self.active_choice -= 1
+                elif char == 'g':  # Move to top
+                    self.active_choice = 0
+                elif char == 'G':  # Move to last item
+                    self.active_choice = len(self.hosts) - 1
+                elif char.upper() == 'E':
+                    self.launch_editor()
+                elif char.upper() == 'R':  # Refresh screen
+                    self.load_ssh_hosts()
+                elif char.upper() == 'H':  # Print help screen
+                    self.print_help_screen()
+                elif char_ord == curses.ascii.LF or char.upper() == 'L' or char_ord == curses.KEY_RIGHT:  # Enter, L or Right
+                    # Choice has been selected already, exit the menu system
+                    break
+            except Exception as e:
+                print('[-] Invalid keypress detected.')
+                print(e)
+
+        # After breaking out of loop, ssh to the active target
+        self.cleanup_curses()
+        system('ssh %s' % self.hosts[self.active_choice])
+
+    def cleanup_curses(self):
+        self.screen.keypad(0)
+        curses.curs_set(1)
+        curses.echo()
+        curses.endwin()
+
+    def shutdown(self):
+        self.cleanup_curses()
+        sys.exit(0)
+
+    def launch_editor(self):
+        editor = environ.get('EDITOR')
+        if editor is None:  # Default editors
+            if sys.platform == 'win32':
+                editor = 'notepad.exe'
+            elif sys.platform == 'darwin':
+                editor = 'nano'
+            elif 'linux' in sys.platform:
+                editor = 'vi'
+        system("%s %s" % (editor, self.ssh_config_path))
+        self.load_ssh_hosts()  # Reload hosts after changes
+
+    def print_help_screen(self):
+        self.screen.clear()
+
+        self.screen.addstr(0, 0, "Help information:")
+        self.screen.addstr(2, 0, "  H - This help screen")
+        self.screen.addstr(3, 0, "  Q or ESC - Quit the program")
+        self.screen.addstr(4, 0, "  E - Edit SSH config file")
+        self.screen.addstr(5, 0, "  R - Reload SSH hosts from config file")
+        self.screen.addstr(6, 0, "  Down or J - Move selection down")
+        self.screen.addstr(7, 0, "  Up or K - Move selection up")
+        self.screen.addstr(8, 0, "  Right or L or Enter - SSH to current selection")
+        self.screen.addstr(9, 0, "  G - Move to last item")
+        self.screen.addstr(10, 0, "  g - Move to first item")
+        self.screen.addstr(12, 0, "Press any key to continue")
+
+        # Wait for any key press
+        self.screen.getch()
+
+
+def main_wrapper(main_screen):
+    issh = ISSH(main_screen)
+    issh.run()
+
+
+def main():
+    curses.wrapper(main_wrapper)
+
+
+if __name__ == '__main__':
+    main()
