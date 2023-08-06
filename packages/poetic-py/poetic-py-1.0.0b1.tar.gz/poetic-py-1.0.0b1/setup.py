00f7546cba19a905b825e48af35433ae636c1035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:25 +0000

    Add package poetic-py/1.0.0b1/poetic-py-1.0.0b1.tar.gz

---
+++ packages/poetic-py/poetic-py-1.0.0b1.tar.gz/poetic-py-1.0.0b1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+description = "For documentation and detials, "
+description += "please refer to http://github.com/kevin931/poetic"
+
+setuptools.setup(
+    name = "poetic-py",
+    version = "1.0.0b1",
+    url = "https://github.com/kevin931/poetic",
+    author = "Kevin Wang",
+    author_email = "bridgemarian@gmail.com",
+    description = "Predicts how poetic sentences are.",
+    long_description = description,
+    packages=["poetic"],
+    python_requires=">3.5, <3.8",
+    install_requires=["tensorflow==2.0",
+                      "gensim>=3.8",
+                      "nltk>=3.4"
+    ],
+    install_package_data=True,
+    package_data={"poetic":["./data/word_dictionary_complete.txt"]},
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only"
+    ]
+)
