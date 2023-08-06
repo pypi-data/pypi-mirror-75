e58d0ff60fa515b3f2efb0f049fad0e47e5d416a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:21 +0000

    Add package distributionsVeer/0.1/distributionsVeer-0.1.tar.gz

---
+++ packages/distributionsVeer/distributionsVeer-0.1.tar.gz/distributionsVeer-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionsVeer',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributionsVeer'],
+      author = 'Veeru Talreja',
+      zip_safe=False)
