812ee8a1e525d241c9da0d14347732844905bb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:21 +0000

    Add package easy-to-go-distributions/0.1/easy_to_go_distributions-0.1.tar.gz

---
+++ packages/easy-to-go-distributions/easy_to_go_distributions-0.1.tar.gz/easy_to_go_distributions-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+    
+setup(name='easy_to_go_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['easy_to_go_distributions'],
+      author = 'Anurag Chakrabarti',
+      author_email = 'anuragchakr@gmail.com',
+      url= 'https://test.pypi.org/projects/easy_to_go_distributions',
+      zip_safe=False)
