b6410c9d2a3a59c561bcf08e47405853dc54967c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:36 +0000

    Add package lykkellemainoceania/0.0.1/lykkellemainoceania-0.0.1-py2-none-any.whl

---
+++ packages/lykkellemainoceania/lykkellemainoceania-0.0.1-py2-none-any.whl/lykkellemainall/healthcheck.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 20:47:59 2019
+daily checklist that runs a health check on the load details
+@author: debaj
+"""
+from os.path import expanduser
+from lykkelledatahandler.exceptioneod import exception
+
+home = expanduser("~")
+
+class exceptionlog:
+    def __init__(self):
+        print("-------------------------stockexceptionlog-------------------------------")
+        exception.stockexception()
+        print("-------------------------End-------------------------------")
+        print("\n")
+        print("-------------------------Benchmarkexceptionlog-------------------------------")
+        exception.benchmarkexception()
+        print("-------------------------End-------------------------------")
+#        print("-------------------------Bondexceptionlog-------------------------------")
+#        exception.bondexception()
+#        print("-------------------------End-------------------------------")
+
+#exceptionlog()
+
+
