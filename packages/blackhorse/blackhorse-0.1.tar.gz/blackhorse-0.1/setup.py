1234f85ad02e7eafe8afbeb384a45d3e9f0a4e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:51 +0000

    Add package blackhorse/0.1/blackhorse-0.1.tar.gz

---
+++ packages/blackhorse/blackhorse-0.1.tar.gz/blackhorse-0.1/setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name="blackhorse",
+    packages=["blackhorse"],
+    version="0.1",
+    license="MIT",
+    description="A beautiful black horse placeholder",
+    author="Radu Ghitescu",
+    author_email="radu.ghitescu@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha", "Intended Audience :: Developers"
+    ],
+    zip_safe=False,
+)
