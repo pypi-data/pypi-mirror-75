760a0f2038dc5aef3e952b69fc72ebe040dd94b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:56 +0000

    Add package replit/1.4.0/replit-1.4.0-py3-none-any.whl

---
+++ packages/replit/replit-1.4.0-py3-none-any.whl/replit/__init__.py
@@ -0,0 +1,12 @@
+"""The replit python module."""
+from . import maqpy
+from .audio import Audio
+from .database import db
+
+
+def clear() -> None:
+    """Clear the terminal."""
+    print("\033[H\033[2J", end="", flush=True)
+
+
+audio = Audio()
