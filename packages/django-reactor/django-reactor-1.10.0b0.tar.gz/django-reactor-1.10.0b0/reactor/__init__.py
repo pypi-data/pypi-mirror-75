1c3ccdedd4ffdc8b1be0699e8b2cb9771bfeede9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:36 +0000

    Add package django-reactor/1.10.0b0/django-reactor-1.10.0b0.tar.gz

---
+++ packages/django-reactor/django-reactor-1.10.0b0.tar.gz/django-reactor-1.10.0b0/reactor/__init__.py
@@ -0,0 +1,30 @@
+from django.core.checks import Warning, register
+from django.conf import settings as dj_settings
+
+from . import settings
+from . import auto_broadcast  # noqa
+from .component import (  # noqa
+    Component, AuthComponent, StaffComponent, on_commit, broadcast
+)
+
+
+@register()
+def check_for_turbolinks_middleware(app_configs, **kwargs):
+    reactor_middleware = 'reactor.middleware.turbolinks_middleware'
+    problem = (
+        settings.INCLUDE_TURBOLINKS and
+        reactor_middleware not in dj_settings.MIDDLEWARE
+    )
+    if problem:
+        return [Warning(
+            'Turbolinks middleware is missing',
+            hint=(
+                f'When you set REACTOR_INCLUDE_TURBOLINKS, during the '
+                f'redirects the URL of the browser could be that is not up to '
+                f'date. To avoid that include `{reactor_middleware}` in the '
+                f'`MIDDLEWARE`.'
+            ),
+            id='reactor.W001',
+        )]
+    else:
+        return []
