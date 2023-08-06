8abfe69575d9e6758f93ea865a80e8bcc4367d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:44 +0000

    Add package PyHand-Earth/0.2.21/PyHand_Earth-0.2.21.tar.gz

---
+++ packages/PyHand-Earth/PyHand_Earth-0.2.21.tar.gz/PyHand_Earth-0.2.21/PyHand_Earth/capture_window.py
@@ -0,0 +1,68 @@
+"""Stand-alone program that uses OpenCV to capture live webcam images
+and test TensorFlow model predictions using saved model"""
+import os
+import cv2
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+from PyQt5.QtWidgets import QVBoxLayout, QLabel
+from PyQt5.QtGui import QPixmap
+from keras.models import load_model
+from .capture_thread import CaptureThread
+
+# Suppress TensorFlow info and warnings. Errors still displayed.
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+class QtCapture(QtWidgets.QWidget):
+
+    def __init__(self, earth, desktop, screen, camera, *args, **kwargs):
+        super(QtCapture, self).__init__(*args, **kwargs)
+
+        self.model = load_model('pyearth_cnn_model_0724.h5')
+        self.class_names = ['INDEX_UP', 'V_SIGN', 'THUMB_LEFT', 'THUMB_RIGHT', 'FIST', 'FIVE_WIDE',
+                            'PALM', 'SHAKA', 'NOTHING']
+
+        self.earth = earth
+        self.desktop = desktop
+        self.screen = screen
+
+        self.video_frame = QLabel(self)
+        self.layout_one = QVBoxLayout()
+        self.layout_one.setContentsMargins(0, 0, 0, 0)
+        self.layout_one.addWidget(self.video_frame)
+        self.setLayout(self.layout_one)
+
+
+        self.camera = camera
+        
+        # if self.camera is None or not self.camera.isOpened():
+        #     print("Unable to find webcam. Please connect a camera.")
+
+        self.output = ""
+
+        self.cap_thread = None
+
+        self.start_thread()
+
+    def start_thread(self):
+        self.cap_thread = CaptureThread(self.earth, self.model, self.class_names, self.camera, self.desktop, self.screen)
+        self.cap_thread.updatePixmap.connect(self.setVideoFrame)
+        self.cap_thread.updateOutput.connect(self.setOutput)
+        self.cap_thread.start()
+
+    def stop_thread(self):
+        if self.cap_thread:
+            self.cap_thread.stop_thread()
+
+    @pyqtSlot(QPixmap)
+    def setVideoFrame(self, frame):
+        self.video_frame.setPixmap(frame)
+
+    @pyqtSlot(str)
+    def setOutput(self, output):
+        self.output = output
+
+    def get_output(self):
+        return self.output
+
+    def delete(self):
+        self.camera.release()
---
+++ packages/PyHand-Earth/PyHand_Earth-0.2.21.tar.gz/PyHand_Earth-0.2.21/PyHand_Earth/google_earth.py
@@ -0,0 +1,143 @@
+"""
+Implements the Google Earth class for interacting with Google Earth program.
+"""
+import os
+import time
+import psutil
+from .keyboard_commands import KeyboardCommands
+
+class GoogleEarth():
+    """
+    This is a class that starts Google Earth and performs operations
+    on the Google Earth window.
+
+    Attributes:
+        keyboard_commands : Instantiates keyboard command class object for sending
+            commands to Google Earth
+        screen_position (list) : Position values for moving and resizing windows based
+            on screen resolution
+        resolution (string) : Resolution of the current monitor
+    """
+    def __init__(self, desktop_geo, screen_geo):
+        """
+        Please see help(GoogleEarth) for more info
+        """
+        self.keyboard_commands = KeyboardCommands()
+
+        self.new_width = None
+        self.new_height = None
+
+        self.desktop_geometry = desktop_geo
+        self.screen_geometry = screen_geo
+
+    def check_process_running(self, process_name):
+        """
+        Checks if there are any processes currently running that contain a given name.
+
+        Parameters:
+        process_name (string) : Name of the process to be checked
+
+        Returns:
+        bool: Returns True if process found, else returns False
+        """
+        # Check if there is any running process that contains the given name processName.
+        # Iterate over the all the running process
+        for proc in psutil.process_iter():
+            try:
+                if process_name.lower() in proc.name().lower():
+                    return True
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return False
+
+    def initialize_google_earth(self):
+        """
+        Checks if Google Earth is running and starts Google Earth if it is not running.
+        Following start up, closes two Google Earth toolbars.
+        """
+        #Checking if Google Earth is already running
+        if self.check_process_running('google-earth'):
+            self.start_google_earth()
+
+        else:
+            #Start Google Earth if it is not already running and resize window
+            os.system("nohup google-earth-pro </dev/null >/dev/null 2>&1 &")
+            time.sleep(2)
+            self.start_google_earth()
+
+        self.toggle_buttons_off()
+
+    def start_google_earth(self):
+        """
+        Gets the current screen resolution and starts Google Earth. Repositions Google Earth
+        in the center of the screen with a size determined by the current screen resolution.
+        """
+        self.check_if_fullscreen()
+        self.set_screen_resolution()
+        comm = "wmctrl -r 'Google Earth' -e 0,0,0," + str(int(self.new_width)) + "," + \
+                str(int(self.new_height))
+        os.system(comm)
+        time.sleep(2)
+
+    def set_screen_resolution(self):
+        """
+        Sets the Google Earth window resize and screen position based on monitor resolution.
+        """
+
+        self.new_width = self.desktop_geometry.width()
+        self.new_height = ((self.desktop_geometry.height() * (3/4)) - 37)
+
+    def reposition_earth_small(self):
+        # REPLACE with hotkey for ALT + F10 to toggle full screen
+        self.check_if_fullscreen()
+
+        comm = "wmctrl -r 'Google Earth' -e 0,0,0," + str(int(self.new_width / 2)) + "," + \
+                str(int(self.new_height))
+        os.system(comm)
+        time.sleep(2)
+
+    def reposition_earth_large(self):
+        # REPLACE with hotkey for ALT + F10 to toggle full screen
+        self.check_if_fullscreen()
+
+        comm = "wmctrl -r 'Google Earth' -e 0,0,0," + str(int(self.new_width)) + "," + \
+                str(int(self.new_height))
+        os.system(comm)
+        time.sleep(2)
+
+    def toggle_buttons_off(self):
+        """
+        Toggles sidebar in Google Earth that cause problems when sending commands
+        to the Google Earth window.
+        """
+        fileDir = os.path.dirname(os.path.abspath(__file__))
+        sidebar_coords = self.keyboard_commands.locate_image(fileDir+'/images/clicked_sidebar.png')
+
+        if sidebar_coords:
+            self.keyboard_commands.close_sidebar()
+
+    def start_up_tips(self):
+        """
+        Returns True/False flag to pop-up message to close Start-up Tips Google Earth Pro window.
+        """
+        fileDir = os.path.dirname(os.path.abspath(__file__))
+        start_up_tips_coords = self.keyboard_commands.locate_image(fileDir+'/images/close.png')
+
+        if start_up_tips_coords:
+            return True
+        else:
+            return False
+
+    def check_if_fullscreen(self):
+        fileDir = os.path.dirname(os.path.abspath(__file__))
+        fullscreen = self.keyboard_commands.locate_image(fileDir+'/images/fullscreen.png')
+
+        if fullscreen:
+            self.keyboard_commands.click_with_location(fullscreen)
+            time.sleep(2)
+
+    def close_earth(self):
+        """
+        Closes the Google Earth window.
+        """
+        os.system("wmctrl -c Google Earth Pro")
