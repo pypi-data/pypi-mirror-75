20c5556ed93e432b2beb233bfdd1971e381f0bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package pyros-discovery/1.0.1/pyros-discovery-1.0.1.tar.gz

---
+++ packages/pyros-discovery/pyros-discovery-1.0.1.tar.gz/pyros-discovery-1.0.1/setup.py
@@ -0,0 +1,38 @@
+
+
+################################################################################
+# Copyright (C) 2016-2020 Abstract Horizon
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License v2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Contributors:
+#    Daniel Sendula - initial API and implementation
+#
+#################################################################################
+
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="pyros-discovery",
+    version="1.0.1",
+    author="Daniel Sendula",
+    description="Simple discovery service for PyROS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Abstract-Horizon/pyros-discovery",
+    zip_safe=False, # Doesn't create an egg - easier to debug and hack on
+    packages=['discovery'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
