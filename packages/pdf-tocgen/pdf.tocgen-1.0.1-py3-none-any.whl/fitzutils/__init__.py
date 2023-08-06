a70621b6be06107ebd7c5a32734668fe30b34479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:25:22 +0000

    Add package pdf-tocgen/1.0.1/pdf.tocgen-1.0.1-py3-none-any.whl

---
+++ packages/pdf-tocgen/pdf.tocgen-1.0.1-py3-none-any.whl/fitzutils/__init__.py
@@ -0,0 +1,19 @@
+"""A collection of utility functions to work with PyMuPDF"""
+
+__version__ = '0.1.0'
+
+from .fitzutils import (
+    open_pdf,
+    get_pages,
+    ToCEntry,
+    dump_toc,
+    pprint_toc
+)
+
+__all__ = [
+    'open_pdf',
+    'get_pages',
+    'ToCEntry',
+    'dump_toc',
+    'pprint_toc',
+]
