7949aadd56cba21271549c81ee5a07ade85b3df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:52 +0000

    Add package pymetrick/0.54.0/pymetrick-0.54.0-py3-none-any.whl

---
+++ packages/pymetrick/pymetrick-0.54.0-py3-none-any.whl/pymetrick/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Fco.Javier Tamarit <javtamvi@telefonica.net>"
+__copyright__ = "Copyright (C) 2012 PYMETRICK.ORG"
+__license__ = "AGPLv3"
+
+"""A package for parsing, handling, and generating html5."""
+
+__version__ = '0.49.0'
+__all__ = ["common","device","helpers","mail","routing","rst","session","sqldb","template"]
+
+
