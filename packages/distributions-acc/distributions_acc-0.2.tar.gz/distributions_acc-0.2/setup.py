01f334863ebec280676c00a1d78fef420edc7cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:06 +0000

    Add package distributions-acc/0.2/distributions_acc-0.2.tar.gz

---
+++ packages/distributions-acc/distributions_acc-0.2.tar.gz/distributions_acc-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_acc',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_acc'],
+      author = 'Andrea Cruz Castillo',
+      zip_safe=False)
