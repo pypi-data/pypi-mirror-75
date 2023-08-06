052ac7587c95491b5bb4331468602964b3a48664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:03 +0000

    Add package mplstereonet/0.6.2/mplstereonet-0.6.2.tar.gz

---
+++ packages/mplstereonet/mplstereonet-0.6.2.tar.gz/mplstereonet-0.6.2/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+dirname = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(dirname, 'README.rst'), 'r') as infile:
+    long_description = infile.read()
+
+setup(
+    name = 'mplstereonet',
+    version = '0.6.2',
+    description = "Stereonets for matplotlib",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author = 'Joe Kington',
+    author_email = 'joferkington@gmail.com',
+    license = 'MIT',
+    url = 'https://github.com/joferkington/mplstereonet/',
+    packages = ['mplstereonet'],
+    install_requires = [
+        'matplotlib >= 1.1',
+        'numpy >= 1.1']
+)
