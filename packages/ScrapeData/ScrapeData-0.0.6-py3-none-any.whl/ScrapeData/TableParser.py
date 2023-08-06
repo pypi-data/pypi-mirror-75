9853b528e515167a6885380a0971ddfbefd23cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:10 +0000

    Add package ScrapeData/0.0.6/ScrapeData-0.0.6-py3-none-any.whl

---
+++ packages/ScrapeData/ScrapeData-0.0.6-py3-none-any.whl/ScrapeData/TableParser.py
@@ -0,0 +1,37 @@
+from ScrapeData import Utils as utils
+
+#single function to convert to json using tag class
+def toJsonByClass(URL, tagClass, orientation, tagId = "table"):
+    responseData = utils.getDataFromUrl(URL)
+    parsedData = utils.parseDataUsingHtmlParser(responseData)
+    tableData = utils.findTableByClass(parsedData,tagId, tagClass)
+    listData = utils.convertTableToList(tableData)
+    jsonData = utils.convertListToJson(listData, orientation)
+    return jsonData
+
+#single function to convert to json using tag occurance index
+def toJsonByIndex(URL, tagIndex, orientation, tagId = "table"):
+    responseData = utils.getDataFromUrl(URL)
+    parsedData = utils.parseDataUsingHtmlParser(responseData)
+    tableData = utils.findTableByIndex(parsedData,tagId, tagIndex)
+    listData = utils.convertTableToList(tableData)
+    jsonData = utils.convertListToJson(listData, orientation)
+    return jsonData
+
+#single function to convert to csv using tag class
+def toCsvByClass(URL, tagClass, fileName, tagId = "table"):
+    responseData = utils.getDataFromUrl(URL)
+    parsedData = utils.parseDataUsingHtmlParser(responseData)
+    tableData = utils.findTableByClass(parsedData,tagId, tagClass)
+    listData = utils.convertTableToList(tableData)
+    jsonData = utils.convertListToCsv(listData, fileName)
+    return jsonData
+
+#single function to convert to csv using tag occurance index
+def toCsvByIndex(URL, tagIndex, fileName, tagId = "table"):
+    responseData = utils.getDataFromUrl(URL)
+    parsedData = utils.parseDataUsingHtmlParser(responseData)
+    tableData = utils.findTableByIndex(parsedData,tagId, tagIndex)
+    listData = utils.convertTableToList(tableData)
+    jsonData = utils.convertListToCsv(listData, fileName)
+    return jsonData
