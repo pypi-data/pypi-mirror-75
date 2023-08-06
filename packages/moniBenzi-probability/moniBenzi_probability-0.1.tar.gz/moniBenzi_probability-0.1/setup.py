daa643aa6af676a3c4a917539446d15fda5e47fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:48 +0000

    Add package moniBenzi-probability/0.1/moniBenzi_probability-0.1.tar.gz

---
+++ packages/moniBenzi-probability/moniBenzi_probability-0.1.tar.gz/moniBenzi_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='moniBenzi_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['moniBenzi_probability'],
+      zip_safe=False)
