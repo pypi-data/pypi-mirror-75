ee9c776008a5aa41d117735f07123e3a4780eded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:42 +0000

    Add package distributions9989/0.1/distributions9989-0.1.tar.gz

---
+++ packages/distributions9989/distributions9989-0.1.tar.gz/distributions9989-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions9989',
+      version='0.1',
+      description='Gaussian and Binomial distributions ',
+      packages=['distributions9989'],
+      zip_safe=False)
