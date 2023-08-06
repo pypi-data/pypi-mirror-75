5900d33b7d84b9d12710664ac6c900b8a3a75d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:46 +0000

    Add package imp-distributions/0.1/imp_distributions-0.1.tar.gz

---
+++ packages/imp-distributions/imp_distributions-0.1.tar.gz/imp_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='imp_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['imp_distributions'],
+      zip_safe=False)
