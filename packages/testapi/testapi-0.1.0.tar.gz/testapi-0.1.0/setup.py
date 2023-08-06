237228de16ee2ba4c543d6ea1d29dcae79ffd262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:46 +0000

    Add package testapi/0.1.0/testapi-0.1.0.tar.gz

---
+++ packages/testapi/testapi-0.1.0.tar.gz/testapi-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['testapi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['flask>=1.1.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'testapi',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
