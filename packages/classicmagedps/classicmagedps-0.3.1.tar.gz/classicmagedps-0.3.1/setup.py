b2137e73a31d0217e3f03c94da3c17eef34643f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:05 +0000

    Add package classicmagedps/0.3.1/classicmagedps-0.3.1.tar.gz

---
+++ packages/classicmagedps/classicmagedps-0.3.1.tar.gz/classicmagedps-0.3.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['classicmagedps']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['simpy>=4.0.1,<5.0.0', 'tqdm>=4.46.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'classicmagedps',
+    'version': '0.3.1',
+    'description': '',
+    'long_description': None,
+    'author': 'mcdallas',
+    'author_email': 'mcdallas@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.3,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
