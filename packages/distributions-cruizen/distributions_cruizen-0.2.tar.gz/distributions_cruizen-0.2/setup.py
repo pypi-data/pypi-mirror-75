d5f876e2e9390ef46edb2c48f351c2129d62b098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:13 +0000

    Add package distributions-cruizen/0.2/distributions_cruizen-0.2.tar.gz

---
+++ packages/distributions-cruizen/distributions_cruizen-0.2.tar.gz/distributions_cruizen-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_cruizen',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_cruizen'],
+      zip_safe=False)
