bbfe23f9ad600863eecc829f5cb5db0d046df5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:26 +0000

    Add package gokg-distributions/1.0/gokg_distributions-1.0.tar.gz

---
+++ packages/gokg-distributions/gokg_distributions-1.0.tar.gz/gokg_distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gokg_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['gokg_distributions'],
+      zip_safe=False)
