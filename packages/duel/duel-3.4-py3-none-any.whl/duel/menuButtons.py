b9c57ffdf61705885df878055d31c20d510093f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:43 +0000

    Add package duel/3.4/duel-3.4-py3-none-any.whl

---
+++ packages/duel/duel-3.4-py3-none-any.whl/duel/menuButtons.py
@@ -0,0 +1,19 @@
+import pygame
+import os
+
+
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, sprite_path, x_pos, y_pos, rect_width, rect_height):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprite = sprite_path
+        self.button_rect = pygame.Rect(x_pos, y_pos, rect_width, rect_height)
+        self.image = getImage(sprite_path)
+        self.sprite_mask = pygame.mask.from_surface(self.image)
+
+
+
+def getImage(path):
+    canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+    image = pygame.image.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), canonicalized_path))
+    return image
