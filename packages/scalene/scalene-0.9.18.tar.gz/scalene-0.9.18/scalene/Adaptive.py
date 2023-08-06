1ca6a1da5e0b98cca61b582a23ec11e89db23811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:26 +0000

    Add package scalene/0.9.18/scalene-0.9.18.tar.gz

---
+++ packages/scalene/scalene-0.9.18.tar.gz/scalene-0.9.18/scalene/Adaptive.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Adaptive:
+    """Implements sampling to achieve the effect of a uniform random sample."""
+
+    sample_array: List[float] = []
+    current_index = 0
+    max_samples = 0
+
+    def __init__(self, size: int):
+        self.max_samples = size
+        # must be a power of two
+        self.sample_array = [0] * size
+
+    def add(self, value: float) -> None:
+        if self.current_index >= self.max_samples:
+            # Decimate
+            new_array = [0.0] * self.max_samples
+            for i in range(0, self.max_samples // 3):
+                arr = [self.sample_array[i * 3 + j] for j in range(0, 3)]
+                arr.sort()
+                new_array[i] = arr[1]  # Median
+            self.current_index = self.max_samples // 3
+            self.sample_array = new_array
+        self.sample_array[self.current_index] = value
+        self.current_index += 1
+
+    def get(self) -> List[float]:
+        return self.sample_array
+
+    def len(self) -> int:
+        return self.current_index
