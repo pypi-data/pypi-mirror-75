6d43008b9c64de4526afc33c9f04c6860e6e8fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:11 +0000

    Add package gaussian-dstrbtn/0.1/gaussian_dstrbtn-0.1.tar.gz

---
+++ packages/gaussian-dstrbtn/gaussian_dstrbtn-0.1.tar.gz/gaussian_dstrbtn-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussian_dstrbtn',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gaussian_dstrbtn'],
+      zip_safe=False)
