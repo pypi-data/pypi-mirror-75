e0d936ff59f4693b1f90b90e99431085775f53f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:47 +0000

    Add package distributions-rp/0.1/distributions_rp-0.1.tar.gz

---
+++ packages/distributions-rp/distributions_rp-0.1.tar.gz/distributions_rp-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_rp',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_rp'],
+      author = 'Ritika Pal',
+      author_email = 'palritika71@gmail.com',
+      zip_safe=False)
