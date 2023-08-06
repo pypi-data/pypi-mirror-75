efd4f6e4104d840eced7b45447f09c6eba3ae59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:33 +0000

    Add package atomic-sequence/0.0.6/atomic-sequence-0.0.6.tar.gz

---
+++ packages/atomic-sequence/atomic-sequence-0.0.6.tar.gz/atomic-sequence-0.0.6/setup.py
@@ -0,0 +1,46 @@
+from setuptools import Extension, setup, find_packages
+from distutils.command.sdist import sdist as _sdist
+
+
+# Can't import cython until setup is read, can't read setup until
+# Cython is installed.
+try:
+    from Cython.Build import cythonize
+    use_cython = True
+except ImportError:
+    use_cython = False
+
+if use_cython:
+    ext_modules = cythonize(
+        Extension("atomic", sources=["sequence/sequence.pyx"], language="c++",),
+        compiler_directives={"embedsignature": True},
+    )
+else:
+    ext_modules = [Extension("atomic", sources=["sequence/sequence.cpp"], language="c++")]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    author="Andriy Rossoshynskyy",
+    author_email="andriy.rossoshynskyy@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    description="A lock-free, thread-safe sequence counter using the C++ atomic library.",
+    ext_modules=ext_modules,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="atomic-sequence",
+    # include_package_data=True,
+    install_requires=["Cython"],
+    packages=find_packages(),
+    package_data={"": ["src/*", "*.pxd", "*.pyx", "*.cpp", ".h"],},
+    python_requires=">=3.6",
+    setup_requires=["Cython"],
+    url="https://github.com/arossoshynskyy/atomic-sequence",
+    version="0.0.6",
+)
