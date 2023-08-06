b229990cc5879b2c1dc48a0717d34ec22ea7cbf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:31 +0000

    Add package distri5kfdsnkdsa/0.1/distri5kfdsnkdsa-0.1.tar.gz

---
+++ packages/distri5kfdsnkdsa/distri5kfdsnkdsa-0.1.tar.gz/distri5kfdsnkdsa-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distri5kfdsnkdsa',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distri5kfdsnkdsa'],
+      author= 'hajar',
+      email= 'hajarwer@gmailcom',
+      zip_safe=False)
