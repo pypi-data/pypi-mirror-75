1b5d88543c91e89c20d3427bb6923d871e989d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:16 +0000

    Add package haiderbhai-distributions/10.0/haiderbhai_distributions-10.0.tar.gz

---
+++ packages/haiderbhai-distributions/haiderbhai_distributions-10.0.tar.gz/haiderbhai_distributions-10.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='haiderbhai_distributions',
+      version='10.0',
+      description='Binomial and Gaussian distributions',
+      packages=['haiderbhai_distributions'],
+      author = 'Syed Haider Ali Shah',
+      author_email = '22110282@lums.edu.pk',
+      zip_safe=False)
