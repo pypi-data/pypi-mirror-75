a459782a1c3ca3ef9e0cfabe57de1fb826e15435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:41 +0000

    Add package lib-parameter/1.1.4/lib_parameter-1.1.4-py3-none-any.whl

---
+++ packages/lib-parameter/lib_parameter-1.1.4-py3-none-any.whl/lib_parameter/__init__conf__.py
@@ -0,0 +1,20 @@
+name = 'lib_parameter'
+title = 'small gist,to return a default value if the parameter is None'
+version = '1.1.4'
+url = 'https://github.com/bitranox/lib_parameter'
+author = 'Robert Nowotny'
+author_email = 'bitranox@gmail.com'
+shell_command = 'lib_parameter'
+
+
+def print_info() -> None:
+    print("""\
+
+Info for lib_parameter:
+
+    small gist,to return a default value if the parameter is None
+
+    Version : 1.1.4
+    Url     : https://github.com/bitranox/lib_parameter
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com""")
