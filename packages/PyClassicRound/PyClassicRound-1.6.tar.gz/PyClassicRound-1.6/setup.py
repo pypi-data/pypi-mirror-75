330d341a8a7674c49ae9e2ac9c0000fe1368102c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:50 +0000

    Add package PyClassicRound/1.6/PyClassicRound-1.6.tar.gz

---
+++ packages/PyClassicRound/PyClassicRound-1.6.tar.gz/PyClassicRound-1.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyClassicRound",
+    version="1.6",
+    author="Rohit",
+    author_email="rohit.shrivastava93@gmail.com",
+    description="Current python 3's round function uses Banker's rounding, this package will help you to round off numbers the classical way.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rohitxsh93/PyClassicRound",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
