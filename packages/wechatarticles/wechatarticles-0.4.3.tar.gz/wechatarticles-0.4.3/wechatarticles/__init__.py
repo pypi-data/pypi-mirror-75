8e0b91042d65543bb846cdd87039cd6571b96a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:18 +0000

    Add package wechatarticles/0.4.3/wechatarticles-0.4.3.tar.gz

---
+++ packages/wechatarticles/wechatarticles-0.4.3.tar.gz/wechatarticles-0.4.3/wechatarticles/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from .ArticlesUrls import ArticlesUrls
+from .ArticlesInfo import ArticlesInfo
+from .ArticlesAPI import ArticlesAPI
+from .GetUrls import PCUrls, MobileUrls
+
+try:
+    from .ReadOutfile import Reader
+except Exception:
+    print("not use mitmproxy")
+
+__version__ = "0.4.3"
