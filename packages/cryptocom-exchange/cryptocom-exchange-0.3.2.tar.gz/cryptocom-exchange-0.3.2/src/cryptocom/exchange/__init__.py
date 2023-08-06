b3f164a1547b70a825a9d27d37b46b1aa44d904e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:07 +0000

    Add package cryptocom-exchange/0.3.2/cryptocom-exchange-0.3.2.tar.gz

---
+++ packages/cryptocom-exchange/cryptocom-exchange-0.3.2.tar.gz/cryptocom-exchange-0.3.2/src/cryptocom/exchange/__init__.py
@@ -0,0 +1,14 @@
+from .structs import (
+    OrderSide, OrderStatus, OrderType, Pair, Period, Candle, Trade
+)
+from .base import Exchange, Account
+from .api import ApiError, ApiProvider
+
+__all__ = [
+    'OrderSide', 'OrderStatus', 'OrderType', 'Pair',
+    'Period', 'Candle', 'Trade',
+    'Exchange', 'Account',
+    'ApiError', 'ApiProvider'
+]
+
+VERSION = '0.3.2'
