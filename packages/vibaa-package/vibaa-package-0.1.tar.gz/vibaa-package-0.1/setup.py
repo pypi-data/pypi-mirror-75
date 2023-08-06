a1319f7eb2283b0e44264893e9923b0e61dd5e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:18 +0000

    Add package vibaa-package/0.1/vibaa-package-0.1.tar.gz

---
+++ packages/vibaa-package/vibaa-package-0.1.tar.gz/vibaa-package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vibaa-package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vibaa-package'],
+      zip_safe=False)
