1bb858f596a679357b9a801f6bbbe8a10b7fc395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:19 +0000

    Add package bilili/0.2.6/bilili-0.2.6-py2.py3-none-any.whl

---
+++ packages/bilili/bilili-0.2.6-py2.py3-none-any.whl/bilili/utils/thread.py
@@ -0,0 +1,48 @@
+import time
+import queue
+import threading
+
+
+class ThreadPool():
+    """ 线程池类
+    快速创建多个相同任务的线程池
+    """
+    def __init__(self, num):
+        self.num = num
+        self._taskQ = queue.Queue()
+        self.threads = []
+        self.__flag = True
+
+    def add_task(self, task):
+        """ 添加任务　"""
+        self._taskQ.put(task)
+
+    def _run_task(self):
+        """ 启动任务线程　"""
+        while True:
+            if not self._taskQ.empty():
+                task = self._taskQ.get(block = True, timeout = 1)
+                task()
+                self._taskQ.task_done()
+            elif not self.__flag:
+                time.sleep(1)
+            else:
+                break
+
+    def wait(self):
+        self.__flag = False
+
+    def resume(self):
+        self.__flag = True
+
+    def run(self):
+        """ 启动线程池　"""
+        for _ in range(self.num):
+            th = threading.Thread(target=self._run_task)
+            th.setDaemon(True)
+            self.threads.append(th)
+            th.start()
+
+    def join(self):
+        """ 等待所有任务结束　"""
+        self._taskQ.join()
