02399f5a5b38d5ea3047943ca89e21aea4dab208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:33 +0000

    Add package trylmz1/0.0.11/trylmz1-0.0.11.tar.gz

---
+++ packages/trylmz1/trylmz1-0.0.11.tar.gz/trylmz1-0.0.11/trylmz1/Checkers.py
@@ -0,0 +1,21 @@
+
+from tensorflow.python.keras.preprocessing.text import Tokenizer
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+import pickle
+import os
+import tensorflow as tf
+this_dir, this_filename = os.path.split(__file__)
+tokenizerpath = os.path.join(this_dir, "tokenizers", "tokenizer_deda.pickle")
+
+weigthpath = os.path.join(this_dir, "weights", "Model_deda.h5")
+
+
+def check_de(string):
+    with open(tokenizerpath, 'rb') as handle:
+        turkish_tokenizer = pickle.load(handle)
+    model = load_model(weigthpath, custom_objects= 'auc')
+    tokens = turkish_tokenizer.texts_to_sequences(string)
+    tokens_pad = pad_sequences(tokens, maxlen=7)
+    print(tokens)
+
