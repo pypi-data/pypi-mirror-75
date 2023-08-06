ea8987a8a980688cb1ac54d4acc0be780f33c841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:02 +0000

    Add package CombinatorialProbability/0.0.1/CombinatorialProbability-0.0.1.tar.gz

---
+++ packages/CombinatorialProbability/CombinatorialProbability-0.0.1.tar.gz/CombinatorialProbability-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="CombinatorialProbability", # Replace with your own username
+    version="0.0.1",
+    author="Stephen DeSalvo",
+    author_email="stephendesalvo@gmail.com",
+    description="A POC for a combinatorial probability library using integer partitions as one example",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/combinatorialprobability",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
