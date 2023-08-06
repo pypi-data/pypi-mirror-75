88de0e89e79377c772243790c797166bd18bed4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:12 +0000

    Add package tttDistributions/0.1/tttDistributions-0.1.tar.gz

---
+++ packages/tttDistributions/tttDistributions-0.1.tar.gz/tttDistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tttDistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['tttDistributions'],
+      author = 'EmmaW',
+      author_email = 'emmayt@outlook.com',
+      zip_safe=False)
