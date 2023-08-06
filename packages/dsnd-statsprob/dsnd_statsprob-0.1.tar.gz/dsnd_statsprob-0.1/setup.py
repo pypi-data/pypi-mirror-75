296c8501bfdeb26d8b7d9def9cb966eb4336e8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:11 +0000

    Add package dsnd-statsprob/0.1/dsnd_statsprob-0.1.tar.gz

---
+++ packages/dsnd-statsprob/dsnd_statsprob-0.1.tar.gz/dsnd_statsprob-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_statsprob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_statsprob'],
+      author = 'Navneet Sajwan',
+      zip_safe=False)
