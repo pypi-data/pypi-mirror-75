f8b43dbadb699b73b64c138b62e4fded4329f5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:30 +0000

    Add package dsnd-probability-sabelo/1.2/dsnd_probability_sabelo-1.2.tar.gz

---
+++ packages/dsnd-probability-sabelo/dsnd_probability_sabelo-1.2.tar.gz/dsnd_probability_sabelo-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_sabelo',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_sabelo'],
+      author = 'Sabelo Sibanda',
+      author_email = 'myemail@something.com',
+      zip_safe=False)
