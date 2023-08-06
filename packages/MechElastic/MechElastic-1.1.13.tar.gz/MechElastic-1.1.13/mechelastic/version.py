2594803ad29ad9c3e6d0e00578d037bcaa1856ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:25 +0000

    Add package MechElastic/1.1.13/MechElastic-1.1.13.tar.gz

---
+++ packages/MechElastic/MechElastic-1.1.13.tar.gz/MechElastic-1.1.13/mechelastic/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM MECHELASTIC SETUP.PY.
+name = 'MechElastic'
+version = '1.1.13'
+description = 'A Python library to calculate elastic properties of materials. '
+url = 'https://github.com/romerogroup/MechElastic'
+author = 'Sobhit Singh'
+email = 'smsingh@mail.wvu.edu'
+status = 'development'
+copyright = 'Copyright 2020'
+date = 'Aug 4th, 2020'
