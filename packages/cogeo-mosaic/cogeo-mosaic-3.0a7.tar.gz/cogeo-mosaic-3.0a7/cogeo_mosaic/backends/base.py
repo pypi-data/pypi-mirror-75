0c5a4c08b00e3b62190a16a1c7d27340831e218f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:53 +0000

    Add package cogeo-mosaic/3.0a7/cogeo-mosaic-3.0a7.tar.gz

---
+++ packages/cogeo-mosaic/cogeo-mosaic-3.0a7.tar.gz/cogeo-mosaic-3.0a7/cogeo_mosaic/backends/base.py
@@ -0,0 +1,150 @@
+"""cogeo_mosaic.backend.base: base Backend class."""
+
+import abc
+from contextlib import AbstractContextManager
+from typing import Any, Dict, List, Optional, Sequence, Tuple
+
+import mercantile
+import numpy
+from rio_tiler.constants import MAX_THREADS
+from rio_tiler.io import BaseReader, COGReader
+from rio_tiler.mosaic import _filter_tasks, mosaic_reader
+from rio_tiler.mosaic.reader import _create_tasks
+
+from cogeo_mosaic.backends.utils import get_hash
+from cogeo_mosaic.errors import NoAssetFoundError
+from cogeo_mosaic.mosaic import MosaicJSON
+from cogeo_mosaic.utils import bbox_union
+
+
+class BaseBackend(AbstractContextManager):
+    """Base Class for cogeo-mosaic backend storage."""
+
+    path: str
+    mosaic_def: MosaicJSON
+    reader: BaseReader = COGReader
+    _backend_name: str
+    _file_byte_size: Optional[int] = 0
+
+    @abc.abstractmethod
+    def __init__(self, *args, **kwargs):
+        """Connect to backend"""
+
+    def __enter__(self):
+        """Support using with Context Managers"""
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Support using with Context Managers"""
+
+    @property
+    def metadata(self) -> Dict:
+        """Retrieve Mosaic metadata
+
+        Returns
+        -------
+        MosaicJSON as dict without `tiles` key.
+        """
+        return self.mosaic_def.dict(exclude={"tiles"}, exclude_none=True)
+
+    @abc.abstractmethod
+    def assets_for_tile(self, x: int, y: int, z: int) -> List[str]:
+        """Retrieve assets for tile."""
+
+    @abc.abstractmethod
+    def assets_for_point(self, lng: float, lat: float) -> List[str]:
+        """Retrieve assets for point."""
+
+    def tile(
+        self, x: int, y: int, z: int, **kwargs: Any,
+    ) -> Tuple[numpy.ndarray, numpy.ndarray]:
+        """Get Tile from multiple observation."""
+
+        assets = self.assets_for_tile(x, y, z)
+        if not assets:
+            raise NoAssetFoundError(f"No assets found for tile {z}-{x}-{y}")
+
+        def _reader(asset: str, x: int, y: int, z: int, **kwargs: Any):
+            with self.reader(asset) as src_dst:
+                return src_dst.tile(x, y, z, **kwargs)
+
+        return mosaic_reader(assets, _reader, x, y, z, **kwargs)
+
+    def point(
+        self, lon: float, lat: float, threads=MAX_THREADS, **kwargs: Any
+    ) -> List[Dict]:
+        """Get Point value from multiple observation."""
+        assets = self.assets_for_point(lon, lat)
+        if not assets:
+            raise NoAssetFoundError(f"No assets found for point ({lon},{lat})")
+
+        def _reader(asset: str, lon: float, lat: float, **kwargs) -> Dict:
+            with self.reader(asset) as src_dst:
+                return src_dst.point(lon, lat, **kwargs)
+
+        tasks = _create_tasks(_reader, assets, threads, lon, lat, **kwargs)
+        return [{"asset": asset, "values": pt} for pt, asset in _filter_tasks(tasks)]
+
+    @abc.abstractmethod
+    def _read(self) -> MosaicJSON:
+        """Fetch mosaic definition"""
+
+    @property
+    def mosaicid(self) -> str:
+        """Return sha224 id of the mosaicjson document."""
+        return get_hash(**self.mosaic_def.dict(exclude_none=True))
+
+    @property
+    def quadkey_zoom(self) -> int:
+        """Return Quadkey zoom property."""
+        return self.mosaic_def.quadkey_zoom or self.mosaic_def.minzoom
+
+    @property
+    def _quadkeys(self) -> List[str]:
+        """Return the list of quadkeys"""
+        return list(self.mosaic_def.tiles.keys())
+
+    @abc.abstractmethod
+    def write(self):
+        """Upload new MosaicJSON to backend."""
+
+    def update(
+        self,
+        features: Sequence[Dict],
+        add_first: bool = True,
+        quiet: bool = False,
+        **kwargs,
+    ):
+        """Update existing MosaicJSON on backend."""
+        new_mosaic = self.mosaic_def.from_features(
+            features,
+            self.mosaic_def.minzoom,
+            self.mosaic_def.maxzoom,
+            quadkey_zoom=self.quadkey_zoom,
+            quiet=quiet,
+            **kwargs,
+        )
+
+        for quadkey, new_assets in new_mosaic.tiles.items():
+            tile = mercantile.quadkey_to_tile(quadkey)
+            assets = self.assets_for_tile(*tile)
+            assets = [*new_assets, *assets] if add_first else [*assets, *new_assets]
+
+            # add custom sorting algorithm (e.g based on path name)
+            self.mosaic_def.tiles[quadkey] = assets
+
+        bounds = bbox_union(new_mosaic.bounds, self.mosaic_def.bounds)
+
+        self.mosaic_def._increase_version()
+        self.mosaic_def.bounds = bounds
+        self.mosaic_def.center = (
+            (bounds[0] + bounds[2]) / 2,
+            (bounds[1] + bounds[3]) / 2,
+            self.mosaic_def.minzoom,
+        )
+
+        # We only write if path is set
+        if self.path:
+            self.write()
+
+        return
---
+++ packages/cogeo-mosaic/cogeo-mosaic-3.0a7.tar.gz/cogeo-mosaic-3.0a7/cogeo_mosaic/backends/s3.py
@@ -0,0 +1,110 @@
+"""cogeo-mosaic AWS S3 backend."""
+
+import json
+from typing import Any, Dict, List, Optional, Union
+
+import mercantile
+from boto3.session import Session as boto3_session
+from botocore.exceptions import ClientError
+from cachetools.keys import hashkey
+from rio_tiler.io import BaseReader, COGReader
+
+from cogeo_mosaic.backends.base import BaseBackend
+from cogeo_mosaic.backends.utils import (
+    _compress_gz_json,
+    _decompress_gz,
+    get_assets_from_json,
+)
+from cogeo_mosaic.cache import lru_cache
+from cogeo_mosaic.errors import _HTTP_EXCEPTIONS, MosaicError
+from cogeo_mosaic.mosaic import MosaicJSON
+
+
+class S3Backend(BaseBackend):
+    """S3 Backend Adapter"""
+
+    _backend_name = "AWS S3"
+
+    def __init__(
+        self,
+        bucket: str,
+        key: str,
+        mosaic_def: Optional[Union[MosaicJSON, Dict]] = None,
+        reader: BaseReader = COGReader,
+        client: Optional[boto3_session.client] = None,
+        **kwargs: Any,
+    ):
+        """Initialize S3Backend."""
+        self.client = client or boto3_session().client("s3")
+        self.key = key
+        self.bucket = bucket
+        self.path = f"s3://{bucket}/{key}"
+        self.reader = reader
+
+        if mosaic_def is not None:
+            self.mosaic_def = MosaicJSON(**dict(mosaic_def))
+        else:
+            self.mosaic_def = self._read(**kwargs)
+
+    def assets_for_tile(self, x: int, y: int, z: int) -> List[str]:
+        """Retrieve assets for tile."""
+        return get_assets_from_json(self.mosaic_def.tiles, self.quadkey_zoom, x, y, z)
+
+    def assets_for_point(self, lng: float, lat: float) -> List[str]:
+        """Retrieve assets for point."""
+        tile = mercantile.tile(lng, lat, self.quadkey_zoom)
+        return get_assets_from_json(
+            self.mosaic_def.tiles, self.quadkey_zoom, tile.x, tile.y, tile.z
+        )
+
+    def write(self, gzip: bool = None, **kwargs: Any):
+        """Write mosaicjson document to AWS S3."""
+        mosaic_doc = self.mosaic_def.dict(exclude_none=True)
+        if gzip or (gzip is None and self.key.endswith(".gz")):
+            body = _compress_gz_json(mosaic_doc)
+        else:
+            body = json.dumps(mosaic_doc).encode("utf-8")
+
+        _aws_put_data(self.key, self.bucket, body, client=self.client, **kwargs)
+
+    @lru_cache(key=lambda self, gzip=None: hashkey(self.path, gzip))
+    def _read(self, gzip: bool = None) -> MosaicJSON:  # type: ignore
+        """Get mosaicjson document."""
+        body = _aws_get_data(self.key, self.bucket, client=self.client)
+
+        self._file_byte_size = len(body)
+
+        if gzip or (gzip is None and self.key.endswith(".gz")):
+            body = _decompress_gz(body)
+
+        return MosaicJSON(**json.loads(body))
+
+
+def _aws_get_data(key, bucket, client: boto3_session.client = None) -> bytes:
+    if not client:
+        session = boto3_session()
+        client = session.client("s3")
+    try:
+        response = client.get_object(Bucket=bucket, Key=key)
+    except ClientError as e:
+        status_code = e.response["ResponseMetadata"]["HTTPStatusCode"]
+        exc = _HTTP_EXCEPTIONS.get(status_code, MosaicError)
+        raise exc(e.response["Error"]["Message"]) from e
+
+    return response["Body"].read()
+
+
+def _aws_put_data(
+    key: str, bucket: str, body: bytes, client: boto3_session.client = None, **kwargs
+) -> str:
+    if not client:
+        session = boto3_session()
+        client = session.client("s3")
+    try:
+        client.put_object(Bucket=bucket, Key=key, Body=body, **kwargs)
+    except ClientError as e:
+        status_code = e.response["ResponseMetadata"]["HTTPStatusCode"]
+        exc = _HTTP_EXCEPTIONS.get(status_code, MosaicError)
+        raise exc(e.response["Error"]["Message"]) from e
+
+    return key
