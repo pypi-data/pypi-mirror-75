67259ef168c83be87dde6580fd39fa240599ab5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:43 +0000

    Add package mrtutils/0.1.65/mrtutils-0.1.65.tar.gz

---
+++ packages/mrtutils/mrtutils-0.1.65.tar.gz/mrtutils-0.1.65/mrtutils/templates/ble/app_service_source.c
@@ -0,0 +1,62 @@
+/**
+ * @file app_${obj.prefix}_svc.c
+ * @brief Application layer for ${obj.name} service
+ * @date ${obj.profile.genTime}
+ * 
+ */
+
+/* user-block-includes-start -------------------------------------------------*/
+#include "Utilities/Interfaces/GattServer/gatt_server.h"
+#include "svc/${obj.prefix}_svc.h"
+
+/*user-block-includes-end*/
+
+
+/* Post Init -----------------------------------------------------------------*/
+
+/**
+ * @brief Called after GATT Server is intialized
+ */
+void ${obj.prefix}_svc_post_init_handler(void)
+{
+    //TODO initialize characteristic values 
+}
+
+/* Characteristic Event Handlers----------------------------------------------*/
+
+% for char in obj.chars:
+/**
+ * @brief Handles GATT event on ${char.name} Characteristic
+ * @param event - ptr to mrt_gatt_evt_t event with data and event type
+ */
+${"mrt_status_t {0}_{1}_handler(mrt_gatt_evt_t* event)".format(obj.prefix,char.name.lower())}
+{
+
+    //${t.cTypeDict[char.type]} val = *((${t.cTypeDict[char.type]}*) event->mData.data); /* Cast to correct data type*/
+
+% if char.isEnum> 0:
+    //switch(*ptrVal)
+    //{
+    % for val in char.vals:
+    //    case ${t.padAfter(obj.prefix +"_"+char.name + "_"+val.name+":" , 45).upper()}  /* ${val.desc} */
+    //        break;
+    % endfor
+    //    default: 
+    //        break;
+    //}
+
+%endif
+% if char.isMask> 0:
+    % for val in char.vals:
+    //if(val & ${t.padAfter(obj.prefix +"_"+char.name + "_"+val.name+")" , 45).upper()}  /* ${val.desc} */
+    //{
+    //  //TODO Hanlde ${val.name} 
+    //}
+    % endfor
+
+%endif
+    //TODO Handle ${char.name} Characteristic events
+    return MRT_STATUS_OK;
+}
+
+% endfor
