0400c8ce79d48fb1a7c2c2511ba6b3f831ddccc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:54 +0000

    Add package yqn-jupyterhub-ldapauthenticator/1.3.1.dev0/yqn_jupyterhub_ldapauthenticator-1.3.1.dev0-py3-none-any.whl

---
+++ packages/yqn-jupyterhub-ldapauthenticator/yqn_jupyterhub_ldapauthenticator-1.3.1.dev0-py3-none-any.whl/ldapauthenticator/__init__.py
@@ -0,0 +1,13 @@
+from ldapauthenticator.ldapauthenticator import LDAPAuthenticator
+
+__version__ = '1.0.0.dev'
+
+__version__ = '1.1.0.dev'
+
+__version__ = '1.2.0.dev'
+
+__version__ = '1.3.0.dev'
+
+__version__ = '1.3.0.dev'
+
+__version__ = '1.3.1.dev'
