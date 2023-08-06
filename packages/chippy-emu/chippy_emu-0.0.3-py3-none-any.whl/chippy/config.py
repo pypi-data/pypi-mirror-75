c35a520364742407574602184085c8d9d9d72ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:15 +0000

    Add package chippy-emu/0.0.3/chippy_emu-0.0.3-py3-none-any.whl

---
+++ packages/chippy-emu/chippy_emu-0.0.3-py3-none-any.whl/chippy/config.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    color_off = (0, 0, 0)
+    color_on = (255, 255, 255)
+
+    @property
+    def color_scheme(self):
+        off = ",".join(str(a) for a in self.color_off)
+        on = ",".join(str(a) for a in self.color_on)
+        return f"{off}:{on}"
+
+    @color_scheme.setter
+    def color_scheme(self, scheme):
+        off, on = scheme.split(':')
+        self.color_off = tuple(int(a) for a in off.split(','))
+        self.color_on = tuple(int(a) for a in on.split(','))
