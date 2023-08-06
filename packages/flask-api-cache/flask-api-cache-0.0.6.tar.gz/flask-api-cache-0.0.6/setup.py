df1782fae7065a622bc99f0ca79f1e332090d96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:40 +0000

    Add package flask-api-cache/0.0.6/flask-api-cache-0.0.6.tar.gz

---
+++ packages/flask-api-cache/flask-api-cache-0.0.6.tar.gz/flask-api-cache-0.0.6/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*
+"""
+      ┏┓       ┏┓
+    ┏━┛┻━━━━━━━┛┻━┓
+    ┃      ☃      ┃
+    ┃  ┳┛     ┗┳  ┃
+    ┃      ┻      ┃
+    ┗━┓         ┏━┛
+      ┗┳        ┗━┓
+       ┃          ┣┓
+       ┃          ┏┛
+       ┗┓┓┏━━━━┳┓┏┛
+        ┃┫┫    ┃┫┫
+        ┗┻┛    ┗┻┛
+    God Bless,Never Bug
+"""
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='flask-api-cache',
+    version='0.0.6',
+    author='Jimmy Yeh',
+    author_email='chienfeng0719@hotmail.com',
+    description='A tool for flask api cache with args',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
