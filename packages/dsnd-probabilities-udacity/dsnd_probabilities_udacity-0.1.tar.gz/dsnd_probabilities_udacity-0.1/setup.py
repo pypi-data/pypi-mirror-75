0fc3c77e027ad9d93199ba4fa65bb4541abf2d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:50 +0000

    Add package dsnd-probabilities-udacity/0.1/dsnd_probabilities_udacity-0.1.tar.gz

---
+++ packages/dsnd-probabilities-udacity/dsnd_probabilities_udacity-0.1.tar.gz/dsnd_probabilities_udacity-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probabilities_udacity',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probabilities_udacity'],
+      author = 'Sifat Nabil',
+      author_email = 'sifatnabil@gmail.com',
+      zip_safe=False)
