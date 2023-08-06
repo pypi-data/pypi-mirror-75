761d905b907edca6270bb434611c9e0fc0388082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:21 +0000

    Add package magicapi/0.1.59/magicapi-0.1.59-py3-none-any.whl

---
+++ packages/magicapi/magicapi-0.1.59-py3-none-any.whl/magicapi/Errors/MagicExceptions.py
@@ -0,0 +1,38 @@
+from fastapi import Request
+
+from magicapi import g
+
+from fastapi.responses import JSONResponse
+
+
+class MagicException(Exception):
+    def __init__(self, message: str):
+        self.status_code: int = 452
+        self.message: str = message
+
+
+class BackendException(MagicException):
+    pass
+
+
+class FrontendException(MagicException):
+    pass
+
+
+class FirestoreException(MagicException):
+    pass
+
+
+class TwilioException(MagicException):
+    pass
+
+
+@g.app.exception_handler(MagicException)
+def backend_exception_handler(request: Request, exc: MagicException):
+    if "aws.context" in request.scope:
+        context = request.scope["aws.context"]
+        context.serverless_sdk.capture_exception(exc)
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"success": False, "message": f"{exc.message}"},
+    )
