344409dffafacbc530ef208044919514a944d9a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:06 +0000

    Add package drf-guard/0.3.0/drf-guard-0.3.0.tar.gz

---
+++ packages/drf-guard/drf-guard-0.3.0.tar.gz/drf-guard-0.3.0/drf_guard/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'DRF Guard'
+__description__ = 'Flexible permissions for Django REST Framework'
+__url__ = 'https://github.com/yezyilomo/drf-guard'
+__version__ = '0.3.0'
+__author__ = 'Yezy Ilomo'
+__author_email__ = 'yezileliilomo@hotmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Yezy Ilomo'
+
+# Version synonym
+VERSION = __version__
