ea5da662b5c87ecad805c8685cc32b053c73b904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:52 +0000

    Add package anu-r-distributions/1.1/anu_r_distributions-1.1.tar.gz

---
+++ packages/anu-r-distributions/anu_r_distributions-1.1.tar.gz/anu_r_distributions-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='anu_r_distributions',
+      version='1.1',
+      description='Gaussian and binomial distributions',
+      packages=['anu_r_distributions'],
+      author='Anubhav Ranjan',
+      zip_safe=False)
