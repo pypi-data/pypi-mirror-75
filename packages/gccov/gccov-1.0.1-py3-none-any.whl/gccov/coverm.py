406187461404c982bf0ab7f096f1f612b3f4ab2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:05 +0000

    Add package gccov/1.0.1/gccov-1.0.1-py3-none-any.whl

---
+++ packages/gccov/gccov-1.0.1-py3-none-any.whl/gccov/coverm.py
@@ -0,0 +1,26 @@
+
+###########################################################
+#
+# coverm.py - use this to pileup coverm from bam files  ###
+#
+###########################################################
+
+import os
+import sys
+from biosut import gt_exe, gt_file
+
+class coverm:
+	def __init__(self, bam, outfile):
+		gt_exe.is_executable('coverm')
+		self.bam = bam
+		self.outfile = outfile
+
+	def run(self):
+		cmd = ['coverm', 'contig', '--bam-files', self.bam, '--methods', 'mean',
+				'--min-covered-fraction', '0', '--output-format', 'dense',
+				'--contig-end-exclusion', '0']
+		#proc = subprocess.Popen(cmd, shell=True, stdout=self.outfile, encoding='utf-8')
+		print(self.outfile)
+		with open(self.outfile, 'w') as outf:
+			gt_exe.exe_cmd(cmd)
+		gt_file.check_exist(self.outfile, check_empty=True)
