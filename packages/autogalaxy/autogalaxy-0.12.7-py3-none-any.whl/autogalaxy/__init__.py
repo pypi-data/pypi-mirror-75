87ecd2f2b0cd0fe4e7918727d7672d09c3a70a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:06 +0000

    Add package autogalaxy/0.12.7/autogalaxy-0.12.7-py3-none-any.whl

---
+++ packages/autogalaxy/autogalaxy-0.12.7-py3-none-any.whl/autogalaxy/__init__.py
@@ -0,0 +1,64 @@
+from autoarray import preprocess
+from autoarray.mask.mask import Mask
+from autoarray.structures.arrays import Values
+from autoarray.structures.grids import (
+    Grid,
+    GridIterate,
+    GridInterpolate,
+    GridCoordinates,
+    GridRectangular,
+    GridVoronoi,
+)
+from autoarray.structures.kernel import Kernel
+from autoarray.structures.visibilities import Visibilities
+from autoarray.dataset.imaging import Imaging
+from autoarray.dataset.interferometer import Interferometer
+from autoarray.operators.convolver import Convolver
+from autoarray.operators.transformer import TransformerDFT
+from autoarray.operators.transformer import TransformerNUFFT
+from autoarray.operators.inversion.mappers import mapper as Mapper
+from autoarray.operators.inversion.inversions import inversion as Inversion
+from autoarray.operators.inversion import pixelizations as pix, regularization as reg
+from autoconf import conf
+
+from .structures.arrays import Array
+from . import aggregator as agg
+from .dataset.imaging import MaskedImaging, SimulatorImaging
+from .dataset.interferometer import MaskedInterferometer, SimulatorInterferometer
+from . import dimensions as dim
+from . import util
+from .profiles import (
+    light_profiles as lp,
+    mass_profiles as mp,
+    light_and_mass_profiles as lmp,
+)
+from .galaxy.galaxy import Galaxy, HyperGalaxy, Redshift
+from .galaxy.galaxy_data import GalaxyData
+from .galaxy.masked_galaxy_data import MaskedGalaxyDataset
+from .galaxy.fit_galaxy import FitGalaxy
+from .galaxy.galaxy_model import GalaxyModel
+from .plane.plane import Plane
+from .fit.fit import FitImaging, FitInterferometer
+from .hyper import hyper_data
+from . import plot
+from .pipeline.phase.settings import PhaseSettingsImaging
+from .pipeline.phase.settings import PhaseSettingsInterferometer
+from .pipeline.phase.abstract import phase
+from .pipeline.phase.abstract.phase import AbstractPhase
+from .pipeline.phase.extensions import CombinedHyperPhase
+from .pipeline.phase.extensions import HyperGalaxyPhase
+from .pipeline.phase.extensions.hyper_galaxy_phase import HyperGalaxyPhase
+from .pipeline.phase.extensions.hyper_phase import HyperPhase
+from .pipeline.phase.extensions.inversion_phase import ModelFixingHyperPhase
+from .pipeline.phase.extensions.inversion_phase import InversionPhase
+from .pipeline.phase.abstract.phase import AbstractPhase
+from .pipeline.phase.dataset.phase import PhaseDataset
+from .pipeline.phase.dataset.meta_dataset import MetaDataset
+from .pipeline.phase.imaging.phase import PhaseImaging
+from .pipeline.phase.interferometer.phase import PhaseInterferometer
+from .pipeline.phase.phase_galaxy import PhaseGalaxy
+from .pipeline.pipeline import PipelineDataset
+from .pipeline.setup import PipelineSetup
+from .util import convert
+
+__version__ = '0.12.7'
