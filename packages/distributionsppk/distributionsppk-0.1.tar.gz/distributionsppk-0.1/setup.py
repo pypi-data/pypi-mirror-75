ee671646b5f4c1bda5adec0e9ccafbe89295a140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:15 +0000

    Add package distributionsppk/0.1/distributionsppk-0.1.tar.gz

---
+++ packages/distributionsppk/distributionsppk-0.1.tar.gz/distributionsppk-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='distributionsppk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsppk'],
+      author='Pyae Phyo Kyaw',
+      author_email='pyaekyaw1571999@gmail.com',
+      
+      zip_safe=False)
+
