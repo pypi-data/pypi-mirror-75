dac28018b30c6fdb6b343dfb67dbdb150a320034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:46 +0000

    Add package tinkoff-api/1.4.1/tinkoff-api-1.4.1.tar.gz

---
+++ packages/tinkoff-api/tinkoff-api-1.4.1.tar.gz/tinkoff-api-1.4.1/tinkoff/investments/model/market/instruments.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import Optional, List
+
+from tinkoff.investments.model.base import (
+    BaseModel,
+    Currency,
+    FigiName,
+    TickerName,
+    InstrumentType,
+)
+
+
+@dataclass
+class MarketInstrument(BaseModel):
+    figi: FigiName
+    ticker: TickerName
+    lot: int
+    name: str
+    type: InstrumentType
+    currency: Optional[Currency] = None
+    isin: Optional[str] = None
+    minPriceIncrement: Optional[float] = None
+
+
+@dataclass
+class MarketInstrumentList(BaseModel):
+    total: int
+    instruments: List[MarketInstrument]
+
+
+__all__ = [
+    'MarketInstrument',
+    'MarketInstrumentList',
+]
