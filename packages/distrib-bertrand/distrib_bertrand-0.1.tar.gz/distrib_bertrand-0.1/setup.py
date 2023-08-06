93651cc3e9f1784dbcd51e494a5b8fc2590eccbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:19 +0000

    Add package distrib-bertrand/0.1/distrib_bertrand-0.1.tar.gz

---
+++ packages/distrib-bertrand/distrib_bertrand-0.1.tar.gz/distrib_bertrand-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distrib_bertrand',
+      version='0.1',
+      description='Gaussian distributions of Bertrand',
+      packages=['distrib_bertrand'],
+      author = 'Bertrand',
+      author_email = 'mctchuente@gmail.com',
+      zip_safe=False)
