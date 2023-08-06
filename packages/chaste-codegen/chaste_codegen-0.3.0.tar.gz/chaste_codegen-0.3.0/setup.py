65c134f3b2eb0defa048cde30d9dbd151bb2e7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:23 +0000

    Add package chaste-codegen/0.3.0/chaste_codegen-0.3.0.tar.gz

---
+++ packages/chaste-codegen/chaste_codegen-0.3.0.tar.gz/chaste_codegen-0.3.0/setup.py
@@ -0,0 +1,72 @@
+#
+# Chaste codegen setuptools script
+#
+import os
+
+from setuptools import find_packages, setup
+
+
+# Load text for description
+with open('README.md') as f:
+    readme = f.read()
+
+# Load version number
+with open(os.path.join('chaste_codegen', 'version.txt'), 'r') as f:
+    version = f.read()
+
+# Go!
+setup(
+    # Module name (lowercase)
+    name='chaste_codegen',
+
+    version=version,
+    description='Code generation for cardiac Chaste',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Maurice Hendrix, Michael Clerx, Jonathan Cooper',
+    author_email='Maurice.Hendrix@nottingham.ac.uk',
+    maintainer='Maurice Hendrix',
+    maintainer_email='Maurice.Hendrix@nottingham.ac.uk',
+    url='https://github.com/ModellingWebLab/chaste-codegen',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+
+    # Packages to include
+    packages=find_packages(
+        include=('chaste_codegen', 'chaste_codegen.*')),
+
+    # Include non-python files (via MANIFEST.in)
+    include_package_data=True,
+
+    # Required Python version
+    python_requires='>=3.5',
+
+    # List of dependencies
+    install_requires=[
+        'cellmlmanip>=0.2.0',
+        'Jinja2>=2.11',
+    ],
+    extras_require={
+        'docs': [
+            'sphinx>=3.0',
+            'sphinx-automodapi>=0.12',
+        ],
+        'test': [
+            'pytest-cov>=2.10',     # For coverage checking
+            'pytest>=4.6',          # For unit tests
+            'flake8>=3',            # For code style checking
+            'isort',
+            'mock>=3.0.5',         # For mocking command line args etc.
+            'codecov>=2.1.3',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'chaste_codegen='
+            'chaste_codegen._command_line_script:chaste_codegen',
+        ],
+    },
+)
