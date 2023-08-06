15ce8288f85fafb97f5a2a62c7091efdd26b9a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:45 +0000

    Add package flowrisk/0.3.0/flowrisk-0.3.0.tar.gz

---
+++ packages/flowrisk/flowrisk-0.3.0.tar.gz/flowrisk-0.3.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# Created by Xixuan on Nov 22, 2018
+#
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='flowrisk',
+    version='0.3.0',
+    description='Order flow risk measures in Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/hanxixuana/flowrisk',
+    author='Xixuan Han',
+    author_email='xixuanhan@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy', 'pandas', 'matplotlib', 'scipy'
+    ],
+    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True
+)
---
+++ packages/flowrisk/flowrisk-0.3.0.tar.gz/flowrisk-0.3.0/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from tqdm import tqdm
+
+import flowrisk as fr
+
+
+class Config(fr.BulkConfVPINConfig):
+
+    N_TIME_BAR_FOR_INITIALIZATION = 50
+    BUCKET_MAX_VOLUME = 10000.0
+    N_BUCKET_OR_BUCKET_DECAY = 0.95
+    BUCKETS = fr.bulk.RecursiveBulkClassEWMABuckets
+
+
+if __name__ == '__main__':
+    config = Config()
+    config.summary()
+
+    example = fr.examples.USStocks(config)
+
+    symbols = example.list_symbols('small')
+    for symbol in tqdm(symbols):
+        result = example.estimate_vpin_and_conf_interval(symbol)
+        example.draw_price_vpins_and_conf_intervals()
+
+    symbols = example.list_symbols('large')
+    for symbol in tqdm(symbols):
+        result = example.estimate_vpin_and_conf_interval(symbol)
+        example.draw_price_vpins_and_conf_intervals()
