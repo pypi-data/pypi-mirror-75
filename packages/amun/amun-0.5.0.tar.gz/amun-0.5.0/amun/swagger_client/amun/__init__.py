6859e7ad6fdfedf3861265fb847eb4a6d53aabd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:25:24 +0000

    Add package amun/0.5.0/amun-0.5.0.tar.gz

---
+++ packages/amun/amun-0.5.0.tar.gz/amun-0.5.0/amun/swagger_client/amun/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from amun.swagger_client.amun.debug_api import DebugApi
+from amun.swagger_client.amun.inspection_api import InspectionApi
+from amun.swagger_client.amun.service_api import ServiceApi
