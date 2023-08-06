73f685f88d4f9058b3c84888310e25aba8f91682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:27 +0000

    Add package distributions-throwntech/1.0.3/distributions-throwntech-1.0.3.tar.gz

---
+++ packages/distributions-throwntech/distributions-throwntech-1.0.3.tar.gz/distributions-throwntech-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='distributions-throwntech',
+      version='1.0.3',
+      description='Gaussian  and Binomial Distributions',
+      packages=['distributions-throwntech'],
+      author='Arnav Deep',
+      author_email='arnavd4@gmail.com',
+      url="https://github.com/arnav-deep/distributions-package",
+      classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
