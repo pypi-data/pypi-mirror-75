f40b8efb9efd22638b134cca8f92870474da99b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:15 +0000

    Add package amqp/2.6.1/amqp-2.6.1.tar.gz

---
+++ packages/amqp/amqp-2.6.1.tar.gz/amqp-2.6.1/t/unit/test_basic_message.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, unicode_literals
+
+from amqp.basic_message import Message
+from case import Mock
+
+
+class test_Message:
+
+    def test_message(self):
+        m = Message(
+            'foo',
+            channel=Mock(name='channel'),
+            application_headers={'h': 'v'},
+        )
+        m.delivery_info = {'delivery_tag': '1234'}
+        assert m.body == 'foo'
+        assert m.channel
+        assert m.headers == {'h': 'v'}
+        assert m.delivery_tag == '1234'
