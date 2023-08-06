da683066b4a2c875974167d53a1087683cbf53c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:16 +0000

    Add package Wasim_pro/1.0/Wasim_pro-1.0.tar.gz

---
+++ packages/Wasim_pro/Wasim_pro-1.0.tar.gz/Wasim_pro-1.0/setup.py
@@ -0,0 +1,10 @@
+# coding=gbk
+from distutils.core import setup
+setup(
+    name='Wasim_pro', # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='这是第一个对外发布的模块，测试哦', #描述
+    author='Wasimlyel', # 作者
+    author_email='271498576@qq.com',
+    py_modules=['Wasim_pro.sss'] # 要发布的模块
+)
