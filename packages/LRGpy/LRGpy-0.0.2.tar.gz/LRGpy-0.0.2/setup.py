1519f6e554a36937bac4f3d4dc864aadadb0fb7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:06 +0000

    Add package LRGpy/0.0.2/LRGpy-0.0.2.tar.gz

---
+++ packages/LRGpy/LRGpy-0.0.2.tar.gz/LRGpy-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+  name='LRGpy',
+  version = '0.0.2',
+  description = 'LRG Python code repsository.',
+  url='https://github.com/GMU-LRG/LRGpy',
+  author='Lattanzi Research Group',
+  author_email='dlattanz@gmu.edu',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Natural Language :: English',
+    'Topic :: Scientific/Engineering'
+  ],
+  #dependencies
+  install_requires = [
+    'scikit-learn',
+    'matplotlib'
+  ]
+)
+
