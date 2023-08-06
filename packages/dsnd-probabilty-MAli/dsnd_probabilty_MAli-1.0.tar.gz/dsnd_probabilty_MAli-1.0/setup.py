545cbe8b96937387ae3c183a588f53da256834d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:22 +0000

    Add package dsnd-probabilty-MAli/1.0/dsnd_probabilty_MAli-1.0.tar.gz

---
+++ packages/dsnd-probabilty-MAli/dsnd_probabilty_MAli-1.0.tar.gz/dsnd_probabilty_MAli-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probabilty_MAli',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['dsnd_probabilty_MAli'],
+      author = 'Mohammed M.Ali',
+      author_email = 'Mohamed.mamdouh2@gmail.com',
+      zip_safe=False)
