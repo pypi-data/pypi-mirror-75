805746d17898b6fa8c403657a4cca284a1be7914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:24 +0000

    Add package ontquery/0.2.5/ontquery-0.2.5.tar.gz

---
+++ packages/ontquery/ontquery-0.2.5.tar.gz/ontquery-0.2.5/ontquery/exceptions.py
@@ -0,0 +1,26 @@
+class OntQueryError(Exception):
+    pass
+
+
+class NotFoundError(OntQueryError):
+    pass
+
+
+class ManyResultsError(OntQueryError):
+    pass
+
+
+class NoExplicitIdError(OntQueryError):
+    pass
+
+
+class ReadOnlyError(OntQueryError):
+    pass
+
+
+class ShouldNotHappenError(OntQueryError):
+    pass
+
+
+class FetchingError(OntQueryError):
+    """ A really good looking error you've got there! """
