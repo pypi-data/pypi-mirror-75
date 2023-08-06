3d87331b16ed69c98be7e6c36b9d7873dcaeee78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:48 +0000

    Add package SentimentAnalysis/0.5/SentimentAnalysis-0.5.tar.gz

---
+++ packages/SentimentAnalysis/SentimentAnalysis-0.5.tar.gz/SentimentAnalysis-0.5/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'SentimentAnalysis',         # How you named your package folder (MyLib)
+  packages = ['AnalyseSentiment'],   # Chose the same as "name"
+  version = '0.5',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Helps to analyse sentiment',   # Give a short description about your library
+  author = 'Arun Kesavan',                   # Type in your name
+  author_email = 'arunkottilukkal@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/arunkottilukkal/AnalyseSentiment',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/arunkottilukkal/AnalyseSentiment/archive/v_05.tar.gz',    # I explain this later on
+  keywords = ['Sentiment Analysis', 'Sentence Sentiment', 'Emotion Analysis'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'requests',
+          'urllib3',
+          'vaderSentiment',
+          'nltk',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
