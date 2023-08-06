4c38a0cee93729c58381e7d8fb7543fbeb4e96a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:23 +0000

    Add package tanbih-pipeline/0.6.1/tanbih-pipeline-0.6.1.tar.gz

---
+++ packages/tanbih-pipeline/tanbih-pipeline-0.6.1.tar.gz/tanbih-pipeline-0.6.1/src/pipeline/utils.py
@@ -0,0 +1,17 @@
+import argparse
+import os
+
+from .cache import KindsOfCache
+from .tap import KindsOfSource
+
+
+def parse_kind(args):
+    kindParser = argparse.ArgumentParser(add_help=False)
+    kindParser.add_argument('--kind', type=str, default=os.environ.get('PIPELINE', None),
+                            choices=KindsOfSource(),
+                            help='pipeline kind, can be {}'.format(','.join(KindsOfSource())))
+    kindParser.add_argument('--cacheKind', type=str, default=os.environ.get('CACHEKIND', None),
+                            choices=KindsOfCache(),
+                            help='cache kind, can be {}'.format(','.join(KindsOfCache())))
+    known, extras = kindParser.parse_known_args(args)
+    return known, extras
