629818d77f1e701da9226db50d44100307b397ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:16 +0000

    Add package tensorflow-consciousness/0.1/tensorflow-consciousness-0.1.tar.gz

---
+++ packages/tensorflow-consciousness/tensorflow-consciousness-0.1.tar.gz/tensorflow-consciousness-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='tensorflow-consciousness',
+      version='0.1',
+      description='Supports a variety of biological learning algorithms.',
+      url='http://wikipedia.org',
+      author='NA',
+      author_email='kallada@dal.ca',
+      license='MIT',
+      packages=[],
+      install_requires=['numpy>=1.11',
+                        'matplotlib>=1.5'])
