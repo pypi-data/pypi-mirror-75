215d8a136dc9e51199187674614f6ffcd75e8b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:29 +0000

    Add package def-list/1.1.0/def_list-1.1.0.tar.gz

---
+++ packages/def-list/def_list-1.1.0.tar.gz/def_list-1.1.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup( name = 'def_list',
+       version  = '1.1.0',
+       py_modules = ['def_list'],
+       author = 'umarfaruk',
+       author_email = 'umarfaruk@hotmail.com',
+       url = 'https://pypi.org/manage/projects/',
+       despcription = 'A simple printer of nested lists',
+       )
