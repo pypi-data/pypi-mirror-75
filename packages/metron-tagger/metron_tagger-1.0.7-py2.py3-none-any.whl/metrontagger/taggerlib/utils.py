85d4c8cd2bbc8bb4f95f1e643d337710017f13ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:15 +0000

    Add package metron-tagger/1.0.7/metron_tagger-1.0.7-py2.py3-none-any.whl

---
+++ packages/metron-tagger/metron_tagger-1.0.7-py2.py3-none-any.whl/metrontagger/taggerlib/utils.py
@@ -0,0 +1,35 @@
+"""Some miscellaneous functions"""
+from urllib.parse import quote_plus
+
+from darkseid.filenameparser import FileNameParser
+
+
+def cleanup_string(path_name):
+    """Function to remove some characters that don't play nicely on Windows machines filesystem"""
+    path_name = path_name.replace("/", "-")
+    path_name = path_name.replace(" :", " -")
+    path_name = path_name.replace(": ", " - ")
+    path_name = path_name.replace(":", "-")
+    path_name = path_name.replace("?", "")
+
+    return path_name
+
+
+def create_issue_query_dict(filename):
+    """Function to create a diction of values based on the provided filename"""
+    fnp = FileNameParser()
+    fnp.parse_filename(filename)
+
+    # Substitute colon for hyphen when searching for series name
+    fixed_txt = fnp.series.replace(" - ", ": ")
+    series_word_list = fixed_txt.split()
+    series_string = " ".join(series_word_list).strip()
+    series_string = quote_plus(series_string.encode("utf-8"))
+    # If there isn't an issue number, let's assume it's "1".
+    number = quote_plus(fnp.issue.encode("utf-8")) if fnp.issue else "1"
+    return {
+        "series": series_string,
+        "volume": fnp.volume,
+        "number": number,
+        "year": fnp.year,
+    }
