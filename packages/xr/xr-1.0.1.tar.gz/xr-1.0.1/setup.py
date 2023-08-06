5b1a9efec6ff76858b2c2f5dcc5f5c4aa28fb245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:27 +0000

    Add package xr/1.0.1/xr-1.0.1.tar.gz

---
+++ packages/xr/xr-1.0.1.tar.gz/xr-1.0.1/setup.py
@@ -0,0 +1,35 @@
+import os.path
+from setuptools import setup
+from unittest import TestLoader
+
+def tests():
+    return TestLoader().discover('test', pattern='test_*.py')
+
+
+setup(
+    name='xr',
+    version='1.0.1',
+    description="Easy Regular expression builder for people.",
+    url="https://xr.deprince.io",
+    author="Adam DePrince",
+    author_email="adamdeprince@gmail.com",
+    license="Apache",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",],
+    packages=[
+        'xr/',
+        'xr/regex/'
+    ],
+    install_requires=[
+        "six"
+    ],
+    test_suite='setup.tests',
+    test_dir='tests',
+    scripts=[])
