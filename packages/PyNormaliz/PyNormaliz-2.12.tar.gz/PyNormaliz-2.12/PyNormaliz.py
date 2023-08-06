c5a416cf777754255f77697e5a79da46bcbe321d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:17 +0000

    Add package PyNormaliz/2.12/PyNormaliz-2.12.tar.gz

---
+++ packages/PyNormaliz/PyNormaliz-2.12.tar.gz/PyNormaliz-2.12/PyNormaliz.py
@@ -0,0 +1,231 @@
+# encoding=utf8
+
+import PyNormaliz_cpp
+from PyNormaliz_cpp import *
+
+
+class Cone:
+
+    def __init__(self, *args, **kwargs): 
+        input_list = [k for k in args]
+        for i in kwargs:
+            current_input = kwargs[i]
+            if type(current_input) == list and len(current_input) > 0 and type(current_input[0]) != list:
+                kwargs[i] = [current_input]
+            elif type(current_input) == bool and current_input == True:
+                kwargs[i] = current_input = [[]]
+            elif type(current_input) == bool and current_input == False:
+                kwargs.pop(i)
+        self.cone = PyNormaliz_cpp.NmzCone(input_list,**kwargs)
+
+    def __process_keyword_args(self, keywords):
+        input_list = []
+        for i in keywords:
+            if keywords[i] == True:
+                input_list.append(i)
+        return input_list
+
+    def print_properties(self):
+        props = PyNormaliz_cpp.NmzListConeProperties()
+        goals = props[0]
+        for x in goals:
+            if (PyNormaliz_cpp.NmzIsComputed(self.cone, x)):
+                print(x + ":")
+                print(PyNormaliz_cpp.NmzResult(self.cone, x))
+                print("\n")
+
+    def __str__(self):
+        return "<Normaliz Cone>"
+
+    def __repr__(self):
+        return "<Normaliz Cone>"
+
+    def Compute(self, *args):
+        return PyNormaliz_cpp.NmzCompute(self.cone, args)
+
+    def setVerbose(self, verbose=True):
+        return NmzSetVerbose(self.cone, verbose)
+
+    # This one is not like the others!
+    def IntegerHull(self, **kwargs):
+        input_list = self.__process_keyword_args(kwargs)
+        input_list.append("IntegerHull")
+        PyNormaliz_cpp.NmzCompute(self.cone, input_list)
+        new_inner_cone = PyNormaliz_cpp.NmzResult(self.cone, "IntegerHull")
+        return_cone = Cone.__new__(Cone)
+        return_cone.cone = new_inner_cone
+        return return_cone
+
+    def ProjectCone(self, **kwargs):
+        input_list = self.__process_keyword_args(kwargs)
+        input_list.append("ProjectCone")
+        PyNormaliz_cpp.NmzCompute(self.cone, input_list)
+        new_inner_cone = PyNormaliz_cpp.NmzResult(self.cone, "ProjectCone")
+        return_cone = Cone.__new__(Cone)
+        return_cone.cone = new_inner_cone
+        return return_cone
+
+    def Polynomial(self, **kwargs):
+        return PyNormaliz_cpp.NmzGetPolynomial(self.cone)
+
+    def NrCoeffQuasiPol(self, bound=-1):
+        return PyNormaliz_cpp.NmzSetNrCoeffQuasiPol(self.cone, bound)
+
+    def SymmetrizedCone(self, **kwargs):
+        new_inner_cone = PyNormaliz_cpp.NmzSymmetrizedCone(self.cone)
+        if new_inner_cone == None:
+            return None
+        return_cone = Cone.__new__(Cone)
+        return_cone.cone = new_inner_cone
+        return return_cone
+
+    def HilbertSeriesExpansion(self,degree):
+        return NmzGetHilbertSeriesExpansion(self.cone,degree)
+    
+    def EhrhartSeriesExpansion(self,degree):
+        return NmzGetEhrhartSeriesExpansion(self.cone,degree)
+
+    def WeightedEhrhartSeriesExpansion(self,degree):
+        return NmzGetWeightedEhrhartSeriesExpansion(self.cone,degree)
+
+    def PrettyPolynomialTuple(self, numCoefficients, denCoefficients):
+        """
+        Strings for numerator and denominator of the a hilbert series.
+
+        Parameters
+        ----------
+        numCoefficients : list
+            The coefficients for the numerator.
+        denCofficients : list
+            The coefficients for the denominator where the value represents the
+            exponent of 't' and the frequency indicates the outer coefficient.
+
+        Returns
+        -------
+        PrettyPolynomialTuple: tuple of strings
+
+        Examples
+        --------
+
+        >>> numCoefficients = [3, 7, 4, -4, -6, 5]
+        >>> denCoefficients = [1, 1, 2, 2, 2, 4]
+        >>> PrettyPolynomialTuple(numCoefficients,denCoefficients)
+
+        ('(3 + 7t + 4t² - 4t³ - 6t⁴ + 5t⁵)', '(1 - t)² (1 - t²)³ (1 - t⁴)')
+
+        """
+        def to_sup(s):
+            sups = {u'0': u'\u2070',
+                    u'1': u'\xb9',
+                    u'2': u'\xb2',
+                    u'3': u'\xb3',
+                    u'4': u'\u2074',
+                    u'5': u'\u2075',
+                    u'6': u'\u2076',
+                    u'7': u'\u2077',
+                    u'8': u'\u2078',
+                    u'9': u'\u2079'}
+            if s is 1:
+                return ''
+            # lose the list comprehension
+            return ''.join(sups.get(str(char), str(char)) for char in str(s))
+
+        def getNumerator(coefficients):
+
+            numerator = ''
+
+            def isPositive(x):
+                return x > 0
+
+            firstNonZero = next(
+                (i for i, x in enumerate(coefficients) if x != 0), 0)
+            for exp, coefficient in enumerate(coefficients):
+                if coefficient is 0:
+                    continue
+                # Exponent is 0 so keep only the coefficient
+                if exp is 0:
+                    numerator += '({}{!s}'.format('-' if not isPositive(coefficient)
+                                                  else '', abs(coefficient))
+                # Only include sign if `coefficient` is negative
+                elif exp is firstNonZero:
+                    numerator += '{}{!s}t{}'.format('-' if not isPositive(
+                        coefficient) else '', abs(coefficient), to_sup(exp))
+                else:
+                    numerator += ' {}{!s}t{}'.format('+ ' if isPositive(
+                        coefficient) else '- ', abs(coefficient), to_sup(exp))
+            numerator += ')'
+            return numerator
+
+        def getDenominator(coefficients):
+            exponents = [(inner, coefficients.count(inner))
+                         for inner in set(coefficients)]
+            denominator = ' '.join('(1 - t{}){}'. format(to_sup(x[0]) if x[0] is not 1 else '', to_sup(x[1]) if x[1] is not 1 else '') for x in exponents)
+            return denominator
+
+        num = getNumerator(numCoefficients)
+        den = getDenominator(denCoefficients)
+        prettyPolynomial = (num, den)
+        return prettyPolynomial
+
+    def PrintPrettyHilbertSeries(self, numCoefficients, denCoefficients):
+        """
+        Make a pretty hilbert series string
+
+        Parameters
+        ----------
+        numCoefficients : list of ints
+            The coefficients for the numerator.
+        denCofficients : list of ints
+            The coefficients for the denominator where the value represents
+            the exponent of 't' and the frequency indicates the outer
+            coefficient.
+
+        Returns
+        -------
+        PrintPrettyHilbertSeries : string
+
+        Examples
+        --------
+
+        >>> numCoefficients = [3, 7, 4, -4, -6, 5]
+        >>> deCoefficients = [1, 1, 2, 2, 2, 4]
+        >>> PrintPrettyHilbertSeries(numCoefficients,deCoefficients)
+
+        (3 + 7t + 4t² - 4t³ - 6t⁴ + 5t⁵)
+        --------------------------------
+           (1 - t)² (1 - t²)³ (1 - t⁴)
+
+        """
+        num, den = self.PrettyPolynomialTuple(numCoefficients, denCoefficients)
+        prettyPolynomial = '{:^}\n{:-^{width}}\n{:^{width}}'.format(
+            num, '', den, width=max(len(den),len(num)))
+        return prettyPolynomial
+
+    def PrintHilbertSeries(self):
+        hilbert_series=self.HilbertSeries()
+        shift=hilbert_series[2]
+        shift=[ 0 for x in range(1,shift) ]
+        numerator=shift+hilbert_series[0]
+        denominator=hilbert_series[1]
+        print(self.PrintPrettyHilbertSeries(numerator,denominator))
+        return None
+
+    def _generic_getter(self, name, **kwargs):
+        input_list = self.__process_keyword_args(kwargs)
+        input_list.append(name)
+        PyNormaliz_cpp.NmzCompute(self.cone, input_list)
+        return PyNormaliz_cpp.NmzResult(self.cone, name)
+
+
+# Generate getters for a bunch of Normalize properties
+def add_dyn_getter(name):
+    if hasattr(Cone, name):
+        return
+    def inner(self, **kwargs):
+        return self._generic_getter(name, **kwargs)
+    inner.__doc__ = "docstring for %s" % name
+    inner.__name__ = name
+    setattr(Cone, name, inner)
+
+for name in PyNormaliz_cpp.NmzListConeProperties()[0]:
+    add_dyn_getter(name)
---
+++ packages/PyNormaliz/PyNormaliz-2.12.tar.gz/PyNormaliz-2.12/README.md
@@ -0,0 +1,108 @@
+[![Build Status](https://travis-ci.org/Normaliz/PyNormaliz.svg)](https://travis-ci.org/Normaliz/PyNormaliz)
+[![Code Coverage](https://codecov.io/github/Normaliz/PyNormaliz/coverage.svg)](https://codecov.io/gh/Normaliz/PyNormaliz)
+
+# PyNormaliz - A python interface to Normaliz
+
+
+PyNormaliz provides an interface to [Normaliz](https://www.normaliz.uni-osnabrueck.de) via libNormaliz.
+It offers the complete functionality of Normaliz, and can be used interactively from python.
+For a first example, see [this introduction](doc/PyNormaliz_Tutorial.pdf) by Richard Sieg (Slighty outdated: for the installation follow the instructions below).
+
+
+## Requirements
+
+* python 2.7 or higher or python 3.4 or higher
+* Normaliz 3.8.5 or higher <https://github.com/Normaliz/Normaliz/releases>
+
+The source packages of the Normaliz realeases contain PyNormaliz.
+
+## Installation
+
+The PyNormaliz install script assumes that you have executed
+
+    ./install_normaliz_with_eantic.sh
+
+within the Normaliz directory. To install PyNormaliz navigate to the Normaliz directory and type
+
+    ./install_pynormaliz.sh --user
+
+Also see Appendix E of the
+[Normaliz manual](https://github.com/Normaliz/Normaliz/blob/master/doc/Normaliz.pdf).
+
+## Usage
+
+The command Cone creates a cone (and a lattice), and the member functions
+of Cone compute its properties. For a full list of input and output
+properties, see the Normaliz manual.
+
+We assume that you are running python 3.
+
+Start by
+
+    import PyNormaliz
+    from PyNormaliz import *
+
+To create a simple example, type
+
+    C = Cone(cone = [[1,0],[0,1]])
+
+
+All possible Normaliz input types can be given as keyword arguments.
+
+The member functions allow the computation of the data of our cone.  For example,
+
+    C.HilbertBasis()
+
+returns what its name says:
+
+    [[0, 1], [1, 0]]
+
+is the matrix of the two Hilbert basis vectors.
+
+One can pass options to the compute functions as in
+
+    C.HilbertSeries(HSOP = True)
+
+Note that some Normaliz output types must be specially encoded for python. Our Hilbert Series is returned as
+
+    [[1], [1, 1], 0]
+
+to be read as follows: [1] is the numerator polynomial, [1,1] is the vector of exponents of t that occur in the denominator, which is (1-t)(1-t) in our case, and 0 is the shift.  So the Hilbert series is given by the rational function 1/(1-t)(1-t). (Also see [this introduction](doc/PyNormaliz_Tutorial.pdf).)
+
+One can also compute several data simultaneously and specify options ("PrimalMode" only added as an example, not because it is particularly useful here):
+
+    C.Compute("LatticePoints", "Volume", "PrimalMode")
+    
+Then
+
+    C.Volume()
+    
+retrieves the already computed result [1,1], which encodes the fraction 1/1 = 1. This is the lattice length of the diagonal in the square. The euclidean length, that has been computed simultaneously, is returned by
+
+    C.EuclideanVolume()
+    
+with the expected value
+
+    1.4142135623730951
+
+By using Python functions, the functionality of Normaliz can be extended. For example, 
+    
+    def intersection(cone1, cone2):
+        intersection_ineq = cone1.SupportHyperplanes()+cone2.SupportHyperplanes()
+        intersection_eq = cone1.Equations()+cone2.Equations()
+        C = Cone(inequalities = intersection_ineq, equations = intersection_eq)
+        return C
+        
+computes the intersection of two cones. So
+
+    C1 = Cone(cone=[[1,2],[2,1]])
+    C2 = Cone(cone=[[1,1],[1,3]])
+    intersection(C1,C2).ExtremeRays()
+    
+yields the result
+
+    [[1, 1], [1, 2]]
+    
+If you want to see what Normaliz is doing (especually in longer computations) activate the terminal output by
+
+    C.setVerbose(True)
