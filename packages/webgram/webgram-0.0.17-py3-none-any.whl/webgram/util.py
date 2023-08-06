2192ec66be6ada6b0ae9ab51ae81b8a1258bc978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:53 +0000

    Add package webgram/0.0.17/webgram-0.0.17-py3-none-any.whl

---
+++ packages/webgram/webgram-0.0.17-py3-none-any.whl/webgram/util.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+# not use telegram_util once, because that one mess up with markdown
+# also, this way reduce dependency
+def cutText(text, cut):
+	if len(text) <= cut + 3:
+		return text
+	return text[:cut] + '...'
+
+def textJoin(*parts):
+	result = ' '.join(parts)
+	parts = [x for x in result.split() if x]
+	return ' '.join(parts)
+
+def getText(*soups):
+	result = []
+	for soup in soups:
+		if soup:
+			result.append(' '.join(soup.text.strip().split()))
+	return textJoin(*result)
