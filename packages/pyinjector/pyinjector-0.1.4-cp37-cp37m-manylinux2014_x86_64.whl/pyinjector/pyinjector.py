371c4809e7ee84a49ab627e260283204dee7e790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:27 +0000

    Add package pyinjector/0.1.4/pyinjector-0.1.4-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/pyinjector/pyinjector-0.1.4-cp37-cp37m-manylinux2014_x86_64.whl/pyinjector/pyinjector.py
@@ -0,0 +1,52 @@
+from importlib.util import find_spec
+from ctypes import CDLL, Structure, POINTER, c_int32, byref, c_char_p
+from typing import AnyStr, Callable, Any
+
+libinjector_path = find_spec('.libinjector', __package__).origin
+libinjector = CDLL(libinjector_path)
+
+injector_t = type('injector_t', (Structure,), {})
+injector_pointer_t = POINTER(injector_t)
+pid_t = c_int32
+
+libinjector.injector_attach.argtypes = POINTER(injector_pointer_t), pid_t
+libinjector.injector_attach.restype = c_int32
+libinjector.injector_inject.argtypes = injector_pointer_t, c_char_p
+libinjector.injector_inject.restype = c_int32
+libinjector.injector_detach.argtypes = injector_pointer_t,
+libinjector.injector_detach.restype = c_int32
+
+
+def call_c_func(func: Callable[..., int], *args: Any) -> None:
+    ret = func(*args)
+    assert ret == 0, '{} returned {}, see injector/include/injector.h'.format(func.__name__, ret)
+
+
+class Injector:
+    def __init__(self, injector_p: injector_pointer_t):
+        self.injector_p = injector_p
+
+    @classmethod
+    def attach(cls, pid: int) -> 'Injector':
+        assert isinstance(pid, int)
+        injector_p = injector_pointer_t()
+        call_c_func(libinjector.injector_attach, byref(injector_p), pid)
+        return cls(injector_p)
+
+    def inject(self, library_path: AnyStr) -> None:
+        if isinstance(library_path, str):
+            library_path = library_path.encode()
+        assert isinstance(library_path, bytes)
+        call_c_func(libinjector.injector_inject, self.injector_p, library_path)
+
+    def detach(self) -> None:
+        call_c_func(libinjector.injector_detach, self.injector_p)
+
+
+def inject(pid: int, library_path: AnyStr) -> None:
+    injector = Injector.attach(pid)
+    try:
+        injector.inject(library_path)
+    finally:
+        injector.detach()
+
