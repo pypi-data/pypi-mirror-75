877f5051f44f22c9acdea0c96ef481953f7922b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:36 +0000

    Add package dagster-graphql/0.8.10/dagster-graphql-0.8.10.tar.gz

---
+++ packages/dagster-graphql/dagster-graphql-0.8.10.tar.gz/dagster-graphql-0.8.10/dagster_graphql_tests/graphql/snapshots/snap_test_materializations.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+snapshots = Snapshot()
+
+snapshots['TestMaterializations.test_materializations[in_memory_instance_in_process_env] 1'] = [
+    'PipelineStartEvent',
+    'ExecutionStepStartEvent',
+    'StepMaterializationEvent',
+    'ExecutionStepOutputEvent',
+    'ExecutionStepSuccessEvent',
+    'PipelineSuccessEvent'
+]
+
+snapshots['TestMaterializations.test_materializations[sqlite_with_sync_run_launcher_in_process_env] 1'] = [
+    'PipelineStartEvent',
+    'ExecutionStepStartEvent',
+    'StepMaterializationEvent',
+    'ExecutionStepOutputEvent',
+    'ExecutionStepSuccessEvent',
+    'PipelineSuccessEvent'
+]
+
+snapshots['TestMaterializations.test_materializations[sqlite_with_cli_api_run_launcher_in_process_env] 1'] = [
+    'PipelineStartEvent',
+    'ExecutionStepStartEvent',
+    'StepMaterializationEvent',
+    'ExecutionStepOutputEvent',
+    'ExecutionStepSuccessEvent',
+    'PipelineSuccessEvent'
+]
+
+snapshots['TestMaterializations.test_materializations[sqlite_with_grpc_run_launcher_in_process_env] 1'] = [
+    'PipelineStartEvent',
+    'ExecutionStepStartEvent',
+    'StepMaterializationEvent',
+    'ExecutionStepOutputEvent',
+    'ExecutionStepSuccessEvent',
+    'PipelineSuccessEvent'
+]
