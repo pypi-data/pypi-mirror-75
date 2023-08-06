2426a9e9af101fd652af3ba2ab192281971feb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:21 +0000

    Add package wafer/0.9.2/wafer-0.9.2.tar.gz

---
+++ packages/wafer/wafer-0.9.2.tar.gz/wafer-0.9.2/wafer/tickets/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from wafer.tickets.views import ClaimView, zapier_cancel_hook, zapier_guest_hook
+
+
+urlpatterns = [
+    url(r'^claim/$', ClaimView.as_view(), name='wafer_ticket_claim'),
+    url(r'^zapier_guest_hook/$', zapier_guest_hook),
+    url(r'^zapier_cancel_hook/$', zapier_cancel_hook),
+]
