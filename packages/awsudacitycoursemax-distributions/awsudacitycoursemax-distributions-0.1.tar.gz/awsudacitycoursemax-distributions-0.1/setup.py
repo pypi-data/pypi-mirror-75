ee1ef6b7b527d1adb0b211188829985816fc103d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:11 +0000

    Add package awsudacitycoursemax-distributions/0.1/awsudacitycoursemax-distributions-0.1.tar.gz

---
+++ packages/awsudacitycoursemax-distributions/awsudacitycoursemax-distributions-0.1.tar.gz/awsudacitycoursemax-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='awsudacitycoursemax-distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions'],
+      author=['Maximilian Muench'],
+      zip_safe=False)
