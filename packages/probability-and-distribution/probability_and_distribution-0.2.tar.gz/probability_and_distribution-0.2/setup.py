0e35a70ab1774f3adfe9fa757b7d2ef26699e34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:40 +0000

    Add package probability-and-distribution/0.2/probability_and_distribution-0.2.tar.gz

---
+++ packages/probability-and-distribution/probability_and_distribution-0.2.tar.gz/probability_and_distribution-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_and_distribution',
+      version='0.2',
+      description='Probability and distributions',
+      packages=['probability_and_distribution'],
+      zip_safe=False)
