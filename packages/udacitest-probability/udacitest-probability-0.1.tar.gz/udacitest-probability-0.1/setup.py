8b077019e0a3b576aa4d9ced84006265b65dc699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:48 +0000

    Add package udacitest-probability/0.1/udacitest-probability-0.1.tar.gz

---
+++ packages/udacitest-probability/udacitest-probability-0.1.tar.gz/udacitest-probability-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='udacitest-probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacitest-probability'],
+      zip_safe=False,
+      author="Riley Rangel",
+      author_email="rrangel91@gmail.com"
+     )
