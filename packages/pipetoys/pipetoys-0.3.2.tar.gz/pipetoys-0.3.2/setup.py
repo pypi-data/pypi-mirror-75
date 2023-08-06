00cc494a5175a41b2d08d07f66e81b154b9ac3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:32 +0000

    Add package pipetoys/0.3.2/pipetoys-0.3.2.tar.gz

---
+++ packages/pipetoys/pipetoys-0.3.2.tar.gz/pipetoys-0.3.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pipetoys']
+setup_kwargs = {
+    'name': 'pipetoys',
+    'version': '0.3.2',
+    'description': 'Pythonic pipelines.',
+    'long_description': '# pipetoys\n\nPythonic pipelines. Just higher-order functions.\n\nOnly used for personal projects and small scripts.\nI have no plans for documentation.\n',
+    'author': 'SeparateRecords',
+    'author_email': 'me@rob.ac',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3,<4',
+}
+
+
+setup(**setup_kwargs)
