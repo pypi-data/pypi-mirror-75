7854f42cd244951b79705ef9277e07889c2442d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:07 +0000

    Add package client-agreement-api/0.0.6/client_agreement_api-0.0.6.tar.gz

---
+++ packages/client-agreement-api/client_agreement_api-0.0.6.tar.gz/client_agreement_api-0.0.6/client_agreement_api/controllers/user_controller.py
@@ -0,0 +1,37 @@
+from werkzeug.exceptions import abort
+
+from app import DB
+from models.user import User, UserSchema
+
+
+def get_users_list():
+    users = User.query.order_by(User.id).all()
+    user_schema = UserSchema(many=True)
+    return user_schema.dump(users)
+
+
+def get_user(email):
+    user = User.query.filter(User.email == email).one_or_none()
+    if not user:
+        abort(404, "No user exists for provided email.")
+
+    user_schema = UserSchema()
+    data = user_schema.dump(user)
+
+    return data
+
+
+def add_user(body):
+    user_email = body.get("email")
+    user = User.query.filter_by(email=user_email).one_or_none()
+    if user:
+        abort(409, "User with same email already exists.")
+
+    user = User(email=user_email)
+    DB.session.add(user)
+    DB.session.commit()
+
+    user_schema = UserSchema()
+    d_user = user_schema.dump(user)
+
+    return d_user, 201
