dfa1ad6c24ae86b564ef07ef85f2817b1a562759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:31 +0000

    Add package mapsplotlib/1.2.1/mapsplotlib-1.2.1.tar.gz

---
+++ packages/mapsplotlib/mapsplotlib-1.2.1.tar.gz/mapsplotlib-1.2.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+version = '1.2.1'
+
+setup(
+    name='mapsplotlib',
+    packages=['mapsplotlib'],
+    version=version,
+    description='''
+        Custom Python plots on a Google Maps background.
+        A flexible matplotlib like interface to generate many types of plots on top of Google Maps.
+    ''',
+    url='https://github.com/tcassou/mapsplotlib',
+    download_url='https://github.com/tcassou/mapsplotlib/archive/{}.tar.gz'.format(version),
+    keywords=['google', 'maps', 'matplotlib', 'python', 'plot'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+    install_requires=[
+        'numpy>=1.8.2',
+        'pandas>=0.13.1',
+        'scipy>=0.13.3',
+        'matplotlib>=1.3.1',
+        'requests>=2.18.4',
+        'pillow>=4.3.0',
+    ],
+)
