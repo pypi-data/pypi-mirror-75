05629350eed611ef7f96f5b441f59e6d5a92c96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:14 +0000

    Add package devashish-probability/0.2/devashish_probability-0.2.tar.gz

---
+++ packages/devashish-probability/devashish_probability-0.2.tar.gz/devashish_probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='devashish_probability',
+      version='0.2',
+      description='Gaussian distributions and Binomial Distributions',
+      packages=['devashish_probability'],
+      author = 'Devashish Shivmurthy',
+      author_email = 'devashish.shivmurthy@gmail.com',
+      zip_safe=False)
