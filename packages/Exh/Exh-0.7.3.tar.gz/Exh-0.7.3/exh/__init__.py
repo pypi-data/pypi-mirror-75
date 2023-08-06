c9727e4d8b65c2180e3da46c488be9a11e820cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:26 +0000

    Add package Exh/0.7.3/Exh-0.7.3.tar.gz

---
+++ packages/Exh/Exh-0.7.3.tar.gz/Exh-0.7.3/exh/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.7.3"
+
+from .prop    import *
+from .model   import *
+from .fol     import *
+from .exhaust import *
+# from .worlds import *
+
+# Defining the names that are exported with from exh import *
+# __all__ = ["function1", "function2"]
