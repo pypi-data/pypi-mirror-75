e38c962af90a830678f41a6489a61d79a3fc0afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:48 +0000

    Add package dsnd-probability-0108-v1/1.0/dsnd_probability_0108_v1-1.0.tar.gz

---
+++ packages/dsnd-probability-0108-v1/dsnd_probability_0108_v1-1.0.tar.gz/dsnd_probability_0108_v1-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_0108_v1',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_0108_v1'],
+      author = 'Utkarsh Bansal',
+      zip_safe=False)
