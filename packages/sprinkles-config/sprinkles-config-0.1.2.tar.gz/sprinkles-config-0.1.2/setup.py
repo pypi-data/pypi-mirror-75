0b16a9322f69d827a55ba26e109cf78b85d7c608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:23 +0000

    Add package sprinkles-config/0.1.2/sprinkles-config-0.1.2.tar.gz

---
+++ packages/sprinkles-config/sprinkles-config-0.1.2.tar.gz/sprinkles-config-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sprinkles']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2', 'boto3', 'click', 'tomlkit']
+
+entry_points = \
+{'console_scripts': ['sprinkles = sprinkles.commands:generate_config']}
+
+setup_kwargs = {
+    'name': 'sprinkles-config',
+    'version': '0.1.2',
+    'description': 'Generate config files from AWS Secrets',
+    'long_description': None,
+    'author': 'Okal Otieno',
+    'author_email': 'okal@justokal.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
