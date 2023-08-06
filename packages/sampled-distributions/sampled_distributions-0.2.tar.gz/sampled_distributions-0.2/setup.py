11b0f56433db0ed7e0214a17e2bf9ba728e41a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:37 +0000

    Add package sampled-distributions/0.2/sampled_distributions-0.2.tar.gz

---
+++ packages/sampled-distributions/sampled_distributions-0.2.tar.gz/sampled_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sampled_distributions',
+      version='0.2',
+      description='General, Gaussian and Binomial distributions',
+      packages=['sampled_distributions'],
+      author='Victor Mochengo',
+      author_email='victormochengo@gmail.com',
+      zip_safe=False)
