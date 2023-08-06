b27bc7c12aa89f06433497dfaa00b01e1e521da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:35 +0000

    Add package distributions-pkg-rename/0.2/distributions_pkg_rename-0.2.tar.gz

---
+++ packages/distributions-pkg-rename/distributions_pkg_rename-0.2.tar.gz/distributions_pkg_rename-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_pkg_rename',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_pkg_rename'],
+      zip_safe=False)
