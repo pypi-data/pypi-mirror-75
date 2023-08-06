f0c7f4a05f5c726c29e739c0af1aecfa605891a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:35 +0000

    Add package pneuro/1.3.5/pneuro-1.3.5-py3-none-any.whl

---
+++ packages/pneuro/pneuro-1.3.5-py3-none-any.whl/pneuro/__init__.py
@@ -0,0 +1,9 @@
+import pathlib
+HERE = pathlib.Path(__file__).parent
+dataLoadLogs = HERE / "logs" / "dataLoadLogs.txt"
+classificationTrainingLogs = HERE / "logs" / "classificationTrainingLogs.txt"
+fileOperationLogs = HERE / "logs" / "fileOperationLogs.txt"
+predictionLogs = HERE / "logs" / "predictionLogs.txt"
+dataPreprocessingLogs = HERE/"logs"/"dataPreprocessingLogs.txt"
+RegressionTrainingLogs = HERE/"logs"/"RegressionTrainingLogs.txt"
+dataVisualizationLogs = HERE/"logs"/"dataVisualizationLogs.txt"
