a0ec03d1a8b6a59f15f8a9a7a9abb7e1f3078711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:15 +0000

    Add package omniprob-distributions/0.1/omniprob_distributions-0.1.tar.gz

---
+++ packages/omniprob-distributions/omniprob_distributions-0.1.tar.gz/omniprob_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='omniprob_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['omniprob_distributions'],
+      author = 'ehizzz',
+      author_email = 'ehizrendezvouz@gmail.com',
+      zip_safe=False)
