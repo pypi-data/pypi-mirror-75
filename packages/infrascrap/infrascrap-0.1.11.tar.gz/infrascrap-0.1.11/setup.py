c1324693beab779cba1a7d54480be73eac033a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:18 +0000

    Add package infrascrap/0.1.11/infrascrap-0.1.11.tar.gz

---
+++ packages/infrascrap/infrascrap-0.1.11.tar.gz/infrascrap-0.1.11/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['infrascrap']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bitwarden-keyring>=0.3.0,<0.4.0',
+ 'keyring>=21.2.1,<22.0.0',
+ 'python-telegram-bot>=12.8,<13.0',
+ 'pyvirtualdisplay>=1.3.2,<2.0.0',
+ 'requests>=2.24.0,<3.0.0',
+ 'selenium>=3.141.0,<4.0.0',
+ 'toml>=0.10.1,<0.11.0']
+
+setup_kwargs = {
+    'name': 'infrascrap',
+    'version': '0.1.11',
+    'description': 'Scraping infrastructure',
+    'long_description': None,
+    'author': 'Sivan Becker',
+    'author_email': 'sivanbecker@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
