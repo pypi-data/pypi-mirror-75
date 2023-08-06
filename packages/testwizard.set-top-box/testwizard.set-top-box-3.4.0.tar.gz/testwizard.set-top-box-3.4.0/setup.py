4ec28e1ab15aac4f4964b5cd6207892956c12e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:58 +0000

    Add package testwizard.set-top-box/3.4.0/testwizard.set-top-box-3.4.0.tar.gz

---
+++ packages/testwizard.set-top-box/testwizard.set-top-box-3.4.0.tar.gz/testwizard.set-top-box-3.4.0/setup.py
@@ -0,0 +1,49 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.set-top-box",
+    version="3.4.0",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard for set-top box testobjects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.set_top_box'],
+    install_requires=[
+        'testwizard.test==3.4.0',
+        'testwizard.testobjects-core==3.4.0',
+        'testwizard.commands-audio==3.4.0',
+        'testwizard.commands-mobile==3.4.0',
+        'testwizard.commands-powerswitch==3.4.0',
+        'testwizard.commands-remotecontrol==3.4.0',
+        'testwizard.commands-video==3.4.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
