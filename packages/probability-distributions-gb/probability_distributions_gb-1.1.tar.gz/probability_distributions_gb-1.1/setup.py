00f535fd9424b9dbdd47549562de01e79535cd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:27 +0000

    Add package probability-distributions-gb/1.1/probability_distributions_gb-1.1.tar.gz

---
+++ packages/probability-distributions-gb/probability_distributions_gb-1.1.tar.gz/probability_distributions_gb-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distributions_gb',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distributions_gb'],
+      author = 'Ansh Srivastava',
+      email = 'ansh_srtv@outlook.com', 
+      zip_safe=False)
