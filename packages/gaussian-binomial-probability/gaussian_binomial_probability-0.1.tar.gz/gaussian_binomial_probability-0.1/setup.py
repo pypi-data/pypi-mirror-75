55af86897e2a700d12b61dd390f4196faefdbbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:14 +0000

    Add package gaussian-binomial-probability/0.1/gaussian_binomial_probability-0.1.tar.gz

---
+++ packages/gaussian-binomial-probability/gaussian_binomial_probability-0.1.tar.gz/gaussian_binomial_probability-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussian_binomial_probability'],
+      author='Chinh Le',
+      author_email='chinh.tester11@gmail.com',
+      url='https://gitlab.com/chinh-dev11/python-opp-modularize/-/tree/lab/5_exercise_upload_to_pypi',
+      zip_safe=False)  # package can't be run directly from a zip file
