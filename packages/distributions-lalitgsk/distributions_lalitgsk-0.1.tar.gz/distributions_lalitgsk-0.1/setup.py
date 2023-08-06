e460e245f872386cdec5e0a8070239dbe91c29ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:55 +0000

    Add package distributions-lalitgsk/0.1/distributions_lalitgsk-0.1.tar.gz

---
+++ packages/distributions-lalitgsk/distributions_lalitgsk-0.1.tar.gz/distributions_lalitgsk-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_lalitgsk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_lalitgsk'],
+      zip_safe=False)
