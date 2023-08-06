5bb6b1217437b924cf33e61775884414f0791790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:21 +0000

    Add package Arithemtic-operators-basic/0.0/Arithemtic_operators_basic-0.0.tar.gz

---
+++ packages/Arithemtic-operators-basic/Arithemtic_operators_basic-0.0.tar.gz/Arithemtic_operators_basic-0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="Arithemtic_operators_basic",
+	  version="0.0",
+	  description="Common arithmetic operators",
+	  packages=["operators"],
+	  zip_safe=False)
