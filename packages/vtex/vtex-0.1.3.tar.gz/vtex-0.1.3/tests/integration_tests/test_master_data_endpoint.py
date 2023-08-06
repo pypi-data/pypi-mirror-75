b2465f09dd79eceaff0e86f44e8cb3a9c1ca9333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:08 +0000

    Add package vtex/0.1.3/vtex-0.1.3.tar.gz

---
+++ packages/vtex/vtex-0.1.3.tar.gz/vtex-0.1.3/tests/integration_tests/test_master_data_endpoint.py
@@ -0,0 +1,44 @@
+# Integration Tests
+from tests.integration_tests import OK, client
+from vtex import Vtex
+import pytest
+import os
+
+
+@pytest.fixture
+def order_id():
+    return os.environ.get("ORDER_ID")
+
+
+@pytest.fixture
+def email():
+    return os.environ.get("VTEX_EMAIL")
+
+
+@pytest.fixture
+def user_profile_id():
+    return os.environ.get("CUSTOMER_ID")
+
+
+@pytest.mark.skip("working for now")
+def test_get_profile_by_email(client: Vtex, email):
+    result = client.master_data.get_profile_by_email(email)
+    assert result.status_code == OK
+
+
+@pytest.mark.skip("working for now")
+def test_get_profile_by_user_profile_id(client: Vtex, user_profile_id):
+    result = client.master_data.get_profile_by_user_profile_id(user_profile_id)
+    assert result.status_code == OK
+
+
+def test_get_clients_scroll(client: Vtex):
+    result = client.master_data.get_clients_scroll()
+    assert result.status_code == OK
+
+
+@pytest.mark.skip("working for now")
+def test_get_clients_next_scroll(client: Vtex):
+    initial = client.master_data.get_clients_scroll()
+    result = client.master_data.get_clients_next_scroll(initial.token)
+    assert result.status_code == OK
