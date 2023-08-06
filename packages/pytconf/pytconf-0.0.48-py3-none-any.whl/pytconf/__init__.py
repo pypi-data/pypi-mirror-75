d3f5dd8693357ec2e2a7089fccab5996a0f35c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:18 +0000

    Add package pytconf/0.0.48/pytconf-0.0.48-py3-none-any.whl

---
+++ packages/pytconf/pytconf-0.0.48-py3-none-any.whl/pytconf/__init__.py
@@ -0,0 +1,10 @@
+from pytconf.param import ParamCreator
+from pytconf.config import (
+    Config,
+    register_main,
+    register_function,
+    register_function_group,
+    config_arg_parse_and_launch,
+    register_endpoint,
+    get_free_args,
+)
