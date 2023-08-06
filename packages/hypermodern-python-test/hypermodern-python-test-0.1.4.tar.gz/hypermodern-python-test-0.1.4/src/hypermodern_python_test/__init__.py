ea5dce9c0e4595b92dffdbb4385fe18e0bc1c9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:51 +0000

    Add package hypermodern-python-test/0.1.4/hypermodern-python-test-0.1.4.tar.gz

---
+++ packages/hypermodern-python-test/hypermodern-python-test-0.1.4.tar.gz/hypermodern-python-test-0.1.4/src/hypermodern_python_test/__init__.py
@@ -0,0 +1,7 @@
+"""The hypermodern Python project."""
+from importlib_metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
