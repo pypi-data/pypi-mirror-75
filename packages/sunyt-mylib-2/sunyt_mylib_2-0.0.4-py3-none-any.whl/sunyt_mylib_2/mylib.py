1654ee961bfb3dc51a12257beacba203a6f00a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:49 +0000

    Add package sunyt-mylib-2/0.0.4/sunyt_mylib_2-0.0.4-py3-none-any.whl

---
+++ packages/sunyt-mylib-2/sunyt_mylib_2-0.0.4-py3-none-any.whl/sunyt_mylib_2/mylib.py
@@ -0,0 +1,26 @@
+#-*- coding:utf-8 -*-
+"""
+@Author: SunYiTan
+@Date: 2020/7/31 15:19
+@LastEditTime: 2020/7/31 15:19
+@LastEditors: SunYiTan
+@Description: 
+"""
+
+def add(x,y):
+    return x+y
+
+def division(x,y):
+    return x/y
+
+def mutiply(x,y):
+    return x*y
+
+def subtract(x,y):
+    return x-y
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
