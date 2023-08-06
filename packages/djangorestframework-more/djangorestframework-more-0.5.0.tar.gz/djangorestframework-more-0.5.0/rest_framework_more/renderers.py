4902d26437bd6ff3fd8747e3fc11d91a637f0ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:00 +0000

    Add package djangorestframework-more/0.5.0/djangorestframework-more-0.5.0.tar.gz

---
+++ packages/djangorestframework-more/djangorestframework-more-0.5.0.tar.gz/djangorestframework-more-0.5.0/rest_framework_more/renderers.py
@@ -0,0 +1,31 @@
+from rest_framework_csv.renderers import CSVRenderer
+
+
+class NonPaginatedCSVRenderer(CSVRenderer):
+    format = "csv (non-paginated)"
+
+    def render(self, data, accepted_media_type, renderer_context):
+        response = renderer_context["response"]
+
+        if response.status_code != 200:
+            return ""
+
+        view = renderer_context["view"]
+        request = renderer_context["request"]
+
+        queryset = view.queryset
+        query_params = request.query_params.dict()
+        query_params.pop("format", None)
+
+        # filter out keys with empty strings
+        filters = {k: v for k, v in query_params.items() if v != ""}
+
+        queryset = queryset.filter(**filters)
+
+        serializer = view.serializer_class
+
+        data = serializer(queryset, many=True).data
+
+        return super(NonPaginatedCSVRenderer, self).render(
+            data, accepted_media_type, renderer_context
+        )
