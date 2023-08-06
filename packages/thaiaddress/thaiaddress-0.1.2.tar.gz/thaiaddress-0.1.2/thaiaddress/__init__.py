17aa24abc75a1949211b4f449afc651aac11cbcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:59 +0000

    Add package thaiaddress/0.1.2/thaiaddress-0.1.2.tar.gz

---
+++ packages/thaiaddress/thaiaddress-0.1.2.tar.gz/thaiaddress-0.1.2/thaiaddress/__init__.py
@@ -0,0 +1,28 @@
+"""thaiaddress: A python parser for Thai address"""
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.devN' where N is an integer.
+#
+
+__version__ = "0.1.2"
+
+from .parser import parse
+from .utils import (
+    preprocess,
+    is_stopword,
+    merge_tokens,
+    merge_labels,
+)
+from .train import train
