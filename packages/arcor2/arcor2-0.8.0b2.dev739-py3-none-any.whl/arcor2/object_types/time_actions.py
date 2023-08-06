41b80761f91db934986a3d414eae8f54576f6f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:39 +0000

    Add package arcor2/0.8.0b2.dev739/arcor2-0.8.0b2.dev739-py3-none-any.whl

---
+++ packages/arcor2/arcor2-0.8.0b2.dev739-py3-none-any.whl/arcor2/object_types/time_actions.py
@@ -0,0 +1,54 @@
+import time
+from typing import Optional
+
+from arcor2.data.common import ActionMetadata
+from arcor2.object_types.abstract import Generic
+
+
+class TimeActions(Generic):
+    """
+    Time-related actions.
+    """
+
+    _ABSTRACT = False
+
+    def __init__(self, obj_id: str, name: str) -> None:
+        super(TimeActions, self).__init__(obj_id, name)
+        self._rate_start_time: Optional[float] = None
+
+    def sleep(self, seconds: float = 1.0) -> None:
+
+        assert 0.0 <= seconds <= 10.0e6
+
+        time.sleep(seconds)
+
+    def rate(self, period: float = 1.0) -> None:
+        """
+        Can be used to maintain desired rate of the loop. Should be the first action.
+        :param period: Desired period in seconds.
+        :return:
+        """
+
+        assert 0.0 <= period <= 10.0e6
+
+        now = time.monotonic()
+
+        if self._rate_start_time is None:
+            self._rate_start_time = now
+            return
+
+        dif = self._rate_start_time + period - now
+
+        if dif > 0:
+            time.sleep(dif)
+
+    def time_ns(self) -> int:
+        """
+        Returns nanoseconds since Unix Epoch.
+        :return:
+        """
+        return time.time_ns()
+
+    sleep.__action__ = ActionMetadata(blocking=True)  # type: ignore
+    rate.__action__ = ActionMetadata(blocking=True)  # type: ignore
+    time_ns.__action__ = ActionMetadata(blocking=True)  # type: ignore
