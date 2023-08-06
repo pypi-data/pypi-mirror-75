ffc71e47e7966e74d5391e99b0413079083d1741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:02 +0000

    Add package fbscrape-ted-marozzi/0.0.1/fbscrape-ted-marozzi-0.0.1.tar.gz

---
+++ packages/fbscrape-ted-marozzi/fbscrape-ted-marozzi-0.0.1.tar.gz/fbscrape-ted-marozzi-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fbscrape-ted-marozzi", # Replace with your own username
+    version="0.0.1",
+    author="Edward Marozzi",
+    author_email="marozzi.t@gmail.com",
+    description="A package to scrape the likes of a facebook page and its posts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ted-marozzi/facebook-likes-scrape",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
