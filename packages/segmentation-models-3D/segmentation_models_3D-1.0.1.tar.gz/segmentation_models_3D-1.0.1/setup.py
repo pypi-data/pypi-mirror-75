02fb763249800d5469fb7ed79d69d575cddf12a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:11 +0000

    Add package segmentation-models-3D/1.0.1/segmentation_models_3D-1.0.1.tar.gz

---
+++ packages/segmentation-models-3D/segmentation_models_3D-1.0.1.tar.gz/segmentation_models_3D-1.0.1/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='segmentation_models_3D',
+    version='1.0.1',
+    author='Roman Sol (ZFTurbo)',
+    packages=['segmentation_models_3D', 'segmentation_models_3D/backbones', 'segmentation_models_3D/base', 'segmentation_models_3D/models'],
+    url='https://github.com/ZFTurbo/segmentation_models_3D',
+    description='Set of Keras models for segmentation of 3D volumes .',
+    long_description='3D variants of popular models for segmentation like FPN, Unet, Linknet and PSPNet.'
+                     'Models work with keras and tensorflow.keras.'
+                     'More details: https://github.com/ZFTurbo/segmentation_models_3D',
+    install_requires=[
+        'keras>=2.2.0',
+        "classification_models_3D",
+        "efficientnet_3D",
+    ],
+)
