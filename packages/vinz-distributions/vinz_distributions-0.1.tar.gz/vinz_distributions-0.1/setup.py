5c236245f1ad9c7a170efc4e7d0070d699527eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:50 +0000

    Add package vinz-distributions/0.1/vinz_distributions-0.1.tar.gz

---
+++ packages/vinz-distributions/vinz_distributions-0.1.tar.gz/vinz_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vinz_distributions',
+      version='0.1',
+      description='Distributions by Vinz',
+      packages=['vinz_distributions'],
+      author='Vinz',
+      author_email='dev@vinz.in',
+  zip_safe=False)
