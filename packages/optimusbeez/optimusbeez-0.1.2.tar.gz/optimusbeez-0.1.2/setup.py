9789ae93a042c18ea289ef8f1055cbb151224fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:02 +0000

    Add package optimusbeez/0.1.2/optimusbeez-0.1.2.tar.gz

---
+++ packages/optimusbeez/optimusbeez-0.1.2.tar.gz/optimusbeez-0.1.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(
+	name = 'optimusbeez',
+	version = '0.1.2',
+	description = 'Simple Particle Swarm Optimization',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	url='https://github.com/kaisalepajoe/Optimus-Beez',
+	author='Kaisa Lepajõe',
+	author_email='kaisa.lepajoe@gmail.com',
+	license='MIT',
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	package_data={'': ['*.txt'],},
+	classifiers=['Programming Language :: Python :: 3'],
+	install_requires=[
+		'numpy',
+		'matplotlib',
+		'tqdm'
+		],
+	test_suite='nose.collector',
+	tests_require=['nose']
+	
+	)
