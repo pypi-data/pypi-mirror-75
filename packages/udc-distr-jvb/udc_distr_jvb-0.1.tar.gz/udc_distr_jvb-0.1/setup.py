3c8b424402407192c0faf3a37c6d7c84f65282da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:28 +0000

    Add package udc-distr-jvb/0.1/udc_distr_jvb-0.1.tar.gz

---
+++ packages/udc-distr-jvb/udc_distr_jvb-0.1.tar.gz/udc_distr_jvb-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='udc_distr_jvb',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['udc_distr_jvb'],
+      author='Jorge Villarroel',
+      zip_safe=False)
