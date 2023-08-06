54babd4791929813377492ffc981a02f47e918f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:57 +0000

    Add package label-studio/0.7.4rc1/label-studio-0.7.4rc1.tar.gz

---
+++ packages/label-studio/label-studio-0.7.4rc1.tar.gz/label-studio-0.7.4rc1/label_studio/static/editor_old/precache-manifest.48623cec770daa98e4897b631c42f3ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a6d29933e40fdd655dfa6a205cc289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc17adc9865ae7611bc",
+    "url": "/static/css/main.09b8161e.css"
+  },
+  {
+    "revision": "4bc17adc9865ae7611bc",
+    "url": "/static/js/main.cc0a95e8.js"
+  },
+  {
+    "revision": "5a68f21b3cb3c487a0a1de2274234479",
+    "url": "/static/js/main.cc0a95e8.js.LICENSE.txt"
+  }
+]);
