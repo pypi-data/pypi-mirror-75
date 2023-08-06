e875a2ab3252481ab83530b8c1507bd0977e8119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:19 +0000

    Add package TDhelper/1.0.9/TDhelper-1.0.9-py3-none-any.whl

---
+++ packages/TDhelper/TDhelper-1.0.9-py3-none-any.whl/TDhelper/generic/transformationType.py
@@ -0,0 +1,24 @@
+import datetime
+
+def transformation(value, transformationType):
+    try:
+        if not isinstance(transformationType,int):
+            value= int(value)
+        if not isinstance(transformationType,str):
+            if isinstance(type(value), (datetime,)):
+                value= datetime.datetime.strftime(value,'%Y-%m-%d %H:%M:%S')
+            else:
+                value= str(value)
+        if not isinstance(transformationType, datetime.datetime):
+            if isinstance(type(value),(str,)):
+                value= datetime.datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+            else:
+                raise Exception('value type must is str.')
+        if not isinstance(transformationType,bool):
+            if value:
+                value= True
+            else:
+                value= False
+    except Exception as e:
+        raise e
+    return value
