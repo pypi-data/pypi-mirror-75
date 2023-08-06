b63a92f3ca405630f6b81f50f4fe65a6e35d45ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:14 +0000

    Add package easy-distributions-dm/2.0/easy_distributions_dm-2.0.tar.gz

---
+++ packages/easy-distributions-dm/easy_distributions_dm-2.0.tar.gz/easy_distributions_dm-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='easy_distributions_dm',
+      version='2.0',
+      description='Gaussian and Binomial distributions',
+      packages=['easy_distributions_dm'],
+      author= 'Delong Meng',
+      author_email= 'delongmeng@hotmail.com',
+      zip_safe=False)
