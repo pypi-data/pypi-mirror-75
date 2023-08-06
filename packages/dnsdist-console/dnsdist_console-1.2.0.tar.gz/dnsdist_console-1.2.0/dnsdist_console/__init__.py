04bb7acde51227ef2ffac6349d18ed8c4bfd9201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:01 +0000

    Add package dnsdist-console/1.2.0/dnsdist_console-1.2.0.tar.gz

---
+++ packages/dnsdist-console/dnsdist_console-1.2.0.tar.gz/dnsdist_console-1.2.0/dnsdist_console/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "1.2.0"
+
+from dnsdist_console.console import Console
+from dnsdist_console.key import Key
+from dnsdist_console.statistics import Statistics
+from dnsdist_console.dashboard import Dashboard
