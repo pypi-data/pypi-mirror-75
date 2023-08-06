4f3dbb7debe5fee57ccc868b872839c3606c8ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:15 +0000

    Add package pysnobal/0.2.1.dev0/pysnobal-0.2.1.dev0.tar.gz

---
+++ packages/pysnobal/pysnobal-0.2.1.dev0.tar.gz/pysnobal-0.2.1.dev0/pysnobal/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for PySnobal."""
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = 'unknown'
