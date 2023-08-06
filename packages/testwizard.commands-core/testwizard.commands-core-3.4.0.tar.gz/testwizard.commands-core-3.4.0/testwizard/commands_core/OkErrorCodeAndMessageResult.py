86b4093ec030251d2a1ecac304f85b547b4ca982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:52 +0000

    Add package testwizard.commands-core/3.4.0/testwizard.commands-core-3.4.0.tar.gz

---
+++ packages/testwizard.commands-core/testwizard.commands-core-3.4.0.tar.gz/testwizard.commands-core-3.4.0/testwizard/commands_core/OkErrorCodeAndMessageResult.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+from .ResultBase import ResultBase
+
+class OkErrorCodeAndMessageResult(ResultBase):
+    def __init__(self, result, successMessage, failMessage):
+        ResultBase.__init__(self, result["ok"] is True, successMessage, failMessage)
+
+        if self.success is True:
+            return
+
+        if "errorCode" in result:
+            self.message = self.getMessageForErrorCode(self.message, result["errorCode"])
