5032022b681bff4dced33bbb3cd8a35dcfebbf14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:49 +0000

    Add package distributions-aws-123456/0.2/distributions_aws_123456-0.2.tar.gz

---
+++ packages/distributions-aws-123456/distributions_aws_123456-0.2.tar.gz/distributions_aws_123456-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_aws_123456',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_aws_123456'],
+      author = 'AWS_UDACITY',
+      author_email = 'mexample@something.com',
+      zip_safe=False)
