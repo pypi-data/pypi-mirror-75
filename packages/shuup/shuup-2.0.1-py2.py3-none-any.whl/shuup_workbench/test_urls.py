1d6742e56d8b95c4296b48ae5183548a482f946a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:08 +0000

    Add package shuup/2.0.1/shuup-2.0.1-py2.py3-none-any.whl

---
+++ packages/shuup/shuup-2.0.1-py2.py3-none-any.whl/shuup_workbench/test_urls.py
@@ -0,0 +1,28 @@
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2020, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+import django
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+
+"""
+Modify these only for Shuup tests. For testing modify urls.py instead.
+"""
+if django.VERSION < (2, 0):
+    urlpatterns = [
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^sa/', include('shuup.admin.urls', namespace="shuup_admin", app_name="shuup_admin")),
+        url(r'^', include('shuup.front.urls', namespace="shuup", app_name="shuup")),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    from django.urls import path
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        url(r'^sa/', include('shuup.admin.urls', namespace="shuup_admin")),
+        url(r'^', include('shuup.front.urls', namespace="shuup")),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
