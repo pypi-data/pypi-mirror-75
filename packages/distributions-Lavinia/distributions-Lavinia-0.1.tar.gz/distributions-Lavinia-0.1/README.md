be3829ba31c8038417c8aba4a2fe0c10ec052130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:28 +0000

    Add package distributions-Lavinia/0.1/distributions-Lavinia-0.1.tar.gz

---
+++ packages/distributions-Lavinia/distributions-Lavinia-0.1.tar.gz/distributions-Lavinia-0.1/README.md
@@ -0,0 +1,7 @@
+Summary of the package
+
+1. How to instal
+
+2. How to use
+
+3. Functionality
---
+++ packages/distributions-Lavinia/distributions-Lavinia-0.1.tar.gz/distributions-Lavinia-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions-Lavinia',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions-Lavinia'],
+      author='Lavinia Calinoiu',
+      zip_safe=False)
