78f6261f1e8847f5fbb7990a3840673b174184a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:46 +0000

    Add package ao-aws-ml-course/0.1/ao_aws_ml_course-0.1.tar.gz

---
+++ packages/ao-aws-ml-course/ao_aws_ml_course-0.1.tar.gz/ao_aws_ml_course-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ao_aws_ml_course',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ao_aws_ml_course'],
+      zip_safe=False)
