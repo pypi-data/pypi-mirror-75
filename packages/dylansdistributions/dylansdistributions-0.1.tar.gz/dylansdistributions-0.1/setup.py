45deb01a574aaaef7b17748770249c1b02a643f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:30 +0000

    Add package dylansdistributions/0.1/dylansdistributions-0.1.tar.gz

---
+++ packages/dylansdistributions/dylansdistributions-0.1.tar.gz/dylansdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dylansdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dylansdistributions'],
+      author = 'Dylan Rigney',
+      author_email = 'dylanrigney@hotmail.com',
+    zip_safe=False)
