4169e2a512e15b4e0994dd4fe0de6729e6fc0def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:40 +0000

    Add package khachhibhoya-distributions/1.1/khachhibhoya_distributions-1.1.tar.gz

---
+++ packages/khachhibhoya-distributions/khachhibhoya_distributions-1.1.tar.gz/khachhibhoya_distributions-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='khachhibhoya_distributions',
+      version='1.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions'],
+      author="neha",
+      zip_safe=False)
