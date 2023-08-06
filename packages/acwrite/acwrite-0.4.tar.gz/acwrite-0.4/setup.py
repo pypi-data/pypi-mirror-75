d25a51a8225ac1af46ab310007c1cb98bba7e9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:42 +0000

    Add package acwrite/0.4/acwrite-0.4.tar.gz

---
+++ packages/acwrite/acwrite-0.4.tar.gz/acwrite-0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name= "acwrite",
+    version = "0.4",
+    author="Yi Yu",
+    author_email="q1499114179@gmail.com",
+    description="A python package for writing academic papers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yoki0/acwriting",
+    packages = find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    )
