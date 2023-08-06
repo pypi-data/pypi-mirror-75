06977a69d0f1fe6838f745469f8924a9b7a07e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:42 +0000

    Add package selenium2mysql/1.0.2/selenium2mysql-1.0.2.tar.gz

---
+++ packages/selenium2mysql/selenium2mysql-1.0.2.tar.gz/selenium2mysql-1.0.2/selenium2mysql/__init__.py
@@ -0,0 +1,6 @@
+from .info import __VERSION__, __version__
+from .Json2Mysql import Json2Mysql
+from .QueueManager import QueueManager
+from .SeleniumCrawler import SeleniumCrawler
+
+
---
+++ packages/selenium2mysql/selenium2mysql-1.0.2.tar.gz/selenium2mysql-1.0.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+install_requireent = []
+
+setup_requires = [
+        'csv2sqllike',
+        'selenium',
+        'dict',
+        'validators'
+        ]
+
+install_requires = [
+        'csv2sqllike',
+        'selenium',
+        'dict',
+        'validators'
+        ]
+
+setup(
+    name='selenium2mysql',
+    author='Junsang Park',
+    author_email='publichey@gmail.com',
+    url='https://github.com/hoosiki/selenium2mysql',
+    version='1.0.2',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    description='scraper using selenium for general purposes',
+    packages=find_packages(),
+    license='BSD',
+    include_package_date=False,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    download_url='https://github.com/hoosiki/selenium2mysql/blob/master/dist/selenium2mysql-1.0.2.tar.gz'
+)
