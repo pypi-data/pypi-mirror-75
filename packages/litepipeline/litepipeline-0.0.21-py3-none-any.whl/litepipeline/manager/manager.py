3b8542991301bdf71d156b82116f62866e200624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:19 +0000

    Add package litepipeline/0.0.21/litepipeline-0.0.21-py3-none-any.whl

---
+++ packages/litepipeline/litepipeline-0.0.21-py3-none-any.whl/litepipeline/manager/manager.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import signal
+import logging
+import argparse
+
+import tornado.ioloop
+import tornado.httpserver
+import tornado.web
+
+from litepipeline.version import __version__
+from litepipeline.manager.handlers import info
+from litepipeline.manager.handlers import application
+from litepipeline.manager.handlers import task
+from litepipeline.manager.handlers import workflow
+from litepipeline.manager.handlers import work
+from litepipeline.manager.handlers import schedule
+from litepipeline.manager.utils.listener import Connection
+from litepipeline.manager.utils.listener import DiscoveryListener
+from litepipeline.manager.models.applications import Applications
+from litepipeline.manager.models.application_history import ApplicationHistory
+from litepipeline.manager.models.tasks import Tasks
+from litepipeline.manager.models.workflows import Workflows
+from litepipeline.manager.models.works import Works
+from litepipeline.manager.models.schedules import Schedules
+from litepipeline.manager.utils.app_manager import AppManager
+from litepipeline.manager.utils.scheduler import Scheduler
+from litepipeline.manager.utils import common
+from litepipeline.manager.utils.litedfs import LDFS, LiteDFS
+from litepipeline.manager.config import CONFIG, load_config
+from litepipeline.manager import logger
+
+LOG = logging.getLogger(__name__)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", info.AboutHandler),
+            (r"/cluster/info", info.ClusterInfoHandler),
+            (r"/app/create", application.CreateApplicationHandler),
+            (r"/app/list", application.ListApplicationHandler),
+            (r"/app/delete", application.DeleteApplicationHandler),
+            (r"/app/update", application.UpdateApplicationHandler),
+            (r"/app/info", application.InfoApplicationHandler),
+            (r"/app/download", application.DownloadApplicationHandler),
+            (r"/app/history/list", application.ApplicationHistoryListHandler),
+            (r"/app/history/info", application.ApplicationHistoryInfoHandler),
+            (r"/app/history/activate", application.ApplicationHistoryActivateHandler),
+            (r"/app/history/delete", application.ApplicationHistoryDeleteHandler),
+            (r"/task/create", task.CreateTaskHandler),
+            (r"/task/list", task.ListTaskHandler),
+            (r"/task/info", task.InfoTaskHandler),
+            (r"/task/delete", task.DeleteTaskHandler),
+            (r"/task/rerun", task.RunTaskHandler),
+            (r"/task/recover", task.RecoverTaskHandler),
+            (r"/task/stop", task.StopTaskHandler),
+            (r"/action/update", task.UpdateActionHandler),
+            (r"/workspace/delete", task.DeleteTaskWorkspaceHandler),
+            (r"/workspace/pack", task.PackTaskWorkspaceHandler),
+            (r"/workspace/download", task.DownloadTaskWorkspaceHandler),
+            (r"/workflow/create", workflow.CreateWorkflowHandler),
+            (r"/workflow/list", workflow.ListWorkflowHandler),
+            (r"/workflow/delete", workflow.DeleteWorkflowHandler),
+            (r"/workflow/update", workflow.UpdateWorkflowHandler),
+            (r"/workflow/info", workflow.InfoWorkflowHandler),
+            (r"/work/create", work.CreateWorkHandler),
+            (r"/work/list", work.ListWorkHandler),
+            (r"/work/info", work.InfoWorkHandler),
+            (r"/work/delete", work.DeleteWorkHandler),
+            (r"/work/rerun", work.RunWorkHandler),
+            (r"/work/recover", work.RecoverWorkHandler),
+            (r"/work/stop", work.StopWorkHandler),
+            (r"/schedule/create", schedule.CreateScheduleHandler),
+            (r"/schedule/update", schedule.UpdateScheduleHandler),
+            (r"/schedule/list", schedule.ListScheduleHandler),
+            (r"/schedule/delete", schedule.DeleteScheduleHandler),
+            (r"/schedule/info", schedule.InfoScheduleHandler),
+        ]
+        settings = dict(debug = False)
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+def main():
+    parser = argparse.ArgumentParser(prog = 'litemanager')
+    parser.add_argument("-c", "--config", required = True, help = "configuration file path")
+    parser.add_argument("-v", "--version", action = 'version', version = '%(prog)s ' + __version__)
+    args = parser.parse_args()
+
+    if args.config:
+        success = load_config(args.config)
+        if success:
+            common.init_storage()
+            logger.config_logging(file_name = "manager.log",
+                                  log_level = CONFIG["log_level"],
+                                  dir_name = CONFIG["log_path"],
+                                  day_rotate = False,
+                                  when = "D",
+                                  interval = 1,
+                                  max_size = 20,
+                                  backup_count = 5,
+                                  console = True)
+
+            LOG.info("service start")
+
+            try:
+                if CONFIG["ldfs_http_host"] and CONFIG["ldfs_http_port"]:
+                    LDFS = LiteDFS(CONFIG["ldfs_http_host"], CONFIG["ldfs_http_port"])
+                tasks_db = Tasks()
+                applications_db = Applications()
+                application_history_db = ApplicationHistory()
+                workflows_db = Workflows()
+                works_db = Works()
+                schedules_db = Schedules()
+                app_manager = AppManager()
+                task_scheduler = Scheduler(CONFIG["scheduler_interval"])
+                http_server = tornado.httpserver.HTTPServer(
+                    Application(),
+                    max_buffer_size = CONFIG["max_buffer_size"],
+                    chunk_size = 10 * 1024 * 1024
+                )
+                http_server.listen(CONFIG["http_port"], address = CONFIG["http_host"])
+                # http_server.bind(CONFIG["http_port"], address = CONFIG["http_host"])
+                listener = DiscoveryListener(Connection, task_scheduler)
+                listener.listen(CONFIG["tcp_port"], CONFIG["tcp_host"])
+                common.Servers.HTTP_SERVER = http_server
+                common.Servers.SERVERS.append(applications_db)
+                common.Servers.SERVERS.append(application_history_db)
+                common.Servers.SERVERS.append(tasks_db)
+                common.Servers.SERVERS.append(workflows_db)
+                common.Servers.SERVERS.append(works_db)
+                common.Servers.SERVERS.append(schedules_db)
+                common.Servers.SERVERS.append(app_manager)
+                common.Servers.SERVERS.append(task_scheduler)
+                signal.signal(signal.SIGTERM, common.sig_handler)
+                signal.signal(signal.SIGINT, common.sig_handler)
+                tornado.ioloop.IOLoop.instance().start()
+            except Exception as e:
+                LOG.exception(e)
+
+            LOG.info("service end")
+        else:
+            print("failed to load configuration: %s" % args.config)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/litepipeline/litepipeline-0.0.21-py3-none-any.whl/litepipeline/manager/utils/common.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import json
+import hashlib
+import logging
+
+from tornado import ioloop
+from tornado import gen
+
+from litepipeline.manager.config import CONFIG
+
+LOG = logging.getLogger(__name__)
+
+BUF_SIZE = 65536
+
+
+class Servers(object):
+    HTTP_SERVER = None
+    SERVERS = []
+    TORNADO_INSTANCE = None
+
+
+async def shutdown():
+    LOG.info("Stopping Service(%s:%s)", CONFIG["http_host"], CONFIG["http_port"])
+    if Servers.HTTP_SERVER:
+        Servers.HTTP_SERVER.stop()
+        LOG.info("Stop http server!")
+    for s in Servers.SERVERS:
+        s.close()
+        if hasattr(s, "name"):
+            LOG.info("Stop %s server!", s.name)
+        else:
+            LOG.info("Stop nameless server!")
+    await gen.sleep(1)
+    LOG.info("Will shutdown ...")
+    ioloop.IOLoop.current().stop()
+
+
+def sig_handler(sig, frame):
+    LOG.warning("sig_handler Caught signal: %s", sig)
+    ioloop.IOLoop.current().add_callback_from_signal(shutdown)
+
+
+class Errors(object):
+    OK = "ok"
+    errors = {
+        "ServerException": {"name": "ServerException", "message": "server exception"},
+        "InvalidParameters": {"name": "InvalidParameters", "message": "invalid parameters"},
+        "OperationFailed": {"name": "OperationFailed", "message": "operation failed"},
+        "AppNotExists": {"name": "AppNotExists", "message": "application not exists"},
+        "AppWrongFormat": {"name": "AppWrongFormat", "message": "application wrong format"},
+        "AppHistoryNotExists": {"name": "AppHistoryNotExists", "message": "application history not exists"},
+        "TaskNotExists": {"name": "TaskNotExists", "message": "task not exists"},
+        "TaskAlreadyFinished": {"name": "TaskAlreadyFinished", "message": "task already finished"},
+        "TaskAlreadySuccess": {"name": "TaskAlreadySuccess", "message": "task already success"},
+        "TaskStillRunning": {"name": "TaskStillRunning", "message": "task still running"},
+        "WorkNotExists": {"name": "WorkNotExists", "message": "work not exists"},
+        "WorkAlreadyFinished": {"name": "WorkAlreadyFinished", "message": "work already finished"},
+        "WorkAlreadySuccess": {"name": "WorkAlreadySuccess", "message": "work already success"},
+        "WorkStillRunning": {"name": "WorkStillRunning", "message": "work still running"},
+        "WorkflowDisabled": {"name": "WorkflowDisabled", "message": "workflow disabled"},
+        "WorkflowNotExists": {"name": "WorkflowNotExists", "message": "workflow not exists"},
+        "NodeNotExists": {"name": "NodeNotExists", "message": "node not exists"},
+        "ActionNotFinished": {"name": "ActionNotFinished", "message": "action not finished"},
+        "ActionNoNodeId": {"name": "ActionNoNodeId", "message": "action no node id"},
+        "ActionNotRun": {"name": "ActionNotRun", "message": "action not run"},
+        "ScheduleNotExists": {"name": "ScheduleNotExists", "message": "schedule no exists"},
+    }
+
+    @classmethod
+    def set_result_error(cls, error_name, result, message = ""):
+        if error_name in cls.errors:
+            result["result"] = error_name
+            if message == "":
+                result["message"] = cls.errors[error_name]["message"]
+            else:
+                result["message"] = message
+        else:
+            result["result"] = "UnknownError"
+            result["message"] = "unknown error"
+
+
+class Stage(object):
+    pending = "pending"
+    running = "running"
+    finished = "finished"
+    stopping = "stopping"
+    recovering = "recovering"
+
+
+class Status(object):
+    fail = "fail"
+    success = "success"
+    kill = "kill"
+    cancel = "cancel"
+    terminate = "terminate"
+    error = "error"
+
+
+class Signal(object):
+    kill = -9
+    terminate = -15
+    stop = -15
+    cancel = -50
+
+
+class Event(object):
+    events = ["fail", "success"]
+    fail = "fail"
+    success = "success"
+
+
+class JSONLoadError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class MetaNotDictError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class OperationError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+def file_sha1sum(file_path):
+    sha1 = hashlib.sha1()
+    with open(file_path, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha1.update(data)
+    return sha1.hexdigest()
+
+
+def file_md5sum(file_path):
+    md5 = hashlib.md5()
+    with open(file_path, 'rb') as fp:
+        while True:
+            data = fp.read(BUF_SIZE)
+            if not data:
+                break
+            md5.update(data)
+    return md5.hexdigest()
+
+
+def splitall(path):
+    allparts = []
+    while True:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
+
+
+def init_storage():
+    directories = [
+        os.path.join(CONFIG["data_path"], "applications"),
+        os.path.join(CONFIG["data_path"], "tmp"),
+    ]
+    for d in directories:
+        if not os.path.exists(d) or not os.path.isdir(d):
+            os.makedirs(d)
