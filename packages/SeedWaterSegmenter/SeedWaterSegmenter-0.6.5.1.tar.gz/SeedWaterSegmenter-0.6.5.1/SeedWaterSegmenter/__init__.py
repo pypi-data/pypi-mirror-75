805b6aeda66376a9384d333d1d2a722dfca419b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:15 +0000

    Add package SeedWaterSegmenter/0.6.5.1/SeedWaterSegmenter-0.6.5.1.tar.gz

---
+++ packages/SeedWaterSegmenter/SeedWaterSegmenter-0.6.5.1.tar.gz/SeedWaterSegmenter-0.6.5.1/SeedWaterSegmenter/__init__.py
@@ -0,0 +1,7 @@
+# This should import everything relevant:
+from SeedWaterSegmenter import *
+import SWHelpers
+from ImageContour import SubContourTools
+from _version import *
+
+__all__ = ["SeedWaterSegmenter", "SWHelpers"]
