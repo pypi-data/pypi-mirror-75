62ab5e6eaf02afee7f576a3dbe7c9c8456016c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:44 +0000

    Add package coiled/0.0.17/coiled-0.0.17.tar.gz

---
+++ packages/coiled/coiled-0.0.17.tar.gz/coiled-0.0.17/coiled/cli/core.py
@@ -0,0 +1,21 @@
+import click
+
+from .login import login
+from .install import install
+from .upload import upload
+from ..compatibility import COILED_VERSION
+
+CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(COILED_VERSION, message="%(version)s")
+def cli():
+    """ Coiled command line tool
+    """
+    pass
+
+
+cli.add_command(login)
+cli.add_command(install)
+cli.add_command(upload)
