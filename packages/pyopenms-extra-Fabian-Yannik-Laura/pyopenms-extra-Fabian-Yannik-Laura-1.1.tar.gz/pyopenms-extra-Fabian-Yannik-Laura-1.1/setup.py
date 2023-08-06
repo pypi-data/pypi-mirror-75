c23e106ea9449481674098594091992194658da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:17 +0000

    Add package pyopenms-extra-Fabian-Yannik-Laura/1.1/pyopenms-extra-Fabian-Yannik-Laura-1.1.tar.gz

---
+++ packages/pyopenms-extra-Fabian-Yannik-Laura/pyopenms-extra-Fabian-Yannik-Laura-1.1.tar.gz/pyopenms-extra-Fabian-Yannik-Laura-1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("pyopenms-extra/README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyopenms-extra-Fabian-Yannik-Laura", # Replace with your own username
+    version="1.1",
+    author="Fabian Rosner",
+    author_email="fabian@rosner.email",
+    description="Teamprojekt SS20 Yannik Haller Fabian Rosner Laura Schöneberg",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://fabian1567.github.io/Teamprojekt-SS20-Laura-Yannik-Fabian/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
