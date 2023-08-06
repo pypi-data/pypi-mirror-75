e93de238b2dd3d3af99786a4079a0fc293096bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:36 +0000

    Add package django-nacos/0.1.3/django-nacos-0.1.3.tar.gz

---
+++ packages/django-nacos/django-nacos-0.1.3.tar.gz/django-nacos-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import django_nacos as nacos
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="django-nacos",
+    version=nacos.__version__,
+    packages=find_packages(exclude=["test"]),
+    url="https://github.com/nacos-group/nacos-sdk-python",
+    license="Apache License 2.0",
+    author="nacos",
+    author_email="755063194@qq.com",
+    description="Python client for Nacos.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+)
