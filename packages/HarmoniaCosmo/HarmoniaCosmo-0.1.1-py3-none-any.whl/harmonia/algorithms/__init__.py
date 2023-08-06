7f5fa8ecfc4fa261f16db041d768df9a2f29dbe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:03 +0000

    Add package HarmoniaCosmo/0.1.1/HarmoniaCosmo-0.1.1-py3-none-any.whl

---
+++ packages/HarmoniaCosmo/HarmoniaCosmo-0.1.1-py3-none-any.whl/harmonia/algorithms/__init__.py
@@ -0,0 +1,11 @@
+"""
+***************************************************************************
+Algorithms (:mod:`~harmonia.algorithms`)
+***************************************************************************
+
+Provide algorithms for Fourier basis evaluation and integration, spectrum
+discretisation and structured array manipulation for cosmological data.
+
+"""
+from .arrays import CartesianArray, SphericalArray
+from .discretisation import DiscreteSpectrum
