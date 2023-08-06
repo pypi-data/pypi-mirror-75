dc2c691184fe28a782192685a8669675a86321c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:44 +0000

    Add package Av-dist-uda/0.1/Av_dist_uda-0.1.tar.gz

---
+++ packages/Av-dist-uda/Av_dist_uda-0.1.tar.gz/Av_dist_uda-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Av_dist_uda',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_av_dist'],
+      zip_safe=False)
