e2e2f487dbb700b53356b6b2caa3e4460b4677c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:31 +0000

    Add package interactionfreepy/1.0.2/InteractionFreePy-1.0.2.tar.gz

---
+++ packages/interactionfreepy/InteractionFreePy-1.0.2.tar.gz/InteractionFreePy-1.0.2/InteractionFreePy/__init__.py
@@ -0,0 +1,3 @@
+from interactionfreepy.core import IFLoop, IFDefinition, IFException, Invocation, Message, IFLoop
+from interactionfreepy.broker import IFBroker
+from interactionfreepy.worker import IFWorker
