5d4622199e9241ec8d2395ec755b1f6dfc4e95c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:11 +0000

    Add package danielpops/0.0.1/danielpops-0.0.1.tar.gz

---
+++ packages/danielpops/danielpops-0.0.1.tar.gz/danielpops-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import danielpops
+import setuptools
+
+setuptools.setup(
+    name='danielpops',
+    version='0.0.1',
+    author='Daniel Popescu',
+    author_email='danielpops@gmail.com',
+    description='Security research package',
+    long_description='This is just a placeholder',
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+    ]
+    )
