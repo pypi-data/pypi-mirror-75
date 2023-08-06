172e8be7d3a25808a70901db0e0284fa79978d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:48 +0000

    Add package distributions2021/0.1/distributions2021-0.1.tar.gz

---
+++ packages/distributions2021/distributions2021-0.1.tar.gz/distributions2021-0.1/README.md
@@ -0,0 +1,1 @@
+This is a test Exercise to upload package to Pypi
---
+++ packages/distributions2021/distributions2021-0.1.tar.gz/distributions2021-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions2021',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions2021'],
+      zip_safe=False)
