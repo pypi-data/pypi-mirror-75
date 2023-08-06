a55dc7af52a80532f85e8e01e7802c9741d8fa8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:32 +0000

    Add package aiotrivia/1.2.5/aiotrivia-1.2.5-py3-none-any.whl

---
+++ packages/aiotrivia/aiotrivia-1.2.5-py3-none-any.whl/aiotrivia/question.py
@@ -0,0 +1,34 @@
+"""
+-*- coding: utf-8 -*-
+The files containing question data
+"""
+
+from html import unescape
+from random import sample
+
+
+class Question:
+    """
+    The question type returned when getting questions
+    """
+    __slots__ = ('category', 'type', 'question', 'answer', 'incorrect_answers', 'difficulty')
+
+    def __init__(self, data: dict):
+        self.category = data.get('category')
+        self.type = data.get('type')
+        self.question = unescape(str(data.get('question')))
+        self.answer = unescape(str(data.get('correct_answer')))
+        self.incorrect_answers: list = sample([unescape(answer) for answer in data.get('incorrect_answers')], len([unescape(answer) for answer in data.get('incorrect_answers')]))
+        self.difficulty = data.get('difficulty')
+
+    def __repr__(self):
+        return f"<aiotrivia.question.Question: question={self.question}, category={self.category}, type={self.type}>"
+
+    @property
+    def responses(self):
+        responses = self.incorrect_answers + [self.answer]
+        return responses
+
+    def add_incorrect_answers(self, *args):
+        for item in args:
+            self.incorrect_answers.append(item)
