73b361c659edfb11ec65523f3c758b1e3cfe9034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:57 +0000

    Add package diss-prob/0.2/diss_prob-0.2.tar.gz

---
+++ packages/diss-prob/diss_prob-0.2.tar.gz/diss_prob-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='diss_prob',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['diss_prob'],
+      author = 'Ose Daniel',
+      author_email = 'okojieose111@gmail.com',
+      zip_safe=False)
