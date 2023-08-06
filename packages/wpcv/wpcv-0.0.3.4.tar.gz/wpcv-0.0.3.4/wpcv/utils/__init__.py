aca886b76317f706684030229127feea4e82169f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:34 +0000

    Add package wpcv/0.0.3.4/wpcv-0.0.3.4.tar.gz

---
+++ packages/wpcv/wpcv-0.0.3.4.tar.gz/wpcv-0.0.3.4/wpcv/utils/__init__.py
@@ -0,0 +1,4 @@
+from .imutils import *
+from .boxutils import *
+from .dataset import split_files,split_list,split_dir,split_train_val_imagefolder,split_train_val
+from .data_aug import img_aug,det_aug
