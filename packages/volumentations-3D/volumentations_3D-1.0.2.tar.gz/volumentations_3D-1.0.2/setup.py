098b45aebf08193c9e1e7b5310434e20048a579c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:03 +0000

    Add package volumentations-3D/1.0.2/volumentations_3D-1.0.2.tar.gz

---
+++ packages/volumentations-3D/volumentations_3D-1.0.2.tar.gz/volumentations_3D-1.0.2/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='volumentations_3D',
+    version='1.0.2',
+    author='Roman Sol (ZFTurbo)',
+    packages=['volumentations', ],
+    url='https://github.com/ZFTurbo/volumentations',
+    description='Library for 3D augmentations',
+    long_description='Library for 3D augmentations. Inspired by albumentations.'
+                     'More details: https://github.com/ZFTurbo/volumentations',
+    install_requires=[
+        'scikit-image',
+        'scipy',
+        'opencv-python',
+        "numpy",
+    ],
+)
