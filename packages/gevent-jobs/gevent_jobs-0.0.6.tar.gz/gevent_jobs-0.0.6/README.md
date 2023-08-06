91b9f8a2d0d6352d30c0c88985659111d05dd762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:52 +0000

    Add package gevent-jobs/0.0.6/gevent_jobs-0.0.6.tar.gz

---
+++ packages/gevent-jobs/gevent_jobs-0.0.6.tar.gz/gevent_jobs-0.0.6/README.md
@@ -0,0 +1,57 @@
+# gevent_jobs
+### how to install?
+
+```angular2html
+pip  install -U gevent_jobs
+```
+
+### how to use?
+
+```python
+#import Job,JobQueue (导入 Job,JobQueue)
+from gevent_jobs import Job,JobQueue
+
+# define your job , must be implements `do` func ,return bool  (定义你的任务，继承Job类，必须实现 do 方法，返回 bool 类型)
+# 成功时回调 success 方法，失败时回调 error 方法
+class MyJob(Job):
+    def do(self):
+        self.data=self.job_id
+        return True
+    def success(self):
+        print(self.data)
+    def error(self):
+        print(self.data)
+
+#  方式一、 load_job second/per（每秒种加载一次任务）
+def load_job(queue):
+    job=MyJob()
+    queue.put(job)
+    
+# 方式二、 load_job custom interval (recommend) (自定义加载任务间隔【推荐】)
+def load_job(queue):
+    import time
+    while True:
+        for i in range(1,100):
+            job=MyJob()
+            queue.put(job)
+        time.sleep(60)
+
+
+## JobQueue(load_job_func=None,load_job_interval=1,worker_count=10,queue_size=100000,thread_mode='gevent') 
+'''
+load_job_func 加载任务函数，用户自定义，在加载函数中最好用 while True阻塞
+load_job_interval 加载任时间间隔，默认1s
+worker_count 工作的协（线）程数量
+queue_size 队列大小
+thread_mode 线程模型 gevent|thread
+'''
+
+jobQueue=JobQueue(load_job_func=load_job)
+jobQueue.start()
+```
+
+
+### how gevent_jobs compatibility?
+```angular2html
+python2.6+
+```
---
+++ packages/gevent-jobs/gevent_jobs-0.0.6.tar.gz/gevent_jobs-0.0.6/gevent_jobs/gevent_jobs.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'xiaozhang'
+try:
+    from queue import Queue
+except Exception as er:
+    from Queue import Queue
+import time
+import uuid
+import logging
+import threading
+
+
+
+logger = logging.getLogger('JobQueue')
+
+class JobStatus(object):
+    Unkown='unkown'
+    Success='success'
+    Fail='fail'
+    Runing='running'
+    Init='init'
+    Finish='finish'
+
+class Job(object):
+    def __init__(self,data=None,input=None,output=None):
+        self.data=data
+        self.input=input
+        self.output=output
+        self.job_id=str(uuid.uuid4())
+        self.job_status=JobStatus.Init
+    def job_switch(self):
+        time.sleep(0.001)
+    def error(self):
+        pass
+    def success(self):
+        pass
+    def do(self):
+        raise Exception('must implements do function')
+
+
+class JobQueue(object):
+    '''
+    class MyJob(Job):
+        def do(self):
+            self.data=self.job_id
+            return True
+        def success(self):
+            print(self.data)
+
+    def load(queue):
+        job=MyJob()
+        queue.put(job)
+
+    jobQueue=JobQueue(load_job_func=load)
+    jobQueue.start()
+
+    '''
+    class Status(object):
+        INIT=0
+        RUNNING=1
+        STOP=2
+        PAUSE=3
+    def __init__(self,load_job_func=None,load_job_interval=1,worker_count=10,queue_size=100000,thread_mode='gevent'):
+        self.status=self.Status.INIT
+        self.job_queue=Queue(queue_size)
+        self.job_queue_error=Queue(queue_size)
+        self.job_queue_success=Queue(queue_size)
+        self.worker_count=worker_count
+        self.load_job_func=load_job_func
+        self.thread_mode=thread_mode
+        self.load_job_interval=load_job_interval
+
+    def _add_job(self,job):
+        if not hasattr(job, 'do'):
+            logger.error('job must be implements do function')
+            return False
+        self.job_queue.put(job)
+        return True
+
+    def put(self,job):
+        return self._add_job(job)
+
+    def _load_job(self):
+        while True:
+            try:
+                if callable(self.load_job_func) and self.job_queue.qsize()==0 and self.status==self.Status.RUNNING:
+                    jobs=self.load_job_func(self)
+                    if jobs!=None and isinstance(jobs,list):
+                        for job in jobs:
+                            if self._add_job(jobs):
+                                logger.info('add job fail')
+                time.sleep(self.load_job_interval)
+            except Exception as er:
+                print(er)
+                logger.error(er)
+
+    def do_success(self):
+        while True:
+            try:
+                job=self.job_queue_success.get()
+                if hasattr(job, 'success') and callable(job.success):
+                    job.success()
+                else:
+                    logger.info('please implements success funcion')
+                time.sleep(0.001)
+            except Exception as er:
+                logger.error(er)
+    def do_error(self):
+        while True:
+            try:
+                job=self.job_queue_error.get()
+                if hasattr(job, 'error') and callable(job.error):
+                    job.error()
+                else:
+                    logger.info('please implements error funcion')
+                time.sleep(0.001)
+            except Exception as er:
+                logger.error(er)
+
+    def do(self):
+        while True:
+            try:
+                if self.status==self.Status.PAUSE:
+                    time.sleep(0.2)
+                    continue
+                if self.status==self.Status.STOP:
+                    break
+                job=self.job_queue.get()
+                if not hasattr(job,'do'):
+                    logger.error('job must be implements do function')
+                    continue
+                try:
+                    result=job.do()
+                    job.job_status=JobStatus.Finish
+                except Exception as er:
+                    logger.error(er)
+                    job.job_status = JobStatus.Fail
+                    result=False
+                if result==None or result:
+                    self.job_queue_success.put(job)
+                else:
+                    self.job_queue_error.put(job)
+                time.sleep(0.001)
+            except Exception as er:
+                logger.error(er)
+
+    def _start_gevent(self):
+        try:
+            from gevent import monkey
+            monkey.patch_all()
+            import gevent
+            def nohup():
+                while True:
+                    time.sleep(100)
+            self.status=self.Status.RUNNING
+            workers=[]
+            workers.append(gevent.spawn(nohup))
+            time.sleep(0.02)
+            workers.append(gevent.spawn(self.do_success))
+            workers.append(gevent.spawn(self._load_job))
+            workers.append(gevent.spawn(self.do_error))
+            for i in range(1,self.worker_count):
+                workers.append(gevent.spawn(self.do))
+            gevent.joinall(workers)
+        except Exception as er:
+            logger.info('using threading mode')
+            logger.error(er)
+            self._start_thread()
+
+    def _start_thread(self):
+        import threading
+        def nohup():
+            while True:
+                time.sleep(100)
+        self.status=self.Status.RUNNING
+        workers=[]
+        workers.append(threading.Thread(target=nohup))
+        time.sleep(0.02)
+        workers.append(threading.Thread(target=self.do_success))
+        workers.append(threading.Thread(target=self._load_job))
+        workers.append(threading.Thread(target=self.do_error))
+        for i in range(1,self.worker_count):
+            workers.append(threading.Thread(target=self.do))
+        for i in workers:
+            i.setDaemon(True)
+            i.start()
+        for i in workers:
+            i.join()
+
+    def start(self):
+        if self.thread_mode=='thread':
+            self._start_thread()
+        else:
+            self._start_gevent()
+
+    def pause(self):
+        self.status=self.Status.PAUSE
+
+    def stop(self):
+        self.status=self.Status.STOP
+
