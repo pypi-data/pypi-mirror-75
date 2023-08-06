5fa94669f392aff80a4feb201201335ffa2dec6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:54 +0000

    Add package aws-course-nbd/1.1/aws_course_nbd-1.1.tar.gz

---
+++ packages/aws-course-nbd/aws_course_nbd-1.1.tar.gz/aws_course_nbd-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='aws_course_nbd',
+      version='1.01',
+      description='Gaussian distributions',
+      packages=['aws_course_nbd'],
+      zip_safe=False)
