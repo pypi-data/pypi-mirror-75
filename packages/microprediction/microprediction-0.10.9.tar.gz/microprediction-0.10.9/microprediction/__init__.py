12f087bdb5227be2c4b57a904ef5f24d80d373c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:24 +0000

    Add package microprediction/0.10.9/microprediction-0.10.9.tar.gz

---
+++ packages/microprediction/microprediction-0.10.9.tar.gz/microprediction-0.10.9/microprediction/__init__.py
@@ -0,0 +1,9 @@
+from microprediction.conventions import MicroConventions, new_key
+from microprediction.reader import MicroReader
+from microprediction.writer import MicroWriter
+from microprediction.crawler import MicroCrawler
+from microprediction.simplecrawler import SimpleCrawler, RegularCrawler
+from microprediction.onlinecrawler import OnlineStreamCrawler, OnlineHorizonCrawler
+from microprediction.statefulcrawler import StreamCrawler
+from microprediction.supporter import donate
+from microprediction.looping import PandasLoop
