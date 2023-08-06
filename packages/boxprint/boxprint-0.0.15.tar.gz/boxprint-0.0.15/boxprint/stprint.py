e38da6655ff5fed9259b4c82535f9de8a0a7e580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:32 +0000

    Add package boxprint/0.0.15/boxprint-0.0.15.tar.gz

---
+++ packages/boxprint/boxprint-0.0.15.tar.gz/boxprint-0.0.15/boxprint/stprint.py
@@ -0,0 +1,20 @@
+import sys
+
+from .bprint import bprint
+from collections import OrderedDict as OD
+from colors import *
+
+
+def stprint(width=80):
+    frame = sys._getframe(1)
+    description = []
+    description.append("\n")
+    for local, value in OD(frame.f_locals).items():
+        description.append(f"  {str(local)} = {str(value)}")
+        description.append("\n")
+    description = "".join(description)
+    bprint(
+        description,
+        title=f"{frame.f_code.co_name} - {frame.f_code.co_filename}:{frame.f_lineno}",
+        stroke_func=lambda text: color(text, fg="red"),
+        width=width)
