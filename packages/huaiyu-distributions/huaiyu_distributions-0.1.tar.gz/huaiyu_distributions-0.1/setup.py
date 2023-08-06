cb1b32ad09e232c5133bff2622e8f8389fb97511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:33 +0000

    Add package huaiyu-distributions/0.1/huaiyu_distributions-0.1.tar.gz

---
+++ packages/huaiyu-distributions/huaiyu_distributions-0.1.tar.gz/huaiyu_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='huaiyu_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['huaiyu_distributions'],
+      zip_safe=False)
