d3cac845f4aab80921dbc636f7b5c6ec8301bb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:12 +0000

    Add package scripterminator/1.0.0/scripterminator-1.0.0.tar.gz

---
+++ packages/scripterminator/scripterminator-1.0.0.tar.gz/scripterminator-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='scripterminator',
+    version='1.0.0',
+    description='Useful tool to terminate any Python script just using file path and name',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Shahmin Rahman Rinkey',
+    author_email='shahminrinkey02@gmail.com',
+    keywords=['process', 'psutil', 'pid', 'terminate', 'system', 'utility', 'os'],
+    url='https://github.com/shahminrinkey/scripterminator',
+    download_url='https://pypi.org/project/scripterminator/'
+)
+
+install_requires = [
+    'time',
+    'psutil',
+    'subprocess',
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
