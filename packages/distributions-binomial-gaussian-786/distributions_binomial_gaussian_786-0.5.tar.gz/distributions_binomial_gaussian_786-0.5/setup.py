021e742e92740fb9f40fd5875d25e79dd248660c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:03 +0000

    Add package distributions-binomial-gaussian-786/0.5/distributions_binomial_gaussian_786-0.5.tar.gz

---
+++ packages/distributions-binomial-gaussian-786/distributions_binomial_gaussian_786-0.5.tar.gz/distributions_binomial_gaussian_786-0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_binomial_gaussian_786',
+      version='0.5',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_binomial_gaussian_786'],
+      author = 'A7MED',
+      author_email = 'waqasne@gmail.com',
+      zip_safe=False)
