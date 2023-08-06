dd5aed8fb64676426537eb41adab5454292a8ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:42 +0000

    Add package pandabase/0.5.0/pandabase-0.5.0.tar.gz

---
+++ packages/pandabase/pandabase-0.5.0.tar.gz/pandabase-0.5.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    version='0.5.0',
+    author='Sam Beck',
+    author_email='notsambeck@gmail.com',
+    name='pandabase',
+    packages=['pandabase'],
+    description="pandabase links pandas DataFrames to SQL databases. Upsert, append, read, drop, describe...",
+    long_description=long_description,
+    long_description_content_type = 'text/markdown',
+    url="https://github.com/notsambeck/pandabase",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    install_requires=[
+        'pandas>=0.24.0',
+        'sqlalchemy>=1.3.0',
+        ],
+    python_requires='>=3.6',
+)
