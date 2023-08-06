2bbfad5bbc385bfc1dc221fd7098c8265e4221c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:17 +0000

    Add package ayushabg-distributions/0.1/ayushabg_distributions-0.1.tar.gz

---
+++ packages/ayushabg-distributions/ayushabg_distributions-0.1.tar.gz/ayushabg_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ayushabg_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ayushabg_distributions'],
+      author= 'Ayush Gupta',
+      author_email = 'ayushabg@gmail.com',
+      zip_safe=False)
