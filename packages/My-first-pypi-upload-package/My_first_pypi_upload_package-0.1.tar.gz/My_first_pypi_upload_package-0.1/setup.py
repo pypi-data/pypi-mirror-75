11c2b5036b1e19e4e661421ffe8219395705fea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:22 +0000

    Add package My-first-pypi-upload-package/0.1/My_first_pypi_upload_package-0.1.tar.gz

---
+++ packages/My-first-pypi-upload-package/My_first_pypi_upload_package-0.1.tar.gz/My_first_pypi_upload_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='My_first_pypi_upload_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['My_first_pypi_upload_package'],
+      zip_safe=False)
