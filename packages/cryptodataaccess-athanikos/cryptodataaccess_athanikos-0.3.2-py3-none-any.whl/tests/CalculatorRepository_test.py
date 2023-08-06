0777ebba06b1bf56873d81c1a7eb9287c97b429d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:03 +0000

    Add package cryptodataaccess-athanikos/0.3.2/cryptodataaccess_athanikos-0.3.2-py3-none-any.whl

---
+++ packages/cryptodataaccess-athanikos/cryptodataaccess_athanikos-0.3.2-py3-none-any.whl/tests/CalculatorRepository_test.py
@@ -0,0 +1,25 @@
+import mock
+from cryptomodel.coinmarket import  prices
+from mongoengine import  Q
+from cryptodataaccess.config import configure_app
+import pytest
+from cryptodataaccess.helpers import do_connect
+from tests.helpers import insert_prices_record
+
+
+@pytest.fixture(scope='module')
+def mock_log():
+    with mock.patch("cryptodataaccess.helpers.log_error"
+                    ) as _mock:
+        _mock.return_value = True
+        yield _mock
+
+
+def test_eval_collection():
+    config = configure_app()
+    do_connect(config)
+    actual_count = prices.objects()
+    j = "prices"
+    assert(len(eval(j).objects()) == len(actual_count))
+
+
