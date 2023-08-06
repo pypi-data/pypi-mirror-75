9a40da27d228503b30c521d662ecf5839225af46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:25 +0000

    Add package HTBao/1.0/HTBao-1.0.tar.gz

---
+++ packages/HTBao/HTBao-1.0.tar.gz/HTBao-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='HTBao',  # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='这是第一个对外发布的模块，测试哦',  #描述
+    author='hlf', # 作者
+    author_email='1548532643@qq.com',
+    py_modules=['baizhanMath2.demo1','baizhanMath2.demo2'] # 要发布的模块
+)
