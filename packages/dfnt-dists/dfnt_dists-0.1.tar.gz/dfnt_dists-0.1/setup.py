41254a27fe0836538057b4840499ebfe9185cfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:24 +0000

    Add package dfnt-dists/0.1/dfnt_dists-0.1.tar.gz

---
+++ packages/dfnt-dists/dfnt_dists-0.1.tar.gz/dfnt_dists-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dfnt_dists',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dfnt_dists'],
+      zip_safe=False)
