1fa4e71ed109df9d4eaeb9e988dced1bc23e4cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:31 +0000

    Add package prodist/0.0.1/prodist-0.0.1.tar.gz

---
+++ packages/prodist/prodist-0.0.1.tar.gz/prodist-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prodist',
+      version='0.0.1',
+      description='probability distributions',
+      packages=['prodist'],
+      author = 'Sagar Tansar',
+      author_email = 'myemail@something.com',
+      zip_safe=False)
