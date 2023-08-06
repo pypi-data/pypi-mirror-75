4b0239dfb4205b68ae9d2344d3bd040c865f778a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:22 +0000

    Add package subwaydetec/0.0.3/subwaydetec-0.0.3-py3-none-any.whl

---
+++ packages/subwaydetec/subwaydetec-0.0.3-py3-none-any.whl/subway_detector/__init__.py
@@ -0,0 +1,3 @@
+from detec import *
+from class_2 import *
+from class_n import *
---
+++ packages/subwaydetec/subwaydetec-0.0.3-py3-none-any.whl/subway_detector/class_2/__init__.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+import math
+import torch.utils.model_zoo as model_zoo
+from config import Config
+import heapq
+from PIL import Image
+
+
+__all__ = ['ResNet', 'resnet34', 'resnet50', 'resnet101', 'resnet152','model_2']
