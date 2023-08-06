1850fc699a965e191287ff3b0327970dbf9d981d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package issh/1.4.0/issh-1.4.0.tar.gz

---
+++ packages/issh/issh-1.4.0.tar.gz/issh-1.4.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from sys import platform
+
+requirements = []
+if platform == "win32":
+    requirements.append('windows-curses')
+
+setup(
+    name='issh',
+    version='1.4.0',
+    description='Improved SSH: TUI menu for connecting to SSH config hosts',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    url='https://github.com/DevDungeon/issh',
+    author='DevDungeon',
+    author_email='nanodano@devdungeon.com',
+    py_modules=['issh'],
+    entry_points={
+        'console_scripts': [
+            'issh = issh:main',
+        ],
+    }
+    ,
+    zip_safe=False,
+    install_requires=[
+        requirements,
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
