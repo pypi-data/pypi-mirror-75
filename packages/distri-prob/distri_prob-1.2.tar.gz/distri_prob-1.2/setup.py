aa6ec39310ba09f037409bba2c024060d0fc3426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:21 +0000

    Add package distri-prob/1.2/distri_prob-1.2.tar.gz

---
+++ packages/distri-prob/distri_prob-1.2.tar.gz/distri_prob-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distri_prob',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      author = 'Naruto Uzumaki',
+      author_email = 'narutouzumaki@konoha.com',
+      packages=['distri_prob'],
+      zip_safe=False)
