2a92251ae7a8a7f3c26b23690f52f1fac5873da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:03 +0000

    Add package bgbg-prob/0.1/bgbg_prob-0.1.tar.gz

---
+++ packages/bgbg-prob/bgbg_prob-0.1.tar.gz/bgbg_prob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bgbg_prob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bgbg_prob'],
+      zip_safe=False)
