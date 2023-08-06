af036c3f5f80134a7c13cbd57d0e1a43e21b5986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:18 +0000

    Add package timecast-nightly/0.4.0.dev20200802173114/timecast_nightly-0.4.0.dev20200802173114-py3-none-any.whl

---
+++ packages/timecast-nightly/timecast_nightly-0.4.0.dev20200802173114-py3-none-any.whl/timecast/modules/_predict_last.py
@@ -0,0 +1,32 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""timecast/modules/_predict_last.py"""
+import jax.numpy as jnp
+
+from timecast.modules.core import Module
+
+
+class PredictLast(Module):
+    """Predict last value"""
+
+    def __init__(self, steps=1):
+        """init"""
+        self.steps = steps
+        self.history = jnp.zeros(steps)
+
+    def __call__(self, x):
+        """call"""
+        self.history = jnp.roll(self.history, shift=1)
+        self.history = self.history.at[0].set(x)
+        return self.history[self.steps - 1]
