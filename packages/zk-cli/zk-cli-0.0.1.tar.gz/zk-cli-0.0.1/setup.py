272ca1c057cddbfe5281a9fd1d8b8d1f861b3011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package zk-cli/0.0.1/zk-cli-0.0.1.tar.gz

---
+++ packages/zk-cli/zk-cli-0.0.1.tar.gz/zk-cli-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zk-cli",
+    version="0.0.1",
+    author="Tom Firth",
+    description="A command lind utility for Zettelkasten",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tdfirth/zk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["zk=zk.zk:main"]},
+    extras_require={
+        "dev": ["nose"]
+    }
+)
