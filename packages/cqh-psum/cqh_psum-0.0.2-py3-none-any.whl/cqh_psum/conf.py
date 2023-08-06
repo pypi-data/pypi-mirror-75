371b3c1ea8889f173466b3a9ee20511b692bc5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package cqh-psum/0.0.2/cqh_psum-0.0.2-py3-none-any.whl

---
+++ packages/cqh-psum/cqh_psum-0.0.2-py3-none-any.whl/cqh_psum/conf.py
@@ -0,0 +1,22 @@
+doc = """
+                      cqh_psum
+========
+
+
+Usage
+-----
+
+Example
+---------------------------
+*  统计一个进程
+
+ ``cqh_psum --name=python``
+
+* 统计多个进程
+
+  ``cqh_psum --name=python,coder,nginx``  
+
+
+
+
+                      """
