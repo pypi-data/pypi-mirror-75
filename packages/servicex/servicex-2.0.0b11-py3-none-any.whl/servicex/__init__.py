92284c781b20f2b99439914bd4a40b42ef10d9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:05 +0000

    Add package servicex/2.0.0b11/servicex-2.0.0b11-py3-none-any.whl

---
+++ packages/servicex/servicex-2.0.0b11-py3-none-any.whl/servicex/__init__.py
@@ -0,0 +1,12 @@
+from .servicex import ServiceXDataset  # NOQA
+from .utils import (  # NOQA
+    ServiceXException,
+    ServiceXUnknownRequestID,
+    ServiceXFailedFileTransform,
+    StatusUpdateCallback,
+    StatusUpdateFactory,
+    clean_linq,
+)
+from .servicex_adaptor import ServiceXAdaptor  # NOQA
+from .minio_adaptor import MinioAdaptor  # NOQA
+from .cache import Cache  # NOQA
