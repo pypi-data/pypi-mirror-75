e8ee5a13fd24af4eaaf4a30abf129824a20aedeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:45 +0000

    Add package lib-programname/1.1.5/lib_programname-1.1.5.tar.gz

---
+++ packages/lib-programname/lib_programname-1.1.5.tar.gz/lib_programname-1.1.5/CHANGES.rst
@@ -0,0 +1,13 @@
+Changelog
+=========
+
+- new MAJOR version for incompatible API changes,
+- new MINOR version for added functionality in a backwards compatible manner
+- new PATCH version for backwards compatible bug fixes
+
+tasks:
+    - python 3.9 changes, __main__ should be now absolut path - check it
+
+
+1.1.5
+2020-07-31: initial release
---
+++ packages/lib-programname/lib_programname-1.1.5.tar.gz/lib_programname-1.1.5/lib_programname/__init__conf__.py
@@ -0,0 +1,20 @@
+name = 'lib_programname'
+title = 'get reliably the name of the executed script'
+version = '1.1.5'
+url = 'https://github.com/bitranox/lib_programname'
+author = 'Robert Nowotny'
+author_email = 'bitranox@gmail.com'
+shell_command = 'lib_programname'
+
+
+def print_info() -> None:
+    print("""\
+
+Info for lib_programname:
+
+    get reliably the name of the executed script
+
+    Version : 1.1.5
+    Url     : https://github.com/bitranox/lib_programname
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com""")
