d85648a07431f909a074edc6de30592494f07ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:31 +0000

    Add package abstractalgorithms/0.1/abstractalgorithms-0.1.tar.gz

---
+++ packages/abstractalgorithms/abstractalgorithms-0.1.tar.gz/abstractalgorithms-0.1/README.md
@@ -0,0 +1,2 @@
+# abstractalgorithms
+Reference implementations of useful algorithms, presented in an abstracted form
---
+++ packages/abstractalgorithms/abstractalgorithms-0.1.tar.gz/abstractalgorithms-0.1/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup    
+setup(name='abstractalgorithms',
+      version='0.1',
+      description='Reference implementations of useful algos',
+      author='madgen',
+      author_email='madgencontent@gmail.com',
+      url='https://github.com/madgen-content/abstractalgorithms',
+      packages=['abstractalgorithms']
+     )
