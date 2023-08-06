67461b43625429cfb07d4052c6ccd8d265fafe57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:56 +0000

    Add package zeptosoft/0.0/zeptosoft-0.0.tar.gz

---
+++ packages/zeptosoft/zeptosoft-0.0.tar.gz/zeptosoft-0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from distutils.core import setup
+import os
+setup(name='zeptosoft',
+      version='0.0',
+      description='creating our first package',
+      packages=['zeptosoft'],
+      author="D.Achuth",
+      py_modules=["greenviz"],
+      author_email='achuthaiml@gmail.com',
+      data_files=[("",["C:/Users/Anil Kumar/PycharmProject/loginpage/resized one.jpg"])],
+      install_requires=["pillow","matplotlib","tkinter"]
+      )
