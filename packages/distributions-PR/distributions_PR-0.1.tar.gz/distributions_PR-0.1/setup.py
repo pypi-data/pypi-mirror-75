0bf8d34d16e9fb016c58aef65aef670cfc4550a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:46 +0000

    Add package distributions-PR/0.1/distributions_PR-0.1.tar.gz

---
+++ packages/distributions-PR/distributions_PR-0.1.tar.gz/distributions_PR-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_PR',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_PR'],
+      author=['Pouya'],
+      zip_safe=False)
