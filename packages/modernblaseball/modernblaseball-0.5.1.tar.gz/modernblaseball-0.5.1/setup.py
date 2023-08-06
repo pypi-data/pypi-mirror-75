754954e23ba715183772d64690b0c3fc7d9a027a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:39 +0000

    Add package modernblaseball/0.5.1/modernblaseball-0.5.1.tar.gz

---
+++ packages/modernblaseball/modernblaseball-0.5.1.tar.gz/modernblaseball-0.5.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="modernblaseball",
+    version="0.5.1",
+    author="Owen Sanders",
+    author_email="helloimowens@gmail.com",
+    description="An interface for the blaseball API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/helloimowen/modern-blaseball",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
