2e6e1e6bc56a188c7986a3831e761834a7d94db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:12 +0000

    Add package BMPM/2.0.0/BMPM-2.0.0.tar.gz

---
+++ packages/BMPM/BMPM-2.0.0.tar.gz/BMPM-2.0.0/bmpm/__main__.py
@@ -0,0 +1,53 @@
+import argparse
+import oead
+import os
+from bmpm import functions
+
+def main():
+#   Cli Arguments Setup
+    progDesc = "A program for bulk replacing and deleting parameters and actors in BotW BYML map files."
+
+    parser = argparse.ArgumentParser(description=progDesc)
+    subParser = parser.add_subparsers(description='temp', dest='subParserType')
+    parser.add_argument('fileIn', type=str, help="File to open and read data from.")
+    parser.add_argument('--noCompression', '-nc', action='store_true', help='Add this if you would prefer the program to not compress the output file with yaz0.')
+
+    editParser = subParser.add_parser('edit', help='edit')
+    editParser.add_argument('termToSearch', type=str, help='Term to look through the file and find.')
+    editParser.add_argument('replacementTerm', help='Value to replace the original termToSearch value with.')
+    editParser.add_argument('--type', '-t', metavar='value', dest='type', choices=['0', '1'], help='A value of 0 meaning an integer or 1 meaning a string to set the replacement term as. (defaults to zero)', required=False, default=0)
+
+    remParser = subParser.add_parser('delete', help='delete')
+    remParser.add_argument('ActorToDelete', help='The actor name or HashID you would like removed from the map file.')
+    remParser.add_argument('--type', '-t', metavar='value', dest='type', choices=['0', '1', 'hash', 'name'], help='Type of string that was inputted for actor removal(0 maps to hash and 1 to actor name, will default to actor name.)', required=False, default=1)
+    args = parser.parse_args()
+
+#   Global Variables
+    fileName = str(args.fileIn).split('.')[0]
+    fileExt = ('.' + str(args.fileIn).split('.')[-1])
+    fileToOpen = args.fileIn
+    openFile = open(fileToOpen, 'rb')
+
+
+    if (args.subParserType == 'edit'):
+        termToSearch = args.termToSearch
+        replaceTerm = args.replacementTerm
+
+        replacementParamType = None
+        if (int(args.type) == 0):
+            replacementParamType = oead.S32(value=int(replaceTerm))
+        elif (int(args.type) == 1):
+            replacementParamType = str(replaceTerm)
+
+        functions.replaceParam(fileToOpen, fileName, fileExt, termToSearch, replacementParamType, args)
+
+    elif (args.subParserType == 'delete'):
+        actorToDel = args.ActorToDelete
+        nameHash = args.type
+        functions.removeActor(fileToOpen, fileName, fileExt, actorToDel, nameHash, args)
+
+    else:
+        print(f'The option {str(args.subParserType)} could not be found.')
+
+if __name__ == "__main__":
+    main()
---
+++ packages/BMPM/BMPM-2.0.0.tar.gz/BMPM-2.0.0/bmpm/functions.py
@@ -0,0 +1,134 @@
+import oead
+import os
+
+# A function for checking if a file is yaz0 compressed and then determining whether or not to decompress it based off of that
+def checkCompression(fileCheck):
+    fileInRead = fileCheck
+    if (oead.yaz0.get_header(fileInRead) is not None):
+        print("File is Yaz0 compressed, decompressing")
+        uncompressedFile = oead.yaz0.decompress(fileInRead)
+    else:
+        print('File is not compressed with Yaz0')
+        uncompressedFile = fileInRead
+    return(uncompressedFile)
+
+# Function for replacing inputted parameter with other user input
+def replaceParam(fileToOpen, fileName, fileExt, termToSearch, replacementParamType, args):
+    fileDict = {}
+    entryDict = {}
+    paramDict = {}
+    iterate = 0
+    objList = []
+    fileToOpen = open(fileToOpen, 'rb').read()
+    uncompressedFile = checkCompression(fileToOpen)
+    extractByml = oead.byml.from_binary(uncompressedFile)
+    for key in extractByml.keys():
+        fileDict.update({key: extractByml.get(key)})
+    array = fileDict.get('Objs')
+
+    for entry in array:
+        exactItem = array[iterate]
+        entryDict.update(exactItem)
+        iterate += 1
+
+        for key in entryDict.keys():
+            if (key.lower() == termToSearch.lower()):
+                entryDict.update({key: replacementParamType})
+
+        if (entryDict.get('!Parameters') is not None):
+#                print('Found "!Parameters" value in entry from file')
+            paramDict.update(entryDict.get('!Parameters'))
+            for key in paramDict.keys():
+#                    print('Checking if param is the same as user input to be replaced')
+                if (key.lower() == termToSearch.lower()):
+                    paramDict.update({key: replacementParamType})
+                    entryDict.update({'!Parameters': paramDict})
+#                    print('Successfully replaced parameter')
+        
+#        print(entryDict)
+        objList.append(oead.byml.Hash(entryDict))
+        paramDict.clear()
+        entryDict.clear()
+
+    fileDict.update({'Objs': objList})
+    if (args.noCompression):
+            extList = []
+            fileExt = fileExt.lstrip('.s')
+            fileExt = ('.') + fileExt
+            fileWrite = open(fileName + fileExt, 'wb')
+            fileWrite.write(oead.byml.to_binary(fileDict, False))
+
+    else:
+        fileWrite = open(fileName + fileExt, 'wb')
+        fileWrite.write(oead.yaz0.compress(oead.byml.to_binary(fileDict, False)))
+        print("Compressing file.")
+
+    fileWrite.close()
+    print('Done!')
+
+def removeActor(fileToOpen, fileName, fileExt, actorToDel, nameHash, args):
+    fileDict = {}
+    entryDict = {}
+    paramDict = {}
+    iterate = 0
+    objList = []
+    deleted = False
+    fileToOpen = open(fileToOpen, 'rb').read()
+    uncompressedFile = checkCompression(fileToOpen)
+    extractByml = oead.byml.from_binary(uncompressedFile)
+    for key in extractByml.keys():
+        fileDict.update({key: extractByml.get(key)})
+    array = fileDict.get('Objs')
+
+    if (int(nameHash) == 0 or str(nameHash).lower() == 'hash'):
+        actorToDel = int(actorToDel)
+        actorToDel = oead.U32(value=actorToDel)
+    elif (int(nameHash) == 1 or str(nameHash).lower() == 'name'):
+        actorToDel = str(actorToDel)
+
+    for entry in array:
+        exactItem = array[iterate]
+        entryDict.update(exactItem)
+        iterate += 1
+
+        for key in entryDict.keys():
+            if (str(entryDict.get(key)).lower() == str(actorToDel).lower()):
+                deleted = True
+
+        if (entryDict.get('!Parameters') is not None):
+#                print('Found "!Parameters" value in entry from file')
+            paramDict.update(entryDict.get('!Parameters'))
+            for key in paramDict.keys():
+#                    print('Checking if param is the same as user input to be replaced')
+                if (str(paramDict.get(key)).lower() == str(actorToDel).lower()):
+                    deleted = True
+        
+#        print(entryDict)
+        if (deleted != True):
+            objList.append(oead.byml.Hash(entryDict))
+        elif (deleted == True):
+            paramDict.clear()
+            entryDict.clear()
+            deleted = False
+            continue
+        paramDict.clear()
+        entryDict.clear()
+        deleted = False
+
+    fileDict.update({'Objs': objList})
+    if (args.noCompression):
+            extList = []
+            fileExt = fileExt.lstrip('.s')
+            fileExt = ('.') + fileExt
+            fileWrite = open(fileName + fileExt, 'wb')
+            fileWrite.write(oead.byml.to_binary(fileDict, False))
+
+    else:
+        fileWrite = open(fileName + fileExt, 'wb')
+        fileWrite.write(oead.yaz0.compress(oead.byml.to_binary(fileDict, False)))
+        print("Compressing file.")
+    fileWrite.close()
+    print('Done!')
+
+
+
