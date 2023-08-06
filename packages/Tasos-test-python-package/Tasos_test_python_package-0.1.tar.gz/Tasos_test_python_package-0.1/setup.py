02116392e437c6d39f24b1c425a096d6c5c9d4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:26 +0000

    Add package Tasos-test-python-package/0.1/Tasos_test_python_package-0.1.tar.gz

---
+++ packages/Tasos-test-python-package/Tasos_test_python_package-0.1.tar.gz/Tasos_test_python_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Tasos_test_python_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Tasos_test_python_package'],
+      zip_safe=False)
