88f5029d5ce4e33a9c637902fb7e01d19ed772be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:37 +0000

    Add package mcfly/3.1.0/mcfly-3.1.0.tar.gz

---
+++ packages/mcfly/mcfly-3.1.0.tar.gz/mcfly-3.1.0/mcfly/models/__init__.py
@@ -0,0 +1,12 @@
+from .cnn import CNN
+from .deep_conv_lstm import DeepConvLSTM
+from .inception_time import InceptionTime
+from .resnet import ResNet
+
+
+__all__ = [
+    "CNN",
+    "DeepConvLSTM",
+    "InceptionTime",
+    "ResNet"
+]
