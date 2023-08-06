b9eb2c3dfac918b33f5413016e76ac9626d2bcef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:07 +0000

    Add package dsnd-probability-exercise/0.1/dsnd_probability_exercise-0.1.tar.gz

---
+++ packages/dsnd-probability-exercise/dsnd_probability_exercise-0.1.tar.gz/dsnd_probability_exercise-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_exercise',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_exercise'],
+      author = 'Jennifer Hui',
+      author_email = 'jenniferm.hui@gmail.com',
+      zip_safe=False)
