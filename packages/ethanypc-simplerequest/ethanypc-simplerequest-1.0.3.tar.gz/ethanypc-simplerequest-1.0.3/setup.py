dd56d374361a61b9dfacf0f0c604ef37cf98a842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:46 +0000

    Add package ethanypc-simplerequest/1.0.3/ethanypc-simplerequest-1.0.3.tar.gz

---
+++ packages/ethanypc-simplerequest/ethanypc-simplerequest-1.0.3.tar.gz/ethanypc-simplerequest-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", 'r') as fid:
+    README = fid.read()
+
+setup(
+    name="ethanypc-simplerequest",
+    version="1.0.3",
+    description="Wrapper around http.client",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EthanChenYen-Peng/simplerequest",
+    author="Ethan",
+    author_email="ypcethan@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=["simplehttp"],
+)
