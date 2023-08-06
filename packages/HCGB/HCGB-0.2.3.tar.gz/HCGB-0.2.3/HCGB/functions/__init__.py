3333c81e87497c17685dcea41d26fa80ca74cfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:04 +0000

    Add package HCGB/0.2.3/HCGB-0.2.3.tar.gz

---
+++ packages/HCGB/HCGB-0.2.3.tar.gz/HCGB-0.2.3/HCGB/functions/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'aesthetics_functions',
+    'blast_functions',
+    'fasta_functions',
+    'files_functions',
+    'main_functions',
+    'system_call_functions',
+    'time_functions'
+]
+
+from HCGB.functions import *
+
