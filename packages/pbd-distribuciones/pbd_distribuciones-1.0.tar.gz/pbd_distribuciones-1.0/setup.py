564ab84aa086b28d1baceb55ff0ceaa49b25b06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:33 +0000

    Add package pbd-distribuciones/1.0/pbd_distribuciones-1.0.tar.gz

---
+++ packages/pbd-distribuciones/pbd_distribuciones-1.0.tar.gz/pbd_distribuciones-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pbd_distribuciones',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['pbd_distribuciones'],
+      author = 'Alejandrina Jimenez Guzman',
+      zip_safe=False)
