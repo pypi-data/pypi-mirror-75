55adb0e054e093a4e0ab86a94f50f255a44dab46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:17 +0000

    Add package disuniv/0.1/disuniv-0.1.tar.gz

---
+++ packages/disuniv/disuniv-0.1.tar.gz/disuniv-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='disuniv',
+      version='0.1',
+      description='Gaussian distributions and Binomial Distributions',
+      packages=['disuniv'],
+      author='Rushikesh Dixit',
+      author_email='r29dixit@gmail.com',
+      zip_safe=False)
