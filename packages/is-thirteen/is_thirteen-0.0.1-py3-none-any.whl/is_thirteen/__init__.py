aa7fedf2b46257fdc4749c5231c564003e594ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:12 +0000

    Add package is-thirteen/0.0.1/is_thirteen-0.0.1-py3-none-any.whl

---
+++ packages/is-thirteen/is_thirteen-0.0.1-py3-none-any.whl/is_thirteen/__init__.py
@@ -0,0 +1,1 @@
+from is_thirteen.is_thirteen import *
---
+++ packages/is-thirteen/is_thirteen-0.0.1-py3-none-any.whl/is_thirteen/is_thirteen.py
@@ -0,0 +1,52 @@
+from math import ceil
+
+
+class Number:
+    def __init__(self, number):
+        self.number = number
+
+    def thirteen(self):
+        return self.number == 13
+
+    def roughly(self):
+        self.number = ceil(self.number)
+        return self
+
+    def within(self, _range: int):
+        if self.number + _range > 13 or self.number - _range < 13:
+            self.number = 13
+            return self
+
+    def plus(self, item: int):
+        self.number += item
+        return self
+
+    def minus(self, item: int):
+        self.number -= item
+        return self
+
+    def times(self, item: int):
+        self.number *= item
+        return self
+
+    def divides(self, item: int):
+        try:
+            self.number /= item
+        except ZeroDivisionError as e:
+            print(e)
+        return self
+
+
+class Str:
+    def __init__(self, string: str):
+        self.string = string
+
+    def thirteen(self):
+        return self.string.lower() == "thirteen"
+
+    def backwards(self):
+        temp = list(self.string)
+        self.string = ''.join(temp.pop() for _ in range(len(self.string)))
+        del temp
+        return self
+
