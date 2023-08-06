ddf89c4ed53137f96110c18a2d4599d18e159579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:53 +0000

    Add package dist-ex5/1.0/dist_ex5-1.0.tar.gz

---
+++ packages/dist-ex5/dist_ex5-1.0.tar.gz/dist_ex5-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_ex5',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_ex5'],
+      author = 'Amr Gharib',
+      author_email = 'amriagharib@gmail.com',
+      zip_safe=False)
