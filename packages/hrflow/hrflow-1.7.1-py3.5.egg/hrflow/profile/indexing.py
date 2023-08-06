db0d00fb75f44921413c0fd051c0975f1de1da43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:00 +0000

    Add package hrflow/1.7.1/hrflow-1.7.1-py3.5.egg

---
+++ packages/hrflow/hrflow-1.7.1-py3.5.egg/hrflow/profile/indexing.py
@@ -0,0 +1,43 @@
+import json
+
+from hrflow.utils import get_profile, get_item
+
+
+class ProfileIndexing():
+    """Manage embedding related profile calls."""
+
+    def __init__(self, api):
+        """Init."""
+        self.client = api
+
+    def add(self, profile_json):
+        """Use the api to add a new profile using profile_data."""
+        response = self.client.post("profile/indexing", json=profile_json)
+        return response.json()
+
+    def edit(self, profile_json):
+        """Use the api to add a new profile using profile_data."""
+        response = self.client.put("profile/indexing", json=profile_json)
+        return response.json()
+
+    def get(self, source_key, key, reference=None, email=None):
+        """
+        Retrieve the interpretability information.
+
+        Args:
+            source_key:             <string>
+                                    source_key
+            key:                    <string>
+                                    key
+            reference:              <string>
+                                    profile_reference
+            email:                  <string>
+                                    profile_email
+
+        Returns
+            Get information
+
+        """
+        query_params = get_item('profile', source_key, key, reference, email)
+        response = self.client.get('profile/indexing', query_params)
+        return response.json()
