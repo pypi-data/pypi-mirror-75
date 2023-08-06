64298df8c1e617b9d3835688c443766ee78e4fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:52 +0000

    Add package ioppytest-agent/0.2.0/ioppytest-agent-0.2.0.tar.gz

---
+++ packages/ioppytest-agent/ioppytest-agent-0.2.0.tar.gz/ioppytest-agent-0.2.0/setup.py
@@ -0,0 +1,54 @@
+# Authors:
+#     Federico Sismondi
+#     Remy Leone
+
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+from agent import VERSION
+
+name = 'ioppytest-agent'
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Internet',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Scientific/Engineering',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS'
+]
+
+with open('version.py', 'w') as f:
+    f.write('__version__ = "{version}"\n'.format(version=VERSION))
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name=name,
+    author='Federico Sismondi',
+    author_email='federicosismondi@gmail.com',
+    maintainer='Federico Sismondi',
+    maintainer_email='federicosismondi@gmail.com',
+    url='https://github.com/fsismondi/ioppytest-agent',
+    description='Component for creating VPN client environments',
+    version=VERSION,
+    license='GPLv3+',
+    classifiers=CLASSIFIERS,
+    packages=find_packages(exclude=['tests']),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'click',
+        'six',
+        'kombu',
+        'pika',
+        'pyserial',
+    ],
+    entry_points={'console_scripts': ['ioppytest-agent=agent.agent_cli:main']},
+)
