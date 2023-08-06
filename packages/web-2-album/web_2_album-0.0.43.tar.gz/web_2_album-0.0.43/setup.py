cee66f3d12af20bd7a40c4beb1d84e1d669697ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:53 +0000

    Add package web-2-album/0.0.43/web_2_album-0.0.43.tar.gz

---
+++ packages/web-2-album/web_2_album-0.0.43.tar.gz/web_2_album-0.0.43/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="web_2_album",
+    version="0.0.43",
+    author="Yunzhi Gao",
+    author_email="gaoyunzhi@gmail.com",
+    description="Return photo list and caption (markdown format) from web.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaoyunzhi/web_2_album",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'cached_url',
+        'bs4',
+        'telegram_util',
+        'pic_cut',
+        'export_to_telegraph',
+        'readee',
+        'pyyaml',
+    ],
+    python_requires='>=3.0',
+)
