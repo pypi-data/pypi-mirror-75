78a7131248ddbb6d4b91127927e90f164f14a2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:48 +0000

    Add package minot/0.1.0/minot-0.1.0-py2.py3-none-any.whl

---
+++ packages/minot/minot-0.1.0-py2.py3-none-any.whl/minot/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.1.0'
+
+try:
+    from .model import Cluster
+except ImportError:
+    print('WARNING: Could not import Cluster from model. You')
+    print('         may try (re)installing dependencies by  ')
+    print('         hand. For example running:              ')
+    print('             $ conda install matplotlib          ')
+    print('             $ conda install numpy               ')
+    print('             $ conda install scipy               ')
+    print('             $ conda install astropy             ')
