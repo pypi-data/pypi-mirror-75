96ada59c9c84c29bf931ac61d02d7b3726613d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:02 +0000

    Add package stock-rider/0.2.20200804/stock-rider-0.2.20200804.tar.gz

---
+++ packages/stock-rider/stock-rider-0.2.20200804.tar.gz/stock-rider-0.2.20200804/krider/utils/report_generator.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from urllib import parse
+
+from pandas import DataFrame
+
+
+class ReportGenerator:
+    def prepare_output(self, ticker, df: DataFrame):
+        session_dt = datetime.strptime(df["Datetime"], "%Y-%m-%d %H:%M:%S.%f").date()
+        session_volume = float(df["Volume"])
+        mean_volume = float("{:.0f}".format(df["MeanVolume"]))
+        ticker_exchange = df["Exchange"]
+        ticker_exchange_symbol = parse.quote_plus(
+            "{}:{}".format(ticker_exchange, ticker)
+        )
+        md_post = f"""
+## {ticker}
+
+**Date:** {session_dt}
+
+**Volume:** {session_volume:,.0f}
+
+**Mean Volume:** {mean_volume:,.0f}
+
+[Trading View](https://www.tradingview.com/chart/?symbol={ticker_exchange_symbol}) | 
+[Robinhood](https://robinhood.com/stocks/{ticker})
+
+---
+
+        """
+        return md_post
+
+
+report_generator = ReportGenerator()
