e6c5df077f4e38610bc06d0e05f01f5a4aa67755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:26 +0000

    Add package python-cuisineaz/1.0/python-cuisineaz-1.0.tar.gz

---
+++ packages/python-cuisineaz/python-cuisineaz-1.0.tar.gz/python-cuisineaz-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='python-cuisineaz',
+	version='1.0',
+	description='api to scrape cuisineaz with python.',
+	packages=['python-cuisineaz'],
+	url='https://github.com/starnakin/python-CuisineAZ',
+	author="Starnakin",
+	author_email='starnakin2@gmail.com',
+	license='MIT',
+	zip_safe=False,
+	install_requires=['bs4'],
+	)
