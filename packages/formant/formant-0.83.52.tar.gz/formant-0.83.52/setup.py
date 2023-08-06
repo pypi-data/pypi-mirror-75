0535bdde86d2f8b5290f7106f5e82da90be09554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:29 +0000

    Add package formant/0.83.52/formant-0.83.52.tar.gz

---
+++ packages/formant/formant-0.83.52.tar.gz/formant-0.83.52/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="formant",
+    version="0.83.52",
+    author="Formant",
+    author_email="eng@formant.io",
+    description="Formant python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://formant.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["grpcio==1.29.0", "protobuf==3.11.3"],
+    python_requires='>=2.7',
+)
