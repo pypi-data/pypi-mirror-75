33225f2ab953b800000b1a97006f14a3c8dd75aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:45 +0000

    Add package abdalla-dsnd-distributions/0.1/abdalla_dsnd_distributions-0.1.tar.gz

---
+++ packages/abdalla-dsnd-distributions/abdalla_dsnd_distributions-0.1.tar.gz/abdalla_dsnd_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='abdalla_dsnd_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['abdalla_dsnd_distributions'],
+      zip_safe=False)
