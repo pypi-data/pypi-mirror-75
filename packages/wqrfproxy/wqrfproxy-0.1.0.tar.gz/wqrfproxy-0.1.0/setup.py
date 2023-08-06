5255b094ed0cd3409b4160f38a2f68a575a0cc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:56 +0000

    Add package wqrfproxy/0.1.0/wqrfproxy-0.1.0.tar.gz

---
+++ packages/wqrfproxy/wqrfproxy-0.1.0.tar.gz/wqrfproxy-0.1.0/setup.py
@@ -0,0 +1,21 @@
+#############################################
+# File Name: setup.py
+# Author: wqrf
+# Mail: 1074321997@qq.com
+# Created Time:  2019-10-08 14:39:00
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "wqrfproxy",
+    version = "0.1.0",
+    keywords = ("wqrf"),
+    description = "catch the http/api from device(android/ios) for assert",
+    long_description = "If you wangt monitor the api from deivce,please use this.And you can get help in https://github.com/Woqurefan/wqrfproxy",
+    license = "MIT",
+    url = "https://github.com/Woqurefan/wqrfproxy",
+    author = "wqrf",
+    author_email = "1074321997@qq.com",
+    packages = ['wqrfproxy'],
+)
---
+++ packages/wqrfproxy/wqrfproxy-0.1.0.tar.gz/wqrfproxy-0.1.0/wqrfproxy/wqrfproxy.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def ppp():
+    print('hahaha')
