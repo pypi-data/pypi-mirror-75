345d9d2dc30b85ef8b6ca683065c6d3aa0895ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:56 +0000

    Add package caispp/0.4.2/caispp-0.4.2.tar.gz

---
+++ packages/caispp/caispp-0.4.2.tar.gz/caispp-0.4.2/caispp/__init__.py
@@ -0,0 +1,5 @@
+# allows for caispp.ImageDataset instead of caispp.ImageClassifier.ImageDataset
+name = "caispp"
+
+from pathlib import Path
+from .ImageClassifier import ImageClassifier, ImageDataset 
---
+++ packages/caispp/caispp-0.4.2.tar.gz/caispp-0.4.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="caispp",
+    version="0.4.2",
+    author="Zane Durante",
+    author_email="zanedurante@gmail.com",
+    description="High level ML library used in CAIS++ Curriculum",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zanedurante/caispp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'numpy==1.16.4',
+          'tensorflow>=2.0', 
+          'Pillow', 
+          'matplotlib',
+          'sklearn',
+          'pandas',
+          'gast>=0.2.2',
+      ],
+)
