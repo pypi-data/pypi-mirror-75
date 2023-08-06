6baf2cb97cc9b4680f94f03de08b15df4d992e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:48 +0000

    Add package PyHand-Earth/0.2.23/PyHand_Earth-0.2.23-py3-none-any.whl

---
+++ packages/PyHand-Earth/PyHand_Earth-0.2.23-py3-none-any.whl/PyHand_Earth/command_thread.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import QThread, pyqtSlot
+
+class CommandThread(QThread):
+
+    def __init__(self, capture, commands):
+        super(CommandThread, self).__init__()
+        self.capture = capture
+        self.commands = commands
+        self.issue_commands = True
+
+    @pyqtSlot()
+    def run(self):
+
+        # While stop command false, get commands from hand_recognition
+        # and send commands to Google Earth window
+        while (not self.isInterruptionRequested()):
+            current_input = self.capture.get_output()
+
+            self.commands.end_command()
+            self.commands.set_command(current_input)
+            self.commands.send_command()
+
+    def end_thread(self):
+        self.requestInterruption()
+        self.wait()
+        self.commands.send_single_command("space")
+        del self
