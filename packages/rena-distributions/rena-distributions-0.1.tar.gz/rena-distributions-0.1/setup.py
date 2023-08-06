a00a59c91bf9f8996e47b482519c0e6c7d3545ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:02 +0000

    Add package rena-distributions/0.1/rena-distributions-0.1.tar.gz

---
+++ packages/rena-distributions/rena-distributions-0.1.tar.gz/rena-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rena-distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['rena-distributions'],
+      zip_safe=False)
