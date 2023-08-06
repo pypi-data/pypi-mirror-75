746bdde4d204a6a266a646d3c1417d662cd5f170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:36 +0000

    Add package ecom-wh-com/0.0.3/ecom-wh-com-0.0.3.tar.gz

---
+++ packages/ecom-wh-com/ecom-wh-com-0.0.3.tar.gz/ecom-wh-com-0.0.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+from setuptools import find_packages, setup
+
+
+setup(
+    author="ecomm warehouse Services",
+    install_requires=[],
+    license="MIT-0",
+    name="ecom-wh-com",
+    packages=find_packages(),
+    setup_requires=["pytest-runner"],
+    test_suite="tests",
+    tests_require=["pytest"],
+    version="0.0.3"
+)
