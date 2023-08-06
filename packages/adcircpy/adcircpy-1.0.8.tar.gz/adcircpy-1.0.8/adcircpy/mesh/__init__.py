20b1f6eca8fead6ec2ae314b0fcc0cd0e8935edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:10 +0000

    Add package adcircpy/1.0.8/adcircpy-1.0.8.tar.gz

---
+++ packages/adcircpy/adcircpy-1.0.8.tar.gz/adcircpy-1.0.8/adcircpy/mesh/__init__.py
@@ -0,0 +1,11 @@
+"""
+Namespace definitions for pyschism.mesh module
+"""
+
+from adcircpy.mesh.mesh import AdcircMesh
+from adcircpy.mesh.base import EuclideanMesh2D
+
+__all__ = [
+    "AdcircMesh",
+    "EuclideanMesh2D",
+]
