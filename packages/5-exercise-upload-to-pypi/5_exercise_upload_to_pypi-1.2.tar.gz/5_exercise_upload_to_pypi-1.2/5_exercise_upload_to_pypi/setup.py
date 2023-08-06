744d45de93cd4799c421771b39198b52764d1a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:50 +0000

    Add package 5-exercise-upload-to-pypi/1.2/5_exercise_upload_to_pypi-1.2.tar.gz

---
+++ packages/5-exercise-upload-to-pypi/5_exercise_upload_to_pypi-1.2.tar.gz/5_exercise_upload_to_pypi-1.2/5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_distributions'],
+      author = 'Satyendra Jaladi',
+      author_email = 'sam.satya38@gmail.com',
+      zip_safe=False)
---
+++ packages/5-exercise-upload-to-pypi/5_exercise_upload_to_pypi-1.2.tar.gz/5_exercise_upload_to_pypi-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='5_exercise_upload_to_pypi',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['5_exercise_upload_to_pypi'],
+      author = 'Satyendra Jaladi',
+      author_email = 'sam.satya38@gmail.com',
+      zip_safe=False)
