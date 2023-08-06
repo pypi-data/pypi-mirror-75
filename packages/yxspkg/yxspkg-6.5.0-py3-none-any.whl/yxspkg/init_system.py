be062b9022a1b7070c57330e643410d0ec571092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:38 +0000

    Add package yxspkg/6.5.0/yxspkg-6.5.0-py3-none-any.whl

---
+++ packages/yxspkg/yxspkg-6.5.0-py3-none-any.whl/yxspkg/init_system.py
@@ -0,0 +1,29 @@
+import subprocess
+def run_command(s,error_file):
+    print('run command:',s)
+    t = subprocess.call(s,shell=True) 
+    if t != 0:
+        error_file.write(s)
+        error_file.write('\n')
+    return t
+def main():
+    error_file = open('error_log.std','w')
+    error_file.write('wrong command:\n')
+    apt_commands=[
+        'sudo apt-get install gcc g++ gfortran',
+        'sudo apt-get install texlive texlive-science',
+        'sudo python -m pip install yxspkg_pip',
+    ]
+    for i in apt_commands:
+        t = run_command(i,error_file)
+        
+    python_module = ['lxml','matplotlib','pandas','numpy','scipy','imageio',
+                     'git+https://gitee.com/blacksong/SciencePlots.git',
+                     'rsa','tushare','PyQt5','lxml','you-get','quandl',
+                     'mpl_finance','pandas_datareaderf','twine','cheroot','opencv-python',
+                     'xlrd','pyqtgraph','web.py']
+    for i in python_module:
+        command = 'python -m pip install '+i +' --user'
+        run_command(command,error_file)
+if __name__=='__main__':
+    main()
