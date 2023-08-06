762933567fd7f03867acb2e336bb3089745443f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:14 +0000

    Add package brk/0.0.3/brk-0.0.3.tar.gz

---
+++ packages/brk/brk-0.0.3.tar.gz/brk-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="brk",
+    version="0.0.3",
+    author="Barkin Kaplan",
+    author_email="barkinkaplan@example.com",
+    description="Utility package for file system and network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
