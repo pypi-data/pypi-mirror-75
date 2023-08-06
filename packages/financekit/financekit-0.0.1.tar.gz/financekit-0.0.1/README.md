b74760889a3203ab76b5ecceec11e4ecc49364a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:27 +0000

    Add package financekit/0.0.1/financekit-0.0.1.tar.gz

---
+++ packages/financekit/financekit-0.0.1.tar.gz/financekit-0.0.1/README.md
@@ -0,0 +1,3 @@
+# financekit
+
+This is a Python package for financial analysis. The project is in a pre-development release version.
---
+++ packages/financekit/financekit-0.0.1.tar.gz/financekit-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='financekit',
+      version='0.0.1',
+      description='A financial package for Python',
+      url='https://github.com/GuillaumeKark/financekit',
+      author='Guillaume Karklins',
+      author_email='guillaume.karklins_marchay@edu.escp.eu',
+      license='Apache License 2.0',
+        classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"],
+      packages=setuptools.find_packages())
