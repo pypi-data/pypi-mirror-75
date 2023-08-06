6db3166a27b31a547b64b0e36b15f239c232faae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:05 +0000

    Add package distributions-b01/0.1/distributions_b01-0.1.tar.gz

---
+++ packages/distributions-b01/distributions_b01-0.1.tar.gz/distributions_b01-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_b01',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_b01'],
+      author = 'Botha Moseti',
+      author_email = 'derrickbotha2@gmail.com',
+      zip_safe=False)
