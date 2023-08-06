f4ab4ce365fc10255c69fd7249b74152aba62c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:17 +0000

    Add package duel/2.7/duel-2.7.tar.gz

---
+++ packages/duel/duel-2.7.tar.gz/duel-2.7/duel/__init__.py
@@ -0,0 +1,22 @@
+from duel.camera import *
+from duel.menuButtons import *
+from duel.tile import *
+from duel.map import *
+from duel.redForestMap import *
+from duel.pauseButtons import *
+from duel.mainMenuFrame import *
+from duel.swordHitBoxes import *
+from duel.gameFrame import *
+from duel.playerSkinsList import *
+from duel.player import *
+from duel.desolateDesertMap import *
+from duel.redForestMap import *
+from duel.mapSelectionList import *
+from duel.sword import *
+from duel.audioEngine import *
+from duel.gameEngine import *
+from duel.window import *
+from duel.__main__ import *
+
+
+
