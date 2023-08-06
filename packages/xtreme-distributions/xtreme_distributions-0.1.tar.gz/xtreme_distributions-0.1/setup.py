91398834318792a86acac849bdb03440c9331e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:27 +0000

    Add package xtreme-distributions/0.1/xtreme_distributions-0.1.tar.gz

---
+++ packages/xtreme-distributions/xtreme_distributions-0.1.tar.gz/xtreme_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='xtreme_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['xtreme_distributions'],
+      zip_safe=False)
