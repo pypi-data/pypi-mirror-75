4173b401485ae74cd3a643550a823effcaa7ccd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:53 +0000

    Add package django-google-cloud-tasks/0.0.1a8/django-google-cloud-tasks-0.0.1a8.tar.gz

---
+++ packages/django-google-cloud-tasks/django-google-cloud-tasks-0.0.1a8.tar.gz/django-google-cloud-tasks-0.0.1a8/django_cloud_tasks/exceptions.py
@@ -0,0 +1,23 @@
+import json
+
+from django.http import HttpResponse
+
+
+class GoogleCredentialsException(Exception):
+    def __init__(self, e):
+        message = f'GCP_B64 or GOOGLE_APPLICATION_CREDENTIALS env variable not set properly: {e}'
+        super().__init__(message)
+
+
+class TaskNotFound(Exception):
+    def __init__(self, task_name):
+        self.error = {'error': f"Task {task_name} not found"}
+
+    @property
+    def response(self):
+        return HttpResponse(status=404, content=json.dumps(self.error), content_type='application/json')
+
+
+class QueueNotFound(Exception):
+    def __init__(self, queue_name):
+        self.error = {'error': f"Queue {queue_name} not found"}
