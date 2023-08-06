388c952c1048b79617546cc1cd1f737fbd2f9ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:09 +0000

    Add package phcli/0.1.0/phcli-0.1.0.tar.gz

---
+++ packages/phcli/phcli-0.1.0.tar.gz/phcli-0.1.0/ph_data_clean/model/clean_result.py
@@ -0,0 +1,34 @@
+import json
+from enum import Enum
+
+
+class Tag(Enum):
+    SUCCESS = 1
+    EMPTY_DICT = 0
+    MISSING_COL = -1
+    WARNING = 2
+    PH_ERR = -9999
+    UNDEFINED = -888
+
+
+class CleanResult(object):
+    """
+    清洗结果
+    """
+
+    def __init__(self, data: dict, metadata: dict, raw_data: dict, tag: Tag, err_msg: str = ''):
+        self.data = data
+        self.metadata = metadata
+        self.raw_data = raw_data
+        self.tag = tag
+        self.err_msg = err_msg
+
+    def __str__(self):
+        result = {
+            "data": self.data,
+            "metadata": self.metadata,
+            "raw_data": self.raw_data,
+            "tag": self.tag.value,
+            "errMsg": self.err_msg
+        }
+        return json.dumps(result, ensure_ascii=False)
