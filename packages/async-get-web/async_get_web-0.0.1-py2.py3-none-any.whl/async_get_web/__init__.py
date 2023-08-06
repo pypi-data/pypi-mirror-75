7284af28f70fb17e6e142abd722f11c1692a698e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:11 +0000

    Add package async-get-web/0.0.1/async_get_web-0.0.1-py2.py3-none-any.whl

---
+++ packages/async-get-web/async_get_web-0.0.1-py2.py3-none-any.whl/async_get_web/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for AsyncGetWeb."""
+
+__author__ = """Ruslan Iskov"""
+__email__ = 'ruslan.rv.ua@gmail.com'
+__version__ = '0.0.1'
+
+from .async_get_web import *
+from .exceptions import *
