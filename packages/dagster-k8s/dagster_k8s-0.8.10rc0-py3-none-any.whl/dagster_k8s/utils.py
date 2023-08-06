e375d351ec5a097714d154d17f3509fb1402eda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:38 +0000

    Add package dagster-k8s/0.8.10rc0/dagster_k8s-0.8.10rc0-py3-none-any.whl

---
+++ packages/dagster-k8s/dagster_k8s-0.8.10rc0-py3-none-any.whl/dagster_k8s/utils.py
@@ -0,0 +1,65 @@
+from .client import (
+    DEFAULT_JOB_POD_COUNT,
+    DEFAULT_WAIT_BETWEEN_ATTEMPTS,
+    DEFAULT_WAIT_TIMEOUT,
+    DagsterKubernetesClient,
+    WaitForPodState,
+)
+
+
+def retrieve_pod_logs(pod_name, namespace):
+    return DagsterKubernetesClient.production_client().retrieve_pod_logs(pod_name, namespace)
+
+
+def get_pod_names_in_job(job_name, namespace):
+    return DagsterKubernetesClient.production_client().get_pod_names_in_job(job_name, namespace)
+
+
+def delete_job(job_name, namespace):
+    return DagsterKubernetesClient.production_client().delete_job(job_name, namespace)
+
+
+def wait_for_job_success(
+    job_name,
+    namespace,
+    instance=None,
+    run_id=None,
+    wait_timeout=DEFAULT_WAIT_TIMEOUT,
+    wait_time_between_attempts=DEFAULT_WAIT_BETWEEN_ATTEMPTS,
+    num_pods_to_wait_for=DEFAULT_JOB_POD_COUNT,
+):
+    return DagsterKubernetesClient.production_client().wait_for_job_success(
+        job_name,
+        namespace,
+        instance,
+        run_id,
+        wait_timeout,
+        wait_time_between_attempts,
+        num_pods_to_wait_for,
+    )
+
+
+def wait_for_job(
+    job_name,
+    namespace,
+    wait_timeout=DEFAULT_WAIT_TIMEOUT,
+    wait_time_between_attempts=DEFAULT_WAIT_BETWEEN_ATTEMPTS,
+):
+    return DagsterKubernetesClient.production_client().wait_for_job(
+        job_name=job_name,
+        namespace=namespace,
+        wait_timeout=wait_timeout,
+        wait_time_between_attempts=wait_time_between_attempts,
+    )
+
+
+def wait_for_pod(
+    pod_name,
+    namespace,
+    wait_for_state=WaitForPodState.Ready,
+    wait_timeout=DEFAULT_WAIT_TIMEOUT,
+    wait_time_between_attempts=DEFAULT_WAIT_BETWEEN_ATTEMPTS,
+):
+    return DagsterKubernetesClient.production_client().wait_for_pod(
+        pod_name, namespace, wait_for_state, wait_timeout, wait_time_between_attempts
+    )
