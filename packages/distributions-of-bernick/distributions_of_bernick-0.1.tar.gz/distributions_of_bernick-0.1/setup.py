1f5e8438d0fb1197c5bdaeeb648e72a7c5de7ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:18 +0000

    Add package distributions-of-bernick/0.1/distributions_of_bernick-0.1.tar.gz

---
+++ packages/distributions-of-bernick/distributions_of_bernick-0.1.tar.gz/distributions_of_bernick-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_of_bernick',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_of_bernick'],
+      author='Bernick Salvador',
+      authot_email='bsalvador.ro@uni.pe',
+      zip_safe=False)
