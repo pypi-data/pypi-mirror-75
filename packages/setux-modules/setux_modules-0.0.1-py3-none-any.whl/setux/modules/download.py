c8976444a7462bc63ac9c31063e3173c1c0f09d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:27 +0000

    Add package setux-modules/0.0.1/setux_modules-0.0.1-py3-none-any.whl

---
+++ packages/setux-modules/setux_modules-0.0.1-py3-none-any.whl/setux/modules/download.py
@@ -0,0 +1,32 @@
+from setux.main.logger import error
+from . import Module
+
+
+class Distro(Module):
+    def deploy(self, target, **kw):
+        url, dst = kw['url'], kw.get('dst')
+        dst = dst or 'downloaded'
+        try:
+            ret, out, err = target.run(f'http --download {url} -o {dst}')
+            if ret!=0:
+                raise RuntimeError
+        except:
+            try:
+                ret, out, err = target.run(f'curl -sfL {url} -o {dst}')
+                if ret!=0:
+                    raise RuntimeError
+            except:
+                try:
+                    ret, out, err = target.run(f'wget -q {url} -O {dst}')
+                    if ret!=0:
+                        raise RuntimeError
+                except:
+                    target.Pip.install('httpie')
+                    ret, out, err = target.run(f'http --download {url} -o {dst}')
+                    if ret!=0:
+                        msg = err[0]
+                        error(msg)
+                        raise RuntimeError(msg)
+        target(f'ls -l {dst}')
+        return True
+
