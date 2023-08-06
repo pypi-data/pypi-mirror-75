087b098e11a457ad10844262111c8d2c420b4509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:31 +0000

    Add package nlp-rules/1.0.3/nlp_rules-1.0.3.tar.gz

---
+++ packages/nlp-rules/nlp_rules-1.0.3.tar.gz/nlp_rules-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='nlp_rules',
+    version='1.0.3',
+    description='an regular tools for nlp classify and ner',
+    author='QinHaining',
+    author_email='2364839934@qq.com',
+    url='https://github.com/scmsqhn/nlp_rules',
+    packages=[],
+    include_package_data=True,
+    install_requires=[],
+    license='MIT License',
+    zip_safe=False,
+)
