ec278eb4387091d4eb9b54a17461ed7b93cc0a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:58 +0000

    Add package interactionfreepy/1.0.1/InteractionFreePy-1.0.1.tar.gz

---
+++ packages/interactionfreepy/InteractionFreePy-1.0.1.tar.gz/InteractionFreePy-1.0.1/InteractionFreePy/__init__.py
@@ -0,0 +1,3 @@
+from InteractionFreePy.broker import IFBroker, Manager
+from InteractionFreePy.core import IFDefinition, IFException, IFLoop, Message, Invocation
+from InteractionFreePy.worker import IFWorker, InvokeTarget, RemoteObject, DynamicRemoteObject, AsyncRemoteObject, InvokeFuture
---
+++ packages/interactionfreepy/InteractionFreePy-1.0.1.tar.gz/InteractionFreePy-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='InteractionFreePy',
+    packages=['InteractionFreePy'],
+    version='1.0.1',
+    license='gpl-3.0',
+    description='A intuitive and cross-languige RCP lib for Python.',
+    author='Hwaipy',
+    author_email='hwaipy@gmail.com',
+    url='https://github.com/hwaipy/InteractionFreePy',
+    download_url='https://github.com/hwaipy/InteractionFreePy/archive/v1.0.0.tar.gz',
+    keywords=['msgpack', 'zeromq', 'zmq', '0mq', 'rcp', 'cross-languige'],
+    install_requires=[
+        'msgpack',
+        'tornado',
+        'pyzmq',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
