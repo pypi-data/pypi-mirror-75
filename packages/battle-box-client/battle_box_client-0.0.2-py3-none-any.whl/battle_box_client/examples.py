86229203287e02e08288229ed8d5ebc4fa0c40d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:33 +0000

    Add package battle-box-client/0.0.2/battle_box_client-0.0.2-py3-none-any.whl

---
+++ packages/battle-box-client/battle_box_client-0.0.2-py3-none-any.whl/battle_box_client/examples.py
@@ -0,0 +1,61 @@
+from .client import RobotGameBot
+
+
+class Tortuga(RobotGameBot):
+    NAME = "tortuga"
+
+    def commands(self, commands_request, settings):
+        return [robot.guard() for robot in commands_request["my_robots"]]
+
+
+class Kansas(RobotGameBot):
+    NAME = "kansas"
+
+    def commands(self, commands_request, settings):
+        row_midpoint = settings["terrain"].rows // 2
+        col_midpoint = settings["terrain"].cols // 2
+
+        commands = []
+
+        for robot in commands_request["my_robots"]:
+            [x, y] = robot.location
+
+            if y > row_midpoint:
+                commands.append(robot.move([x, y - 1]))
+            elif y < row_midpoint:
+                commands.append(robot.move([x, y + 1]))
+            elif x > col_midpoint:
+                commands.append(robot.move([x - 1, y]))
+            else:
+                commands.append(robot.move([x + 1, y]))
+
+        return commands
+
+
+class HoneyBadger(RobotGameBot):
+    NAME = "honey-badger"
+
+    def commands(self, commands_request, settings):
+        commands = []
+
+        for robot in commands_request["my_robots"]:
+            adjacent_enemies = [
+                enemy for enemy in commands_request["enemy_robots"]
+                if enemy.location in robot.adjacent_locations
+            ]
+
+            if adjacent_enemies:
+                target = adjacent_enemies[0].location
+                command = robot.attack(target)
+            elif commands_request["enemy_robots"]:
+                closest_enemy = min(
+                    commands_request["enemy_robots"],
+                    key=lambda enemy: robot.manhattan_distance(
+                        enemy.location))
+                command = robot.move_towards(closest_enemy.location)
+            else:
+                command = robot.guard()
+
+            commands.append(command)
+
+        return commands
