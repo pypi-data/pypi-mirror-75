6736d58be0d906c8f8230eed90af58da6844e002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:50 +0000

    Add package listen/0.2.1/listen-0.2.1.tar.gz

---
+++ packages/listen/listen-0.2.1.tar.gz/listen-0.2.1/README.md
@@ -0,0 +1,27 @@
+listen
+======
+
+Simple but powerful signal handling to process OS signals in python
+
+##Example Usage:
+
+    import listen
+    import subprocess
+
+### Instanciate SignalHandler
+    sig_hand = listen.SignalHandler()
+
+### Start external process
+    external_process = subprocess.Popen(['sh', '-c', 'sleep 30'])
+
+### Register some signal handlers to kill external process on SIGINT (ctrl-c)
+    kill_event = sig_hand.reg_on_status(external_process.kill)
+
+### Wait for external process
+    external_process.wait()
+
+### Press ctrl-c to send SIGINT or alternatively use the following to unregister the event
+    sig_hand.del_status_event(kill_event)
+
+For a more detailed example including interaction with bash please see
+the tests/example.py and tests/external_process.bash
---
+++ packages/listen/listen-0.2.1.tar.gz/listen-0.2.1/setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+"""
+The MIT License (MIT)
+
+Copyright (c) 2014 Antonia Stevens
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import os
+import sys
+
+# prefer using setuptools/Distribute, else use distutils
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Parse README markdown
+project_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(project_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="listen",
+    version="0.2.1",
+    author="Antonia Stevens",
+    author_email="a@antevens.com",
+    packages=["listen", "test"],
+    scripts=(),
+    url="https://github.com/antevens/listen",
+    keywords=["signal", "unix", "kill", "abort", "quit", "info"],
+    license="LICENSE",
+    description="Simple but powerful signal handling to process OS signals in python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # Support both formats for required packages, build automatically
+    install_requires=(),
+    requires=(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
