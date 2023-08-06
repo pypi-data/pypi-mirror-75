bf74919adc985b9bd0e8c2463cf4f1f5c0e6c61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:37 +0000

    Add package Sunscreen-Api-Test/0.0.7/Sunscreen_Api_Test-0.0.7.tar.gz

---
+++ packages/Sunscreen-Api-Test/Sunscreen_Api_Test-0.0.7.tar.gz/Sunscreen_Api_Test-0.0.7/Data/Headers/headers_data.py
@@ -0,0 +1,32 @@
+headers_warrant = {
+            "Content-Type": "application/json",
+            "token":"MTU5NTU3NzMzNjo0NTk3MTo0NTk3MTVmMWE5M2Y4ODE0OGM=",
+            "userId":"45971",
+            "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6"
+        }
+headers_Sunscreen_web = {
+    "Content-Type": "application/json",
+    "token": "ibtl5ta9omvrs904m8a5hcmg16",
+    "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6"
+}
+
+headers_Sunscreen_h5_City = {
+    "Content-Type": "application/json",
+    "token":"MTU5NjQxOTI4Nzo0NjA4Nzo0NjA4NzVmMjc2Y2Q3OTY4NDU=",
+    "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6",
+    "userId":"46087"
+}
+
+headers_Sunscreen_h5_Province = {
+    "Content-Type": "application/json",
+    "token":"MTU5NjQxOTE0ODo0NjA4Njo0NjA4NjVmMjc2YzRjODhkMmI=",
+    "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6",
+    "userId":"46086"
+}
+
+headers_Sunscreen_h5_Two = {
+    "Content-Type":"application/json",
+    "token":"MTU5NjQyMzg4Njo0NTk3MTo0NTk3MTVmMjc3ZWNlZWY2ODc=",
+    "userId":"45971",
+    "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6"
+}
