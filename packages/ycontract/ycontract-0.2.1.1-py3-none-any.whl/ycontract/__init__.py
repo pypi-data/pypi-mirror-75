07b3fd07aef56eaf84c51df64da3c61d037184e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:39 +0000

    Add package ycontract/0.2.1.1/ycontract-0.2.1.1-py3-none-any.whl

---
+++ packages/ycontract/ycontract-0.2.1.1-py3-none-any.whl/ycontract/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ycontract.contract import (  # noqa: F401
+    SYS_STATE,
+    ContractException,
+    disable_contract,
+    in_contract,
+    out_contract,
+)
