449f7a00f28a8f1b7353e5df0d367d97f11554e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:19 +0000

    Add package distributions-1107/1.0/distributions_1107-1.0.tar.gz

---
+++ packages/distributions-1107/distributions_1107-1.0.tar.gz/distributions_1107-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_1107',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_1107'],
+      zip_safe=False)
