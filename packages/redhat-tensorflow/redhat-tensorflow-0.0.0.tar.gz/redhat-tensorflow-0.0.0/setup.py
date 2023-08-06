b2dbf8ea5b2a1c5a590860ff641400e61561cb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:17 +0000

    Add package redhat-tensorflow/0.0.0/redhat-tensorflow-0.0.0.tar.gz

---
+++ packages/redhat-tensorflow/redhat-tensorflow-0.0.0.tar.gz/redhat-tensorflow-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import os
+import sys
+
+setup(
+    name="redhat-tensorflow",
+    version="0.0.0",
+    description="A build of TensorFlow by Red Hat",
+    url="https://github.com/thoth-station/",
+    py_modules=["tensorflow"],
+    classifiers=[
+    ],
+    extras_require={
+    },
+)
