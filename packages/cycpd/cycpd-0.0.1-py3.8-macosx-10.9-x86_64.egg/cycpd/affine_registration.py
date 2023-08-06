fe0c45cfac47d5ab87ec9346f2a7928a9b08b662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:33 +0000

    Add package cycpd/0.0.1/cycpd-0.0.1-py3.8-macosx-10.9-x86_64.egg

---
+++ packages/cycpd/cycpd-0.0.1-py3.8-macosx-10.9-x86_64.egg/cycpd/affine_registration.py
@@ -0,0 +1,50 @@
+from builtins import super
+import numpy as np
+import time
+from .expectation_maximization_registration import expectation_maximization_registration
+
+class affine_registration(expectation_maximization_registration):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        tic = time.time()
+        self.B = np.eye(self.D)
+        self.t = np.zeros(self.D)
+        self.B1 = None
+        self.B2 = None
+
+        toc = time.time()
+        self.time_to_initialize_registration = toc - tic
+
+    def update_transform(self):
+        self.muX = np.divide(np.matmul(self.X.T, self.Pt1), self.Np)
+        self.muY = np.divide(np.matmul(self.Y.T, self.P1),
+                             self.Np)  # changed previous code from PyCPD because we no longer store self.P
+
+        self.B1 = np.matmul(self.PX.T, self.Y) - self.Np * np.matmul(self.muX[:,None], self.muY[:,None].T)
+        self.B2 = np.matmul((self.Y * self.P1[:,None]).T, self.Y) - self.Np * np.matmul(self.muY[:, None], self.muY[:,None].T)
+        self.B = np.linalg.solve(self.B2.T, self.B1.T).T
+
+        self.t = np.squeeze(self.muX[:,None] - np.matmul(self.B, self.muY[:,None]))
+
+        self.err = abs(self.E - self.E_old)
+
+    def transform_point_cloud(self, Y=None):
+        if Y is None:
+            self.TY = np.dot(self.Y, self.B.T) + self.t
+            return
+        else:
+            return np.dot(Y, self.B.T) + self.t
+
+    def update_variance(self):
+        self.sigma2_prev = self.sigma2
+        A = np.sum(np.square(self.X) * self.Pt1[:,None])
+        B = self.Np * np.matmul(self.muX[:,None].T, self.muX[:,None])
+        C = np.trace(np.matmul(self.B1, self.B.T))
+        self.sigma2 = abs(A - B - C)/(self.Np * self.D)
+
+        if self.sigma2 <= 0:
+            self.sigma2 = self.tolerance / 10
+
+    def get_registration_parameters(self):
+        return self.B, self.t
---
+++ packages/cycpd/cycpd-0.0.1-py3.8-macosx-10.9-x86_64.egg/cycpd/deformable_registration.py
@@ -0,0 +1,106 @@
+from builtins import super
+import numpy as np
+import time
+from .expectation_maximization_registration import expectation_maximization_registration
+
+def gaussian_kernel(X, beta, Y=None):
+    if Y is None:
+        Y = X
+    diff = X[:, None, :] - Y[None, :, :]
+    diff = diff**2
+    diff = np.sum(diff, 2)
+    return np.exp(-diff / (2 * beta**2))
+
+def lowrankQS(G, num_eig, eig_fgt=False):
+    # if we do not use FGT we construct affinity matrix G and find the
+    # first eigenvectors/values directly
+
+    if eig_fgt is False:
+        S, Q = np.linalg.eigh(G)
+        eig_indices = list(np.argsort(np.abs(S))[::-1][:num_eig])
+        Q = Q[:, eig_indices]  # eigenvectors
+        S = S[eig_indices]  # eigenvalues.
+
+        return Q, S
+
+    elif eig_fgt is True:
+        raise Exception('Fast Gauss Transform Not Yet Implemented!')
+
+class deformable_registration(expectation_maximization_registration):
+    def __init__(self, alpha=2, beta=2, low_rank=True, num_eig=100, eig_fgt=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        tic = time.time()
+        self.alpha         = 2 if alpha is None else alpha
+        self.beta          = 2 if beta is None else beta
+        self.num_eig       = num_eig
+        self.eig_fgt       = eig_fgt
+        self.W             = np.zeros((self.M, self.D))
+        self.G             = gaussian_kernel(self.Y, self.beta)
+        self.low_rank      = low_rank
+
+        if self.low_rank is True:
+            self.Q, self.S = lowrankQS(self.G, num_eig=self.num_eig, eig_fgt=self.eig_fgt)
+            self.inv_S = np.diag(1./self.S)
+            self.S = np.diag(self.S)
+        toc = time.time()
+        self.time_to_initialize_registration = toc - tic
+
+    def update_transform(self):
+        if self.low_rank is False:
+            tic = time.time()
+            A = np.dot(np.diag(self.P1), self.G) + self.alpha * self.sigma2 * np.eye(self.M)
+            toc = time.time()
+            self.A_times.append(toc-tic)
+            tic = time.time()
+            B = self.PX - np.dot(np.diag(self.P1), self.Y)
+            toc = time.time()
+            self.B_times.append(toc - tic)
+            tic = time.time()
+            self.W = np.linalg.solve(A, B)
+            toc = time.time()
+            self.solve_times.append(toc - tic)
+        else:
+            dP = np.diag(self.P1)
+            dPQ = np.matmul(dP, self.Q)
+            F = self.PX - np.matmul(dP, self.Y)
+
+            self.W = 1 / (self.alpha * self.sigma2) * (F - np.matmul(dPQ, (
+                np.linalg.solve((self.alpha * self.sigma2 * self.inv_S + np.matmul(self.Q.T, dPQ)), (np.matmul(self.Q.T, F))))))
+            QtW = np.matmul(self.Q.T, self.W)
+            self.E = self.E + self.alpha/2 * np.trace(np.matmul(QtW.T, np.matmul(self.S, QtW)))
+            # self.err = abs((self.E - self.E_old) / self.E)
+            self.err = abs(self.E - self.E_old)
+            # The absolute difference is more conservative (does more iterations) than the line above it which
+            # is calculating the normalized change in the E(L). This calculation was changed to match the matlab
+            # code created for low_rank matrices.
+
+    def transform_point_cloud(self, Y=None):
+        if self.low_rank is False:
+            if Y is None:
+                self.TY = self.Y + np.dot(self.G, self.W)
+                return
+            else:
+                G = gaussian_kernel(Y, self.beta, Y=self.Y)
+                return Y + np.matmul(G, self.W)
+        elif self.low_rank is True:
+            if Y is None:
+                self.TY = self.Y + np.matmul(self.Q, np.matmul(self.S, np.matmul(self.Q.T, self.W)))
+                return
+            else:
+                G = gaussian_kernel(Y, beta=self.beta, Y=self.Y)
+                return Y + np.matmul(G, self.W)
+
+    def update_variance(self):
+        self.sigma2_prev = self.sigma2
+        A = np.sum(np.square(self.X) * self.Pt1[:, None])
+        B = np.sum(np.square(self.TY) * self.P1[:, None])
+        C = 2 * np.trace(np.matmul(self.PX.T, self.TY))
+        self.sigma2 = abs( A + B - C ) / (self.Np * self.D)
+
+        if self.sigma2 <= 0:
+            self.sigma2 = self.tolerance / 10
+
+        # self.err = abs(self.sigma2_prev - self.sigma2)
+
+    def get_registration_parameters(self):
+        return self.G, self.W
