25fc253c622c1c21b3b7f9fb7d74a05d4ed9bf63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:33 +0000

    Add package foko-probability/1.0/foko_probability-1.0.tar.gz

---
+++ packages/foko-probability/foko_probability-1.0.tar.gz/foko_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='foko_probability',
+      version='1.0',
+      description='Probability distributions',
+      packages=['foko_probability'],
+      author='Foko Valdes',
+      author_email = 'research.ciffer@gmail.com',
+      zip_safe=False)
