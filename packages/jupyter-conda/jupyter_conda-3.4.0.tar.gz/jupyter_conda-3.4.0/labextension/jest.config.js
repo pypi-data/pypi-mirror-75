63eab22e3169b9399bf99808f866917c3a786dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:10 +0000

    Add package jupyter-conda/3.4.0/jupyter_conda-3.4.0.tar.gz

---
+++ packages/jupyter-conda/jupyter_conda-3.4.0.tar.gz/jupyter_conda-3.4.0/labextension/jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  automock: false,
+  collectCoverageFrom: ["src/**.{ts,tsx}", "!src/*.d.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text"],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg)$": "@jupyterlab/testutils/lib/jest-file-mock.js"
+  },
+  preset: "ts-jest/presets/js-with-babel",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["@jupyterlab/testutils/lib/jest-script.js"],
+  setupFiles: ["<rootDir>/scripts/jest-setup-files.js"],
+  testPathIgnorePatterns: ["/lib/", "/node_modules/"],
+  testRegex: "src/.*/.*.spec.ts[x]?$",
+  transform: {
+    "\\.(ts|tsx)?$": "ts-jest",
+    "\\.(js|jsx)?$": "<rootDir>/scripts/jest-transform.js",
+    "\\.svg$": "jest-raw-loader"
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(@jupyterlab/.*)/)"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json"
+    }
+  }
+};
