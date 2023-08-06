995d2981ebc12bf404d851054bf274254678663a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:18 +0000

    Add package helpdeskbot/2.2.1/helpdeskbot-2.2.1.tar.gz

---
+++ packages/helpdeskbot/helpdeskbot-2.2.1.tar.gz/helpdeskbot-2.2.1/README.md
@@ -0,0 +1,35 @@
+# HelpDeskBot
+
+Utility bot for Discord written in Python 3
+
+## Project Home Page
+
+https://www.devdungeon.com/content/help-desk-bot
+
+## Installation
+
+```bash
+pip install helpdeskbot
+```
+
+## Updating
+
+```bash
+pip install --upgrade helpdeskbot --no-cache-dir
+```
+
+## Running
+
+Execute `helpdeskbot` and it will load the token and
+channel name from the `.env` file. Create the `.env`
+file in the working directory based on `misc/example.env`.
+
+```bash
+helpdeskbot
+# or
+python -m helpdeskbot
+```
+
+## Contact
+
+nanodano@devdungeon.com
---
+++ packages/helpdeskbot/helpdeskbot-2.2.1.tar.gz/helpdeskbot-2.2.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='helpdeskbot',
+    version='2.2.1',
+    description='Discord utility bot for DevDungeon.com',
+    url='https://github.com/DevDungeon/HelpDeskBot',
+    author='John Leon',
+    author_email='nanodano@devdungeon.com',
+    license='GPL-3.0',
+    packages=['helpdeskbot'],
+    entry_points={
+        'console_scripts': [
+            'helpdeskbot = helpdeskbot.__main__:main',
+        ],
+    },
+    zip_safe=False,
+    python_requires='>=3.7',
+    install_requires=[
+        'discord.py',
+        'aiohttp',
+        'python-dotenv',
+    ]
+)
