4e81e4ef354e643a8b8f825ccc317251b4373af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:44 +0000

    Add package uurl/0.0.1/uurl-0.0.1-py3-none-any.whl

---
+++ packages/uurl/uurl-0.0.1-py3-none-any.whl/uurl/__init__.py
@@ -0,0 +1,32 @@
+"""Package contains a set of interfaces to operate `uurl` application."""
+from typing import Tuple
+from uurl.url import Address, HttpUrl, HttpsUrl, Url
+from uurl.host import Host
+from uurl.path import EmptyPath, Path, UrlPath
+from uurl.protocol import HttpProtocol, HttpsProtocol, NetworkProtocol, Protocol
+from uurl.port import HttpPort, HttpsPort, NetworkPort, Port
+
+__author__: str = 'Volodymyr Yahello'
+__email__: str = 'vyahello@gmail.com'
+__license__: str = 'MIT'
+__copyright__: str = f'Copyright 2020, {__author__}'
+__version__: str = '0.0.1'
+
+__all__: Tuple[str, ...] = (
+    'Address',
+    'Url',
+    'HttpUrl',
+    'HttpsUrl',
+    'Host',
+    'EmptyPath',
+    'UrlPath',
+    'Path',
+    'HttpProtocol',
+    'HttpsProtocol',
+    'NetworkProtocol',
+    'Protocol',
+    'HttpPort',
+    'HttpsPort',
+    'NetworkPort',
+    'Port',
+)
