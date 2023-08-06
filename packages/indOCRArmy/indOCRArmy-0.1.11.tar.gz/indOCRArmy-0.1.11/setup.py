e5e1e4a1bd9801c1200fa14221da94b1c8318200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package indOCRArmy/0.1.11/indOCRArmy-0.1.11.tar.gz

---
+++ packages/indOCRArmy/indOCRArmy-0.1.11.tar.gz/indOCRArmy-0.1.11/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="indOCRArmy",  # Replace with your own username
+    version="0.1.11",
+    author="Mulya Rahardja Madjiah, Andreas; Hervind; S Fawwazi, Ziyad",
+    author_email="ziyad.syauqi95@gmail.com",
+    description="A package for reading id and name on KTP and SIM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests', 'numpy', 'opencv-contrib-python',
+                      'pandas', 'Pillow', 'matplotlib'],
+    python_requires='>=3.7',
+)
