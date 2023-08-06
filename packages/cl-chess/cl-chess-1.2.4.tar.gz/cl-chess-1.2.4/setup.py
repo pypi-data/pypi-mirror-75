c72231ee110f321b57f390b477e6a16371cfd456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:23 +0000

    Add package cl-chess/1.2.4/cl-chess-1.2.4.tar.gz

---
+++ packages/cl-chess/cl-chess-1.2.4.tar.gz/cl-chess-1.2.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cl-chess',
+    packages=['src'],
+    version='1.2.4',
+    description='A program to play chess on the command line',
+    author='Marcus Buffett',
+    author_email='marcusbuffett@me.com',
+    url='https://github.com/marcusbuffett/command-line-chess',
+    #download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+    entry_points={
+        'console_scripts': [
+            'chess = main:main',
+        ],
+    },
+    keywords=['chess', 'game'],
+    classifiers=["Programming Language :: Python :: 3"],
+)
