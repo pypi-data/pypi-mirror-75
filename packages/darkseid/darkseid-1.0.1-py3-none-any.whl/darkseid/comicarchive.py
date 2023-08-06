0dde47149b030279a25a4143ee28bb7351b7466c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:13 +0000

    Add package darkseid/1.0.1/darkseid-1.0.1-py3-none-any.whl

---
+++ packages/darkseid/darkseid-1.0.1-py3-none-any.whl/darkseid/comicarchive.py
@@ -0,0 +1,404 @@
+"""A class to represent a single comic"""
+
+# Copyright 2012-2014 Anthony Beville
+# Copyright 2019 Brian Pepple
+
+import io
+import os
+import sys
+import tempfile
+import zipfile
+
+from natsort import natsorted
+from PIL import Image
+
+from .comicinfoxml import ComicInfoXml
+from .filenameparser import FileNameParser
+from .genericmetadata import GenericMetadata
+
+sys.path.insert(0, os.path.abspath("."))
+
+
+class ZipArchiver:
+
+    """ZIP implementation"""
+
+    def __init__(self, path):
+        self.path = path
+
+    def read_archive_file(self, archive_file):
+        """Read the contents of a comic archive"""
+
+        data = ""
+        zip_file = zipfile.ZipFile(self.path, "r")
+
+        try:
+            data = zip_file.read(archive_file)
+        except zipfile.BadZipfile as bad_zip_error:
+            print(
+                f"bad zipfile [{bad_zip_error}]: {self.path} :: {archive_file}",
+                file=sys.stderr,
+            )
+            zip_file.close()
+            raise IOError
+        except Exception as exception_error:
+            zip_file.close()
+            print(
+                f"bad zipfile [{exception_error}]: {self.path} :: {archive_file}",
+                file=sys.stderr,
+            )
+            raise IOError
+        finally:
+            zip_file.close()
+        return data
+
+    def remove_archive_file(self, archive_file):
+        """Returns a boolean when attempting to remove a file from an archive"""
+
+        try:
+            self.rebuild_zipfile([archive_file])
+        except zipfile.BadZipfile:
+            return False
+        else:
+            return True
+
+    def write_archive_file(self, archive_file, data):
+        #  At the moment, no other option but to rebuild the whole
+        #  zip archive w/o the indicated file. Very sucky, but maybe
+        # another solution can be found
+        try:
+            self.rebuild_zipfile([archive_file])
+
+            # now just add the archive file as a new one
+            zip_file = zipfile.ZipFile(
+                self.path, mode="a", allowZip64=True, compression=zipfile.ZIP_DEFLATED
+            )
+            zip_file.writestr(archive_file, data)
+            zip_file.close()
+            return True
+        except (zipfile.BadZipfile, zipfile.LargeZipFile) as exception_error:
+            print(f"Error writing zipfile: {exception_error}.")
+            return False
+
+    def get_archive_filename_list(self):
+        """Returns a list of the filenames in an archive"""
+
+        try:
+            zip_file = zipfile.ZipFile(self.path, "r")
+            namelist = zip_file.namelist()
+            zip_file.close()
+            return namelist
+        except Exception as exception_error:
+            print(
+                f"Unable to get zipfile list [{exception_error}]: {self.path}",
+                file=sys.stderr,
+            )
+            return []
+
+    def rebuild_zipfile(self, exclude_list):
+        """Zip helper func
+
+        This recompresses the zip archive, without the files in the exclude_list
+        """
+
+        # print ">> sys.stderr, Rebuilding zip {0} without {1}".format(
+        #                                            self.path, exclude_list )
+
+        # generate temp file
+        tmp_fd, tmp_name = tempfile.mkstemp(dir=os.path.dirname(self.path))
+        os.close(tmp_fd)
+
+        zin = zipfile.ZipFile(self.path, "r")
+        zout = zipfile.ZipFile(tmp_name, "w", allowZip64=True)
+        for item in zin.infolist():
+            buffer = zin.read(item.filename)
+            if item.filename not in exclude_list:
+                zout.writestr(item, buffer)
+
+        zout.close()
+        zin.close()
+
+        # replace with the new file
+        os.remove(self.path)
+        os.rename(tmp_name, self.path)
+
+
+# ------------------------------------------
+
+
+class UnknownArchiver:
+
+    """Unknown implementation"""
+
+    def __init__(self, path):
+        self.path = path
+
+    @classmethod
+    def read_archive_file(cls):
+        return ""
+
+    @classmethod
+    def write_archive_file(cls, archive_file, data):
+        return False
+
+    @classmethod
+    def remove_archive_file(cls, archive_file):
+        return False
+
+    @classmethod
+    def get_archive_filename_list(cls):
+        return []
+
+
+# ------------------------------------------------------------------
+
+
+class ComicArchive:
+
+    """Comic Archive implementation"""
+
+    class ArchiveType:
+        """Types of archives supported. Currently only .cbz"""
+
+        Zip, Unknown = list(range(2))
+
+    def __init__(self, path):
+        self.path = path
+
+        self.ci_xml_filename = "ComicInfo.xml"
+        self.has_md = None
+        self.page_count = None
+        self.page_list = None
+        self.metadata = None
+
+        self.archive_type = self.ArchiveType.Unknown
+        self.archiver = UnknownArchiver(self.path)
+
+        if self.zip_test():
+            self.archive_type = self.ArchiveType.Zip
+            self.archiver = ZipArchiver(self.path)
+
+    def reset_cache(self):
+        """Clears the cached data"""
+        self.has_md = None
+        self.page_count = None
+        self.page_list = None
+        self.metadata = None
+
+    def zip_test(self):
+        """Tests whether an archive is a zipfile"""
+
+        return zipfile.is_zipfile(self.path)
+
+    def is_zip(self):
+        """Returns a boolean as to whether an archive is a zipfile"""
+
+        return self.archive_type == self.ArchiveType.Zip
+
+    def is_writable(self):
+        """Returns a boolean as to whether an archive is writable"""
+
+        if self.archive_type == self.ArchiveType.Unknown:
+            return False
+
+        return bool(os.access(self.path, os.W_OK))
+
+    def seems_to_be_a_comic_archive(self):
+        """Returns a boolean as to whether the file is a comic archive"""
+
+        return bool((self.is_zip()) and (self.get_number_of_pages() > 0))
+
+    def get_page(self, index):
+        """Returns an image(page) from an archive"""
+
+        image_data = None
+
+        filename = self.get_page_name(index)
+
+        if filename is not None:
+            try:
+                image_data = self.archiver.read_archive_file(filename)
+            except IOError:
+                print("Error reading in page.", file=sys.stderr)
+
+        return image_data
+
+    def get_page_name(self, index):
+        """Returns the page name from an index"""
+
+        if index is None:
+            return None
+
+        page_list = self.get_page_name_list()
+
+        num_pages = len(page_list)
+        if num_pages == 0 or index >= num_pages:
+            return None
+
+        return page_list[index]
+
+    def get_page_name_list(self, sort_list=True):
+        """Returns a list of page names from an archive"""
+
+        if self.page_list is None:
+            # get the list file names in the archive, and sort
+            files = self.archiver.get_archive_filename_list()
+
+            # seems like some archive creators are on  Windows, and don't know
+            # about case-sensitivity!
+            if sort_list:
+
+                def keyfunc(k):
+                    # hack to account for some weird scanner ID pages
+                    # basename=os.path.split(k)[1]
+                    # if basename < '0':
+                    #    k = os.path.join(os.path.split(k)[0], "z" + basename)
+                    return k.lower()
+
+                files = natsorted(files, key=keyfunc)
+
+            # make a sub-list of image files
+            self.page_list = []
+            for name in files:
+                if (
+                    name[-4:].lower() in [".jpg", "jpeg", ".png", ".gif", "webp"]
+                    and os.path.basename(name)[0] != "."
+                ):
+                    self.page_list.append(name)
+
+        return self.page_list
+
+    def get_number_of_pages(self):
+        """Returns the number of pages in an archive"""
+
+        if self.page_count is None:
+            self.page_count = len(self.get_page_name_list())
+        return self.page_count
+
+    def read_metadata(self):
+        """Reads the metadata from an archive if present"""
+
+        if self.metadata is None:
+            raw_metadata = self.read_raw_metadata()
+            if raw_metadata is None or raw_metadata == "":
+                self.metadata = GenericMetadata()
+            else:
+                self.metadata = ComicInfoXml().metadata_from_string(raw_metadata)
+
+            # validate the existing page list (make sure count is correct)
+            if (
+                len(self.metadata.pages) != 0
+                and len(self.metadata.pages) != self.get_number_of_pages()
+            ):
+                # pages array doesn't match the actual number of images we're seeing
+                # in the archive, so discard the data
+                self.metadata.pages = []
+
+            if len(self.metadata.pages) == 0:
+                self.metadata.set_default_page_list(self.get_number_of_pages())
+
+        return self.metadata
+
+    def read_raw_metadata(self):
+        if not self.has_metadata():
+            return None
+        try:
+            raw_metadata = self.archiver.read_archive_file(self.ci_xml_filename)
+        except IOError:
+            print("Error reading in raw CIX!")
+            raw_metadata = ""
+        return raw_metadata
+
+    def write_metadata(self, metadata):
+        """Write the metadata to the archive"""
+
+        if metadata is None:
+            return False
+        self.apply_archive_info_to_metadata(metadata, calc_page_sizes=True)
+        md_string = ComicInfoXml().string_from_metadata(metadata)
+        write_success = self.archiver.write_archive_file(
+            self.ci_xml_filename, md_string
+        )
+        if write_success:
+            self.has_md = True
+            self.metadata = metadata
+        self.reset_cache()
+        return write_success
+
+    def remove_metadata(self):
+        """Remove the metadata from the archive if present"""
+
+        if self.has_metadata():
+            write_success = self.archiver.remove_archive_file(self.ci_xml_filename)
+            if write_success:
+                self.has_md = False
+                self.metadata = None
+            self.reset_cache()
+            return write_success
+        return True
+
+    def has_metadata(self):
+        """Checks to see if the archive has metadata"""
+
+        if self.has_md is None:
+
+            if (
+                not self.seems_to_be_a_comic_archive()
+                or self.seems_to_be_a_comic_archive()
+                and self.ci_xml_filename
+                not in self.archiver.get_archive_filename_list()
+            ):
+                self.has_md = False
+            else:
+                self.has_md = True
+        return self.has_md
+
+    def apply_archive_info_to_metadata(self, metadata, calc_page_sizes=False):
+        """Apply page information from the archive to the metadata"""
+
+        metadata.page_count = self.get_number_of_pages()
+
+        if calc_page_sizes:
+            for page in metadata.pages:
+                if (
+                    "ImageSize" not in page
+                    or "ImageHeight" not in page
+                    or "ImageWidth" not in page
+                ):
+                    idx = int(page["Image"])
+                    data = self.get_page(idx)
+                    if data is not None:
+                        try:
+                            page_image = Image.open(io.BytesIO(data))
+                            width, height = page_image.size
+
+                            page["ImageSize"] = str(len(data))
+                            page["ImageHeight"] = str(height)
+                            page["ImageWidth"] = str(width)
+                        except IOError:
+                            page["ImageSize"] = str(len(data))
+
+    def metadata_from_filename(self, parse_scan_info=True):
+        """Attempts to get the metadata from the filename"""
+
+        metadata = GenericMetadata()
+
+        fnp = FileNameParser()
+        fnp.parse_filename(self.path)
+
+        if fnp.issue != "":
+            metadata.issue = fnp.issue
+        if fnp.series != "":
+            metadata.series = fnp.series
+        if fnp.volume != "":
+            metadata.volume = fnp.volume
+        if fnp.year != "":
+            metadata.year = fnp.year
+        if fnp.issue_count != "":
+            metadata.issueCount = fnp.issue_count
+        if parse_scan_info and fnp.remainder != "":
+            metadata.scanInfo = fnp.remainder
+
+        metadata.isEmpty = False
+
+        return metadata
---
+++ packages/darkseid/darkseid-1.0.1-py3-none-any.whl/darkseid/genericmetadata.py
@@ -0,0 +1,292 @@
+"""A class for internal metadata storage
+
+The goal of this class is to handle ALL the data that might come from various
+tagging schemes and databases, such as ComicVine or GCD.  This makes conversion
+possible, however lossy it might be
+
+"""
+
+# Copyright 2012-2014 Anthony Beville
+
+from . import utils
+
+
+class PageType:
+
+    """
+    These page info classes are exactly the same as the CIX scheme, since
+    it's unique
+    """
+
+    FrontCover = "FrontCover"
+    InnerCover = "InnerCover"
+    Roundup = "Roundup"
+    Story = "Story"
+    Advertisement = "Advertisement"
+    Editorial = "Editorial"
+    Letters = "Letters"
+    Preview = "Preview"
+    BackCover = "BackCover"
+    Other = "Other"
+    Deleted = "Deleted"
+
+
+class GenericMetadata:
+    def __init__(self):
+
+        self.is_empty = True
+        self.tag_origin = None
+
+        self.series = None
+        self.issue = None
+        self.title = None
+        self.publisher = None
+        self.month = None
+        self.year = None
+        self.day = None
+        self.issue_count = None
+        self.volume = None
+        self.genre = None
+        self.language = None  # 2 letter iso code
+        self.comments = None  # use same way as Summary in CIX
+
+        self.volume_count = None
+        self.critical_rating = None
+        self.country = None
+
+        self.alternate_series = None
+        self.alternate_number = None
+        self.alternate_count = None
+        self.imprint = None
+        self.notes = None
+        self.web_link = None
+        self.format = None
+        self.manga = None
+        self.black_and_white = None
+        self.page_count = None
+        self.maturity_rating = None
+
+        self.story_arc = None
+        self.series_group = None
+        self.scan_info = None
+
+        self.characters = None
+        self.teams = None
+        self.locations = None
+
+        self.credits = []
+        self.tags = []
+        self.pages = []
+
+        # Some CoMet-only items
+        self.price = None
+        self.is_version_of = None
+        self.rights = None
+        self.identifier = None
+        self.last_mark = None
+        self.cover_image = None
+
+    def overlay(self, new_md):
+        """Overlay a metadata object on this one
+
+        That is, when the new object has non-None values, over-write them
+        to this one.
+        """
+
+        def assign(cur, new):
+            if new is not None:
+                if isinstance(new, str) and len(new) == 0:
+                    setattr(self, cur, None)
+                else:
+                    setattr(self, cur, new)
+
+        if not new_md.is_empty:
+            self.is_empty = False
+
+        assign("series", new_md.series)
+        assign("issue", new_md.issue)
+        assign("issue_count", new_md.issue_count)
+        assign("title", new_md.title)
+        assign("publisher", new_md.publisher)
+        assign("day", new_md.day)
+        assign("month", new_md.month)
+        assign("year", new_md.year)
+        assign("volume", new_md.volume)
+        assign("volume_count", new_md.volume_count)
+        assign("genre", new_md.genre)
+        assign("language", new_md.language)
+        assign("country", new_md.country)
+        assign("critical_rating", new_md.critical_rating)
+        assign("alternate_series", new_md.alternate_series)
+        assign("alternate_number", new_md.alternate_number)
+        assign("alternate_count", new_md.alternate_count)
+        assign("imprint", new_md.imprint)
+        assign("web_link", new_md.web_link)
+        assign("format", new_md.format)
+        assign("manga", new_md.manga)
+        assign("black_and_white", new_md.black_and_white)
+        assign("maturity_rating", new_md.maturity_rating)
+        assign("story_arc", new_md.story_arc)
+        assign("series_group", new_md.series_group)
+        assign("scan_info", new_md.scan_info)
+        assign("characters", new_md.characters)
+        assign("teams", new_md.teams)
+        assign("locations", new_md.locations)
+        assign("comments", new_md.comments)
+        assign("notes", new_md.notes)
+
+        assign("price", new_md.price)
+        assign("is_version_of", new_md.is_version_of)
+        assign("rights", new_md.rights)
+        assign("identifier", new_md.identifier)
+        assign("last_mark", new_md.last_mark)
+
+        self.overlay_credits(new_md.credits)
+        # TODO
+
+        # not sure if the tags and pages should broken down, or treated
+        # as whole lists....
+
+        # For now, go the easy route, where any overlay
+        # value wipes out the whole list
+        if len(new_md.tags) > 0:
+            assign("tags", new_md.tags)
+
+        if len(new_md.pages) > 0:
+            assign("pages", new_md.pages)
+
+    def overlay_credits(self, new_credits):
+        for credit in new_credits:
+            primary = True if "primary" in credit and credit["primary"] else False
+            # Remove credit role if person is blank
+            if credit["person"] == "":
+                for r in reversed(self.credits):
+                    if r["role"].lower() == credit["role"].lower():
+                        self.credits.remove(r)
+            # otherwise, add it!
+            else:
+                self.add_credit(credit["person"], credit["role"], primary)
+
+    def set_default_page_list(self, count):
+        # generate a default page list, with the first page marked as the cover
+        for i in range(count):
+            page_dict = {"Image": str(i)}
+            if i == 0:
+                page_dict["Type"] = PageType.FrontCover
+            self.pages.append(page_dict)
+
+    def get_archive_page_index(self, pagenum):
+        # convert the displayed page number to the page index of the file in
+        # the archive
+        if pagenum < len(self.pages):
+            return int(self.pages[pagenum]["Image"])
+        else:
+            return 0
+
+    def get_cover_page_index_list(self):
+        # return a list of archive page indices of cover pages
+        coverlist = [
+            int(page["Image"])
+            for page in self.pages
+            if "Type" in page and page["Type"] == PageType.FrontCover
+        ]
+
+        if not coverlist:
+            coverlist.append(0)
+
+        return coverlist
+
+    def add_credit(self, person, role, primary=False):
+
+        credit = {"person": person, "role": role}
+        if primary:
+            credit["primary"] = primary
+
+        # look to see if it's not already there...
+        found = False
+        for c in self.credits:
+            if (
+                c["person"].lower() == person.lower()
+                and c["role"].lower() == role.lower()
+            ):
+                # no need to add it. just adjust the "primary" flag as needed
+                c["primary"] = primary
+                found = True
+                break
+
+        if not found:
+            self.credits.append(credit)
+
+    def __str__(self):
+        vals = []
+        if self.is_empty:
+            return "No metadata"
+
+        def add_string(tag, val):
+            if val is not None and "{0}".format(val) != "":
+                vals.append((tag, val))
+
+        def add_attr_string(tag):
+            add_string(tag, getattr(self, tag))
+
+        add_attr_string("series")
+        add_attr_string("issue")
+        add_attr_string("issue_count")
+        add_attr_string("title")
+        add_attr_string("publisher")
+        add_attr_string("year")
+        add_attr_string("month")
+        add_attr_string("day")
+        add_attr_string("volume")
+        add_attr_string("volume_count")
+        add_attr_string("genre")
+        add_attr_string("language")
+        add_attr_string("country")
+        add_attr_string("critical_rating")
+        add_attr_string("alternate_series")
+        add_attr_string("alternate_number")
+        add_attr_string("alternate_count")
+        add_attr_string("imprint")
+        add_attr_string("web_link")
+        add_attr_string("format")
+        add_attr_string("manga")
+
+        add_attr_string("price")
+        add_attr_string("is_version_of")
+        add_attr_string("rights")
+        add_attr_string("identifier")
+        add_attr_string("last_mark")
+
+        if self.black_and_white:
+            add_attr_string("black_and_white")
+        add_attr_string("maturity_rating")
+        add_attr_string("story_arc")
+        add_attr_string("series_group")
+        add_attr_string("scan_info")
+        add_attr_string("characters")
+        add_attr_string("teams")
+        add_attr_string("locations")
+        add_attr_string("comments")
+        add_attr_string("notes")
+
+        add_string("tags", utils.list_to_string(self.tags))
+
+        for c in self.credits:
+            primary = ""
+            if "primary" in c and c["primary"]:
+                primary = " [P]"
+            add_string("credit", c["role"] + ": " + c["person"] + primary)
+
+        # find the longest field name
+        flen = 0
+        for i in vals:
+            flen = max(flen, len(i[0]))
+        flen += 1
+
+        # format the data nicely
+        outstr = ""
+        fmt_str = "{0: <" + str(flen) + "} {1}\n"
+        for i in vals:
+            outstr += fmt_str.format(i[0] + ":", i[1])
+
+        return outstr
