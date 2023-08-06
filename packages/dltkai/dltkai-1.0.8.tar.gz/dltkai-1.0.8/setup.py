61618df398a243f636c25eb8f871eab7799caca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:33 +0000

    Add package dltkai/1.0.8/dltkai-1.0.8.tar.gz

---
+++ packages/dltkai/dltkai-1.0.8.tar.gz/dltkai-1.0.8/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dltkai",
+    version="1.0.8",
+    author="DLTK",
+    author_email="connect@qubitai.tech",
+    description="Python Client for DLTK.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dltk-ai/dltkai-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
