ad0d74c08bdea0db3a52bcdfc838499cfd47cb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:29 +0000

    Add package flask-jobs/0.0.15/flask_jobs-0.0.15.tar.gz

---
+++ packages/flask-jobs/flask_jobs-0.0.15.tar.gz/flask_jobs-0.0.15/flask_jobs/worker.py
@@ -0,0 +1,49 @@
+import datetime
+import threading
+from .jobs import Job
+
+
+class Worker:
+    def __init__(self, db=None):
+        self._running = True
+        self._db = db
+        self._timer = None
+        self._lock = threading.Lock()
+
+    def Refresh(self, newTimeout=0):
+        self.StopTimer()
+        self._timer = threading.Timer(newTimeout, self.DoJobs)
+        self._timer.start()
+
+    def StopTimer(self):
+        if self._timer:
+            if self._timer.is_alive():
+                self._timer.cancel()
+
+    def DoJobs(self):
+        if self._running:
+            with self._lock:
+                self.StopTimer()
+                nowDT = datetime.datetime.utcnow()
+
+                # Do all jobs that are past their self['dt']
+                for job in self._db.FindAll(Job, status='pending', _orderBy='dt'):
+                    if job['dt'] < nowDT:
+                        job.DoJob(self)
+                        del job  # forces the job to be committed to db
+
+                # Find the next 'schedule' or 'repeat' Job
+                nextJobList = list(self._db.FindAll(Job, status='pending', _orderBy='dt', _limit=1))
+                # print('nextJobList=', nextJobList)
+                if nextJobList:
+                    nextJob = nextJobList[0]
+                    delta = (nextJob['dt'] - nowDT).total_seconds()
+                    # print('delta=', delta)
+                    self.Refresh(delta)
+
+    def __del__(self):
+        self.Kill()
+
+    def Kill(self):
+        self._running = False
+        self.StopTimer()
