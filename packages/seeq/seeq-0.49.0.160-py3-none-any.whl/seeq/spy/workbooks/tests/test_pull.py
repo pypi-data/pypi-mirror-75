a1cd2ceb4e5ee7fa41980b3740ccac0ea024ac62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:15 +0000

    Add package seeq/0.49.0.160/seeq-0.49.0.160-py3-none-any.whl

---
+++ packages/seeq/seeq-0.49.0.160-py3-none-any.whl/seeq/spy/workbooks/tests/test_pull.py
@@ -0,0 +1,62 @@
+import pytest
+
+from seeq import spy
+from seeq.spy.workbooks._content import Content
+
+from . import test_load
+from .. import Workbook
+from ...tests import test_common
+
+
+def setup_module():
+    test_common.login()
+
+
+# The tests for pulling workbooks are light because so much of the functionality is tested in the push code. I.e.,
+# the push code wouldn't work if the pull code had a problem, since the pull code is what produced the saved workbooks.
+# (Same goes for the spy.workbooks.save() functionality.)
+
+@pytest.mark.system
+def test_pull():
+    push_workbooks = test_load.load_example_export()
+
+    push_df = spy.workbooks.push(push_workbooks, refresh=False, path='test_pull', label='test_pull')
+
+    # Make sure the "include_references" functionality works properly by just specifying the Topic. It'll pull in
+    # the Analyses
+    to_pull_df = push_df[push_df['Workbook Type'] == 'Topic'].copy()
+
+    to_pull_df.drop(columns=['ID'], inplace=True)
+    to_pull_df.rename(columns={'Pushed Workbook ID': 'ID'}, inplace=True)
+    to_pull_df['Type'] = 'Workbook'
+
+    pull_workbooks = spy.workbooks.pull(to_pull_df)
+
+    pull_workbooks = sorted(pull_workbooks, key=lambda w: w['Workbook Type'])
+
+    analysis = pull_workbooks[0]  # type: Workbook
+    assert analysis.id == (push_df[push_df['Workbook Type'] == 'Analysis'].iloc[0]['Pushed Workbook ID'])
+    assert analysis.name == (push_df[push_df['Workbook Type'] == 'Analysis'].iloc[0]['Name'])
+    assert len(analysis.datasource_maps) >= 3
+    assert len(analysis.item_inventory) >= 25
+
+    assert analysis['URL'] == push_df[push_df['Workbook Type'] == 'Analysis'].iloc[0]['URL']
+
+    worksheet_names = [w.name for w in analysis.worksheets]
+    assert worksheet_names == [
+        'Details Pane',
+        'Calculated Items',
+        'Histogram',
+        'Metrics',
+        'Journal',
+        'Global',
+        'Boundaries'
+    ]
+
+    topic = pull_workbooks[1]
+    worksheet_names = [w.name for w in topic.worksheets]
+    assert len(topic.datasource_maps) == 2
+    assert worksheet_names == [
+        'Static Doc',
+        'Live Doc'
+    ]
