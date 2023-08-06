7bc9cdee30f8ccddabbbf2c42f81b835dfd8915b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:48 +0000

    Add package rl-plotter/2.0.0/rl_plotter-2.0.0.tar.gz

---
+++ packages/rl-plotter/rl_plotter-2.0.0.tar.gz/rl_plotter-2.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rl_plotter",
+    version="2.0.0",
+    author="Gong Xiaoyu",
+    author_email="gxywy@hotmail.com",
+    description="A plotter for reinforcement learning (RL)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gxywy/rl-plotter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
