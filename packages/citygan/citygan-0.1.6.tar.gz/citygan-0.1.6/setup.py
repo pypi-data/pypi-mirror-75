707ec34fb7720eecf22cd7e3740e236f510a23e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:49 +0000

    Add package citygan/0.1.6/citygan-0.1.6.tar.gz

---
+++ packages/citygan/citygan-0.1.6.tar.gz/citygan-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+setup(
+  name = 'citygan',
+  packages = ['citygan'],
+  version = '0.1.6',
+  license='MIT',
+  description = 'Procedurally generated cities using generative adversarial networks',
+  author = 'Thomas Allen, Eric Lehmann, Jacob Roberge, Christopher Ward, Adrian Wright',
+  url = 'https://github.com/ProjectCity-Group/gan-cities',
+  download_url = 'https://github.com/ProjectCity-Group/gan-cities/archive/v0.1.3.tar.gz',
+  keywords = ['gan', 'city', 'procedural', 'generation'],
+  install_requires=[
+        'wheel',
+        'imageio',
+        'matplotlib',
+        'numpy',
+        'tensorflow==1.14'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
+
