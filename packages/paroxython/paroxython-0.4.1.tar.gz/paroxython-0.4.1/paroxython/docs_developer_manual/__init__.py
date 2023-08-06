4eda349f49f703aa128914a6f188154fae8ae333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:35 +0000

    Add package paroxython/0.4.1/paroxython-0.4.1.tar.gz

---
+++ packages/paroxython/paroxython-0.4.1.tar.gz/paroxython-0.4.1/paroxython/docs_developer_manual/__init__.py
@@ -0,0 +1,13 @@
+r"""
+.. include:: ../../docs/md/bird_view.md
+
+.. include:: ../../docs/md/helpers.md
+
+.. include:: ../../docs/md/implementation_notes.md
+
+*[AST]: Abstract Syntax Tree
+*[SQL]: Structured Query Language
+*[TSV]: Tab Separated Values
+*[JSON]: JavaScript Object Notation
+*[SLOC]: Source Lines Of Code
+"""
