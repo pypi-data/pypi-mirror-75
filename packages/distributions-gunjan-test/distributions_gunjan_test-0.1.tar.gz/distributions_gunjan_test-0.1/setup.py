13ee3e35d69b4cca8470c0cbacdb2b4dfeeab3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:28 +0000

    Add package distributions-gunjan-test/0.1/distributions_gunjan_test-0.1.tar.gz

---
+++ packages/distributions-gunjan-test/distributions_gunjan_test-0.1.tar.gz/distributions_gunjan_test-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_gunjan_test',
+      version='0.1',
+      description='Gaussian distributions and Binomial distribution',
+      packages=['distributions_gunjan_test'],
+      zip_safe=False)
