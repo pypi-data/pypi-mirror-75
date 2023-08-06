790843a6e03154dbecb9ca8e59cb3293eaf8a275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:55 +0000

    Add package light1/1.0.5/light1-1.0.5.tar.gz

---
+++ packages/light1/light1-1.0.5.tar.gz/light1-1.0.5/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name='light1',
+    version='1.0.5',
+    py_modules=['drowndemo'
+                'drown'
+                'drownade'
+                'onecrossingdemo'
+                'linerproblem'
+                'adelaytime'
+                'doublecrossing'
+                'doublecrossingdemo'],
+    author='xiaohan',
+    author_email='xiahazh@163.com',
+    url='http://www.baidu.com',
+    description='traffic_light_teaching',
+)
