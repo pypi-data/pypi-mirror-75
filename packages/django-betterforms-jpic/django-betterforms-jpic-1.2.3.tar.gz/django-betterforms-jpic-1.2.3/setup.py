5bb4a6c9236ae0c2962f4cf6e1e7505b412e72a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:23 +0000

    Add package django-betterforms-jpic/1.2.3/django-betterforms-jpic-1.2.3.tar.gz

---
+++ packages/django-betterforms-jpic/django-betterforms-jpic-1.2.3.tar.gz/django-betterforms-jpic-1.2.3/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+__doc__ = """
+App for Django featuring improved form base classes.
+"""
+
+version = '1.2.3'
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='django-betterforms-jpic',
+    version=version,
+    description=__doc__,
+    url="https://django-betterforms.readthedocs.org/en/latest/",
+    author="Fusionbox",
+    author_email='programmers@fusionbox.com',
+    packages=[package for package in find_packages()
+              if package.startswith('betterforms')],
+    install_requires=['Django>=1.8', 'six'],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
