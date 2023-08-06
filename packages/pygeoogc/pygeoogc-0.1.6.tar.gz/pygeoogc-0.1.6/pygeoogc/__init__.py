d37b2f87c6a0b3d322a5386fe823579a42d30a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:09 +0000

    Add package pygeoogc/0.1.6/pygeoogc-0.1.6.tar.gz

---
+++ packages/pygeoogc/pygeoogc-0.1.6.tar.gz/pygeoogc-0.1.6/pygeoogc/__init__.py
@@ -0,0 +1,18 @@
+"""Top-level package for PyGeoOGC."""
+from pkg_resources import DistributionNotFound, get_distribution
+
+from .exceptions import (
+    InvalidInputType,
+    InvalidInputValue,
+    MissingInputs,
+    ServerError,
+    ThreadingException,
+    ZeroMatched,
+)
+from .pygeoogc import WFS, WMS, ArcGISRESTful, MatchCRS, RetrySession, ServiceURL
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
