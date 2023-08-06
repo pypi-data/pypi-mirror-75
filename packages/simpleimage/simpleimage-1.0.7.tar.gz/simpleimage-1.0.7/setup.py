7b9af419bfd922da1b1d2866e48785ae060121fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:40 +0000

    Add package simpleimage/1.0.7/simpleimage-1.0.7.tar.gz

---
+++ packages/simpleimage/simpleimage-1.0.7.tar.gz/simpleimage-1.0.7/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'simpleimage',
+  packages = ['simpleimage'],
+  version = '1.0.7',
+  license='MIT',
+  description = 'Simple image API for educational purposes',
+  author = '3e8',
+  author_email = 'info@3e8.ch',
+  url = 'https://github.com/adilu/simpleimage',
+  download_url = 'https://github.com/adilu/simpleimage/archive/v_1.0.7.tar.gz',
+  keywords = ['Image', 'python', 'educational'],
+  install_requires=[
+          'numpy',
+          'matplotlib',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
---
+++ packages/simpleimage/simpleimage-1.0.7.tar.gz/simpleimage-1.0.7/simpleimage/__init__.py
@@ -0,0 +1,3 @@
+# Inside of __init__.py
+from simpleimage.Image import Image
+from simpleimage.Pixel import Pixel
