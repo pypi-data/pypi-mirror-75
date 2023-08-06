b3b0cfd0639f1d181dac6fa2d2bb551bde9a7c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:33 +0000

    Add package distributions-smd/0.1/distributions-smd-0.1.tar.gz

---
+++ packages/distributions-smd/distributions-smd-0.1.tar.gz/distributions-smd-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-smd',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-smd'],
+      author = 'Sandra',
+      author_email = 'sandra@email.com',
+      zip_safe=False)
