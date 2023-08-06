709f84eca8d2e4a04771234b307be317126f0dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:42 +0000

    Add package sgmail/0.0.4/sgmail-0.0.4-py3-none-any.whl

---
+++ packages/sgmail/sgmail-0.0.4-py3-none-any.whl/sgmail/sgmail.py
@@ -0,0 +1,31 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def send_gmail(gmail, password, to_email_addresses, subject='', html_head='', html_body='', html=None):
+    if html is None:
+        html = '''
+        <html>
+            <head>{}</head>
+            <body>{}</body>
+        </html>
+            '''.format(html_head, html_body)
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = gmail
+    msg['To'] = to_email_addresses
+
+    msg.attach(MIMEText(html, 'html'))
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail, password)
+        try:
+            server.sendmail(gmail, to_email_addresses, msg.as_string())
+        finally:
+            server.quit()
+    except():
+        print("Something went wrong sending the email")
