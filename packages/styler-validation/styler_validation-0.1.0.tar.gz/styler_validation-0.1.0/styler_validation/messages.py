cc08e25c248f5be622a03f1334c234ebab1afd88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:39 +0000

    Add package styler-validation/0.1.0/styler_validation-0.1.0.tar.gz

---
+++ packages/styler-validation/styler_validation-0.1.0.tar.gz/styler_validation-0.1.0/styler_validation/messages.py
@@ -0,0 +1,13 @@
+""" Common validation messages
+"""
+
+INVALID_VALUE = 'Invalid value'
+REQUIRED_VALUE = 'Required field'
+GREATER_THAN = 'Should be greater than '
+GREATER_OR_EQUAL_THAN = 'Should be greater or equal than '
+LESS_THAN = 'Should be less than '
+LESS_OR_EQUAL_THAN = 'Should be less or equal than '
+POSITIVE_VALUE = 'Must be positive'
+NOT_ZERO = 'Should not be zero'
+INVALID_TIME = 'Invalid time format, should be in the format HH:MM'
+NOT_EMPTY = 'Should not be empty'
