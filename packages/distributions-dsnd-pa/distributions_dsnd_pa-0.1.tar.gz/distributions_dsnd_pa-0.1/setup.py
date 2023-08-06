e6d2020d33d13dba5a21f7b03b91738590bbe9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:53 +0000

    Add package distributions-dsnd-pa/0.1/distributions_dsnd_pa-0.1.tar.gz

---
+++ packages/distributions-dsnd-pa/distributions_dsnd_pa-0.1.tar.gz/distributions_dsnd_pa-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_dsnd_pa',
+      version='0.1',
+      description='Gaussian amd Binomial distributions',
+      packages=['distributions_dsnd_pa'],
+      zip_safe=False)
