29de99f139a18cb2b8025632caeafbb565effa1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:39 +0000

    Add package hk-distributions/0.1/hk-distributions-0.1.tar.gz

---
+++ packages/hk-distributions/hk-distributions-0.1.tar.gz/hk-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hk-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hk-distributions'],
+      zip_safe=False)
