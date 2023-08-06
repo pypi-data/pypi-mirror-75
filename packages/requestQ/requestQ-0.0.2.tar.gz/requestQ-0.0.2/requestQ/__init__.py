0ef52977b25f4f025e8af221a4d0ea2a6bc5af54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:46 +0000

    Add package requestQ/0.0.2/requestQ-0.0.2.tar.gz

---
+++ packages/requestQ/requestQ-0.0.2.tar.gz/requestQ-0.0.2/requestQ/__init__.py
@@ -0,0 +1,5 @@
+# from request.package.domssql import DoMssql
+from request.package.domysql import DoMysql
+from request.package.dorequest import DoRequest
+from request.package.item import item,fetch
+from request.package.case import Case
