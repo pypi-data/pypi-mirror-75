9660b7126058c18172ced8420bf1a61232974b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:01 +0000

    Add package simpil/1.2.0/simpil-1.2.0.tar.gz

---
+++ packages/simpil/simpil-1.2.0.tar.gz/simpil-1.2.0/simpil/_metadata.py
@@ -0,0 +1,29 @@
+
+# Variables for setup (these must be string only!)
+__module_name__ = u'simpil'
+__description__ = u'Simplifies use is Pillow for adding text to images'
+
+__version__ = u'1.2.0'
+
+__author__ = u'Hywel Thomas'
+__authorshort__ = u'HT'
+__authoremail__ = u'hywel.thomas@mac.com'
+
+__license__ = u'MIT'
+
+__githost__ = 'bitbucket.org'
+__gituser__ = u'daycoder'
+__gitrepo__ = u'simpil.git'
+
+
+# Additional variables
+__copyright__ = u'Copyright (C) 2016 {author}'.format(author=__author__)
+
+__url__ = u'https://{host}/{user}/{repo}'.format(host=__githost__,
+                                                 user=__gituser__,
+                                                 repo=__gitrepo__)
+__downloadurl__ = u'{url}/get/{version}.tar'.format(url=__url__,
+                                                    host=__githost__,
+                                                    user=__gituser__,
+                                                    repo=__gitrepo__,
+                                                    version=__version__)
