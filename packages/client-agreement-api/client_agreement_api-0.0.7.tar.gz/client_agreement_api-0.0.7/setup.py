98196fe35eac768493cd9aa00f7dc2f6b8c36b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:10 +0000

    Add package client-agreement-api/0.0.7/client_agreement_api-0.0.7.tar.gz

---
+++ packages/client-agreement-api/client_agreement_api-0.0.7.tar.gz/client_agreement_api-0.0.7/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_requirements = list()
+
+# with open("_setup_requirements.txt", 'r') as fh:
+#     for line in fh.readlines():
+#         requirement = line.strip("\n")
+#         setup_requirements.append(requirement)
+
+requirements = [
+    "connexion==2.7.0",
+    "flask==1.1.1",
+    "Flask-SQLAlchemy==2.4.4",
+    "flask-marshmallow",
+    "marshmallow",
+    "marshmallow-sqlalchemy",
+    "Werkzeug",
+]
+
+setuptools.setup(
+    name="client_agreement_api",
+    version="0.0.7",
+    author="Abdullah Abid",
+    author_email="abdullahabid3691@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Abdullahabid3691/ClientAgreement-Api",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
