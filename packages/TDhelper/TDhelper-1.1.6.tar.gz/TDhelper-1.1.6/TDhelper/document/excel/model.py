01815d89e932628cce8a2797f1c0d683d57f98a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:26 +0000

    Add package TDhelper/1.1.6/TDhelper-1.1.6.tar.gz

---
+++ packages/TDhelper/TDhelper-1.1.6.tar.gz/TDhelper-1.1.6/TDhelper/document/excel/model.py
@@ -0,0 +1,10 @@
+from TDhelper.document.excel.meta.modelMeta import modelMeta
+class model(metaclass=modelMeta):
+    def __enter__(self):
+        return self
+
+    def readLine(self, lineOffset=1):
+        return True
+
+    def close(self):
+        return None
