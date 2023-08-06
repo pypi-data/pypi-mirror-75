f10a2c192b2e21397010c3c74ed7dd4ba84b1026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:51 +0000

    Add package rawa-dist-distributions/0.1/rawa_dist_distributions-0.1.tar.gz

---
+++ packages/rawa-dist-distributions/rawa_dist_distributions-0.1.tar.gz/rawa_dist_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rawa_dist_distributions',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['rawa_dist_distributions'],
+      zip_safe=False)
