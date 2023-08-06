5fdc707c17368b852f62d561753ea47384840b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:50 +0000

    Add package soldier/0.2.5/soldier-0.2.5.tar.gz

---
+++ packages/soldier/soldier-0.2.5.tar.gz/soldier-0.2.5/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = None
+with open('soldier/__init__.py', 'r') as f:
+    for line in f.readlines():
+        if '__version__' in line:
+            version = line.split('=')[1].replace('\'', '').strip()
+
+if not version:
+    raise Exception("Error fetching version from soldier/__init__.py")
+
+setup(
+    name='soldier',
+    version=version,
+    author='Yash Mehrotra',
+    author_email='yashmehrotra95@gmail.com',
+    packages=['soldier'],
+    url='https://pypi.python.org/pypi/soldier',
+    license='Apache 2.0',
+    description='Subprocess with Style',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+)
---
+++ packages/soldier/soldier-0.2.5.tar.gz/soldier-0.2.5/soldier/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+███████╗ ██████╗ ██╗     ██████╗ ██╗███████╗██████╗
+██╔════╝██╔═══██╗██║     ██╔══██╗██║██╔════╝██╔══██╗
+███████╗██║   ██║██║     ██║  ██║██║█████╗  ██████╔╝
+╚════██║██║   ██║██║     ██║  ██║██║██╔══╝  ██╔══██╗
+███████║╚██████╔╝███████╗██████╔╝██║███████╗██║  ██║
+╚══════╝ ╚═════╝ ╚══════╝╚═════╝ ╚═╝╚══════╝╚═╝  ╚═╝
+
+
+Soldier is an Apache2 licensed library designed for executing system
+processes with ease. It is written on top of subprocess and has a much
+user-friendly and pythonic interface.
+
+"""
+
+__title__ = 'soldier'
+__version__ = '0.2.5'
+__author__ = 'Yash Mehrotra'
+__license__ = 'Apache 2.0'
+
+from .soldier import run
+from .exceptions import (
+    InvalidCommandError,
+    ProcessDoesNotExistError,
+    ProcessTimeoutError
+)
+
+__all__ = ['run', 'InvalidCommandError',
+           'ProcessDoesNotExistError', 'ProcessTimeoutError']
