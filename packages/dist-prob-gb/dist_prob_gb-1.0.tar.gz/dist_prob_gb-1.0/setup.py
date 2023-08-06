6eac1041c4a9f1de73581d13a15219e7c54d7d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:46 +0000

    Add package dist-prob-gb/1.0/dist_prob_gb-1.0.tar.gz

---
+++ packages/dist-prob-gb/dist_prob_gb-1.0.tar.gz/dist_prob_gb-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_prob_gb',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_prob_gb'],
+      author = 'Ismail Musharaf',
+      author_email = 'ismailmushraf@gmail.com',
+      zip_safe=False)
