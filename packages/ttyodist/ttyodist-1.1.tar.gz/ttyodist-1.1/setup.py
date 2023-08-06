3e9b179778c23731506e1592c091b57d69b707de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:45 +0000

    Add package ttyodist/1.1/ttyodist-1.1.tar.gz

---
+++ packages/ttyodist/ttyodist-1.1.tar.gz/ttyodist-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ttyodist',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ttyodist'],
+      author='Thomas DuQuesnay',
+      email='ttyo@nospam.com',
+      zip_safe=False)
