e299cfe96d058d73767817396731707ad856d969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:14 +0000

    Add package ani-probablity/0.1/ani_probablity-0.1.tar.gz

---
+++ packages/ani-probablity/ani_probablity-0.1.tar.gz/ani_probablity-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ani_probablity',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ani_probablity'],
+      author='Anirudh singh sanger',
+      zip_safe=False)
