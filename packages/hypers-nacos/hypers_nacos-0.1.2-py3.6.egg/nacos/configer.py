02d15e359264fe04726aba234823f930ada2b03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:33 +0000

    Add package hypers-nacos/0.1.2/hypers_nacos-0.1.2-py3.6.egg

---
+++ packages/hypers-nacos/hypers_nacos-0.1.2-py3.6.egg/nacos/configer.py
@@ -0,0 +1,16 @@
+import nacos
+from django.conf import settings
+
+
+def client(namespace: str = None):
+    """client 获取nacos客户端
+    需配置settings.NACOS = dict(servers='a,b', namespace='namespace id', ak='username', sk='password')
+
+    :param namespace: 命名空间id, defaults to None
+    :type namespace: str, optional
+    """
+
+    servers = settings.NACOS["servers"]
+    namespace = namespace or settings.NACOS["namespace"]
+    ak = settings.NACOS["ak"]
+    return nacos.NacosClient(servers, namespace=namespace, ak=ak, sk=sk)
