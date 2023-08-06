cde0906d301f47715342135e1da756dcd3cc2d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:54 +0000

    Add package aa-stripe/0.9.7/aa_stripe-0.9.7.tar.gz

---
+++ packages/aa-stripe/aa_stripe-0.9.7.tar.gz/aa_stripe-0.9.7/aa_stripe/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__title__ = "Ro Stripe"
+__version__ = "0.9.7"
+__author__ = "Remigiusz Dymecki"
+__license__ = "MIT"
+__copyright__ = "Copyright 2019 Ro"
+
+# Version synonym
+VERSION = __version__
