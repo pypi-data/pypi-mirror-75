e7f3b253e61ac1bda7784f1fe83c1eed23e65aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:13 +0000

    Add package hidebound/0.8.1/hidebound-0.8.1.tar.gz

---
+++ packages/hidebound/hidebound-0.8.1.tar.gz/hidebound-0.8.1/hidebound/core/traits.py
@@ -0,0 +1,59 @@
+from typing import Union
+
+from pathlib import Path
+
+import skimage.io
+# ------------------------------------------------------------------------------
+
+
+'''
+The traits module contains functions that return file traits give a filepath.
+These traits are used for validation of specifications.
+'''
+
+
+def get_image_width(filepath):
+    # type: (Union[str, Path]) -> int
+    '''
+    Gets the width of the given image.
+
+    Args:
+        filepath (str or Path): filepath to image file.
+
+    Returns:
+        int: Image width.
+    '''
+    img = skimage.io.imread(Path(filepath).as_posix())
+    return img.shape[1]
+
+
+def get_image_height(filepath):
+    # type: (Union[str, Path]) -> int
+    '''
+    Gets the height of the given image.
+
+    Args:
+        filepath (str or Path): filepath to image file.
+
+    Returns:
+        int: Image height.
+    '''
+    img = skimage.io.imread(Path(filepath).as_posix())
+    return img.shape[0]
+
+
+def get_image_channels(filepath):
+    # type: (Union[str, Path]) -> int
+    '''
+    Gets the number of channels of the given image.
+
+    Args:
+        filepath (str or Path): filepath to image file.
+
+    Returns:
+        int: Number of channels.
+    '''
+    img = skimage.io.imread(Path(filepath).as_posix())
+    if len(img.shape) > 2:
+        return img.shape[2]
+    return 1
