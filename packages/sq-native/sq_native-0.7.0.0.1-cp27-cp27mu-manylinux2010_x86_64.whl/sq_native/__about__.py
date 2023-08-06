9feaaba4ea6b014ac32b4ee9621c2a71f96cddc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:47 +0000

    Add package sq-native/0.7.0.0.1/sq_native-0.7.0.0.1-cp27-cp27mu-manylinux2010_x86_64.whl

---
+++ packages/sq-native/sq_native-0.7.0.0.1-cp27-cp27mu-manylinux2010_x86_64.whl/sq_native/__about__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016, 2017, 2018, 2019, 2020, Sqreen. All rights reserved.
+# Please refer to our terms for more information:
+#
+#     https://www.sqreen.io/terms.html
+#
+
+__author__ = "Sqreen"
+__copyright__ = "Copyright 2016, 2017, 2018, 2019, 2020, Sqreen"
+__email__ = "contact@sqreen.io"
+__license__ = "proprietary"
+# The lib version is always contained at the beginning of __version__
+__version__ = "0.7.0.0.1"
+__lib_version__ = "0.7.0"
