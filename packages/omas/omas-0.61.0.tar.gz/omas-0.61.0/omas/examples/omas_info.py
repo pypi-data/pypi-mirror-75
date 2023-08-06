1ca1f5aed655283103cf151e4861d0f966648590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:23 +0000

    Add package omas/0.61.0/omas-0.61.0.tar.gz

---
+++ packages/omas/omas-0.61.0.tar.gz/omas-0.61.0/omas/examples/omas_info.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+IDS info
+========
+This is an example showing how to query OMAS for structural information about IMAS IDSs
+"""
+
+from pprint import pprint
+from omas import *
+
+# test getting information about ids structures
+infos = omas_info(['equilibrium', 'core_profiles'])
+
+pprint(infos['equilibrium']['time_slice'][0]['global_quantities']['ip'])
+
+###################################
+# The above example prints the following::
+#
+#     {'data_type': 'FLT_0D',
+#      'documentation': 'Plasma current. Positive sign means anti-clockwise when viewed from above.',
+#      'full_path': 'equilibrium/time_slice(itime)/global_quantities/ip',
+#      'type': 'dynamic',
+#      'units': 'A'}
