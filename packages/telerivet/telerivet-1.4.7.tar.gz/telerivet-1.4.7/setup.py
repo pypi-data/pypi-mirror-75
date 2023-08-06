4e9b0489b28f62296e5fcae57943ce62d668d5b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:18 +0000

    Add package telerivet/1.4.7/telerivet-1.4.7.tar.gz

---
+++ packages/telerivet/telerivet-1.4.7.tar.gz/telerivet-1.4.7/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='telerivet',
+      version='1.4.7',
+      description='Python client library for Telerivet REST API',
+      author='Telerivet, Inc.',
+      author_email='support@telerivet.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://telerivet.com/',
+      packages=['telerivet'],
+      python_requires='>=2.6',
+      classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ]
+     )
