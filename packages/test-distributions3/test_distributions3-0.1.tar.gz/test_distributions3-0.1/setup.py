e68191bfef14053fe84536a22a82577df0c325b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:58 +0000

    Add package test-distributions3/0.1/test_distributions3-0.1.tar.gz

---
+++ packages/test-distributions3/test_distributions3-0.1.tar.gz/test_distributions3-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='test_distributions3',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['test_distributions3'],
+      author = 'EmmaW',
+      author_email = 'emmayt@outlook.com',
+      zip_safe=False)
