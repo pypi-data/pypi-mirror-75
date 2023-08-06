8ac5e4753f361a8896d8b03ccbfe6e94efe908e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:16 +0000

    Add package metrics2mqtt/0.1.11/metrics2mqtt-0.1.11.tar.gz

---
+++ packages/metrics2mqtt/metrics2mqtt-0.1.11.tar.gz/metrics2mqtt-0.1.11/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+setup(
+  name = 'metrics2mqtt',
+  packages = ['metrics2mqtt'], 
+  version = '0.1.11',
+  license='MIT', 
+  description = 'Publish hardware monitoring data from psutil to a MQTT broker.',  
+  author = 'Jamie Begin',
+  author_email = 'jjbegin@gmail.com',
+  url = 'https://github.com/jamiebegin/metrics2mqtt', 
+  download_url = 'https://github.com/jamiebegin/metrics2mqtt/archive/v0.1.11.tar.gz', 
+  keywords = ['mqtt', 'psutil', 'metrics'], 
+  setup_requires = [
+    'wheel'
+    ],
+  entry_points = {
+        'console_scripts': [
+            'metrics2mqtt = metrics2mqtt.base:main'
+        ]
+  },    
+  install_requires=[ 
+          'jsons',
+          'psutil',
+          'paho-mqtt',
+      ],
+  zip_safe = False,
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Information Technology', 
+    'Topic :: System :: Monitoring',
+    'Topic :: System :: Logging',
+    'Topic :: System :: Systems Administration',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
