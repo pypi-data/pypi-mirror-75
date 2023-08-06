df0e405b4ab02b468bbea3dc68b62d353dcd75b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:35 +0000

    Add package Spinmob/3.3.4/Spinmob-3.3.4.tar.gz

---
+++ packages/Spinmob/Spinmob-3.3.4.tar.gz/Spinmob-3.3.4/setup.py
@@ -0,0 +1,15 @@
+__version__ = '3.3.4' # Keep this on the first line.
+
+
+
+from distutils.core import setup
+setup(name           = 'Spinmob',
+      version        = __version__,
+      description    = 'Data Handling, Plotting, Analysis, and GUI Building for Laboratories',
+      author         = 'Jack Sankey',
+      author_email   = 'jack.sankey@gmail.com',
+      url            = 'https://github.com/Spinmob/spinmob',
+      packages       = ['spinmob', 'spinmob.egg'],
+      package_dir    = {'spinmob'      :   '.',
+                        'egg'          :   'spinmob/egg'}
+     )
