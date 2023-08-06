d67ed153e63fca96edbf86b0256c22265fed10e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:15 +0000

    Add package mifeco/1.0.3/mifeco-1.0.3.tar.gz

---
+++ packages/mifeco/mifeco-1.0.3.tar.gz/mifeco-1.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from sys import version
+
+if version < '2.2.3':
+	from distutils.dist import DistributionMetadata
+	DistributionMetadata.classifiers = None
+	DistributionMetadata.download_url = None
+	
+from distutils.core import setup
+
+setuptools.setup(
+                    name='mifeco',
+                    version='1.0.3',
+                    author='@rockscripts',
+                    author_email='rockscripts@gmail.com',
+                    description='mifeco',
+                    long_description="mifeco",
+                    install_requires=[],
+                    platforms='any',
+                    url='https://instagram.com/rockscripts/',
+                    packages=['mifeco'],
+                    python_requires='>=2.7.*',
+                )
