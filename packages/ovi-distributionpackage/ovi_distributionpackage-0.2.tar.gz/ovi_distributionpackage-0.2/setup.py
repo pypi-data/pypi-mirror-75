7cf3e5297420723b99f5935908ca2788eecd948f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:12 +0000

    Add package ovi-distributionpackage/0.2/ovi_distributionpackage-0.2.tar.gz

---
+++ packages/ovi-distributionpackage/ovi_distributionpackage-0.2.tar.gz/ovi_distributionpackage-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ovi_distributionpackage',
+      version='0.2',
+      description='Gaussian & Binomial distributions',
+      packages=['ovi_distributionpackage'],
+      author='Muhammad Ovais',
+      author_email='movais996@gmail.com',
+      zip_safe=False)
