1362cf410e4f8074d281140e4a90c5ff273a5819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:40 +0000

    Add package distafra/0.1/distafra-0.1.tar.gz

---
+++ packages/distafra/distafra-0.1.tar.gz/distafra-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distafra',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distafra'],
+      author = 'Arfa',
+      author_email = 'arfat2396@gmail.com',
+      zip_safe=False)
