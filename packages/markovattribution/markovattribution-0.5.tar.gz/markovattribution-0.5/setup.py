452bf2cbd0cf9c4b484293d94b5f8b887ca0ce40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:59 +0000

    Add package markovattribution/0.5/markovattribution-0.5.tar.gz

---
+++ packages/markovattribution/markovattribution-0.5.tar.gz/markovattribution-0.5/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'markovattribution',
+  packages = ['markovattribution'],
+  version = '0.5',
+  license='MIT', 
+  description = 'Markov modelling for channel attribution',
+  author = 'David Searle',
+  author_email = 'connect@deepleaf.com.au',
+  url = 'https://github.com/dsearle90/MarkovAttribution',
+  download_url = 'https://github.com/dsearle90/MarkovAttribution/archive/v_05.tar.gz',
+  keywords = ['MARKOV',
+              'ATTRIBUTION',
+              'CHANNELATTRIBUTION',
+              'DIGITALATTRIBUTION',
+              'MARKETING',
+              'MEDIA'],
+  install_requires=[
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
