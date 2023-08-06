1c4f27da4e3b5a5bb6b24cea969d502282ad5c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:10 +0000

    Add package rook/0.1.112/rook-0.1.112-cp36-cp36m-macosx_10_11_x86_64.whl

---
+++ packages/rook/rook-0.1.112-cp36-cp36m-macosx_10_11_x86_64.whl/rook/processor/namespaces/trace_namespace.py
@@ -0,0 +1,59 @@
+from .namespace import Namespace
+from .container_namespace import ContainerNamespace
+from .python_object_namespace import PythonObjectNamespace
+
+from rook.exceptions import RookAttributeNotFound, RookDependencyError, RookDependencyMissing
+
+
+class TraceNamespace(Namespace):
+    def __init__(self):
+        super(TraceNamespace, self).__init__(self.METHODS)
+        self.context = None
+
+    def dump(self, args):
+        if not self._load():
+            return PythonObjectNamespace(None)
+
+        return ContainerNamespace(
+            {
+                "service": self.read_attribute("service"),
+                "meta": self.read_attribute("meta"),
+                "resource": self.read_attribute("resource"),
+                "name": self.read_attribute("name"),
+                "type": self.read_attribute("type"),
+            }
+        )
+
+    def read_attribute(self, name):
+        if not self._load():
+            return PythonObjectNamespace(None)
+
+        if name == "meta":
+            return PythonObjectNamespace(self.context.get_current_root_span().meta)
+        elif name == "service":
+            return PythonObjectNamespace(self.context.get_current_root_span().service)
+        elif name == "resource":
+            return PythonObjectNamespace(self.context.get_current_root_span().resource)
+        elif name == "name":
+            return PythonObjectNamespace(self.context.get_current_root_span().name)
+        elif name == "type":
+            return PythonObjectNamespace(self.context.get_current_root_span().type)
+
+        raise RookAttributeNotFound(name)
+
+    def _load(self):
+        if self.context is not None:
+            return True
+
+        try:
+            import opentracing
+            self.context = opentracing.tracer.get_call_context()
+        except ImportError:
+            try:
+                import ddtrace
+                self.context = ddtrace.tracer.get_call_context()
+            except ImportError as import_err:
+                return False
+        return True
+
+    METHODS = (dump, )
