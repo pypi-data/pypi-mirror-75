bf9d4a9830438ef9d599738ce7375213792f323c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:22 +0000

    Add package Leono-dist/0.1/Leono_dist-0.1.tar.gz

---
+++ packages/Leono-dist/Leono_dist-0.1.tar.gz/Leono_dist-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Leono_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Leonardo Becerra',
+      packages=['Leono_dist'],
+      zip_safe=False)
