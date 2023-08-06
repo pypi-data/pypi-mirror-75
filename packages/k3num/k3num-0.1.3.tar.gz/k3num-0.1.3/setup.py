521a5968b77c7256f649312780d8b248e12ed2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:03 +0000

    Add package k3num/0.1.3/k3num-0.1.3.tar.gz

---
+++ packages/k3num/k3num-0.1.3.tar.gz/k3num-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+setuptools.setup(
+    name="k3num",
+    packages=["k3num"],
+    version="0.1.3",
+    license='MIT',
+    description='Convert number to human readable format in a string.',
+    long_description="# k3num\n\n[![Build Status](https://travis-ci.com/pykit3/k3num.svg?branch=master)](https://travis-ci.com/pykit3/k3num)\n[![Documentation Status](https://readthedocs.org/projects/k3num/badge/?version=stable)](https://k3num.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3num)](https://pypi.org/project/k3num)\n\nConvert number to human readable format in a string.\n\nk3num is a component of [pykit3] project: a python3 toolkit set.\n\n\n# Install\n\n```\npip install k3num\n```\n\n# Synopsis\n\n```python\n>>> readable(103425)\n'101.0K'\n>>> readable({ 'total': 10240, 'progress': [1, 1024*2.1, 1024*3.2], })\n{'total': '10K', 'progress': ['1', '2.10K', '3.20K']}\n>>> parsenum('5.2K')\n5324.8\n>>> parsenum('10%')\n0.1\n>>> value_to_unit[1024**2]\n'M'\n>>> unit_to_value['K']\n1024\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3",
+    long_description_content_type="text/markdown",
+    author='Zhang Yanpo',
+    author_email='drdr.xp@gmail.com',
+    url='https://github.com/drmingdrmer/k3num',
+    keywords=['human', 'readable', 'number', 'digit', 'integer'],
+    python_requires='>=3.0',
+
+    install_requires=['semantic_version==2.8.5', 'jinja2==2.11.2', 'PyYAML==5.3.1', 'k3ut==0.1.7', 'sphinx==3.0.3'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+    ] + ['Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7', 'Programming Language :: Python :: 3.8', 'Programming Language :: Python :: Implementation :: PyPy'],
+)
