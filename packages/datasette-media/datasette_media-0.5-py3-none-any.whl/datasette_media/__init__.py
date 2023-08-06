6f1ca1b6e763342a1338bd6289b07b9d4806d29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:29 +0000

    Add package datasette-media/0.5/datasette_media-0.5-py3-none-any.whl

---
+++ packages/datasette-media/datasette_media-0.5-py3-none-any.whl/datasette_media/__init__.py
@@ -0,0 +1,160 @@
+import asyncio
+from datasette import hookimpl
+from datasette.utils.asgi import Response, asgi_send_file
+from concurrent import futures
+import httpx
+from mimetypes import guess_type
+from PIL import Image
+import io
+from . import utils
+
+transform_executor = None
+RESERVED_MEDIA_TYPES = ("transform_threads", "enable_transform")
+
+
+@hookimpl
+def register_routes():
+    return [
+        (r"/-/media/(?P<media_type>[^/]+)/(?P<key>.+)", serve_media),
+    ]
+
+
+async def serve_media(datasette, request, send):
+    global transform_executor
+    plugin_config = datasette.plugin_config("datasette-media") or {}
+    pool_size = plugin_config.get("transform_threads") or 4
+    if transform_executor is None:
+        transform_executor = futures.ThreadPoolExecutor(max_workers=pool_size)
+
+    media_type = request.url_vars["media_type"]
+    key = request.url_vars["key"]
+
+    config = plugin_config.get(media_type)
+    if media_type in RESERVED_MEDIA_TYPES or config is None:
+        return Response.html("<h1>Invalid media type</h1>", status=404)
+    sql = config.get("sql")
+    if sql is None:
+        return Response.html("<h1>Missing SQL from configuration</h1>", status=404)
+    database = datasette.get_database(config.get("database"))
+    results = await database.execute(sql, {"key": key})
+    row = results.first()
+    if row is None:
+        return Response.html("<h1>404 - no results</h1>", status=404)
+
+    # We need filepath or content
+    content = None
+    content_type = None
+    content_url = None
+    content_filename = None
+    filepath = None
+
+    row_keys = row.keys()
+    if (
+        "filepath" not in row_keys
+        and "content" not in row_keys
+        and "content_url" not in row_keys
+    ):
+        return Response.html(
+            "<h1>404 - SQL must return 'filepath' or 'content' or 'content_url'</h1>",
+            status=404,
+        )
+    if "content" in row_keys:
+        content = row["content"]
+    elif "content_url" in row_keys:
+        content_url = row["content_url"]
+    else:
+        filepath = row["filepath"]
+
+    if "content_filename" in row_keys:
+        content_filename = row["content_filename"]
+
+    # Images are special cases, triggered by a few different conditions
+    should_transform = utils.should_transform(row, config, request)
+    if should_transform:
+        if content is None and content_url:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(row["content_url"])
+                content = response.content
+                content_type = response.headers["content-type"]
+        image_bytes = content or open(filepath, "rb").read()
+        image = await asyncio.get_event_loop().run_in_executor(
+            transform_executor,
+            lambda: utils.transform_image(image_bytes, **should_transform),
+        )
+        response = utils.ImageResponse(image, format=should_transform.get("format"))
+        if content_filename:
+            response.headers[
+                "content-disposition"
+            ] = 'attachment; filename="{}"'.format(content_filename)
+        return response
+    else:
+        # content_url is proxied as a special case
+        if content_url:
+            client = httpx.AsyncClient()
+            async with client.stream("GET", content_url) as response:
+                content_type = response.headers["content-type"]
+                content_length = response.headers.get("content-length")
+                headers = [(b"content-type", content_type.encode("utf-8"))]
+                if content_length:
+                    headers.append(
+                        (b"content-length", str(content_length).encode("utf-8"))
+                    )
+                if content_filename:
+                    headers.append(
+                        (
+                            b"content-disposition",
+                            'attachment; filename="{}"'.format(content_filename).encode(
+                                "utf-8"
+                            ),
+                        )
+                    )
+
+                await send(
+                    {"type": "http.response.start", "status": 200, "headers": headers}
+                )
+                async for chunk in response.aiter_bytes():
+                    await send(
+                        {
+                            "type": "http.response.body",
+                            "body": chunk,
+                            "more_body": True,
+                        }
+                    )
+                await send({"type": "http.response.body", "body": b""})
+                return
+
+        if "content_type" in row_keys:
+            content_type = row["content_type"]
+
+        # Non-image files are returned directly
+        if content:
+            return Response(
+                content,
+                content_type=content_type or "application/octet-stream",
+                headers={
+                    "content-disposition": 'attachment; filename="{}"'.format(
+                        content_filename
+                    )
+                }
+                if content_filename
+                else None,
+            )
+        else:
+            print(
+                """
+            asgi_send_file(
+                send={},
+                filepath={},
+                filename={},
+                content_type={} or guess_type(filepath)[0],
+            )
+            """.format(
+                    send, filepath, content_filename, content_type
+                )
+            )
+            await asgi_send_file(
+                send,
+                filepath,
+                filename=content_filename,
+                content_type=content_type or guess_type(filepath)[0],
+            )
---
+++ packages/datasette-media/datasette_media-0.5-py3-none-any.whl/datasette_media/utils.py
@@ -0,0 +1,107 @@
+from datasette.utils.asgi import Response
+import imghdr
+import io
+from PIL import Image, ExifTags
+
+try:
+    import pyheif
+except ImportError:
+    pyheif = None
+
+heic_magics = {b"ftypheic", b"ftypheix", b"ftyphevc", b"ftyphevx"}
+ORIENTATION_EXIF_TAG = dict((v, k) for k, v in ExifTags.TAGS.items())["Orientation"]
+
+# Sanity check maximum width/height for resized images
+DEFAULT_MAX_WIDTH_HEIGHT = 4000
+
+
+def image_type_for_bytes(b):
+    image_type = imghdr.what(None, b)
+    if image_type is not None:
+        return image_type
+    # Maybe it's an HEIC?
+    if len(b) < 12:
+        return None
+    if b[4:12] in heic_magics:
+        return "heic"
+    return None
+
+
+def should_transform(row, config, request):
+    # Decides if the provided row should be transformed, based on request AND config
+    # Returns None if it should not be, or a dict of resize/etc options if it should
+    row_keys = row.keys()
+    transform = {}
+    if any(
+        key in row_keys for key in ("resize_width", "resize_height", "output_format")
+    ):
+        transform = dict(
+            width=row["resize_width"] if "resize_width" in row_keys else None,
+            height=row["resize_height"] if "resize_height" in row_keys else None,
+            format=row["output_format"] if "output_format" in row_keys else None,
+        )
+    if config.get("enable_transform"):
+        max_width_height = config.get("max_width_height") or DEFAULT_MAX_WIDTH_HEIGHT
+        # URL arguments over-ride columns
+        if "format" in request.args:
+            transform["format"] = request.args["format"]
+        # If either width or height is set, ignore the ones from the DB row
+        if "w" in request.args or "h" in request.args:
+            transform.pop("width", None)
+            transform.pop("height", None)
+        for urlarg, key in {"w": "width", "h": "height"}.items():
+            if urlarg in request.args and int(request.args[urlarg]) < max_width_height:
+                transform[key] = int(request.args[urlarg])
+
+    return transform or None
+
+
+def transform_image(image_bytes, width=None, height=None, format=None):
+    image_type = image_type_for_bytes(image_bytes)
+    if image_type == "heic" and pyheif is not None:
+        heic = pyheif.read_heif(image_bytes)
+        image = Image.frombytes(mode=heic.mode, size=heic.size, data=heic.data)
+    else:
+        image = Image.open(io.BytesIO(image_bytes))
+    # Does EXIF tell us to rotate it?
+    try:
+        exif = dict(image._getexif().items())
+        if exif[ORIENTATION_EXIF_TAG] == 3:
+            image = image.rotate(180, expand=True)
+        elif exif[ORIENTATION_EXIF_TAG] == 6:
+            image = image.rotate(270, expand=True)
+        elif exif[ORIENTATION_EXIF_TAG] == 8:
+            image = image.rotate(90, expand=True)
+    except (AttributeError, KeyError, IndexError):
+        pass
+
+    # Resize based on width and height, if set
+    image_width, image_height = image.size
+    if width is not None or height is not None:
+        if height is None:
+            # Set h based on w
+            height = int((float(image_height) / image_width) * width)
+        elif width is None:
+            # Set w based on h
+            width = int((float(image_width) / image_height) * height)
+        image = image.resize((width, height))
+
+    return image
+
+
+class ImageResponse(Response):
+    def __init__(self, image, format=None):
+        self.image = image
+        output_image = io.BytesIO()
+        if format is None:
+            if image.format == "GIF":
+                format = "GIF"
+            elif image.mode == "RGBA":
+                format = "PNG"
+            else:
+                format = "JPEG"
+        image.save(output_image, format)
+        super().__init__(
+            body=output_image.getvalue(),
+            content_type="image/{}".format(format or "JPEG").lower(),
+        )
