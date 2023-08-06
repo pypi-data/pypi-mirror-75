3a334775e4927e534de8914cd40dd78bb96a0d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:35 +0000

    Add package helix-scripts/0.0.1/helix-scripts-0.0.1.tar.gz

---
+++ packages/helix-scripts/helix-scripts-0.0.1.tar.gz/helix-scripts-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="helix-scripts", 
+    version="0.0.1",
+    author=".NET Core Engineering Team",
+    author_email="dnceng@microsoft.com",
+    description="Placeholder for helix-scripts package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/helix-scripts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
