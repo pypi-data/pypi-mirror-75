f873c63d85d95333608030162f382ba03e1b56dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:18 +0000

    Add package telegram-url-to-screenshot/1.1/telegram_url_to_screenshot-1.1.tar.gz

---
+++ packages/telegram-url-to-screenshot/telegram_url_to_screenshot-1.1.tar.gz/telegram_url_to_screenshot-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='telegram_url_to_screenshot',
+    version='1.1',
+    packages=[''],
+    url='https://github.com/m0nte-cr1st0/telegram-url-to-screenshot-bot',
+    license='Apache 2.0',
+    author='Dmitriy Dmytruk',
+    author_email='dinamo.mutu111@gmail.com',
+    description='Opens received url and makes screenshot'
+)
