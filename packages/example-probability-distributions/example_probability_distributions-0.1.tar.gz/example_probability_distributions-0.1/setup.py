3f0306e7d97623afdc460ab84f030a9aea8ef686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:54 +0000

    Add package example-probability-distributions/0.1/example_probability_distributions-0.1.tar.gz

---
+++ packages/example-probability-distributions/example_probability_distributions-0.1.tar.gz/example_probability_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='example_probability_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['example_probability_distributions'],
+      author='Novak Zaballa',
+      author_email='zaballa.novak@gmail.com',
+      zip_safe=False)
