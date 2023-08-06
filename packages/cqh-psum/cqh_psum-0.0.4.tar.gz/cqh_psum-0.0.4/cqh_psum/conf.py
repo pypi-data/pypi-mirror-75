8995d5ec7e639c0439ccb4dd49420a806e5a9da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package cqh-psum/0.0.4/cqh_psum-0.0.4.tar.gz

---
+++ packages/cqh-psum/cqh_psum-0.0.4.tar.gz/cqh_psum-0.0.4/cqh_psum/conf.py
@@ -0,0 +1,30 @@
+doc = """
+                      cqh_psum
+========
+
+
+Usage
+-----
+
+Example
+---------------------------
+*  统计一个进程
+
+ ``cqh_psum --name=python``
+
+* 统计多个进程
+
+  ``cqh_psum --name=python,coder,nginx``  
+
+* 忽略某些进程
+
+   ``cqh_psum --name=supervisor --exclude=supervisorctl,grep`` 
+
+* 查看具体的进程
+
+    ``cqh_psum --name=supervisor --show=1``
+
+
+
+
+                      """
