c7d1f2f7119b8064f879772dc178176a591dfa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:06 +0000

    Add package pyreplit/0.2.3/pyreplit-0.2.3.tar.gz

---
+++ packages/pyreplit/pyreplit-0.2.3.tar.gz/pyreplit-0.2.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+import os
+
+setuptools.setup(name="pyreplit",
+    version="0.2.3",
+    description="Fork of sugarfi/pyrepl",
+    long_description=open('README.md','r').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AgeOfMarcus/pyreplit",
+    author="AgeOfMarcus",
+    author_email="marcus@marcusweinberger.com",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'protobuf',
+        'google-nucleus',
+        'base36',
+        'websocket_client',
+        'requests',
+    ],
+)
