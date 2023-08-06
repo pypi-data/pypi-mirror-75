409f708d6edca681eb06752c685567b49b246175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:35 +0000

    Add package leedMx-distributions/0.1/leedMx_distributions-0.1.tar.gz

---
+++ packages/leedMx-distributions/leedMx_distributions-0.1.tar.gz/leedMx_distributions-0.1/README.md
@@ -0,0 +1,2 @@
+# leedMx_Distributions
+Sample package including a Gaussian and a Binomial distribution, made as a part of AWS Machine Learning Foundations Course
---
+++ packages/leedMx-distributions/leedMx_distributions-0.1.tar.gz/leedMx_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='leedMx_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['leedMx_distributions'],
+      author_email="esparza.mexico@gmail.com",
+      zip_safe=False)
