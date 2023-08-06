b93b6d1376d2d50c0d03ef38666bf8d02d26d63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:37 +0000

    Add package shaheen-distributions/0.2/shaheen_distributions-0.2.tar.gz

---
+++ packages/shaheen-distributions/shaheen_distributions-0.2.tar.gz/shaheen_distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='shaheen_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      author = 'talha3111997',
+      packages=['shaheen_distributions'],
+      zip_safe=False)
