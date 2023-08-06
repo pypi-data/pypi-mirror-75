80e614631dd6efa4ff0819a0b301f272dc999eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:24 +0000

    Add package roshan-dist-pack/1.2/roshan_dist_pack-1.2.tar.gz

---
+++ packages/roshan-dist-pack/roshan_dist_pack-1.2.tar.gz/roshan_dist_pack-1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='roshan_dist_pack',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['roshan_dist_pack'],
+      Author = 'Roshan',
+      author_email = 'roshanbisht1992@gmail.com',
+      zip_safe=False)
+
