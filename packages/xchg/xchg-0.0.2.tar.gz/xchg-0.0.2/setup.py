43296f1124e20a9e24f58d34daecc9a79936015f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:33 +0000

    Add package xchg/0.0.2/xchg-0.0.2.tar.gz

---
+++ packages/xchg/xchg-0.0.2.tar.gz/xchg-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+     name='xchg',
+     version='0.0.2',
+     description='Simulator of a currency exchange.',
+     long_description=long_description,
+     long_description_content_type='text/markdown',
+     author='Sergei Bondarenko',
+     author_email='sergei@bondarenko.xyz',
+     url='https://github.com/sergei-bondarenko/xchg',
+     license='Unlicense',
+     packages=['xchg'],
+     install_requires=[
+         'pandas',
+         'poloniex',
+         'numpy'
+     ],
+     entry_points={
+         'console_scripts': ['download_sample=xchg.download_sample:_main'],
+     }
+)
