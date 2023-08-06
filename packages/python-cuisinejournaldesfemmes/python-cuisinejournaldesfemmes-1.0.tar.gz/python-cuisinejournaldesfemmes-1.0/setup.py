75fc8bf6e6cb385ab71ef1fe63e052acb3abfc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:15 +0000

    Add package python-cuisinejournaldesfemmes/1.0/python-cuisinejournaldesfemmes-1.0.tar.gz

---
+++ packages/python-cuisinejournaldesfemmes/python-cuisinejournaldesfemmes-1.0.tar.gz/python-cuisinejournaldesfemmes-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='python-cuisinejournaldesfemmes',
+	version='1.0',
+	description='api to scrape cuisinejournaldesfemmes with python.',
+	packages=['python-cuisinejournaldesfemmes'],
+	url='https://github.com/starnakin/python-CuisineJournalDesFemmes',
+	author="Starnakin",
+	author_email='starnakin2@gmail.com',
+	license='MIT',
+	zip_safe=False,
+	install_requires=['bs4'],
+	)
