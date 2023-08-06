d073fb6c7eddab37525b49e09536b85c8c8e1bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:49 +0000

    Add package ggbdp-distributions/0.1/ggbdp-distributions-0.1.tar.gz

---
+++ packages/ggbdp-distributions/ggbdp-distributions-0.1.tar.gz/ggbdp-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ggbdp-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ggbdp-distributions'],
+      zip_safe=False)
