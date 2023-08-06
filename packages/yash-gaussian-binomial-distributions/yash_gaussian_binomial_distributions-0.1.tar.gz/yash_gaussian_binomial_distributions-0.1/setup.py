14ed03eff0a1a7403264307ccae60075108dfc9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:05 +0000

    Add package yash-gaussian-binomial-distributions/0.1/yash_gaussian_binomial_distributions-0.1.tar.gz

---
+++ packages/yash-gaussian-binomial-distributions/yash_gaussian_binomial_distributions-0.1.tar.gz/yash_gaussian_binomial_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='yash_gaussian_binomial_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['yash_gaussian_binomial_distributions'],
+      author = 'Yasaswi Vedula',
+      author_email = 'thisisa@test.com',
+      zip_safe=False)
