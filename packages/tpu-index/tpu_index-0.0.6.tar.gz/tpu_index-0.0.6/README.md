57a1ae63a7770743a14850de07c83a2f9d6cb198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:31 +0000

    Add package tpu-index/0.0.6/tpu_index-0.0.6.tar.gz

---
+++ packages/tpu-index/tpu_index-0.0.6.tar.gz/tpu_index-0.0.6/README.md
@@ -0,0 +1,28 @@
+[![HitCount](http://hits.dwyl.io/srihari-humbarwadi/tpu_index.svg)](http://hits.dwyl.io/srihari-humbarwadi/tpu_index)
+
+## TPU Index
+
+TPU Index is a package for fast similarity search over large collections of high dimension vectors on TPUs.
+This package was built to support our project that we developed for https://tfworld.devpost.com/.
+
+Link to our project: https://devpost.com/software/naturallanguagerecommendations
+
+
+### Installation
+`!pip install tpu-index`
+
+
+### Basic usage
+```
+from tpu_index import TPUIndex
+
+index = TPUIndex(num_tpu_cores=8)
+index.create_index(vectors)  # vectors = numpy array, shape == [None, None]
+
+...
+D, I = index.search(xq, distance_metric='cosine', top_k=5)
+```
+
+### ToDo:
+ - [ ] Add more distance metrics
+ - [ ] Optional GPU support
---
+++ packages/tpu-index/tpu_index-0.0.6.tar.gz/tpu_index-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='tpu_index',
+    version='0.0.6',
+    author='Srihari Humbarwadi',
+    author_email='sriharihumbarwadi97@gmail.com',
+    description='TPU index is a package for fast similarity search over large collections of high dimension vectors on Google Cloud TPUs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/srihari-humbarwadi/tpu_index',
+    packages=setuptools.find_packages(),
+    install_requires=['tensorflow>=2.0.0'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+)
