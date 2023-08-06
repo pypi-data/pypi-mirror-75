74e174ae3a4b8788197562649463479550896f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:58 +0000

    Add package boldminer/0.2/boldminer-0.2.tar.gz

---
+++ packages/boldminer/boldminer-0.2.tar.gz/boldminer-0.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import setuptools
+from distutils.core import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
+setup(name="boldminer",
+      version='0.2',
+      long_description = readme,
+      long_description_content_type='text/markdown',
+      author='Ulises Rosas',
+      author_email='ulisesfrosasp@gmail.com',
+      url='https://github.com/Ulises-Rosas/boldminer/tree/python',
+      packages = ['boldminer'],
+      # package_dir={"boldminercore": "src"},
+      scripts= [
+          './boldminer/bin/sppsID.py'
+          ],
+      classifiers=[
+             'Programming Language :: Python :: 3',
+             'License :: OSI Approved :: MIT License'
+             ]
+      )
