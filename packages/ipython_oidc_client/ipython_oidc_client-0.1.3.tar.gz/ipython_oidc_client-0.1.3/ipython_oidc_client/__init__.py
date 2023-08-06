24035a8970bc0ddf66604c13fce3a21dda8ab98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:23 +0000

    Add package ipython_oidc_client/0.1.3/ipython_oidc_client-0.1.3.tar.gz

---
+++ packages/ipython_oidc_client/ipython_oidc_client-0.1.3.tar.gz/ipython_oidc_client-0.1.3/ipython_oidc_client/__init__.py
@@ -0,0 +1,30 @@
+"""A Jupyter extension to perform OAuth2 flows (e.g. token, code) in notebooks."""
+
+__version__ = '0.1.3'
+
+import ipykernel.comm
+
+
+def _jupyter_server_extension_paths():
+    return [{
+        "module": "ipython_oidc_client.server"
+    }]
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        "section": "notebook",
+        "src": "client",
+        "dest": "ipython_oidc_client",
+        "require": "ipython_oidc_client/main"
+    }]
+
+
+def authenticate(provider, variable):
+    comm = ipykernel.comm.Comm(target_name='oauth_authenticate')
+
+    @comm.on_msg
+    def _recv(msg):
+        variable['access_token'] = msg['content']['data']
+
+    comm.send(provider)
