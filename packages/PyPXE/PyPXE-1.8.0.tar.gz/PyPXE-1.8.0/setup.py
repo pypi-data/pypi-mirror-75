b5c8684f2daa9a94f6c6ea2d87ff0caaa589f59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:55 +0000

    Add package PyPXE/1.8.0/PyPXE-1.8.0.tar.gz

---
+++ packages/PyPXE/PyPXE-1.8.0.tar.gz/PyPXE-1.8.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from sys import version_info, exit
+
+import pypxe
+
+deps = []
+
+# require argparse on Python <2.7
+if version_info[0] == 2 and version_info[1] < 7:
+    deps.append('argparse')
+
+setup(name='PyPXE',
+      version=pypxe.__version__,
+      description='Pure Python2 PXE (DHCP-(Proxy)/TFTP/HTTP/NBD) Server',
+      url='https://github.com/pypxe/PyPXE',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=deps,
+      entry_points={
+          'console_scripts': ['pypxe=pypxe.server:main']
+      }
+)
