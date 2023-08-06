f4bcc3f616d3d2d50a0ce9d83995ba3d3f523bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:34 +0000

    Add package pyreplit/0.0.6/pyreplit-0.0.6.tar.gz

---
+++ packages/pyreplit/pyreplit-0.0.6.tar.gz/pyreplit-0.0.6/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+import os
+
+setuptools.setup(name="pyreplit",
+    version="0.0.6",
+    description="Fork of sugarfi/pyrepl",
+    long_description=open('README.md','r').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AgeOfMarcus/pyreplit",
+    author="AgeOfMarcus",
+    author_email="marcus@marcusweinberger.com",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=open('requirements.txt','r').read().split('\n'),
+)
